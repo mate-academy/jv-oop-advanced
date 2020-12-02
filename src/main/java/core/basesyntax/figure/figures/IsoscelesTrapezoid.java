79c0a5165289043aec6aefab49587fcfaa94e3b7
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-02 15:41:14 +0200

    implemented IsoscelesTrapezoid and RightTriangle classes

--- src/main/java/core/basesyntax/figure/figures/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/figure/figures/IsoscelesTrapezoid.java
@@ -1,4 +1,56 @@
 package core.basesyntax.figure.figures;
 
-public class IsoscelesTrapezoid {
+import core.basesyntax.color.Color;
+import core.basesyntax.figure.Figure;
+import core.basesyntax.figure.Shape;
+
+public class IsoscelesTrapezoid extends Figure {
+
+    public static final Shape NAME = Shape.ISOSCELES_TRAPEZOID;
+    private double upperSide;
+    private double lowerSide;
+    private double height;
+
+    public IsoscelesTrapezoid(Color color, double upperSide, double lowerSide, double height) {
+        super(color);
+        this.upperSide = upperSide;
+        this.lowerSide = lowerSide;
+        this.height = height;
+    }
+
+    @Override
+    public double getSquare() {
+        return (getLowerSide() + getUpperSide()) * height / 2;
+    }
+
+    public double getUpperSide() {
+        return upperSide;
+    }
+
+    public void setUpperSide(double upperSide) {
+        this.upperSide = upperSide;
+    }
+
+    public double getLowerSide() {
+        return lowerSide;
+    }
+
+    public void setLowerSide(double lowerSide) {
+        this.lowerSide = lowerSide;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return getColor() + " " + NAME + " with height, "
+                + getHeight() + " lower side " + getLowerSide()
+                + ", upper side " + getUpperSide() + ", and square " + getSquare();
+    }
 }
--- src/main/java/core/basesyntax/figure/figures/RightTriangle.java
+++ src/main/java/core/basesyntax/figure/figures/RightTriangle.java
@@ -1,4 +1,45 @@
 package core.basesyntax.figure.figures;
 
-public class RightTriangle {
+import core.basesyntax.color.Color;
+import core.basesyntax.figure.Figure;
+import core.basesyntax.figure.Shape;
+
+public class RightTriangle extends Figure {
+
+    public static final Shape NAME = Shape.RIGHT_TRIANGLE;
+    private double base;
+    private double height;
+
+    public RightTriangle(Color color, double base, double height) {
+        super(color);
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double getSquare() {
+        return base * height / 2;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return getColor() + " " + NAME + " with base "
+                + getBase() + ", height " + getHeight() + ", and square " + getSquare();
+    }
 }
