e36ca4ad862f0bdc2477359a46c05974166dcc56
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-02 16:59:44 +0200

    Interface IFigure was added, array of figures was created

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        FigureProducer figureProducer = new FigureProducer();
-        for (int i = 0; i < new Random().nextInt(1000); i++) {
-            figureProducer.get().getInfo();
+        Figure[] figures = new Figure[new Random().nextInt(1000)];
+        for (Figure figure: figures) {
+            figure = new FigureProducer().get();
+            figure.getInfo();
         }
     }
 }
