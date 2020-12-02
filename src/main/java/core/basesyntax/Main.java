404e10a45031b24aff9d1743757bdcd0d53f6a8f
Alekearl <alexandrhurko@gmail.com>
2020-12-02 23:01:31 +0200

    deleted class HelloWorld and created class Main

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        FigureConstructor figureConstructor = new FigureConstructor();
+        Figure[] figuresArray = new Figure[new Random().nextInt(10) + 5];
+
+        for (int i = 0; i < figuresArray.length; i++) {
+            figuresArray[i] = figureConstructor.getFigure();
+        }
+
+        for (Figure figure : figuresArray) {
+            System.out.println(figure.onScreen());
+        }
+    }
+}
