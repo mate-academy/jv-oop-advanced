c880b6673a93117910cd2d43ee7dd1bd364ebf2f
kerrender <agreed_note@hotmail.com>
2020-12-02 12:15:37 +0200

    added generateColors static method to Figure

--- src/main/java/core/basesyntax/figures/Figure.java
+++ src/main/java/core/basesyntax/figures/Figure.java
@@ -1,5 +1,7 @@
 package core.basesyntax.figures;
 
+import java.util.HashMap;
+
 public interface Figure {
     double getArea();
 
@@ -8,4 +10,13 @@ public interface Figure {
     String getColor();
 
     void draw();
+
+    static HashMap<Integer, String> generateColors() {
+        HashMap<Integer, String> colors = new HashMap<>();
+        colors.put(1, "Red");
+        colors.put(2, "Green");
+        colors.put(3, "Blue");
+
+        return colors;
+    }
 }
--- src/main/java/core/basesyntax/figures/Shape.java
+++ src/main/java/core/basesyntax/figures/Shape.java
@@ -2,17 +2,11 @@ package core.basesyntax.figures;
 
 import core.basesyntax.RandomNumber;
 
-import java.util.HashMap;
-
 public abstract class Shape implements Figure {
     protected String color;
 
     public Shape() {
-        HashMap<Integer, String> colors = new HashMap<>();
-        colors.put(1, "Red");
-        colors.put(2, "Green");
-        colors.put(3, "Blue");
-        this.color = colors.get(RandomNumber.getRandomNumber(3));
+        this.color = Figure.generateColors().get(RandomNumber.getRandomNumber(3));
     }
 
 }
