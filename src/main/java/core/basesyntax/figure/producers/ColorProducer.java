997ccc1bbe68a83f2187307ad07439f5da25a7dd
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-02 15:49:37 +0200

    implemented color producer

--- src/main/java/core/basesyntax/figure/producers/ColorProducer.java
+++ src/main/java/core/basesyntax/figure/producers/ColorProducer.java
@@ -1,4 +1,12 @@
 package core.basesyntax.figure.producers;
 
+import core.basesyntax.color.Color;
+import java.util.Random;
+
 public class ColorProducer {
+
+    public static Color getRandomColor(){
+        return Color.values()[new Random().nextInt(Color.values().length)];
+    }
+
 }
