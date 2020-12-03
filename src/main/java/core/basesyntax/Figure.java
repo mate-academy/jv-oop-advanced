f37ede1c708795e58b8dc7f5cd9a47a05aa1d8ad
Alekearl <alexandrhurko@gmail.com>
2020-12-03 14:07:12 +0200

    created getter in class Figure and implemented new created interface DrawFigure

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
-public abstract class Figure implements Area {
-    public abstract String onScreen();
+public abstract class Figure implements Area, DrawFigure {
+    private Color color;
+
+    public Figure(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
