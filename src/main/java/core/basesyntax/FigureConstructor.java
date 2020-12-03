0e366d25bc3647136f03f1f16ac598d679c745cc
Alekearl <alexandrhurko@gmail.com>
2020-12-03 14:08:24 +0200

    refactored class FigureConstructor

--- src/main/java/core/basesyntax/FigureConstructor.java
+++ src/main/java/core/basesyntax/FigureConstructor.java
@@ -7,19 +7,19 @@ public class FigureConstructor {
         int randomInteger = new Random().nextInt(5);
         switch (randomInteger) {
             case 0:
-                return new Square(new Random().nextInt(21) + 1);
+                return new Square(new Random().nextInt(21) + 1, ColorCreator.getColor());
             case 1:
                 return new Rectangle(new Random().nextInt(21) + 1,
-                        new Random().nextInt(21) + 1);
+                        new Random().nextInt(21) + 1, ColorCreator.getColor());
             case 2:
                 return new RightTriangle(new Random().nextInt(21) + 1,
-                        new Random().nextInt(21) + 1);
+                        new Random().nextInt(21) + 1, ColorCreator.getColor());
             case 3:
-                return new Circle(new Random().nextInt(21) + 1);
+                return new Circle(new Random().nextInt(21) + 1, ColorCreator.getColor());
             case 4:
                 return new IsoscelesTrapezoid(new Random().nextInt(21) + 1,
                         new Random().nextInt(21) + 1,
-                        new Random().nextInt(21) + 1);
+                        new Random().nextInt(21) + 1, ColorCreator.getColor());
             default:
                 return null;
         }
