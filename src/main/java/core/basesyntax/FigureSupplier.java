dd5b14765bdb139ed0cc11367cc288b448f1bd22
Nikber5 <nikber198@gmail.com>
2021-03-29 16:28:37 +0300

    Problem with sames solved

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,17 +5,17 @@ import java.util.Random;
 public class FigureSupplier {
     static final int RANDOM_LIMIT = 100;
 
-    private final Figure[] figures = new Figure[] {
-            new Circle(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
-            new Triangle(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
-            new Square(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
-            new Rectangle(new Random().nextInt(RANDOM_LIMIT), new Random().nextInt(RANDOM_LIMIT),
-                    ColorSupplier.randomColor()),
-            new IsoscelesTrapezoid(new Random().nextInt(RANDOM_LIMIT),
-                    new Random().nextInt(RANDOM_LIMIT),
-                    new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor())};
-
     public Figure getRandomFigure() {
+        final Figure[] figures = new Figure[] {
+                new Circle(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
+                new Triangle(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
+                new Square(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
+                new Rectangle(new Random().nextInt(RANDOM_LIMIT),
+                        new Random().nextInt(RANDOM_LIMIT),
+                        ColorSupplier.randomColor()),
+                new IsoscelesTrapezoid(new Random().nextInt(RANDOM_LIMIT),
+                        new Random().nextInt(RANDOM_LIMIT),
+                        new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor())};
         return figures[new Random().nextInt(figures.length)];
     }
 }
