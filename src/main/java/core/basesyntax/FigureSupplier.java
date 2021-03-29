563f9991d2c718520b75c748994e9e601ce8ea13
Nikber5 <nikber198@gmail.com>
2021-03-29 15:14:03 +0300

    RANDOM_LIMIT constant was added in FigureSupplier class.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,14 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private Figure[] figures = new Figure[] {
-            new Circle(new Random().nextInt(100), ColorSupplier.randomColor()),
-            new Triangle(new Random().nextInt(100), ColorSupplier.randomColor()),
-            new Square(new Random().nextInt(100), ColorSupplier.randomColor()),
-            new Rectangle(new Random().nextInt(100), new Random().nextInt(100),
+    final int RANDOM_LIMIT = 100;
+
+    private final Figure[] figures = new Figure[] {
+            new Circle(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
+            new Triangle(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
+            new Square(new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor()),
+            new Rectangle(new Random().nextInt(RANDOM_LIMIT), new Random().nextInt(RANDOM_LIMIT),
                     ColorSupplier.randomColor()),
-            new IsoscelesTrapezoid(new Random().nextInt(100), new Random().nextInt(100),
-                    new Random().nextInt(100), ColorSupplier.randomColor())};
+            new IsoscelesTrapezoid(new Random().nextInt(RANDOM_LIMIT), new Random().nextInt(RANDOM_LIMIT),
+                    new Random().nextInt(RANDOM_LIMIT), ColorSupplier.randomColor())};
 
     public Figure getRandomFigure() {
         return figures[new Random().nextInt(figures.length)];
