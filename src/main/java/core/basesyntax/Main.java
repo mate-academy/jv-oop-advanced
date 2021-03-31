a4561df93cfb0656cd67ab5ed14a28f15ad652a4
m.kislukhina <marharita.kislukhina@gmail.com>
2021-03-31 16:55:55 +0200

    travis

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Main {
+    private static final int LimitNumber = 10;
+
     public static void main(String[] args) {
-        FigureGenerator[] randomFiguresArray = new FigureGenerator[new Random().nextInt(10)];
+        FigureGenerator[] randomFiguresArray =
+                new FigureGenerator[new Random().nextInt(LimitNumber)];
         Figure[] figures = new Figure[randomFiguresArray.length];
         for (int i = 0; i < randomFiguresArray.length; i++) {
             figures[i] = FigureGenerator.createFigure();
