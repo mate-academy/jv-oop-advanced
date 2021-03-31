5ee2c51ef9fd16aa353109d5b67ba181d4f7a176
roman <nashpatsa@gmai.com>
2021-03-31 14:18:47 +0300

    corrected the mistakes again(

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int RAND_NUMBER = 50;
+
     public Figure randomFigure() {
         int randomNumber = new Random().nextInt(5);
         switch (randomNumber) {
@@ -13,9 +14,11 @@ public class FigureSupplier {
                 return new Rectangle(ColorSuplier.randomColor(), new Random().nextInt(RAND_NUMBER),
                         new Random().nextInt(RAND_NUMBER));
             case 3:
-                return new RightTriangle(ColorSuplier.randomColor(), new Random().nextInt(RAND_NUMBER));
+                return new RightTriangle(ColorSuplier.randomColor(),
+                        new Random().nextInt(RAND_NUMBER));
             case 4:
-                return new IsoscelesTrapezoi(ColorSuplier.randomColor(), new Random().nextInt(RAND_NUMBER),
+                return new IsoscelesTrapezoi(ColorSuplier.randomColor(),
+                        new Random().nextInt(RAND_NUMBER),
                         new Random().nextInt(RAND_NUMBER));
             default:
                 return new Circle(ColorSuplier.randomColor(), new Random().nextInt(RAND_NUMBER));
