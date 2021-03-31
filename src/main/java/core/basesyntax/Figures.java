11e582d3a0661027f0547ef051ca9e63818c660b
Dmitriy <dmitriy4ever97@gmail.com>
2021-03-31 15:02:15 +0300

    Done homework.

---
+++ src/main/java/core/basesyntax/Figures.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public abstract class Figures implements Behaviour {
+    private String name;
+    private String color;
+
+    public Figures(String color, String name) {
+        this.color = color;
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Figures> figuresList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            figuresList.add(FigureSupplier.figuresRandomGenerator());
+        }
+
+        for (Figures object : figuresList) {
+            object.getDraw();
+        }
+    }
+}
