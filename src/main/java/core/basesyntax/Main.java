c69cb85b7e5d4f54e7f0fbf7c62effe6d8dfb066
IhorRome <ihor.rome@gmail.com>
2021-03-31 10:54:20 +0300

    Not sure if I understood about constant for random number of figures

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        final int COUNT_FIGURE = 15;
-        Figure[] figureArray = new Figure[(int)(Math.random() * COUNT_FIGURE) + 1];
+        final int countFigure = 15;
+        Figure[] figureArray = new Figure[(int)(Math.random() * countFigure) + 1];
 
         for (int i = 0; i < figureArray.length; i++) {
             figureArray[i] = new FigureSupplier().figureGenerator();
