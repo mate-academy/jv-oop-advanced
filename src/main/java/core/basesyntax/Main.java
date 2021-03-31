8f2ac2483513fb493ba86ffdddbb92cff9184407
IhorRome <ihor.rome@gmail.com>
2021-03-31 12:02:34 +0300

    Done!!!

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int COUNT_FIGURE = 15;
+
     public static void main(String[] args) {
-        final int countFigure = 15;
-        Figure[] figureArray = new Figure[(int)(Math.random() * countFigure) + 1];
+        Figure[] figureArray = new Figure[(int)(Math.random() * COUNT_FIGURE) + 1];
 
         for (int i = 0; i < figureArray.length; i++) {
             figureArray[i] = new FigureSupplier().figureGenerator();
