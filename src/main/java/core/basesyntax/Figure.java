a2074ebca911aaad1a0098b91c1f02859e641e8a
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-03-31 23:42:36 +0300

    create one more interface

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public abstract class FigureState {
+    private String figureName;
+
+    private String uniqueProperty;
+
+    public String getFigureName() {
+        return figureName;
+    }
+    public void setFigureName(String figureName) {
+        this.figureName = figureName;
+    }
+
+    public String getFigureName() {
+        return figureName;
+    }
+    public void setFigureName(String figureName) {
+        this.figureName = figureName;
+    }
+
+}
