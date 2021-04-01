3baee8826649f825cdbfcb0ecd446ccf340dfb0e
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-01 12:18:08 +0300

    made String[] a constant (final) (https://github.com/mate-academy/jv-oop-advanced/pull/99/files/151b0d552116147890654cdda9a9e87d89ce7564#diff-ba77ca4a477ac262d0344744f60facf433f3a95d7cb6649a7c0145c44cb9b2a2)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class ColorSupplier {
     public String generateColor() {
-        String[] colors = new String[]{"red", "blue", "green",
+        final String[] colors = new String[]{"red", "blue", "green",
                 "white", "yellow", "orange", "purple"};
 
         return colors[(int) (Math.random() * colors.length)];
