487566d59db2d012e7a4b7149cb4e5581c5607d2
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-04-01 16:14:17 +0300

    renamed constant FIGURES_COUNT

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,11 +4,11 @@ import core.basesyntax.figures.Figure;
 import java.util.Random;
 
 public class Main {
-    public static final int COUNT_FIGURES = 10;
+    public static final int FIGURES_COUNT = 10;
 
     public static void main(String[] args) {
         Random random = new Random();
-        Figure[] allFigure = new Figure[random.nextInt(COUNT_FIGURES)];
+        Figure[] allFigure = new Figure[random.nextInt(FIGURES_COUNT)];
 
         for (int i = 0; i < allFigure.length; i++) {
             allFigure[i] = new FigureSupplier().getFigure();
