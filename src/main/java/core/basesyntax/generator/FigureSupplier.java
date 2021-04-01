3885b6306f01e65cfa108dd52d325fc7334afd84
the5tranger <mishaantonyuk00@gmail.com>
2021-04-01 11:45:08 +0300

    HW-3 OOP_Advanced_fixed v2.1

--- src/main/java/core/basesyntax/generator/FigureSupplier.java
+++ src/main/java/core/basesyntax/generator/FigureSupplier.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int NUMBER_OF_FIGURES = 5;
 
-    public  Figure[] generateFigure(int figuresCount) {
+    public Figure[] generateFigure(int figuresCount) {
         Figure[] figures = new Figure[figuresCount];
         Random random = new Random();
         ColorSupplier colorSupplier = new ColorSupplier();
@@ -26,19 +26,20 @@ public class FigureSupplier {
                 case 1:
                     figures[i] = new IsoscelesTrapezoid(random.nextDouble() + random.nextInt(),
                             random.nextDouble()
-                            + random.nextInt(), random.nextDouble()
+                                    + random.nextInt(), random.nextDouble()
                             + random.nextInt(), "isosceles trapezoid",
                             colorSupplier.setColors());
                     break;
                 case 2:
                     figures[i] = new Rectangle(random.nextDouble() + random.nextInt(),
                             random.nextDouble()
-                            + random.nextInt(), "rectangle", colorSupplier.setColors());
+                                    + random.nextInt(), "rectangle", colorSupplier.setColors());
                     break;
                 case 3:
                     figures[i] = new RightTriangle(random.nextDouble() + random.nextInt(),
                             random.nextDouble()
-                            + random.nextInt(), "right triangle", colorSupplier.setColors());
+                                    + random.nextInt(), "right triangle",
+                            colorSupplier.setColors());
                     break;
                 case 4:
                     figures[i] = new Square(random.nextDouble() + random.nextInt(),
