c13df6c4e908388dec10ff1ea67150ffc15166c1
roman <nashpatsa@gmai.com>
2021-04-01 17:24:51 +0300

    changed all mistakes

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -8,7 +8,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         Figure[] figures = new Figure[new Random().nextInt(ARRAY_LIMIT) + 1];
         FigureSupplier supplier = new FigureSupplier();
-
         for (int i = 0; i < figures.length; i++) {
             figures[i] = supplier.getRandomFigure();
         }
