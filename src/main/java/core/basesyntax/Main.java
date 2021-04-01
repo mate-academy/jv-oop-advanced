883b93c6bdab690d8adc6462429741a8c2598822
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-01 12:39:12 +0300

    fixed minimal - maximal size of shapes array

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,11 +6,13 @@ import java.util.Random;
 
 public class Main {
 
-    private static final int SHAPES_LIMIT = 9;
+    private static final int MINIMAL_SIZE_OF_SHAPES_ARRAY = 1;
+    private static final int SHAPES_LIMIT = 10;
 
     public static void main(String[] args) {
         Random random = new Random();
-        Shape[] shapeArray = new Shape[1 + random.nextInt(SHAPES_LIMIT)];
+        Shape[] shapeArray = new Shape[MINIMAL_SIZE_OF_SHAPES_ARRAY
+                + random.nextInt(SHAPES_LIMIT - MINIMAL_SIZE_OF_SHAPES_ARRAY)];
         FigureSupplier figureSupplier = new FigureSupplier();
 
         for (int i = 0; i < shapeArray.length; i++) {
