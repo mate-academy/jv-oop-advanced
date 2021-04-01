e173f7bfb3363cb6135e87e8541534cec4efad3c
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-01 12:07:26 +0300

    Renamed fields. (https://github.com/mate-academy/jv-oop-advanced/pull/99/files/151b0d552116147890654cdda9a9e87d89ce7564#diff-d1ba72bd4a5b17627c4fa78dddf359a06d9c018b51b572b60dbad49bb68e450b)

--- src/main/java/core/basesyntax/shapes/RightTriangle.java
+++ src/main/java/core/basesyntax/shapes/RightTriangle.java
@@ -1,20 +1,20 @@
 package core.basesyntax.shapes;
 
 public class RightTriangle extends Shape {
-    private double width;
-    private double height;
+    private double firstLeg;
+    private double secondLeg;
     private double hypotenuse;
 
     public RightTriangle(String name, String color, double width, double height) {
         super(name, color);
-        this.width = width;
-        this.height = height;
-        this.hypotenuse = Math.sqrt(Math.pow(this.width, 2) + Math.pow(this.height, 2));
+        this.firstLeg = width;
+        this.secondLeg = height;
+        this.hypotenuse = Math.sqrt(Math.pow(this.firstLeg, 2) + Math.pow(this.secondLeg, 2));
     }
 
     @Override
     public double getArea() {
-        return (width * height) / 2;
+        return (firstLeg * secondLeg) / 2;
     }
 
     public double getHypotenuse() {
