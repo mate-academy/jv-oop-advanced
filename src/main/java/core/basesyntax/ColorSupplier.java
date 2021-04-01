7bcdc68903f5aef0086300692a7535fbc834cd40
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-01 14:59:53 +0300

    rewrote ColorSupplier (now it bases on constants)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,34 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
+    private static final String RED_COLOR = "red";
+    private static final String BLUE_COLOR = "blue";
+    private static final String GREEN_COLOR = "green";
+    private static final String WHITE_COLOR = "white";
+    private static final String YELLOW_COLOR = "yellow";
+    private static final String ORANGE_COLOR = "orange";
+    private static final String PURPLE_COLOR = "purple";
+    private static final int COLOR_LIMIT = 7;
+
     public String generateColor() {
         Random random = new Random();
-        final String[] colors = new String[]{"red", "blue", "green",
-                "white", "yellow", "orange", "purple"};
 
-        return colors[random.nextInt(colors.length)];
+        switch (1 + random.nextInt(COLOR_LIMIT)) {
+            case 1 :
+                return RED_COLOR;
+            case 2 :
+                return BLUE_COLOR;
+            case 3 :
+                return GREEN_COLOR;
+            case 4 :
+                return WHITE_COLOR;
+            case 5 :
+                return YELLOW_COLOR;
+            case 6 :
+                return ORANGE_COLOR;
+            default:
+                return PURPLE_COLOR;
+        }
     }
 }
