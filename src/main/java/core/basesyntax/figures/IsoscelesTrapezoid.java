eee890d19fd034a4a2bbe64446625121c75749de
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-04-01 13:35:13 +0300

    fixed all solution

--- src/main/java/core/basesyntax/figures/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/figures/IsoscelesTrapezoid.java
@@ -1,27 +1,27 @@
 package core.basesyntax.figures;
 
 public class IsoscelesTrapezoid extends Figure {
-    private double sideA;
-    private double sideB;
+    private double firstBase;
+    private double secondBase;
     private double height;
 
     public IsoscelesTrapezoid(double sideA, double sideB, double height,
                               String name, String color) {
         super(name, color);
-        this.sideA = sideA;
-        this.sideB = sideB;
+        this.firstBase = firstBase;
+        this.secondBase = secondBase;
         this.height = height;
     }
 
     @Override
     public double getArea() {
-        return ((sideA + sideB) / 2) * height;
+        return ((firstBase + secondBase) / 2) * height;
     }
 
     @Override
     public void draw() {
         System.out.println("Figure: " + getName() + ", getArea: "
-                + getArea() + " sq. units, sideA: " + sideA + " sq. units, sideB"
-                + sideB + " units: color " + getColor());
+                + getArea() + " sq. units, sideA: " + firstBase + " sq. units, sideB"
+                + secondBase + " units: color " + getColor());
     }
 }
