cfaac967430fe62260621e7d37376476fef8a845
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-03 01:08:51 +0300

    Created:     2 Interface: Dram, AreaCalculator.     1 abstract Class: Figure     5 Classes figures: Circle,                        Square,                        Regtangle,                        Right Triangle,                        Isosceles Trapezoid     3 Classes for rand colors and figures:                        Color,                        ColorSupplier,                        FigureSupplier
    *Renamed the file HelloWorld.java to Main.java .
    *Made logic for rand colors and figure.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,7 +8,7 @@ import java.util.Random;
 /**
  * Feel free to remove this class and create your own.
  */
-public class HelloWorld extends FigureSupplier {
+public class Main extends FigureSupplier {
     public static void main(String[] args) {
 
       //Figure figure = getRandomFigure();
