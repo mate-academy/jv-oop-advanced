7f8dd5c24d25ccd567b1115ff1c04d6f4cc5c752
olesiasmitiukh <d.olleska@gmail.com>
2021-06-02 17:16:49 +0300

    added method for realisation/show figuresList

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,21 @@
 package core.basesyntax;
 
 public class Main {
-    public static void main(String[] args) {
+    public static final int FIGURE_COUNT = 5;
 
+    public static void main(String[] args) {
+        Figures[] figuresList = getShapes();
+        for (Figures list : figuresList) {
+            list.draw();
+        }
     }
 
+    private static Figures[] getShapes() {
+        Figures[] figuresList = new Figures[FIGURE_COUNT];
+        FigureSupplier supplier = new FigureSupplier();
+        for (int i = 0; i < FIGURE_COUNT; i++) {
+            figuresList[i] = supplier.getRandomFigure();
+        }
+        return figuresList;
+    }
 }
