429b5a6fb3a61464929f6dfe462ca047dc489e07
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-06-02 13:56:51 +0300

    deleted empty line and brackets

--- src/main/java/core/basesyntax/suppliers/ColorSupplier.java
+++ src/main/java/core/basesyntax/suppliers/ColorSupplier.java
@@ -3,11 +3,11 @@ package core.basesyntax.suppliers;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final String[] COLORS =
+    private static final String[] COLORS_ARRAY =
             new String[]{"grey", "blue", "white", "red", "yellow", "green"};
 
     public String getRandomColor() {
         Random random = new Random();
-        return COLORS[random.nextInt(COLORS.length)];
+        return COLORS_ARRAY[random.nextInt(COLORS_ARRAY.length)];
     }
 }
