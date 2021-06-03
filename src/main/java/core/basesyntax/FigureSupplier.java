f78e7aed900db286be45158a641bdf877d897354
Yura Lug <LUV97@ukr.net>
2021-06-03 13:37:15 +0300

    Really fixing constants and that's all

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,31 +3,31 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int FIGURECOUNT = 5;
-    private static final int DEFAULTSIDE = 10;
+    private static final int FIGURE_COUNT = 5;
+    private static final int DEFAULT_SIDE = 10;
 
     public Figure getRandomFigure() {
         Random random = new Random();
         ColorSupplier colorSupplier = new ColorSupplier();
-        switch (random.nextInt(FIGURECOUNT)) {
+        switch (random.nextInt(FIGURE_COUNT)) {
             case 0:
                 return new Square(colorSupplier.getRandomColor(),
-                            random.nextInt(DEFAULTSIDE) + 1);
+                            random.nextInt(DEFAULT_SIDE));
             case 1:
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                          random.nextInt(DEFAULTSIDE) + 1,
-                        random.nextInt(DEFAULTSIDE) + 1);
+                          random.nextInt(DEFAULT_SIDE),
+                        random.nextInt(DEFAULT_SIDE));
             case 2:
                 return new Circle(colorSupplier.getRandomColor(),
-                        random.nextInt(DEFAULTSIDE) + 1);
+                        random.nextInt(DEFAULT_SIDE));
             case 3:
                 return new Rectangle(colorSupplier.getRandomColor(),
-                               random.nextInt(DEFAULTSIDE) + 1,
-                                    random.nextInt(DEFAULTSIDE) + 1);
+                               random.nextInt(DEFAULT_SIDE),
+                                    random.nextInt(DEFAULT_SIDE));
             default:
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                          random.nextInt(DEFAULTSIDE) + 1,
-                        random.nextInt(DEFAULTSIDE) + 1, random.nextInt(DEFAULTSIDE) + 1);
+                          random.nextInt(DEFAULT_SIDE),
+                        random.nextInt(DEFAULT_SIDE), random.nextInt(DEFAULT_SIDE));
         }
     }
 }
