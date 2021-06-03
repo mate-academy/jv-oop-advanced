872b29f0ed799f79689f36bfb9bfa6ba13a9330f
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-03 15:11:41 +0300

    Delete magic number 5 change randInts(min,max)

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,12 +2,18 @@ package core.basesyntax;
 
 import core.basesyntax.suppliers.FigureSupplier;
 
+import java.util.Random;
+
 public class Main extends FigureSupplier {
     public static void main(String[] args) {
-        Figure[] figures = new Figure[5];
+        Figure[] figures = new Figure[randInts(1,20)];
         for (int i = 0; i < figures.length; i++) {
             figures[i] = getRandomFigure();
             figures[i].draw();
         }
+    }
+
+    public static int randInts(int min, int max) {
+        return 1 + (100 - 1) * (new Random().nextInt());
     }
 }
