d7127876bdaa9a51c55d703a9eb04c2345cce9a6
Optipist <vlad.baydak28@gmail.com>
2021-06-03 16:34:00 +0300

    Added random number limit in FigureSupplier

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAX_FIGURES = 5;
-    private static final int RANDOM_NUMBER_LIMIT = 100;
+    private static final int RANDOM_LIMIT = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random randomNumber = new Random();
 
@@ -12,21 +12,21 @@ public class FigureSupplier {
         switch (new Random().nextInt(MAX_FIGURES)) {
             case 0:
                 return new Circle("circle", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
+                        randomNumber.nextInt(RANDOM_LIMIT));
             case 1:
                 return new IsoscelesTrapezoid("isosceles trapezoid", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT), randomNumber.nextInt(RANDOM_NUMBER_LIMIT),
-                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
+                        randomNumber.nextInt(RANDOM_LIMIT), randomNumber.nextInt(RANDOM_LIMIT),
+                        randomNumber.nextInt(RANDOM_LIMIT));
             case 2:
                 return new Rectangle("rectangle", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT), randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
+                        randomNumber.nextInt(RANDOM_LIMIT), randomNumber.nextInt(RANDOM_LIMIT));
             case 3:
                 return new RightTriangle("right triangle", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT), randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
+                        randomNumber.nextInt(RANDOM_LIMIT), randomNumber.nextInt(RANDOM_LIMIT));
             case 4:
             default:
                 return new Square("square", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
+                        randomNumber.nextInt(RANDOM_LIMIT));
         }
     }
 }
