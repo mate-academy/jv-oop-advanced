7c2b62b7c25c296721d830f848250605fd002e80
PashaTerletskyi <p.terletsky@gmail.com>
2021-06-03 13:45:37 +0300

    Add FIGURES_COUNT_TO_CREATE

--- src/main/java/figure/FigureMain.java
+++ src/main/java/figure/FigureMain.java
@@ -3,9 +3,10 @@ package figure;
 import java.util.Random;
 
 public class FigureMain extends FigureSupplier {
+    public static final int FIGURES_COUNT_TO_CREATE = 50;
 
     public static void main(String[] args) {
-        Figure[] figures = createRandomArrayOfFigures(new Random().nextInt(RANDOM_UNIT) + 1);
+        Figure[] figures = createRandomArrayOfFigures(new Random().nextInt(FIGURES_COUNT_TO_CREATE) + 1);
         for (Figure element : figures) {
             element.draw();
         }
