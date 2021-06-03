901d7f47f9d44b8aa3fb3b324a73e74f7dd2af8a
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-06-03 16:21:33 +0300

    Update FigureSupplier.java

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAX_FIGURES = 5;
+    private static final int RANDOM_NUMBER_LIMIT = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random randomNumber = new Random();
 
@@ -11,21 +12,21 @@ public class FigureSupplier {
         switch (new Random().nextInt(MAX_FIGURES)) {
             case 0:
                 return new Circle("circle", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(100));
+                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
             case 1:
                 return new IsoscelesTrapezoid("isosceles trapezoid", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(100), randomNumber.nextInt(100),
-                        randomNumber.nextInt(100));
+                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT), randomNumber.nextInt(RANDOM_NUMBER_LIMIT),
+                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
             case 2:
                 return new Rectangle("rectangle", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(100), randomNumber.nextInt(100));
+                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT), randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
             case 3:
                 return new RightTriangle("right triangle", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(100), randomNumber.nextInt(100));
+                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT), randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
             case 4:
             default:
                 return new Square("square", colorSupplier.getRandomColor(),
-                        randomNumber.nextInt(100));
+                        randomNumber.nextInt(RANDOM_NUMBER_LIMIT));
         }
     }
 }
