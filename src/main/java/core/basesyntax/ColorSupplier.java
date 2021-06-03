38df67a3ebcec377be27887eedc7a9cb688f9432
olesiasmitiukh <d.olleska@gmail.com>
2021-06-03 16:25:23 +0300

    rename array colorsArr to colors

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        Colors[] colorsArr = Colors.values();
-        int randomIndex = new Random().nextInt(colorsArr.length);
-        return colorsArr[randomIndex].toString();
+        Colors[] colors = Colors.values();
+        int randomIndex = new Random().nextInt(colors.length);
+        return colors[randomIndex].toString();
     }
 }
