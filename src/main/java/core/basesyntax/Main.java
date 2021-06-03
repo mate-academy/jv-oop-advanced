f8c239c6a9a0e7c77ed809aadf314e28d5464493
olesiasmitiukh <d.olleska@gmail.com>
2021-06-03 16:28:07 +0300

    rename array figuresList to figures

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -11,11 +11,11 @@ public class Main {
     }
 
     private static Figures[] getShapes() {
-        Figures[] figuresList = new Figures[FIGURE_COUNT];
+        Figures[] figures = new Figures[FIGURE_COUNT];
         FigureSupplier supplier = new FigureSupplier();
         for (int i = 0; i < FIGURE_COUNT; i++) {
-            figuresList[i] = supplier.getRandomFigure();
+            figures[i] = supplier.getRandomFigure();
         }
-        return figuresList;
+        return figures;
     }
 }
