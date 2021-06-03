230abd101adffee39b6c9bd36ddf408fbc509475
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-06-03 14:19:21 +0300

    Update src/main/java/core/basesyntax/FigureSupplier.java
    Co-authored-by: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int MAXFIGURES = 5;
+    private static final int MAX_FIGURES = 5;
 
     public Figure getRandomFigure() {
         switch (new Random().nextInt(MAXFIGURES)) {
