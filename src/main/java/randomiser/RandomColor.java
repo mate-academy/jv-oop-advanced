36cc76a8e9cfc3e64d06b28162a16268be4551f5
Daniel Kuruch <kuruch1004@gmail.com>
2021-06-03 12:06:01 +0300

    fixed bug

--- src/main/java/randomiser/RandomColor.java
+++ src/main/java/randomiser/RandomColor.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class RandomColor {
     private static final Random rnd = new Random();
-    private static final int NUMBER_OF_FIGURES = 25;
+    private static final int NUMBER_OF_COLORS = 24;
+    private static final int NUMBER_OF_FIGURES = 5;
 
     public static Color getRandomColor() {
         int randomIndex = getRandomIndex();
