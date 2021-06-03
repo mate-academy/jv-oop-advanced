e2a637f2ad4d6bb06ade52fb6f1448b3e4c8aa77
PashaTerletskyi <p.terletsky@gmail.com>
2021-06-03 14:38:52 +0300

    Fixed checkstyle

--- src/main/java/figure/FigureMain.java
+++ src/main/java/figure/FigureMain.java
@@ -6,7 +6,8 @@ public class FigureMain extends FigureSupplier {
     public static final int FIGURES_COUNT_TO_CREATE = 50;
 
     public static void main(String[] args) {
-        Figure[] figures = createRandomArrayOfFigures(new Random().nextInt(FIGURES_COUNT_TO_CREATE) + 1);
+        Figure[] figures =
+                createRandomArrayOfFigures(new Random().nextInt(FIGURES_COUNT_TO_CREATE) + 1);
         for (Figure element : figures) {
             element.draw();
         }
