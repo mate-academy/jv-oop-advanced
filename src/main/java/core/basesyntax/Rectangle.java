aff61597fac2039c579d549d2db8bb0aea86aafe
Nikitozin <nikitozin91@gmail.com>
2021-06-03 13:33:15 +0300

    rename_class

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Rectangle extends Figure implements Area, Drawable {
+public class Rectangle extends Figure {
     private double side1;
     private double side2;
 
