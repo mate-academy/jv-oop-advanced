c99815a24d6d726bf7290ac65dd3f3ecfdc99f5f
yuriiadam <dr.damba@outlook.com>
2021-06-03 12:42:44 +0300

    was added array with figures in the main class

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,26 +3,27 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    public int getRandomNum() {
+        return new Random().nextInt(50);
+    }
 
     public Figure getRandomFigure() {
-        int getRandomIndex = new Random().nextInt(6);
+        int getRandomIndex = new Random().nextInt(5);
         ColorSupplier supplier = new ColorSupplier();
         switch (getRandomIndex) {
             case 1:
-                return new Circle(supplier.getRandomColor(), 3);
+                return new Circle(supplier.getRandomColor(), getRandomNum());
             case 2:
-                return new Rectangle(supplier.getRandomColor(), 5,5);
+                return new Rectangle(supplier.getRandomColor(), getRandomNum(),getRandomNum());
             case 3:
                 return new IsoscelesTrapezoid(supplier.getRandomColor(),
-                        2,
-                        3,
-                        4);
+                        getRandomNum(),
+                        getRandomNum(),
+                        getRandomNum());
             case 4:
-                return new Square(supplier.getRandomColor(), 4);
-            case 5:
-                return new RightTriangle(supplier.getRandomColor(), 4, 5);
+                return new Square(supplier.getRandomColor(), getRandomNum());
             default:
-                return getRandomFigure();
+                return new RightTriangle(supplier.getRandomColor(), getRandomNum(), getRandomNum());
         }
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
 public class Main {
+
+    private static final int NUMBER_OF_FIGURES = 5;
+
     public static void main(String[] args) {
-        FigureSupplier figure = new FigureSupplier();
-        System.out.println(figure.getRandomFigure().getInfo());
+        Figure[] figuresArray = new Figure[NUMBER_OF_FIGURES];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < figuresArray.length; i++) {
+            figuresArray[i] = figureSupplier.getRandomFigure();
+            figuresArray[i].getInfo();
+        }
+        for (int s = 0; s < NUMBER_OF_FIGURES; s++) {
+            System.out.println(figuresArray[s].getInfo());
+        }
     }
 }
