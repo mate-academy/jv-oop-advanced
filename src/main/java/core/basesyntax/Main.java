4adf3ff65a150949adf48435b84e3cb644fd2749
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-03 21:34:30 +0300

    Upper case variable

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import core.basesyntax.suppliers.FigureSupplier;
 
 public class Main {
-    public static final int sizeArray = 5;
+    public static final int ARRAY_SIZE = 5;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
