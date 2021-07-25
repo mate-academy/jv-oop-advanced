807e371b940fbcfe26343ec8081b9c07a8c1d3d2
Kostia Dementiev <konstde00@gmail.com>
2021-07-25 17:17:49 +0300

    Mistakes in Main class was deleted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         double a = Math.random();
         double b = Math.random();
-        int arrayLength = (int)Math.round(a * b * 10000) + 2;
-        System.out.println(arrayLength);
+        int arrayLength = (int)Math.round(a * b * 1000) + 2;
         Figure[] figures = new Figure[arrayLength];
         for (int i = 0; i < figures.length; i++) {
             figures[i] = new FigureSupplier().getRandomFigure();
