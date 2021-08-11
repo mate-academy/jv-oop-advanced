615679474afabe4b163603c93d13a44aa8019634
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-08-11 17:56:47 +0300

    Removed variable typeOfFigure in the class Figure, fixed checkstyle error

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -2,19 +2,13 @@ package core.basesyntax;
 
 public abstract class Figure implements Drawable, AreaCalculable {
     private String color;
-    private String typeOfFigure;
 
-    Figure(String color, String typeOfFigure) {
+    Figure(String color) {
         this.color = color;
-        this.typeOfFigure = typeOfFigure;
     }
 
     String getColor() {
         return this.color;
-    }
-
-    public String getTypeOfFigure() {
-        return this.typeOfFigure;
     }
 
 }
