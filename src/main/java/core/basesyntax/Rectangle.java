b665395ca37d73c1f1e25c59ae18ca275201e0d3
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-08-11 18:23:28 +0300

    Added class Rectangle

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,34 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private int sideA;
+    private int sideB;
+
+    Rectangle(String color, int sideA, int sideB) {
+        super(color);
+        this.sideA = sideA > 0 ? sideA : 0;
+        this.sideB = sideB > 0 ? sideB : 0;
+    }
+
+    @Override
+    public double getArea() {
+        return this.getSideA() * this.getSideB();
+    }
+
+    @Override
+    public void draw() {
+        System.out.println(new StringBuilder().append(" Figure: rectangle, area: ")
+                .append(this.getArea()).append(" sq.units, sideA: ")
+                .append(this.getSideA()).append(" units, ")
+                .append("sideB: ").append(this.getSideB()).append(" units, color: ")
+                .append(this.getColor()).toString());
+    }
+
+    public int getSideB() {
+        return sideB;
+    }
+
+    public int getSideA() {
+        return sideA;
+    }
+}
