ad9f7c8d780aecb3432f9e99c67f6981f3612f47
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-08-11 18:46:42 +0300

    Added class Circle

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private int radius;
+
+    Circle(String color, int radius) {
+        super(color);
+        this.radius = radius > 0 ? radius : 0;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * this.getRadius() * this.getRadius();
+    }
+
+    @Override
+    public void draw() {
+        System.out.println(new StringBuilder().append(" Figure: circle, area: ")
+                .append(this.getArea()).append(" sq.units, radius: ")
+                .append(this.getRadius()).append(" units, color: ")
+                .append(this.getColor()).toString());
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
