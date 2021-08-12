2e7c669ddf7cc53355322155305fad0e982dd591
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-08-12 12:00:29 +0300

    Moved the random variable from the getRandomColor method and changed its access modifier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
         Colors[] allColors = Colors.values();
-        Random random = new Random();
         int randomIndex = random.nextInt(allColors.length);
         return allColors[randomIndex].toString();
     }
