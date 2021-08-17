79a11678d8b6b430814035433ae004cccc6e7bdc
Yehor Zapara <egor.zapara1994@gmail.com>
2021-08-17 20:19:39 +0300

    class ColorSupplier was added

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,37 @@
+package core.basesyntax;
+
+public class ColorSupplier {
+    public String getRandomColor() {
+        String figureColor;
+        switch ((int) (Math.random() * 7)) {
+            case 0:
+                figureColor = "yellow";
+                break;
+            case 1:
+                figureColor = "red";
+                break;
+            case 2:
+                figureColor = "green";
+                break;
+            case 3:
+                figureColor = "blue";
+                break;
+            case 4:
+                figureColor = "cyan";
+                break;
+            case 5:
+                figureColor = "gray";
+                break;
+            case 6:
+                figureColor = "magenta";
+                break;
+            case 7:
+                figureColor = "black";
+                break;
+            default:
+                figureColor = "white";
+                break;
+        }
+        return figureColor;
+    }
+}
