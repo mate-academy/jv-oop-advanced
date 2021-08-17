f9a614079c0184395e4fd3f5fe738e704b9eae92
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-08-17 11:37:56 +0300

    optimized code and created variable for random color

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -13,22 +13,19 @@ public class FigureSupplier {
         int firstSide = random.nextInt(MAX_SIDE_LENGTH);
         int secondSide = random.nextInt(MAX_SIDE_LENGTH);
         int thirdSide = random.nextInt(MAX_SIDE_LENGTH);
+        String color = colorSupplier.getRandomColor();
 
         switch (figureNumber) {
             case 1:
-                return new Square(firstSide, colorSupplier.getRandomColor());
+                return new Square(firstSide, color);
             case 2:
-                return new Rectangle(firstSide, secondSide,
-                        colorSupplier.getRandomColor());
+                return new Rectangle(firstSide, secondSide, color);
             case 3:
-                return new Circle(firstSide,
-                        colorSupplier.getRandomColor());
+                return new Circle(firstSide, color);
             case 4:
-                return new RightTriangle(firstSide, secondSide,
-                        colorSupplier.getRandomColor());
+                return new RightTriangle(firstSide, secondSide, color);
             default:
-                return new IsoscelesTrapezoid(firstSide, secondSide, thirdSide,
-                        colorSupplier.getRandomColor());
+                return new IsoscelesTrapezoid(firstSide, secondSide, thirdSide, color);
         }
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Figure[] figures = new Figure[10];
         FigureSupplier figureSupplier = new FigureSupplier();
-
         for (int i = 0; i < figures.length; i++) {
             figures[i] = figureSupplier.getRandomFigure();
             figures[i].drawFigure();
