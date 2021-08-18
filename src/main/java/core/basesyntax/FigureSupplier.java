eb0c2ed3ddce4d4909fb9e053708a1f194ebd77c
Yuliya Senchenko <usencenko70@gmail.com>
2021-08-18 17:24:03 +0300

    Create project jv-oop-advanced(hw-3) fixed

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,7 +7,7 @@ public class FigureSupplier {
 
     private static final String [] FIGURE = {"Square", "Rectangle", "RightTriangle", "IsoscelesTrapezoid",
             "Circle"};
-    public double getNum() {
+    public double getRandomNumber() {
         return new Random().nextInt(NUMBER_COLOR) + 1;
     }
 
@@ -16,16 +16,16 @@ public class FigureSupplier {
 
         int randomFigure = new Random().nextInt(FIGURE.length);
         if (FIGURE[randomFigure].equals("Square")) {
-            return new Square(randomColor, FIGURE[randomFigure], (int) getNum());
+            return new Square(randomColor, FIGURE[randomFigure], (int) getRandomNumber());
         } else if (FIGURE[randomFigure].equals("Rectangle")) {
-            return new Rectangle(randomColor, FIGURE[randomFigure], (int) getNum(), (int) getNum());
+            return new Rectangle(randomColor, FIGURE[randomFigure], (int) getRandomNumber(), (int) getRandomNumber());
         } else if (FIGURE[randomFigure].equals("RightTriangle")) {
-            return new RightTriangle(randomColor, FIGURE[randomFigure],(int) getNum(), (int) getNum());
+            return new RightTriangle(randomColor, FIGURE[randomFigure],(int) getRandomNumber(), (int) getRandomNumber());
         } else if (FIGURE[randomFigure].equals("IsoscelesTrapezoid")) {
-            return new IsoscelesTrapezoid(randomColor, FIGURE[randomFigure], (int) getNum(), (int) getNum(),
-                    (int) getNum());
+            return new IsoscelesTrapezoid(randomColor, FIGURE[randomFigure], (int) getRandomNumber(), (int) getRandomNumber(),
+                    (int) getRandomNumber());
         } else {
-            return new Circle(randomColor, FIGURE[randomFigure], (int) getNum());
+            return new Circle(randomColor, FIGURE[randomFigure], (int) getRandomNumber());
         }
     }
 }
