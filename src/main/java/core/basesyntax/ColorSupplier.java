26b4c5a98316460a1402857a585515ade25ea66e
Yehor Zapara <egor.zapara1994@gmail.com>
2021-08-18 12:18:31 +0300

    code style mistace was fixed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     public static final int MAX_NUMBER_OF_COLOR = 8;
-    private Random random = new Random();
     private static final String[] COLORS = new String[] {"yellow", "red", "green", "blue",
             "cyan", "gray", "magenta", "black"};
+    private Random random = new Random();
 
     public String getRandomColor() {
         int colorNumber = random.nextInt(MAX_NUMBER_OF_COLOR);
