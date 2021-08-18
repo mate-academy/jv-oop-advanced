f893cd7093e7c9896f1dc6ef5d35018e947a872a
Yuliya Senchenko <usencenko70@gmail.com>
2021-08-18 17:22:17 +0300

    Create project jv-oop-advanced(hw-3) fixed

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,27 +5,27 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int NUMBER_COLOR = 6;
 
-    private static final String [] FIGURE = {"Square", "Rectangle", "Triangle", "Trapeze", "Circl"};
-
-    public double getRandomNumber() {
+    private static final String [] FIGURE = {"Square", "Rectangle", "RightTriangle", "IsoscelesTrapezoid",
+            "Circle"};
+    public double getNum() {
         return new Random().nextInt(NUMBER_COLOR) + 1;
     }
 
     public Figure getRandomFigure() {
-        String color = new ColorSupplier().getRandomColor();
+        String randomColor = new ColorSupplier().getRandomColor();
 
-        int figure = new Random().nextInt(FIGURE.length);
-        if (FIGURE[figure].equals("Square")) {
-            return new Square(color, FIGURE[figure], (int) getRandomNumber());
-        } else if (FIGURE[figure].equals("Rectangle")) {
-            return new Rectangle(color, FIGURE[figure], (int) getRandomNumber(), (int) getRandomNumber());
-        } else if (FIGURE[figure].equals("Triangle")) {
-            return new RightTriangle(color, FIGURE[figure],(int) getRandomNumber(), (int) getRandomNumber());
-        } else if (FIGURE[figure].equals("Trapeze")) {
-            return new IsoscelesTrapezoid(color, FIGURE[figure], (int) getRandomNumber(), (int) getRandomNumber(),
-                    (int) getRandomNumber());
+        int randomFigure = new Random().nextInt(FIGURE.length);
+        if (FIGURE[randomFigure].equals("Square")) {
+            return new Square(randomColor, FIGURE[randomFigure], (int) getNum());
+        } else if (FIGURE[randomFigure].equals("Rectangle")) {
+            return new Rectangle(randomColor, FIGURE[randomFigure], (int) getNum(), (int) getNum());
+        } else if (FIGURE[randomFigure].equals("RightTriangle")) {
+            return new RightTriangle(randomColor, FIGURE[randomFigure],(int) getNum(), (int) getNum());
+        } else if (FIGURE[randomFigure].equals("IsoscelesTrapezoid")) {
+            return new IsoscelesTrapezoid(randomColor, FIGURE[randomFigure], (int) getNum(), (int) getNum(),
+                    (int) getNum());
         } else {
-            return new Circle(color, FIGURE[figure], (int) getRandomNumber());
+            return new Circle(randomColor, FIGURE[randomFigure], (int) getNum());
         }
     }
 }
