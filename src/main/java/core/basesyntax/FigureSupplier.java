12b01c9e0658770b8b8f8f48c620e1e9202d610a
Serhii Orlov <orlov30@gmail.com>
2021-08-18 16:05:47 +0300

    Created main() method, tests and check working programm.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -11,9 +11,21 @@ public class FigureSupplier {
             }
         };
 
-        switch (new Random().nextInt(4) + 1) {
+        switch (new Random().nextInt(5) + 1) {
             case 1:
                 figure =  new Square();
+                break;
+            case 2:
+                figure = new Rectangle();
+                break;
+            case 3:
+                figure = new RightTriangle();
+                break;
+            case 4:
+                figure = new Circle();
+                break;
+            case 5:
+                figure = new IsoscelesTrapezoid();
                 break;
         }
          return figure;
--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 /**
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        Figure[] figureS = new Figure[new Random().nextInt(100)];
+        for (int i = 0; i < figureS.length; i++) {
+            figureS[i] = new FigureSupplier().getRandomFigure();
+            figureS[i].draw();
+        }
+
+    }
 
 }
