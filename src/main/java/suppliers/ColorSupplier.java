d5f525d2f2133f2ca73356c915a61a5c43be5fb8
Vladyslav Gural <vladgural@gmail.com>
2021-08-18 12:02:18 +0300

    changed constant name from colours to COLORS

--- src/main/java/suppliers/ColorSupplier.java
+++ src/main/java/suppliers/ColorSupplier.java
@@ -3,11 +3,11 @@ package suppliers;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static final String[] colours = {"white", "black", "red", "orange",
+    public static final String[] COLORS = {"white", "black", "red", "orange",
             "yellow", "green", "blue", "purple"};
     private Random random = new Random();
 
     public String getRandomColor() {
-        return colours[random.nextInt(colours.length)];
+        return COLORS[random.nextInt(COLORS.length)];
     }
 }
