473d3de9a51394311c00a33d525a4d5d4349bb76
Yehor Zapara <egor.zapara1994@gmail.com>
2021-08-18 12:14:36 +0300

    array of colors was changed to constant

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class ColorSupplier {
     public static final int MAX_NUMBER_OF_COLOR = 8;
     private Random random = new Random();
+    private static final String[] COLORS = new String[] {"yellow", "red", "green", "blue",
+            "cyan", "gray", "magenta", "black"};
 
     public String getRandomColor() {
         int colorNumber = random.nextInt(MAX_NUMBER_OF_COLOR);
-        String[] colors = new String[] {"yellow", "red", "green", "blue",
-                "cyan", "gray", "magenta", "black"};
-        return colors[colorNumber];
+        return COLORS[colorNumber];
     }
 }
