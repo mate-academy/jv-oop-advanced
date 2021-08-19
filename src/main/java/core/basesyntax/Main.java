95518003c630af5cffd127d5746fe6e7dcc96bfe
Svyatos_Furs <50773032+Svyatos-dev@users.noreply.github.com>
2021-08-19 20:47:25 +0300

    Update Main.java
    5 have been implemented as CONSTANT

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,8 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        Figure[] figuresArray = new Figure[5];
+        private final static int CONSTANT = 5;
+        Figure[] figuresArray = new Figure[CONSTANT];
         FigureSupplier getFigure = new FigureSupplier();
         for (int i = 0; i < figuresArray.length; i++) {
             figuresArray[i] = getFigure.randomFigureSupplier();
