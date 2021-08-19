eea03f4bba6f6d6790ac3f9227039765841f7f0a
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-08-19 19:48:00 +0300

    renamed constNumber

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int CONST_NUMBER = 10;
+    private static final int MAX_FIGURE_PARAMETER_VALUE = 10;
     private static final int RANDOM_NUMBER_FIGURE = 5;
     private Random random = new Random();
 
@@ -12,18 +12,23 @@ public class FigureSupplier {
         switch (random.nextInt(RANDOM_NUMBER_FIGURE)) {
             case (0):
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(CONST_NUMBER), random.nextInt(CONST_NUMBER),
-                        random.nextInt(CONST_NUMBER));
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE),
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE),
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE));
             case (1):
-                return new Circle(colorSupplier.getRandomColor(), random.nextInt(CONST_NUMBER));
+                return new Circle(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE));
             case (2):
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextInt(CONST_NUMBER), random.nextInt(CONST_NUMBER));
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE),
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE));
             case (3):
-                return new Rectangle(colorSupplier.getRandomColor(), random.nextInt(CONST_NUMBER),
-                        random.nextInt(CONST_NUMBER));
+                return new Rectangle(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE),
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE));
             default:
-                return new Square(colorSupplier.getRandomColor(), random.nextInt(CONST_NUMBER));
+                return new Square(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_FIGURE_PARAMETER_VALUE));
         }
     }
 }
