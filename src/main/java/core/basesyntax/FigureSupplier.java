16892576bc18922802c38779a715b0ead0b1701d
Kyrylo Dvornyk <kiril75341@gmail.com>
2021-08-19 08:27:34 +0300

    Implemented java oop advanced task

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAX_SIZE = 100;
+    private static final int MIN_SIZE = 1;
     private static final int FIGURES_NUMBER = 4;
 
     public Figure getRandomFigure() {
@@ -11,18 +12,19 @@ public class FigureSupplier {
         String color = new ColorSupplier().getRandomColor();
         switch (random.nextInt(FIGURES_NUMBER)) {
             case 0:
-                return new Rectangle(random.nextInt(MAX_SIZE) + 1, random.nextInt(MAX_SIZE) + 1,
-                        color);
+                return new Rectangle(random.nextInt(MAX_SIZE) + MIN_SIZE, random.nextInt(MAX_SIZE)
+                        + MIN_SIZE, color);
             case 1:
-                return new Circle(random.nextInt(MAX_SIZE) + 1, color);
+                return new Circle(random.nextInt(MAX_SIZE) + MIN_SIZE, color);
             case 2:
-                return new RightTriangle(random.nextInt(MAX_SIZE) + 1, random.nextInt(MAX_SIZE) + 1,
-                        color);
+                return new RightTriangle(random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE, color);
             case 3:
-                return new IsoscelesTrapezoid(random.nextInt(MAX_SIZE) + 1,
-                        random.nextInt(MAX_SIZE) + 1, random.nextInt(MAX_SIZE) + 1, color);
+                return new IsoscelesTrapezoid(random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE, color);
             default:
-                return new Square(random.nextInt(MAX_SIZE) + 1, color);
+                return new Square(random.nextInt(MAX_SIZE) + MIN_SIZE, color);
         }
     }
 }
