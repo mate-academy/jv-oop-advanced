cf34ac3fb1089e2fd165c8ec20e29b257b7bfcfc
Solomiia Hrynda <solomiyaaaa@gmail.com>
2021-08-19 13:42:56 +0300

    Changed the name of interface and deleted an empty line in FigureSupplier class.

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawer, AreaCalculator {
+public abstract class Figure implements Drawable, AreaCalculator {
     private String name;
     private String color;
 
