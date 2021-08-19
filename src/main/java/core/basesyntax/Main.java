722b057cd936eb14e3cf6c029a1337f2a1e7029b
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-08-19 19:55:35 +0300

    renamed constNumber #2

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Main {
-    private static final int CONST_NUMBER = 10;
+    private static final int ARRAY_MAX_SIZE = 10;
 
     public static void main(String[] args) {
-        Figure[] figures = new Figure[new Random().nextInt(CONST_NUMBER)];
+        Figure[] figures = new Figure[new Random().nextInt(ARRAY_MAX_SIZE)];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (Figure figure : figures) {
             figure = figureSupplier.getRandomFigure();
