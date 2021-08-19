9311cc412a40b5b9ed863d6390e455c77f22666b
Dmytro Heleta <dmytroheleta@gmail.com>
2021-08-19 17:21:59 +0300

    refactor class Name

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawable, FigureCalculator {
+public abstract class Figure implements Drawable, AreaCalculator {
     private String color;
 
     public Figure(String figureColor) {
