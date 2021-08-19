6f1bb2ad46a1b08f20a3e878b77535f9c1930834
Serhii Matsko <jmen.serg@gmail.com>
2021-08-19 16:27:38 +0300

    constant bug fixed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Main {
+    private static final int MAX_ARRAY_COUNT = 10;
+
     public static void main(String[] args) {
-        final int MAX_FIGURES_COUNT = 10;
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[new Random().nextInt(MAX_FIGURES_COUNT)];
+        Figure[] figures = new Figure[new Random().nextInt(MAX_ARRAY_COUNT)];
         for (int i = 0; i < figures.length; i++) {
             figures[i] = figureSupplier.getRandomFigure();
             figures[i].draw();
