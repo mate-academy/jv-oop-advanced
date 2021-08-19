db843c83ceb71dd24501949dbac2ce5c826cbc9a
Dmytro Heleta <dmytroheleta@gmail.com>
2021-08-19 16:30:37 +0300

    add private getRandomNumber method

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,36 +7,40 @@ public class FigureSupplier {
     public static final int MAX_SIZE_OF_UNITS = 10;
     private Random random = new Random();
 
+    private int getRandomNumber(int limit) {
+        return random.nextInt(limit) + 1;
+    }
+
     public Figure getRandomFigure() {
-        int numberOfFigure = random.nextInt(NUMBER_OF_FIGURES);
+        int numberOfFigure = getRandomNumber(NUMBER_OF_FIGURES);
         Figure randomFigure;
 
         switch (numberOfFigure) {
             case 0:
                 randomFigure = new Square(new ColorSupplier().getRandomColor(),
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1);
+                        getRandomNumber(MAX_SIZE_OF_UNITS));
                 break;
             case 1:
                 randomFigure = new Rectangle(new ColorSupplier().getRandomColor(),
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1,
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1);
+                        getRandomNumber(MAX_SIZE_OF_UNITS),
+                        getRandomNumber(MAX_SIZE_OF_UNITS));
                 break;
             case 2:
                 randomFigure = new RightTriangle(new ColorSupplier().getRandomColor(),
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1,
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1);
+                        getRandomNumber(MAX_SIZE_OF_UNITS),
+                        getRandomNumber(MAX_SIZE_OF_UNITS));
                 break;
             case 3:
                 randomFigure = new Circle(new ColorSupplier().getRandomColor(),
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1);
+                        getRandomNumber(MAX_SIZE_OF_UNITS));
                 break;
             case 4:
             default:
                 randomFigure = new IsoscelesTrapezoid(new ColorSupplier().getRandomColor(),
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1,
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1,
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1,
-                        random.nextInt(MAX_SIZE_OF_UNITS) + 1);
+                        getRandomNumber(MAX_SIZE_OF_UNITS),
+                        getRandomNumber(MAX_SIZE_OF_UNITS),
+                        getRandomNumber(MAX_SIZE_OF_UNITS),
+                        getRandomNumber(MAX_SIZE_OF_UNITS));
                 break;
         }
         return randomFigure;
