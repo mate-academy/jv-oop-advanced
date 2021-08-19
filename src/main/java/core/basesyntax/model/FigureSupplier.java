03658360f39fd19e2143c67c3c26728fde0e0419
Serhii Blakh <s.a.blakh@ukr.net>
2021-08-19 16:34:58 +0300

    create constant in MainAp and makes local variables (deleted variable)

--- src/main/java/core/basesyntax/model/FigureSupplier.java
+++ src/main/java/core/basesyntax/model/FigureSupplier.java
@@ -9,22 +9,21 @@ public class FigureSupplier {
         final int maxSize = 20;
         Random random = new Random();
         ColorSupplier colorSupplier = new ColorSupplier();
-        Figure figure;
         int figureNumber = random.nextInt(FIGURE_COUNT);
         switch (figureNumber) {
             case 0:
-                return figure = new Circle(colorSupplier.getRandomColor(),
+                return new Circle(colorSupplier.getRandomColor(),
                         random.nextInt(maxSize));
             case 1:
-                return figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
                         random.nextInt(maxSize),
                         random.nextInt(maxSize),
                         random.nextInt(maxSize));
             case 2:
-                return figure = new Rectangle(colorSupplier.getRandomColor(),
+                return new Rectangle(colorSupplier.getRandomColor(),
                         random.nextInt(maxSize), random.nextInt(maxSize));
             case 3:
-                return figure = new RightTriangle(colorSupplier.getRandomColor(),
+                return new RightTriangle(colorSupplier.getRandomColor(),
                         random.nextInt(maxSize), random.nextInt(maxSize));
             default:
                 return new Square(colorSupplier.getRandomColor(), random.nextInt(maxSize));
