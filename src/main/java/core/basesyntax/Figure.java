1b3bb0d9acbfbac131cd88ec2db3d27ebbda8c4b
Sinelife <ya.serzhan@gmail.com>
2021-08-19 12:56:32 +0300

    remove FigureSupplier from Figure class

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public abstract class Figure implements Drawable, AreaCalculator {
-    private static ColorSupplier supplier = new ColorSupplier();
     protected String color;
 
     public Figure(String color) {
