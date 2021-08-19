9fa3f88f9ccac7d4088f818fe33f4a1928772c13
Stas Shepet <milka33349@gmail.com>
2021-08-19 13:29:48 +0300

    all mistakes were resolved (build success)

--- src/main/java/core/basesyntax/CollorSupplier.java
+++ src/main/java/core/basesyntax/CollorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class CollorSupplier {
-    String [] colors = new String[] {"red", "blue", "white", "black", "green"};
+    private final String [] colors = new String[] {"red", "blue", "white", "black", "green"};
 
     public String getRandomColor() {
         return colors[new Random().nextInt(colors.length)];
