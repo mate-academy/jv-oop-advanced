21442f7351a90250f2f0312bc526ccc9b7f50297
Serhii Matsko <jmen.serg@gmail.com>
2021-08-19 16:45:28 +0300

    changed random to work from 1

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 5;
     private static final int MAX_RADIUS = 15;
+    private static final int MIN_SIDE = 20;
     private static final int MAX_SIDE = 30;
     private Random random = new Random();
     private ColorSupplier supplier = new ColorSupplier();
@@ -13,18 +14,18 @@ public class FigureSupplier {
         int figureNumber = random.nextInt(FIGURE_COUNT);
         switch (figureNumber) {
             case 1:
-                return new Circle(random.nextInt(MAX_RADIUS), supplier.getRandomColor());
+                return new Circle(random.nextInt(MAX_RADIUS) + 1, supplier.getRandomColor());
             case 2:
-                return new Square(random.nextInt(MAX_SIDE), supplier.getRandomColor());
+                return new Square(random.nextInt(MAX_SIDE) + 1, supplier.getRandomColor());
             case 3:
-                return new Rectangle(random.nextInt(MAX_SIDE), random.nextInt(MAX_SIDE),
+                return new Rectangle(random.nextInt(MAX_SIDE) + 1, random.nextInt(MAX_SIDE) + 1,
                         supplier.getRandomColor());
             case 4:
-                return new RigthTriangle(random.nextInt(MAX_SIDE), supplier.getRandomColor());
+                return new RigthTriangle(random.nextInt(MAX_SIDE) + 1, supplier.getRandomColor());
             default:
-                return new IsoscelesTrapezoid(random.nextInt(MAX_SIDE - 5),
-                        random.nextInt(MAX_SIDE - 5),
-                        random.nextInt(MAX_SIDE), supplier.getRandomColor());
+                return new IsoscelesTrapezoid(random.nextInt(MAX_SIDE),
+                        random.nextInt(MIN_SIDE) + 1,
+                        random.nextInt(MAX_SIDE) + 1, supplier.getRandomColor());
         }
     }
 }
