9abc07ed727123b690008617d6aabf75d3e8dbf0
Serhii Orlov <orlov30@gmail.com>
2021-08-19 15:26:38 +0300

    rename HelloWorld.java -> Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,9 +2,11 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class HelloWorld {
+public class Main {
+    private static final int RANDOM_FIGURES_NUMBERS = 100;
+
     public static void main(String[] args) {
-        Figure[] figureS = new Figure[new Random().nextInt(100)];
+        Figure[] figureS = new Figure[new Random().nextInt(RANDOM_FIGURES_NUMBERS)];
         for (int i = 0; i < figureS.length; i++) {
             figureS[i] = new FigureSupplier().getRandomFigure();
             figureS[i].draw();
