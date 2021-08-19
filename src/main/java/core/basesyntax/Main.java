15ec5ef61128a6b3ef12cc5816648ad5d608ea09
Serhii Matsko <jmen.serg@gmail.com>
2021-08-19 16:30:03 +0300

    final name of constant

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Main {
-    private static final int MAX_ARRAY_COUNT = 10;
+    private static final int MAX_ARRAY_SIZE = 10;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[new Random().nextInt(MAX_ARRAY_COUNT)];
+        Figure[] figures = new Figure[new Random().nextInt(MAX_ARRAY_SIZE)];
         for (int i = 0; i < figures.length; i++) {
             figures[i] = figureSupplier.getRandomFigure();
             figures[i].draw();
