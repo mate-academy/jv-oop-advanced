b790ed410d3738cbb838e4c1f99f14647e98b010
Dmytro Heleta <dmytroheleta@gmail.com>
2021-08-19 16:15:01 +0300

    refactor FigureArea interface name

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawable, FigureArea {
+public abstract class Figure implements Drawable, FigureCalculator {
     private String color;
 
     public Figure(String figureColor) {
