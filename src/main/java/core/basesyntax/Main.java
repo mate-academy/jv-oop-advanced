ae023ddb1cfdfd0eea7b169e51fc075d1555f0d6
Svyatos_Furs <50773032+Svyatos-dev@users.noreply.github.com>
2021-08-19 20:51:01 +0300

    Update src/main/java/core/basesyntax/Main.java
    Co-authored-by: Dasha <maliuk.dasha@gmail.com>

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         private final static int CONSTANT = 5;
         Figure[] figuresArray = new Figure[CONSTANT];
-        FigureSupplier getFigure = new FigureSupplier();
+        FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figuresArray.length; i++) {
             figuresArray[i] = getFigure.randomFigureSupplier();
             figuresArray[i].displayInfo();
