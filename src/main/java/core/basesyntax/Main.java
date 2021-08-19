0ede8a3c7513d7fc8a98c66e4eda796beee07d73
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-08-19 16:27:17 +0300

    modified cycle in Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,18 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Main {
-    private static final int RANGE = 10;
+    private static final int MAX_ARRAY_SIZE = 10;
 
     public static void main(String[] args) {
         Random random = new Random();
         FigureSupplier figureSupplier = new FigureSupplier();
-        int arraySize = random.nextInt(RANGE) + 1;
-        Figure[] figures = new Figure[arraySize];
+        Figure[] figures = new Figure[random.nextInt(MAX_ARRAY_SIZE) + 1];
         for (int i = 0; i < figures.length; i++) {
             figures[i] = figureSupplier.getRandomFigure();
-        }
-        for (Figure figure : figures) {
-            figure.draw();
+            figures[i].draw();
         }
     }
 }
