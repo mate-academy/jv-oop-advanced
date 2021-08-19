a75e6d55f731af5e413da5ae6afdc28558f5b2c2
Serhii Orlov <orlov30@gmail.com>
2021-08-19 19:19:18 +0300

    fix checkstyle problem

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final String[] COLORS = new String[] {"red", "green", "blue",
-                                                        "black", "white", "purple"};
+                                                         "black", "white", "purple"};
 
     public String getRandomColor() {
         return COLORS[new Random().nextInt(COLORS.length)];
