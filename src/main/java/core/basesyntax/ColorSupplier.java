dd05b660ea24874668a3ce8085353f72a6675c4e
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-08-19 23:04:51 +0300

    slightly refactor the code

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
 public class ColorSupplier {
-    private final Randomizer randomizer = new Randomizer();
-
     public String getRandomColor() {
-        switch (randomizer.getRandomNumber(Randomizer.MIN, Randomizer.MAX)) {
+        switch (new Randomizer().getRandomNumber(Randomizer.MIN, Randomizer.MAX)) {
             case 1: return "yellow";
             case 2: return "green";
             case 3: return "pink";
