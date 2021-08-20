d293eefe4058527e7a4fcd57b415f000fa439bc6
Svatos <fursin997@gmail.com>
2021-08-20 13:15:41 +0300

    Task OOP-Advanced main class constant at line 6 has been renamed.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Main {
-    private static final int CONSTANT = 5;
+    private static final int MAX_FIGURE_OUTPUT = 5;
 
     public static void main(String[] args) {
-        int arrayLength = new Random().nextInt(CONSTANT);
+        int arrayLength = new Random().nextInt(MAX_FIGURE_OUTPUT);
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figuresArray = new Figure[arrayLength];
         for (int i = 0; i < figuresArray.length; i++) {
