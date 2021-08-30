ede5e6cd2e46b857687964b6e613a2562b049708
Angela <angelamatiiva@outlook.com>
2021-08-30 18:02:16 +0300

    refactoring

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        FigureSupplier figureSupplier = new FigureSupplier();
+        FigureSupplier fp = new FigureSupplier();
         for (int i = 0; i < 5; i++) {
-            figureSupplier.getRandomFigure().draw();
+            fp.getRandomFigure().draw();
         }
     }
 }
