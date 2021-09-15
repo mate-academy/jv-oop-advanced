6c42470f949939df32c69028f5b657654a69cd60
Maryna Blank <marinablank96@gmail.com>
2021-09-15 12:56:27 +0300

    Fixed random limit. Deleted accessors that aren't used anywhere.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,17 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private int randomLimit = 11;
+    private static final int RANDOM_LIMIT = 11;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-
-    public Random getRandom() {
-        return random;
-    }
-
-    public void setName(Random newRandom) {
-        this.random = newRandom;
-    }
 
     public Figure getRandomFigure() {
         int figureChoice = random.nextInt(5);
@@ -34,37 +26,36 @@ public class FigureSupplier {
     }
 
     private Figure generateRandomCircle() {
-        int radius = random.nextInt(randomLimit);
+        int radius = random.nextInt(RANDOM_LIMIT);
         String color = colorSupplier.getRandomColor();
         return new Circle(color, radius);
     }
 
     private Figure generateRandomIsoscelesTrapezoid() {
-        int side1 = random.nextInt(randomLimit);
-        int side2 = random.nextInt(randomLimit);
-        int height = random.nextInt(randomLimit);
+        int side1 = random.nextInt(RANDOM_LIMIT);
+        int side2 = random.nextInt(RANDOM_LIMIT);
+        int height = random.nextInt(RANDOM_LIMIT);
         String color = colorSupplier.getRandomColor();
         return new IsoscelesTrapezoid(color, side1, side2, height);
     }
 
     private Figure generateRandomSquare() {
-        int side = random.nextInt(randomLimit);
+        int side = random.nextInt(RANDOM_LIMIT);
         String color = colorSupplier.getRandomColor();
         return new Square(color, side);
     }
 
     private Figure generateRandomRectangle() {
-        int side1 = random.nextInt(randomLimit);
-        int side2 = random.nextInt(randomLimit);
+        int side1 = random.nextInt(RANDOM_LIMIT);
+        int side2 = random.nextInt(RANDOM_LIMIT);
         String color = colorSupplier.getRandomColor();
         return new Rectangle(color, side1, side2);
     }
 
     private Figure generateRandomRightTriangle() {
-        int firstLeg = random.nextInt(randomLimit);
-        int secondLeg = random.nextInt(randomLimit);
+        int firstLeg = random.nextInt(RANDOM_LIMIT);
+        int secondLeg = random.nextInt(RANDOM_LIMIT);
         String color = colorSupplier.getRandomColor();
         return new RightTriangle(color, firstLeg, secondLeg);
     }
-
 }
