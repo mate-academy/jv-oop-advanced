fec1eebcb2b82bea8a4148a0898556fa5e7ef14d
Mykola Skrypka <mykolaskrypka@gmail.com>
2021-09-16 21:48:32 +0300

    created constant UPPER_LIMIT; array colors moved from method; deleted const COLOR_COUNT

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class FigureSupplier {
     public static final int FIGURE_COUNT = 5;
+    public static final int UPPER_LIMIT = 10;
     private Random random = new Random();
 
     public Figure getRandomFigure() {
@@ -11,23 +12,23 @@ public class FigureSupplier {
         switch (figureNumber) {
             case 1:
                 return new Square(new ColorSupplier().getRandomColor(),
-                        new Random().nextInt(10) + 1);
+                        new Random().nextInt(UPPER_LIMIT) + 1);
             case 2:
                 return new Circle(new ColorSupplier().getRandomColor(),
-                        new Random().nextInt(10) + 1);
+                        new Random().nextInt(UPPER_LIMIT) + 1);
             case 3:
                 return new IsoscelesTrapezoid(new ColorSupplier().getRandomColor(),
-                        new Random().nextInt(10) + 1,
-                        new Random().nextInt(10) + 1,
-                        new Random().nextInt(10) + 1);
+                        new Random().nextInt(UPPER_LIMIT) + 1,
+                        new Random().nextInt(UPPER_LIMIT) + 1,
+                        new Random().nextInt(UPPER_LIMIT) + 1);
             case 4:
                 return new Rectangle(new ColorSupplier().getRandomColor(),
-                        new Random().nextInt(10) + 1,
-                        new Random().nextInt(10) + 1);
+                        new Random().nextInt(UPPER_LIMIT) + 1,
+                        new Random().nextInt(UPPER_LIMIT) + 1);
             default:
                 return new RightTriangle(new ColorSupplier().getRandomColor(),
-                        new Random().nextInt(10) + 1,
-                        new Random().nextInt(10) + 1);
+                        new Random().nextInt(UPPER_LIMIT) + 1,
+                        new Random().nextInt(UPPER_LIMIT) + 1);
         }
     }
 }
