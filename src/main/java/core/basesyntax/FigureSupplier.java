d21dfcf94d9fe2596bdfd507c9b234876031a129
agash <alexgash8@gmail.com>
2021-09-23 16:36:12 +0300

    hope last try

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -10,21 +10,21 @@ public class FigureSupplier {
         int figureNumber = random.nextInt(FIGURE_COUNT);
         switch (figureNumber) {
             case 0:
-                double rnd = (Math.random()*10 + 1);
+                double rnd = (Math.random() * 10 + 1);
                 return new Square(rnd);
             case 1:
-                double rnd2 = (Math.random()*10 + 1);
+                double rnd2 = (Math.random() * 10 + 1);
                 return new Circle(rnd2);
             case 2:
-                double rnd3 = (Math.random()*10 + 1);
-                double rnd4 = (Math.random()*10 + 1);
+                double rnd3 = (Math.random() * 10 + 1);
+                double rnd4 = (Math.random() * 10 + 1);
                 return new Triangle(rnd3, rnd4);
             case 3:
-                double rnd5 = (Math.random()*10 + 1);
-                double rnd6 = (Math.random()*10 + 1);
+                double rnd5 = (Math.random() * 10 + 1);
+                double rnd6 = (Math.random() * 10 + 1);
                 return new Trapezoid(rnd5, rnd6);
             default:
-                double rnd7 = (Math.random()*10 + 1);
+                double rnd7 = (Math.random() * 10 + 1);
                 return new Square(rnd7);
         }
     }
