3be2bb7e0cd01aef98600b9bb23ad95477ca0991
Serhii Pedchenko <djmolot@gmail.com>
2021-10-06 14:21:00 +0300

    modified:   src/main/java/core/basesyntax/Figure.java

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -3,12 +3,22 @@ package core.basesyntax;
 import java.awt.*;
 
 public abstract class Figure {
-    private Color color;
-
-    protected Figure(Color color) { //or public
-        this.color = color;
+    private FigureType type;
+    private String color;
+    public Figure() {
+        //this.type = type;
+        this.color = ColorSupplier.getRandomColor();
     }
-
+    private enum FigureType {
+        square,
+        rectangle,
+        right_triangle,
+        circle,
+        isosceles_trapezoid
+    }
+    public String getGigureType() {
+        return "" + type;
+    }
     public abstract double getArea();
     public abstract void draw();
 }
