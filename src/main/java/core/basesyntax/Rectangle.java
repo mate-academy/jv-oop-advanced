349c1ff9e4419578a23b329052d9885351b1eb50
Serhii Pedchenko <djmolot@gmail.com>
2021-10-07 17:53:25 +0300

    created child class Rectangle.java

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+
+    private final int sideA;
+    private final int sideB;
+
+    public Rectangle(int sideA, int sideB) {
+        super();
+        super.setType("rectangle");
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    @Override
+    public double getArea() {
+        return sideA * sideB;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + super.getType() + ", area: " + this.getArea() + "sq.units, sideA: " + this.sideA + " units, sideB: " + this.sideB + " units, color: " + super.getColor());
+    }
+}
