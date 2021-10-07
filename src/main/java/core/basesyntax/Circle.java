63faa7b1592c75e5c50e25acfaacdbf7ad15b290
Serhii Pedchenko <djmolot@gmail.com>
2021-10-07 19:11:41 +0300

    created child class Circle.java

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+
+    private final int radius;
+
+    public Circle(int radius) {
+        super();
+        super.setType("circle");
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + super.getType() + ", area: " + this.getArea() + "sq.units, radius: " + this.radius + " units, color: " + super.getColor());
+    }
+}
