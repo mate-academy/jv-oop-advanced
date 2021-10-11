6c951aaa48e57c2dac1b7ac372d0bb329695d9c6
Serhii Pedchenko <djmolot@gmail.com>
2021-10-11 20:02:34 +0300

    changed case ISOSCELES_TRAPEZOID to default in FigureSupplier.getRandomFigure method

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,46 +7,36 @@ public class FigureSupplier {
         Random randGen = new Random();
         int figurePos = randGen.nextInt(FiguresSet.values().length);
         FiguresSet figureElem = FiguresSet.values()[figurePos];
-        Figure figureObj = null;
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
+        Figure randomFigure = null;
         switch (figureElem) {
             case SQUARE:
                 int side = randGen.nextInt(20) + 1;
-                figureObj = new Square(color, side);
+                randomFigure = new Square(color, side);
                 break;
             case RECTANGLE:
                 int sideA = randGen.nextInt(20) + 1;
                 int sideB = randGen.nextInt(20) + 1;
-                figureObj = new Rectangle(color, sideA, sideB);
+                randomFigure = new Rectangle(color, sideA, sideB);
                 break;
             case RIGHT_TRIANGLE:
                 int firstLeg = randGen.nextInt(20) + 1;
                 int secondLeg = randGen.nextInt(20) + 1;
-                figureObj = new RightTriangle(color, firstLeg, secondLeg);
+                randomFigure = new RightTriangle(color, firstLeg, secondLeg);
                 break;
             case CIRCLE:
                 int radius = randGen.nextInt(20) + 1;
-                figureObj = new Circle(color, radius);
+                randomFigure = new Circle(color, radius);
                 break;
-            case ISOSCELES_TRAPEZOID:
+            default: //case ISOSCELES_TRAPEZOID:
                 int baseA = randGen.nextInt(20) + 1;
                 int baseB = randGen.nextInt(20) + 1;
                 int height = randGen.nextInt(20) + 1;
-                figureObj = new IsoscelesTrapezoid(color, baseA, baseB, height);
+                randomFigure = new IsoscelesTrapezoid(color, baseA, baseB, height);
                 break;
-/*
-            default:
-                System.out.println("There is no figure type" + figureElem + " in enum FiguresSet");
-                return;//error
-*/
         }
-        if (figureObj == null) {
-            //It will never reach here, just to make compiler happy
-            throw new IllegalArgumentException("There is no figure type" + figureElem + " in enum FiguresSet");
-        } else {
-            return figureObj;
-        }
+        return randomFigure;
     }
     private enum FiguresSet {
         SQUARE,
