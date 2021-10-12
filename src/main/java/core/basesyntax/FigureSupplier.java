fa10b34b92d1e83a5bdf9a92c91689721e3fbda2
Serhii Pedchenko <djmolot@gmail.com>
2021-10-12 14:34:49 +0300

    introduced new variable RANDOM_PROP_LIMIT in class FigureSupplier.java

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -9,30 +9,31 @@ public class FigureSupplier {
         FiguresSet figureElem = FiguresSet.values()[figurePos];
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
-        Figure randomFigure = null;
+        Figure randomFigure;
+        int RANDOM_PROP_LIMIT = 20;
         switch (figureElem) {
             case SQUARE:
-                int side = randGen.nextInt(20) + 1;
+                int side = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 randomFigure = new Square(color, side);
                 break;
             case RECTANGLE:
-                int sideA = randGen.nextInt(20) + 1;
-                int sideB = randGen.nextInt(20) + 1;
+                int sideA = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
+                int sideB = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 randomFigure = new Rectangle(color, sideA, sideB);
                 break;
             case RIGHT_TRIANGLE:
-                int firstLeg = randGen.nextInt(20) + 1;
-                int secondLeg = randGen.nextInt(20) + 1;
+                int firstLeg = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
+                int secondLeg = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 randomFigure = new RightTriangle(color, firstLeg, secondLeg);
                 break;
             case CIRCLE:
-                int radius = randGen.nextInt(20) + 1;
+                int radius = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 randomFigure = new Circle(color, radius);
                 break;
             default: //case ISOSCELES_TRAPEZOID:
-                int baseA = randGen.nextInt(20) + 1;
-                int baseB = randGen.nextInt(20) + 1;
-                int height = randGen.nextInt(20) + 1;
+                int baseA = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
+                int baseB = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
+                int height = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 randomFigure = new IsoscelesTrapezoid(color, baseA, baseB, height);
                 break;
         }
