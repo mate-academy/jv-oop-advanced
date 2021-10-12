84a072b39e8c3291c3fd983b0bfc15e3babc5162
Serhii Pedchenko <djmolot@gmail.com>
2021-10-12 17:20:25 +0300

    making RANDOM_PROP_LIMIT the constant of class FigureSupplier.java; removed randomFigure and returning figures from switch cases

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,41 +3,36 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int RANDOM_PROP_LIMIT = 20;
+
     public Figure getRandomFigure() {
         Random randGen = new Random();
         int figurePos = randGen.nextInt(FiguresSet.values().length);
         FiguresSet figureElem = FiguresSet.values()[figurePos];
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
-        Figure randomFigure;
-        int RANDOM_PROP_LIMIT = 20;
         switch (figureElem) {
             case SQUARE:
+                //to exclude situation side=0 and include upper limit we add 1
                 int side = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
-                randomFigure = new Square(color, side);
-                break;
+                return new Square(color, side);
             case RECTANGLE:
                 int sideA = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 int sideB = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
-                randomFigure = new Rectangle(color, sideA, sideB);
-                break;
+                return new Rectangle(color, sideA, sideB);
             case RIGHT_TRIANGLE:
                 int firstLeg = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 int secondLeg = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
-                randomFigure = new RightTriangle(color, firstLeg, secondLeg);
-                break;
+                return new RightTriangle(color, firstLeg, secondLeg);
             case CIRCLE:
                 int radius = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
-                randomFigure = new Circle(color, radius);
-                break;
+                return new Circle(color, radius);
             default: //case ISOSCELES_TRAPEZOID:
                 int baseA = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 int baseB = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
                 int height = randGen.nextInt(RANDOM_PROP_LIMIT) + 1;
-                randomFigure = new IsoscelesTrapezoid(color, baseA, baseB, height);
-                break;
+                return new IsoscelesTrapezoid(color, baseA, baseB, height);
         }
-        return randomFigure;
     }
 
     private enum FiguresSet {
