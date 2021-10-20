40da4c48878f4538a1349b74f8e6c17878874dd1
Yaroslav Kavun <yka40znp@gmail.com>
2021-10-20 16:31:40 +0300

    Realization of different kinds of figures with their properties

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public enum Color {
+    BLACK,
+    BLUE,
+    CRIMSON,
+    ORANGE,
+    PINK,
+    PURPLE,
+    RED,
+    WHITE
+}
+
---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,35 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private float height;
+    private float wigth;
+
+    public Rectangle(String color, float height, float wigth) {
+        super(color);
+        this.height = height;
+        this.wigth = wigth;
+    }
+
+    @Override
+    public float getArea() {
+        return height * wigth;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: "
+                + "rectangle, "
+                + "area: "
+                + getArea()
+                + " sq.units, "
+                + "height: "
+                + height
+                + " units, "
+                + "wigth: "
+                + wigth
+                + " units, "
+                + "color: "
+                + getColor());
+
+    }
+}
