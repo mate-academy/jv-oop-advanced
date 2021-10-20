fcd8491b725dc6fc664c6aed673ee9805fcac2bc
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-10-20 15:28:55 +0300

    Deleted unnecessary getter and setter.

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -8,14 +8,6 @@ public class Circle extends Figure {
         this.radius = radius;
     }
 
-    public int getRadius() {
-        return radius;
-    }
-
-    public void setRadius(int radius) {
-        this.radius = radius;
-    }
-
     @Override
     public double getArea() {
         return Math.PI * radius * radius;
