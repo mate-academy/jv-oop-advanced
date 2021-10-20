ca3320c3ba9c7a50959bcf7160662ea3bead970e
Vladyslav Chyver <lione2009lione@gmail.com>
2021-10-20 23:13:32 +0300

    class Square  was recreate

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -1,4 +1,15 @@
 package core.basesyntax;
 
-public class RightTriangle {
+public class RightTriangle extends Figure implements ArreaCounter {
+    private int side = 0;
+    RightTriangle(int side, Colors color){
+        super();
+        countArea();
+        this.setColor(color);
+    }
+    @Override
+    public void countArea() {
+        float result = (float) (side * side * Math.sqrt(3/4));
+        setArea(result);
+    }
 }
--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -1,4 +1,15 @@
 package core.basesyntax;
 
-public class Square {
+public class Square extends Figure implements ArreaCounter {
+    private int side = 0;
+    Square(int side, Colors color){
+        super();
+        countArea();
+        this.setColor(color);
+    }
+    @Override
+    public void countArea() {
+        float result = (float) (side * side);
+        setArea(result);
+    }
 }
