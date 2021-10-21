c36beca4bb2a907299fcb1f4862de603e7d4b21d
Olha Tankevych <freakystyley85@gmail.com>
2021-10-21 17:45:19 +0300

    added constant NUMBER_OF_FIGURES, changed public Figure getDefaultFigure(), changed access modifiers

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,30 +3,31 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int NUMBER_OF_FIGURES = 5;
     private Random random = new Random();
 
     public Figure getDefaultFigure() {
-        return new Circle("WHITE", 10);
+        return new Circle(Color.WHITE.toString(), 10);
     }
 
-    public int getNumber() {
+    private int getNumber() {
         return random.nextInt(10);
     }
 
     public Figure getRandomFigure() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
-        int figureNumber = random.nextInt(FigureName.values().length);
+        int figureNumber = random.nextInt(NUMBER_OF_FIGURES);
         switch (figureNumber) {
-            case (0):
+            case 0:
                 return new Circle(color, getNumber());
-            case (1):
+            case 1:
                 return new Rectangle(color, getNumber(), getNumber());
-            case (2):
+            case 2:
                 return new Square(color, getNumber());
-            case (3):
+            case 3:
                 return new RightTriangle(color, getNumber(), getNumber());
-            case (4):
+            case 4:
                 return new IsoscelesTrapezoid(color, getNumber(), getNumber(), getNumber());
             default:
                 return getDefaultFigure();
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,16 +1,16 @@
 package core.basesyntax;
 
 public class Main {
-    public static final int FIGURE_COUNT = 6;
+    private static final int FIGURE_COUNT = 6;
 
     public static void main(String[] args) {
         Figure[] figures = new Figure[FIGURE_COUNT];
-        FigureSupplier figure = new FigureSupplier();
+        FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length; i++) {
             if (i < FIGURE_COUNT / 2) {
-                figures[i] = figure.getRandomFigure();
+                figures[i] = figureSupplier.getRandomFigure();
             } else {
-                figures[i] = figure.getDefaultFigure();
+                figures[i] = figureSupplier.getDefaultFigure();
             }
             System.out.println(figures[i].getInfo());
         }
