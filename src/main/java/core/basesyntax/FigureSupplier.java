d0db0302b4babfa8ad90b57dfc573c178d16130e
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-10-21 15:43:07 +0300

    fixed switch structure

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,36 +5,24 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 4;
     private Random random = new Random();
-    private ColorSupplier color = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int index = new Random().nextInt(FIGURE_COUNT);
-        String[] figures = new String[]{"circle", "square", "triangle", "trapezoid"};
-        Figure figure = null;
-        switch (figures[index]) {
-            case ("circle"): {
-                figure = new Circle(color.getRandomColor(), random.nextInt(10) + 1);
-                break;
-            }
-            case ("square"): {
-                figure = new Square(color.getRandomColor(), random.nextInt(10) + 1);
-                break;
-            }
-            case ("triangle"): {
-                figure = new RightTriangle(color.getRandomColor(), random.nextInt(10) + 1);
-                break;
-            }
-            case ("trapezoid"): {
-                figure = new IsoscelesTrapezoid(color.getRandomColor(), random.nextInt(10) + 1,
+        switch (index) {
+            case 0:
+                return new Circle(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
+            case 1:
+                return new Square(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
+            case 2:
+                return new RightTriangle(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
+            case 3:
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                        + random.nextInt(10) + 1,
                         + random.nextInt(10) + 1, random.nextInt(10) + 1);
-                break;
-            }
-            default: {
-                figure = getDefaultFigure();
-                break;
-            }
+            default:
+                return getDefaultFigure();
         }
-        return figure;
     }
 
     public Figure getDefaultFigure() {
