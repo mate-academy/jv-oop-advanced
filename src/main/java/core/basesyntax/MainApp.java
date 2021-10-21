9f06e0b8854f73ae35da7aae156f259e3babd8d8
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-10-21 21:29:40 +0300

    massive figures was created and info about figures was printed

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorld {
-
+public class MainApp {
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = {figureSupplier.getRandomFigure(), figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(), figureSupplier.getDefaultFigure(),
+                figureSupplier.getDefaultFigure(), figureSupplier.getDefaultFigure()};
+        for (Figure figure : figures) {
+            figure.printInfo();
+        }
+    }
 }
