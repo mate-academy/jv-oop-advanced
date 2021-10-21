a6dcee4cbe604a479858658168e1f0e3dbe7ee66
Oleg Shiriaev <mystidreamer@gmail.com>
2021-10-21 12:56:20 +0300

    get random figure and get random color was created

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -1,4 +1,31 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class FigureSupplier {
+    public static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private static Random random;
+    public static final int PHARAMETRE = random.nextInt(20);
+
+    public Figure getRandomFigure() {
+        int number = random.nextInt(5);
+        switch (number) {
+            case 0:
+                return new Circle(COLOR_SUPPLIER.getRandomColor(), PHARAMETRE);
+            case 1:
+                return new Square(COLOR_SUPPLIER.getRandomColor(), PHARAMETRE);
+            case 2:
+                return new Rectangle(COLOR_SUPPLIER.getRandomColor(), PHARAMETRE, PHARAMETRE);
+            case 3:
+                return new IsoscelesTrapezoid(COLOR_SUPPLIER.getRandomColor(),
+                        PHARAMETRE, PHARAMETRE, PHARAMETRE);
+            default:
+                return new RightTriangle(COLOR_SUPPLIER.getRandomColor(), PHARAMETRE, PHARAMETRE);
+        }
+
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(Color.WHITE.toString(), 10);
+    }
 }
