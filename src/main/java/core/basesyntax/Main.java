9532a6e6ac0d90721c90e0a9ceac7c70b8b528c5
Andriy-dz <andriydzdz@gmail.com>
2021-10-22 11:52:12 +0300

    fixedd style errors

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class Main {
-    private static final int FIGURES_LENGTH  = 6;
+    private static final int FIGURES_LENGTH = 6;
 
     public static void main(String[] args) {
-        Figure[] figures = new Figure[FIGURES_LENGTH ];
-        for (int i = 0; i < FIGURES_LENGTH ; i++) {
+        Figure[] figures = new Figure[FIGURES_LENGTH];
+        for (int i = 0; i < FIGURES_LENGTH; i++) {
             FigureSupplier figureSupplier = new FigureSupplier();
-            if (i < FIGURES_LENGTH  / 2) {
+            if (i < FIGURES_LENGTH / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
