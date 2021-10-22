4903de0abeec3a16f593cc76e0f9ab2ef753c2c9
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-10-22 15:14:36 +0300

    checkstyle mistakes was fixed

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 5;
     private static final int MAX_RANDOM_NUMBER = 10;
-    private static final int FIGURE_DEFAULT_VALUE = 10;
+    private static final int DEFAULT_VALUE = 10;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
@@ -22,7 +22,7 @@ public class FigureSupplier {
                 return getRandomCircle();
             case 4 :
             default:
-                return getDefaultFigure();
+                return getRandomIsoscelesTrapezoid();
         }
     }
 
@@ -68,7 +68,7 @@ public class FigureSupplier {
     public Figure getDefaultFigure() {
         Circle circle = new Circle();
         circle.setColor(Color.WHITE.toString());
-        circle.setRadius(FIGURE_DEFAULT_VALUE);
+        circle.setRadius(DEFAULT_VALUE);
         return circle;
     }
 }
