59f4f29314c513b1cee65e9eb47f04320c515d01
Mykola Voronov <www.kolyavoronov@gmail.com>
2021-10-22 09:53:31 +0300

    RANDOM_BOUND_VALUE constant was added

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 5;
     private static final int DEFAULT_RADIUS = 10;
+    private static final int RANDOM_BOUND_VALUE = 10;
     private static final String DEFAULT_COLOR = Color.WHITE.toString();
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
@@ -28,35 +29,35 @@ public class FigureSupplier {
 
     public Figure getRandomSquare() {
         String color = colorSupplier.getRandomColor();
-        int side = random.nextInt(10) + 1;
+        int side = random.nextInt(RANDOM_BOUND_VALUE) + 1;
         return new Square(color, side);
     }
 
     public Figure getRandomRectangle() {
         String color = colorSupplier.getRandomColor();
-        int sideA = random.nextInt(10) + 1;
-        int sideB = random.nextInt(10) + 1;
+        int sideA = random.nextInt(RANDOM_BOUND_VALUE) + 1;
+        int sideB = random.nextInt(RANDOM_BOUND_VALUE) + 1;
         return new Rectange(color,sideA, sideB);
     }
 
     public Figure getRandomRightTriangle() {
         String color = colorSupplier.getRandomColor();
-        int firstLeg = random.nextInt(10) + 1;
-        int secondLeg = random.nextInt(10) + 1;
+        int firstLeg = random.nextInt(RANDOM_BOUND_VALUE) + 1;
+        int secondLeg = random.nextInt(RANDOM_BOUND_VALUE) + 1;
         return new RightTriangle(color, firstLeg, secondLeg);
     }
 
     public Figure getRandomCircle() {
         String color = colorSupplier.getRandomColor();
-        int radius = random.nextInt(10) + 1;
+        int radius = random.nextInt(RANDOM_BOUND_VALUE) + 1;
         return new Circle(color, radius);
     }
 
     public Figure getRandomIsoscelesTrapezoid() {
         String color = colorSupplier.getRandomColor();
-        int topBase = random.nextInt(10) + 1;
-        int bottomBase = topBase + random.nextInt(10) + 1;
-        int side = random.nextInt(10) + 1;
+        int topBase = random.nextInt(RANDOM_BOUND_VALUE) + 1;
+        int bottomBase = topBase + random.nextInt(RANDOM_BOUND_VALUE) + 1;
+        int side = random.nextInt(RANDOM_BOUND_VALUE) + 1;
         return new IsoscelesTrapezoid(color, topBase, bottomBase, side);
     }
 
