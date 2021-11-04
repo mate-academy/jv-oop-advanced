20f023cc39fe111224aad06f29b3acfb06074e73
Yaroslav Boiko <slkawnia@gmail.com>
2021-11-04 16:05:46 +0200

    Variable colorIndex was transferred to the method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
-    private int colorIndex;
     private String colors[] = { "blue", "green", "magenta", "red", "cyan"};
 
     public String getRandomColor() {
-        colorIndex = random.nextInt(colors.length);
+        int colorIndex = random.nextInt(colors.length);
         return colors[colorIndex];
     }
 }
