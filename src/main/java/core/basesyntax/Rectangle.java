057adac57fc1e6f370ef4baccd843a9917d6a157
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-07 04:14:59 +0200

    added Rectangle class with implementation of parents methods

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,30 @@
+package core.basesyntax;
+
+import java.util.Locale;
+
+public class Rectangle extends Figure{
+    private final int sideA;
+    private final int sideB;
+
+    public Rectangle(String color, int sideA, int sideB) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.figureType = FigureType.RECTANGLE.toString().toLowerCase(Locale.ROOT);
+    }
+
+    @Override
+    public double getArea() {
+        return sideB * sideA;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: %s, area: %.0f sq.units, firstSide: %d units, secondSide: %d units, color: %s%n",
+                figureType,
+                getArea(),
+                sideA,
+                sideB,
+                color);
+    }
+}
