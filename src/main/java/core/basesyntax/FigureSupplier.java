2b7a45abf922a7fdeb88126caae3d964e8096b50
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-07 04:11:40 +0200

    implemented getDefaultFigure and getRandomFigure methods in FigureSupplier class

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,41 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    private final int DEFAULT_CIRCLE_RADIUS = 10;
+    private final String DEFAULT_CIRCLE_COLOR = Color.WHITE.toString();
+    private final int MAX_RANDOM_VALUE = 100;
+
+    public Figure getRandomFigure() {
+        Random random = new Random();
+        FigureType randomFigureType = FigureType.values()[random.nextInt(FigureType.values().length)];
+        String randomColor = new ColorSupplier().getRandomColor();
+        switch (randomFigureType) {
+            case CIRCLE :
+                int radius = random.nextInt(MAX_RANDOM_VALUE);
+                return new Circle(randomColor, radius);
+            case SQUARE:
+                int squareSide = random.nextInt(MAX_RANDOM_VALUE);
+                return new Square(randomColor, squareSide);
+            case RECTANGLE:
+                int sideA = random.nextInt(MAX_RANDOM_VALUE);
+                int sideB = random.nextInt(MAX_RANDOM_VALUE);
+                return new Rectangle(randomColor,sideA,sideB);
+            case RIGHT_TRIANGLE:
+                int firstLeg = random.nextInt(MAX_RANDOM_VALUE);
+                int secondLeg = random.nextInt(MAX_RANDOM_VALUE);
+                return new RightTriangle(randomColor, firstLeg, secondLeg);
+            case ISOSCELES_TRAPEZOID:
+            default:
+                int firstSide = random.nextInt(MAX_RANDOM_VALUE);
+                int secondSide = random.nextInt(MAX_RANDOM_VALUE);
+                int height = random.nextInt(MAX_RANDOM_VALUE);
+                return new IsoscelesTrapezoid(randomColor, firstSide, secondSide, height);
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(DEFAULT_CIRCLE_COLOR, DEFAULT_CIRCLE_RADIUS);
+    }
+}
