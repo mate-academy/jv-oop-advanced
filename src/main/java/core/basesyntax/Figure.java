05cf0014be1cc501f3d1d45bbb7037b6539d2db0
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-07 22:34:12 +0200

    Drawable and AreaCalculator intefaces with corresponding methods created, implemented by Figure class

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,11 +1,5 @@
 package core.basesyntax;
 
-public abstract class Figure {
-
+public abstract class Figure implements Drawable, AreaCalculator {
     protected Color color;
-
-    // behaviour
-    public abstract void draw();
-
-    public abstract double getArea();
 }
