03449886732dbed2c699004127071048e7f4382a
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-07 07:43:58 +0200

    created an abstract class to extend by actual figures classes

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public abstract class Figure {
+
+    protected Color color;
+
+    // behaviour
+    public abstract void draw();
+
+    public abstract double getArea();
+}
