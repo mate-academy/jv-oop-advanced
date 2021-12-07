269ab8c2d586e8878b4c9d98cb97e851abaac600
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-07 22:51:09 +0200

    empty lines removed, calss ColorSupplier created

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,50 +3,43 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-
     private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getDefaultFigure() {
-
         return new Circle(10, Color.WHITE);
     }
 
     public Figure getRandomFigure() {
-
         FigureType[] figureTypes = FigureType.values();
         FigureType randomFigureType = figureTypes[random.nextInt(figureTypes.length)];
-        Color[] colors = Color.values();
-        Color randomColor = colors[random.nextInt(colors.length)];
-
         int randomNumber1 = random.nextInt(100);
         int randomNumber2 = random.nextInt(100);
         int randomNumber3 = random.nextInt(100);
-
         Figure figure = getDefaultFigure();
 
         switch (randomFigureType) {
             case CIRCLE:
-                figure = new Circle(randomNumber1, randomColor);
+                figure = new Circle(randomNumber1, colorSupplier.getRandomColor());
                 break;
             case SQUARE:
-                figure = new Square(randomNumber1, randomColor);
+                figure = new Square(randomNumber1, colorSupplier.getRandomColor());
                 break;
             case RECTANGLE:
-                figure = new Rectangle(randomNumber1, randomNumber2, randomColor);
+                figure = new Rectangle(randomNumber1, randomNumber2, colorSupplier.getRandomColor());
                 break;
             case RIGHT_TRIANGLE:
-                figure = new RightTriangle(randomNumber1, randomNumber2, randomColor);
+                figure = new RightTriangle(randomNumber1, randomNumber2, colorSupplier.getRandomColor());
                 break;
             case ISOSCELES_TRAPEZOID:
                 figure = new IsoscelesTrapezoid(randomNumber1,
                         randomNumber2,
                         randomNumber3,
-                        randomColor);
+                        colorSupplier.getRandomColor());
                 break;
             default:
                 break;
         }
-
         return figure;
     }
 }
--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class IsoscelesTrapezoid extends Figure {
-
     private int base;
     private int top;
     private int height;
