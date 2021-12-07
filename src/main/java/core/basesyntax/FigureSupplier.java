c96ae5a47bacc3872bcbc52cdfd828e13020ead4
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-07 23:16:14 +0200

    created methods to get different random figures, Figure object moved to class level, modified switch statement

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,39 +5,61 @@ import java.util.Random;
 public class FigureSupplier {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private Figure figure;
 
     public Figure getDefaultFigure() {
         return new Circle(10, Color.WHITE);
     }
 
+    public Figure getRandomCircle() {
+        return new Circle(random.nextInt(100), colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomSquare() {
+        return new Square(random.nextInt(100), colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomRectangle() {
+        return new Rectangle(random.nextInt(100),
+                random.nextInt(100),
+                colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomRightTriangle() {
+        return new RightTriangle(random.nextInt(100),
+                random.nextInt(100),
+                colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(random.nextInt(100),
+                random.nextInt(100),
+                random.nextInt(100),
+                colorSupplier.getRandomColor());
+    }
+
     public Figure getRandomFigure() {
         FigureType[] figureTypes = FigureType.values();
         FigureType randomFigureType = figureTypes[random.nextInt(figureTypes.length)];
-        int randomNumber1 = random.nextInt(100);
-        int randomNumber2 = random.nextInt(100);
-        int randomNumber3 = random.nextInt(100);
-        Figure figure = getDefaultFigure();
 
         switch (randomFigureType) {
             case CIRCLE:
-                figure = new Circle(randomNumber1, colorSupplier.getRandomColor());
+                figure = getRandomCircle();
                 break;
             case SQUARE:
-                figure = new Square(randomNumber1, colorSupplier.getRandomColor());
+                figure = getRandomSquare();
                 break;
             case RECTANGLE:
-                figure = new Rectangle(randomNumber1, randomNumber2, colorSupplier.getRandomColor());
+                figure = getRandomRectangle();
                 break;
             case RIGHT_TRIANGLE:
-                figure = new RightTriangle(randomNumber1, randomNumber2, colorSupplier.getRandomColor());
+                figure = getRandomRightTriangle();
                 break;
             case ISOSCELES_TRAPEZOID:
-                figure = new IsoscelesTrapezoid(randomNumber1,
-                        randomNumber2,
-                        randomNumber3,
-                        colorSupplier.getRandomColor());
+                figure = getRandomIsoscelesTrapezoid();;
                 break;
             default:
+                figure = getDefaultFigure();
                 break;
         }
         return figure;
