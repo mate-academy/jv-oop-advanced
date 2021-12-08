ae7bcc6c76d55813fa33873dd8bd664912a734d2
Nikita Melnikov <bam19921303@gmail.com>
2021-12-08 17:56:23 +0200

    This program creates an array of shapes using the FigureSupplier class. In this case, the array is half filled with random figures, and the second half is filled with a white Circle shape with a radius of 10

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
-    String randomColor = getRandomColor();
+    private String randomColor = getRandomColor();
 
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
