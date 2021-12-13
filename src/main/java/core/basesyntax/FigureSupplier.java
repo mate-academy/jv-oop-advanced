2252f11a8e00ffe90d7b9081be13591c03ac127f
Vitaliy Zahrenko <vzagorenko@gmail.com>
2021-12-13 23:37:15 +0200

    FigureSupplier randod algorithm has been fixed

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -10,22 +10,28 @@ public class FigureSupplier {
     }
 
     public Figure getRandomFigure() {
-        int[] idx = new int[4];
-        for (int i = 0; i < idx.length; i++) {
-            idx[i] = randomizer.nextInt(100);
+        /*
+        create random color index[0] - and figure segments length index[1-3]
+        which use in figure class constructor
+        */
+        int[] index = new int[4];
+        for (int i = 0; i < index.length; i++) {
+            index[i] = randomizer.nextInt(100);
         }
 
-        idx[0] = idx[0] % (Colors.values().length);
+        index[0] = index[0] % (Colors.values().length);
 
-        switch (idx[0] % 4) {
+        int figureIndex = randomizer.nextInt(4);
+
+        switch (figureIndex) {
             case 0:
-                return (new Circle(Colors.values()[idx[0]],idx[1]));
+                return (new Circle(Colors.values()[index[0]],index[1]));
             case 1:
-                return (new Square(Colors.values()[idx[0]],idx[1]));
+                return (new Square(Colors.values()[index[0]],index[1]));
             case 2:
-                return (new RightTriangle(Colors.values()[idx[0]],idx[1],idx[2]));
+                return (new RightTriangle(Colors.values()[index[0]],index[1],index[2]));
             case 3:
-                return (new Trapezoid(Colors.values()[idx[0]],idx[1],idx[2],idx[3]));
+                return (new Trapezoid(Colors.values()[index[0]],index[1],index[2],index[3]));
             default:
                 return (new Circle(Colors.WHITE,10));
         }
