0116b121d143ade69c7d44df88cf41c02c2e0a59
Natalia Kurhanska <kurhnatali@gmail.com>
2021-12-17 23:45:10 +0200

    array and method getParametres() were removed, used switch, the method access modifiers changed to private

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,69 +3,64 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    public static final int FIGURE_COUNT = 5;
-    private Figure[] figures = {new Circle(), new IsoscelesTrapezoid(),
-            new Rectangle(), new RightTriangle(), new Square()};
-    private Figure figure;
+    private static final int FIGURE_COUNT = 5;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        figure = figures[random.nextInt(FIGURE_COUNT)];
-        getParametres(figure);
+        int randomNumFigure = random.nextInt(FIGURE_COUNT);
+        switch (randomNumFigure) {
+            case 0:
+                return getCircle();
+            case 1:
+                return getIsoscelesTrapezoid();
+            case 2:
+                return getRectangle();
+            case 3:
+                return getRectangle();
+            case 4:
+                return getRightTriangle();
+            default:
+                return getSquare();
+        }
+    }
+
+    private Figure getCircle() {
+        double radius = random.nextInt(9) + 1;
+        Figure figure = new Circle(radius);
         figure.setColor(colorSupplier.getRandomColor());
         return figure;
     }
 
-    public void getParametres(Figure figure) {
-        if (figure instanceof Circle) {
-            getCircle();
-        }
-        if (figure instanceof IsoscelesTrapezoid) {
-            getIsoscelesTrapezoid();
-        }
-        if (figure instanceof Rectangle) {
-            getRectangle();
-        }
-        if (figure instanceof RightTriangle) {
-            getRightTriangle();
-        }
-        if (figure instanceof Square) {
-            getSquare();
-        }
-    }
-
-    public Figure getCircle() {
-        double radius = random.nextInt(9) + 1;
-        figure = new Circle(radius);
-        return figure;
-    }
-
-    public Figure getIsoscelesTrapezoid() {
+    private Figure getIsoscelesTrapezoid() {
         double lowerBaseSide = random.nextInt(9) + 1;
         double upperBaseSide = random.nextInt(9) + 1;
         double height = random.nextInt(9) + 1;
-        figure = new IsoscelesTrapezoid(lowerBaseSide, upperBaseSide, height);
+        Figure figure = new IsoscelesTrapezoid(lowerBaseSide, upperBaseSide, height);
+        figure.setColor(colorSupplier.getRandomColor());
         return figure;
     }
 
-    public Figure getRectangle() {
+    private Figure getRectangle() {
         double length = random.nextInt(9) + 1;
         double width = random.nextInt(9) + 1;
-        figure = new Rectangle(length, width);
+        Figure figure = new Rectangle(length, width);
+        figure.setColor(colorSupplier.getRandomColor());
         return figure;
     }
 
-    public Figure getRightTriangle() {
+    private Figure getRightTriangle() {
         double baseSide = random.nextInt(9) + 1;
         double height = random.nextInt(9) + 1;
-        figure = new RightTriangle(baseSide, height);
+        Figure figure = new RightTriangle(baseSide, height);
+        figure.setColor(colorSupplier.getRandomColor());
         return figure;
     }
 
-    public Figure getSquare() {
+    private Figure getSquare() {
         double side = random.nextInt(9) + 1;
-        figure = new Square(side);
+        Figure figure = new Square(side);
+        figure.setColor(colorSupplier.getRandomColor());
         return figure;
     }
 
