7a812c4c046bd24d4aeee6d18452f1c26371415e
Vlad Nadtochiy <v20102002n@gmail.com>
2022-01-14 21:34:27 +0200

    3rd commit

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,16 +3,33 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    Random rand = new Random();
-    ColorSupplier col = new ColorSupplier();
-    public Square SquareIni() {
+    private Random rand = new Random();
+    private ColorSupplier col = new ColorSupplier();
+
+    public Random getRand() {
+        return rand;
+    }
+
+    public void setRand(Random rand) {
+        this.rand = rand;
+    }
+
+    public ColorSupplier getCol() {
+        return col;
+    }
+
+    public void setCol(ColorSupplier col) {
+        this.col = col;
+    }
+
+    public Square squareIni() {
         Square square = new Square();
         square.setSide_a(rand.nextInt(100));
         square.setColor(col.getRandomColor());
         return square;
     }
 
-    public Rectangle RectangleIni() {
+    public Rectangle rectangleIni() {
         Rectangle rectangle = new Rectangle();
         rectangle.setSide_a(rand.nextInt(100));
         rectangle.setSide_b(rand.nextInt(100));
@@ -20,7 +37,7 @@ public class FigureSupplier {
         return rectangle;
     }
 
-    public Righttriangle RighttriangleIni() {
+    public Righttriangle righttriangleIni() {
         Righttriangle righttriangle = new Righttriangle();
         righttriangle.setFirstLeg(rand.nextInt(100));
         righttriangle.setSecondLeg(rand.nextInt(100));
@@ -28,14 +45,14 @@ public class FigureSupplier {
         return righttriangle;
     }
 
-    public Circle CircleIni() {
+    public Circle circleIni() {
         Circle circle = new Circle();
         circle.setRadius(rand.nextInt(100));
         circle.setColor(col.getRandomColor());
         return circle;
     }
 
-    public Isoscelestrapezoid IsoscelestrapezoidIni() {
+    public Isoscelestrapezoid isoscelestrapezoidIni() {
         Isoscelestrapezoid isoscelestrapezoid = new Isoscelestrapezoid();
         isoscelestrapezoid.setUpper_side(rand.nextInt(100));
         isoscelestrapezoid.setLower_side(rand.nextInt(100));
@@ -43,21 +60,22 @@ public class FigureSupplier {
         isoscelestrapezoid.setColor(col.getRandomColor());
         return isoscelestrapezoid;
     }
+
     public static Figure getRandomFigure() {
         Random rand = new Random();
         FigureSupplier figure = new FigureSupplier();
         int figuresQuantity = 5;
         switch (rand.nextInt(figuresQuantity) + 1) {
             case 1:
-                return figure.SquareIni();
+                return figure.squareIni();
             case 2:
-                return figure.RectangleIni();
+                return figure.rectangleIni();
             case 3:
-                return figure.RighttriangleIni();
+                return figure.righttriangleIni();
             case 4:
-                return figure.CircleIni();
+                return figure.circleIni();
             case 5:
-                return figure.IsoscelestrapezoidIni();
+                return figure.isoscelestrapezoidIni();
             default:
                 break;
         }
