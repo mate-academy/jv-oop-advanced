a8bc0292fb5edfd17d87589eff496100457a524c
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-16 01:23:25 +0200

    Start create a program structure

---
+++ src/main/java/core/basesyntax/Application.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Application {
+    public static void main (String[] args) {
+System.out.println(figureArea);
+    }
+
+}
