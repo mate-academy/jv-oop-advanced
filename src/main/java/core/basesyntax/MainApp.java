8f1f798dbd471b644d354b52428a86c43ecbc083
AprioriKS <sounddummies@gmail.com>
2022-01-17 16:48:35 +0200

    solved task and added some Class

---
+++ src/main/java/core/basesyntax/MainApp.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class MainApp {
+    private static final int NUMBER_OF_FIGURE = 6;
+
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[NUMBER_OF_FIGURE];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+                figures[i].drawFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+                figures[i].drawFigure();
+            }
+        }
+    }
+}
+
