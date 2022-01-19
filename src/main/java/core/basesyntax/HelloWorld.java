79adaa40d4d45996fd13fe3bf048b67b529e6d6c
Liubov Chernohor <95702981+Liubov-Chernohor@users.noreply.github.com>
2022-01-19 18:06:48 +0200

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -8,10 +8,10 @@ public class HelloWorld {
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
-                figures[i] = fs.getRandomFigure();
+                figures[i] = figureSupplier.getRandomFigure();
                 System.out.println(figures[i]);
             } else {
-                figures[i] = fs.getDefaultFigure();
+                figures[i] = figureSupplier.getDefaultFigure();
                 System.out.println(figures[i]);
             }
         }
