887cdedd80580b2828cd36a98a710874ee4e1327
LinkClink <shekaqwe@gmail.com>
2022-01-19 18:50:28 +0200

    for fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,10 +5,10 @@ public class Main {
     private static Figure[] figures = new Figure[6];
 
     public static void main(String[] args) {
-        for (int i = 0; i < figures.length / 2 + 1; i++) {
+        for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
         }
-        for (int i = figures.length - 1; i != figures.length / 2; i--) {
+        for (int i = figures.length - 1; i != figures.length / 2 - 1; i--) {
             figures[i] = figureSupplier.getDefaultFigure();
         }
         for (Figure figure : figures) {
