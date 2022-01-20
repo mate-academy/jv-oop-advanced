951f89184879510cbdd547ff769afa5ed65abef4
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-20 15:04:05 +0200

    created Circle class

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
