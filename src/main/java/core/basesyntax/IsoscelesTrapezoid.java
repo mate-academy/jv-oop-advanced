f8c8be3f515bb2b82e0ca5f011274f051213bcfe
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-20 15:22:05 +0200

    created IsoscelesTrapezoid class

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,31 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private double firstBase;
+    private double secondBase;
+    private double height;
+
+    public double getFirstBase() {
+        return firstBase;
+    }
+
+    public void setFirstBase(double firstBase) {
+        this.firstBase = firstBase;
+    }
+
+    public double getSecondBase() {
+        return secondBase;
+    }
+
+    public void setSecondBase(double secondBase) {
+        this.secondBase = secondBase;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
