746226306eeccfba1c4cedf8aad7f827731fca1f
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-20 15:06:13 +0200

    created Rectangle class

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private double length;
+    private double width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
