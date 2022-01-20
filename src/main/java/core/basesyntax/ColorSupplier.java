30d7718af186de2862e4c31bba29b0c290d4df0d
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-01-20 19:49:10 +0200

    First commit of task Figure

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class ColorSupplier {
+    public String getRandomColor() {
+        Random random = new Random();
+        return Colors.values()[random.nextInt(Colors.values().length)].toString();
+    }
+}
