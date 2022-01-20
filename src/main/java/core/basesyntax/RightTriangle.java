5d3f7509d92181572cc161379be9255e9cca33db
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-20 15:10:14 +0200

    created RightTriangle class

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure {
+    private double base;
+    private double height;
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
