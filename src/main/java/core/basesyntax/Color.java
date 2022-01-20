b3b8a7fbeeafe73a0fc1eb633cd67650ba6a6666
DenTima <tymoshchuk.den@gmail.com>
2022-01-20 14:20:20 +0200

    Realised oop-advanced task

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public enum Color {
+    WHITE,
+    BLACK,
+    BLUE,
+    GRAY,
+    GREEN,
+    RED,
+    YELLOW
+}
---
+++ src/main/java/core/basesyntax/MainApp.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class MainApp {
+    public static final int ARRAY_SIZE = 6;
+
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[ARRAY_SIZE];
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+                figures[i].drawFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+                figures[i].drawFigure();
+            }
+        }
+    }
+}
