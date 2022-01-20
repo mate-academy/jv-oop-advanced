9abd5266d38d3f3ee0bfdc0b552c5e827d44582b
Andriy_Poluhovic <chimbar@ukr.net>
2022-01-20 11:48:55 +0200

    Implemented SwitchCase

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,26 +7,31 @@ public class FigureSupplier {
     public static final String COLOR_Default = "WHITE";
     public static final double RADIUS_Default = 10;
     public static final String CIRCLE_Default = "circle";
+    public static final int NUMBER_OF_FIGURES = 5;
 
     public Figure getRandomFigure() {
         Random random = new Random();
         ColorSupplier supplier = new ColorSupplier();
-        RightTriangle triangle = new RightTriangle("right triangle",
-                supplier.getRandomColor(),
-                random.nextInt(BORDER), random.nextInt(BORDER));
-        Rectangle rectangle = new Rectangle("rectangle",
-                supplier.getRandomColor(),
-                 random.nextInt(BORDER), random.nextInt(BORDER));
-        Square square = new Square("square",
-                supplier.getRandomColor(), random.nextInt(BORDER));
-        Circle circle = new Circle("circle",
-                supplier.getRandomColor(), random.nextInt(BORDER));
-        Trapezoid trapezoid = new Trapezoid("isosceles trapezoid",
-                supplier.getRandomColor(), random.nextInt(BORDER),
-                random.nextInt(BORDER), random.nextInt(BORDER));
-        Figure[] array = new Figure[]{triangle, rectangle, square, circle, trapezoid};
-        int index = random.nextInt(array.length);
-        return array[index];
+        switch (random.nextInt(5)) {
+            case 0:
+                return new RightTriangle("right triangle",
+                        supplier.getRandomColor(),
+                        random.nextInt(BORDER), random.nextInt(BORDER));
+            case 1:
+                return new Rectangle("rectangle",
+                        supplier.getRandomColor(),
+                        random.nextInt(BORDER), random.nextInt(BORDER));
+            case 2:
+                return new Square("square",
+                        supplier.getRandomColor(), random.nextInt(BORDER));
+            case 3:
+                return new Circle("circle",
+                        supplier.getRandomColor(), random.nextInt(BORDER));
+            default:
+                return new Trapezoid("isosceles trapezoid",
+                        supplier.getRandomColor(), random.nextInt(BORDER),
+                        random.nextInt(BORDER), random.nextInt(BORDER));
+        }
     }
 
     public Figure getDefaultFigure() {
