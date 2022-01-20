1a8492b0d419f79ce7d132d01f2000f3401c8f1c
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-01-20 12:01:26 +0200

    Added constanÐt FIGURE_COUNT into Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int FIGURE_COUNT = 5;
+
     public static void main(String[] args) {
-        Figure[] figures = new Figure[5];
+        Figure[] figures = new Figure[FIGURE_COUNT];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
