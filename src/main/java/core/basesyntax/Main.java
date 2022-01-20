44eae7d1e77155bf5f0b1bec81556de419563dcc
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-01-20 09:04:40 +0200

    failed check was fixed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Main {
     private static final int ARRAY_SIZE = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[ARRAY_SIZE];
