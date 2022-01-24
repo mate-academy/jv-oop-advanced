9660f6218264a9861cada6ff6e6c1ec722dff44c
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-24 20:31:36 +0200

    Correct all Maven coments: changes in Figure class

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -4,11 +4,11 @@ public abstract class Figure implements AreaCalculator, Drawable {
 
     private String color;
 
-    public String getColor() {
-        return color;
+    public Figure(String color) {
+        this.color = color;
     }
 
-      public Figure(String color) {
-        this.color = color;
+    public String getColor() {
+        return color;
     }
 }
