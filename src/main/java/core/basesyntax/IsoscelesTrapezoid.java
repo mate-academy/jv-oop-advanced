b7e91d5f3bccd8cd15978446ab938f1d577ed473
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-01-28 16:59:55 +0200

    created class IsoscelesTrapezoid that implements Figure with methods getArea() and draw(), added constructor that has 4 variables. Method getArea() calculate area isosceles trapezoid, method draw() print all information about IsoscelesTrapezoid - height, topBase, bottomBase, area and color

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid implements Figure {
+    private final String color;
+    private final int height;
+    private final int topBase;
+    private final int bottomBase;
+
+    public IsoscelesTrapezoid(String color, int height, int topBase, int bottomBase) {
+        this.color = color;
+        this.height = height;
+        this.topBase = topBase;
+        this.bottomBase = bottomBase;
+    }
+
+    @Override
+    public double getArea() {
+        return (topBase + bottomBase) * height * 0.5;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, area: " + getArea() + " sq.units, height: "
+                + height + " units, top base of trapesoid: " + topBase
+                + " units, bottom base of trapesoid: "
+                + bottomBase + " units, color: " + color);
+    }
+}
