c1cd74c1622437e72ffd3fb60767e95f936cb3f0
Dmytro Chernenko <mitresko12@gmail.com>
2022-02-01 18:12:45 +0200

    implemented util classes

--- src/main/java/core/basesyntax/util/ColorSupplier.java
+++ src/main/java/core/basesyntax/util/ColorSupplier.java
@@ -1,5 +1,21 @@
 package core.basesyntax.util;
 
-public class ColorSupplier {
+import core.basesyntax.entity.Color;
 
+import java.util.Random;
+
+public class ColorSupplier {
+    private final Random random;
+
+    public ColorSupplier() {
+        random = new Random();
+    }
+
+    public Color getRandomColor() {
+        return Color.values()[random.nextInt(Color.values().length)];
+    }
+
+    public Color getDefaultColor() {
+        return Color.WHITE;
+    }
 }
--- src/main/java/core/basesyntax/util/FigureSupplier.java
+++ src/main/java/core/basesyntax/util/FigureSupplier.java
@@ -1,4 +1,22 @@
 package core.basesyntax.util;
 
+import core.basesyntax.entity.Color;
+import core.basesyntax.entity.Figure;
+import core.basesyntax.entity.Square;
+
+import java.util.Random;
+
 public class FigureSupplier {
+    private final ColorSupplier colorSupplier;
+    private final Random random;
+
+    public FigureSupplier() {
+        colorSupplier = new ColorSupplier();
+        random = new Random();
+    }
+
+    public Figure getDefaultFigure() {
+        return new Square(10, colorSupplier.getDefaultColor());
+    }
+
 }
