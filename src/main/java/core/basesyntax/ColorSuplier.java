473c90b117516db807f29d8830a184f1fd88dfbb
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-02-01 14:20:57 +0200

    moved random and array colors from method to fields class

--- src/main/java/core/basesyntax/ColorSuplier.java
+++ src/main/java/core/basesyntax/ColorSuplier.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSuplier {
-    public static String getRandomColor() {
-        String[] colors = {"white", "blue", "green", "black", "yellow"};
-        Random random = new Random();
-        int index = random.nextInt(colors.length);
+    private final static Random random = new Random();
+    private final static String[] colors = {"white", "blue", "green", "black", "yellow"};
 
-        return colors[index];
+    public static String getRandomColor() {
+        return colors[random.nextInt(colors.length)];
     }
 }
