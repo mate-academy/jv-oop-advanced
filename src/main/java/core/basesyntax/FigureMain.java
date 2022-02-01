df652c58dcaf2042113b6c2f34cb623ba9611002
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-02-01 14:16:00 +0200

    changed specific number to number that depend on length array and calculated during running (3 to figure.length/2)

--- src/main/java/core/basesyntax/FigureMain.java
+++ src/main/java/core/basesyntax/FigureMain.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 public class FigureMain {
     public static void main(String[] args) {
         Figure[] figure = new Figure[6];
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < figure.length / 2; i++) {
             figure[i] = FigureSuplier.getRandomFigure();
+
         }
-        for (int i = 3; i < 6; i++) {
+        for (int i = figure.length / 2; i < figure.length; i++) {
             figure[i] = FigureSuplier.getDefaultFigure();
         }
         for (Figure fig : figure) {
