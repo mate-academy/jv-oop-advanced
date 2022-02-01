c75dcae87cc95f79c182cc7e866e43f34ed42a86
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-02-01 15:32:42 +0200

    changed name class that called

--- src/main/java/core/basesyntax/FigureMain.java
+++ src/main/java/core/basesyntax/FigureMain.java
@@ -4,11 +4,11 @@ public class FigureMain {
     public static void main(String[] args) {
         Figure[] figure = new Figure[6];
         for (int i = 0; i < figure.length / 2; i++) {
-            figure[i] = FigureSuplier.getRandomFigure();
+            figure[i] = FigureSupplier.getRandomFigure();
 
         }
         for (int i = figure.length / 2; i < figure.length; i++) {
-            figure[i] = FigureSuplier.getDefaultFigure();
+            figure[i] = FigureSupplier.getDefaultFigure();
         }
         for (Figure fig : figure) {
             fig.getArea();
