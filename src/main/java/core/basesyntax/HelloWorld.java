4415a7004da8f7d8206fa182498566e072e65b87
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-02-03 16:41:21 +0200

    Create an array of figures in launch program

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[6];
+        for (int i = 0; i < figures.length; i++) {
+            figures[i] = (i <= (figures.length - 1) / 2) ? figureSupplier.getRandomFigure()
+                    : figureSupplier.getDefaultFigure();
+            figures[i].draw();
+        }
+    }
 }
