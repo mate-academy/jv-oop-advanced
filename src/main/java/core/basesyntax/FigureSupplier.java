7ba9b955ef4f38f0d802160a762e7f817bd2fa51
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-02-03 17:22:53 +0200

    Add getIntRandom() method to FigureSupplier class

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -8,28 +8,29 @@ public class FigureSupplier {
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
+        String color = colorSupplier.getRandomColor(random);
         int index = random.nextInt(5);
         switch (index) {
             case 1:
-                return new Circle(colorSupplier.getRandomColor(random),
-                        random.nextInt(100));
+                return new Circle(color, getIntRandom());
             case 2:
-                return new Square(colorSupplier.getRandomColor(random),
-                        random.nextInt(100));
+                return new Square(color, getIntRandom());
             case 3:
-                return new Rectangle(colorSupplier.getRandomColor(random),
-                        random.nextInt(100), random.nextInt(100));
+                return new Rectangle(color, getIntRandom(), getIntRandom());
             case 4:
-                return new RightTriangle(colorSupplier.getRandomColor(random),
-                        random.nextInt(100), random.nextInt(100));
+                return new RightTriangle(color, getIntRandom(), getIntRandom());
             default:
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(random),
-                        random.nextInt(100), random.nextInt(100), random.nextInt(100));
+                return new IsoscelesTrapezoid(color, getIntRandom(), getIntRandom(),
+                        getIntRandom());
 
         }
     }
 
     public Figure getDefaultFigure() {
         return new Circle("WHITE", 10);
+    }
+
+    private int getIntRandom() {
+        return random.nextInt(100);
     }
 }
