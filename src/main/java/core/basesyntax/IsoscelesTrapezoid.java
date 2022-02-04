ccd2c4cd10cb6d9e679c7063523d3933605505ff
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-02-04 16:34:37 +0200

    Implements AreaCalculator interface by Figure class

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class IsoscelesTrapezoid extends Figure implements AreaCalculator {
+public class IsoscelesTrapezoid extends Figure {
     private int base1;
     private int base2;
     private int height;
