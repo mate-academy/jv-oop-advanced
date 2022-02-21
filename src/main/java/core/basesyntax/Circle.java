3b45caeeb37229962ea464e0b2b6ff67acda23ff
VladimirShtukar <99186204+VladimirShtukar@users.noreply.github.com>
2022-02-21 17:42:19 +0200

    figure

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class Circle extends Figure implements Area {
+    private String color;
+    private int radius;
+
+    public Circle(String color, int radius) {
+        this.color = color;
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.round(Math.PI * 100.0) / 100.0;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Figure: circle, area: " + getArea()
+                + " sq.units, radius: " + radius
+                + " units, color: " + color;
+    }
+}
