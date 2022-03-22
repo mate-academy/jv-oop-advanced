a98ab3a942d6fe0c13d5068217fddb863d75cfd6
JadeRowan <rougesoul.m@gmail.com>
2022-03-22 14:55:16 +0200

    Naming and magic numbers small checkstyle fix

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,11 +5,11 @@ package core.basesyntax;
  */
 public class Application {
     public static void main(String[] args) {
-        FigureSupplier fs = new FigureSupplier();
+        FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[6];
-        for (int i = 0; i < 3; i++) {
-            figures[i] = fs.getRandomFigure();
-            figures[i + 3] = fs.getDefaultFigure();
+        for (int i = 0; i < figures.length/2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+            figures[i + figures.length/2] = figureSupplier.getDefaultFigure();
         }
         for (Figure figure : figures) {
             figure.draw();
