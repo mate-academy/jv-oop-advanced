fed1e803eb8b3fce6813982cc2799bd60f93d6f4
JadeRowan <rougesoul.m@gmail.com>
2022-03-22 20:27:11 +0200

    added whitespaces for maven checker plugin

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -7,9 +7,9 @@ public class Application {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[6];
-        for (int i = 0; i < figures.length/2; i++) {
+        for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
-            figures[i + figures.length/2] = figureSupplier.getDefaultFigure();
+            figures[i + figures.length / 2] = figureSupplier.getDefaultFigure();
         }
         for (Figure figure : figures) {
             figure.draw();
