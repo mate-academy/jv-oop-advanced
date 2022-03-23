303b6eed41fceafa6e1df76ff8016fd44104969f
Roma Yarishkin <romarijkov@gmail.com>
2022-03-23 12:28:28 +0200

    Task completed

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,32 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure implements Draw, FigureArea {
+  private final int width;
+  private final int height;
+
+  public Rectangle(String color, int width, int height) {
+    super(color);
+    this.width = width;
+    this.height = height;
+  }
+
+  @Override
+  public String getColor() {
+    return super.getColor();
+  }
+
+  @Override
+  public double getArea() {
+    return width * height;
+  }
+
+  @Override
+  public String getDraw() {
+    return "Rectangle: area: " + getArea() + " sq.units, " + "width: " + width + " units, " + "height: " + height + " units, " + "color: " + getColor();
+  }
+
+  @Override
+  public String toString() {
+    return getDraw();
+  }
+}
