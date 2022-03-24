97ba3ffc6977999199d9173ca52add1c2dcb09a2
OLEH VIESHAIEV <sred_a@i.ua>
2022-03-24 23:20:24 +0200

    jv-oop-advanced solution was created

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,40 @@
+package core.basesyntax;
+
+public class Right_triangle extends Figure implements Area {
+    private int firstLeg;
+    private int secondLeg;
+
+    public Right_triangle(int firstLeg, int secondLeg, String color) {
+        super.color = color;
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double getArea() {
+        return firstLeg * secondLeg / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Figure: Right triangle, area: " + getArea() + " sq.units, "
+                + "firstLeg: " + firstLeg + " units, secondLeg: " + secondLeg
+                + " units, color: " + color;
+    }
+
+    public int getFirstLeg() {
+        return firstLeg;
+    }
+
+    public void setFirstLeg(int firstLeg) {
+        this.firstLeg = firstLeg;
+    }
+
+    public int getSecondLeg() {
+        return secondLeg;
+    }
+
+    public void setSecondLeg(int secondLeg) {
+        this.secondLeg = secondLeg;
+    }
+}
