38e9681ac18ae0d91768db58270953b6f110148d
Roman Kachmaryk <roman.kv98@gmail.com>
2022-03-25 13:18:48 +0200

    created classes and implemented methods solving the task

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public enum Color {
+    RED,
+    BLACK,
+    GREEN,
+    YELLOW,
+    PINK,
+    BLUE,
+    GREY,
+    WHITE
+}
---
+++ src/main/java/core/basesyntax/FigureArea.java
@@ -0,0 +1,5 @@
+package core.basesyntax;
+
+public interface FigureArea {
+    double getArea();
+}
