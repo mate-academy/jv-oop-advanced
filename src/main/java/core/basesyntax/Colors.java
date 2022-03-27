d041609ec8aaf65ba7d903baad5ffdc4ae113ad5
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2022-03-27 14:54:18 +0300

    Implement `getRandomColor()` method

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public enum Colors {
+    WHITE,
+    BLACK,
+    GREY,
+    YELLOW,
+    RED,
+    BLUE,
+    GREEN,
+    BROWN,
+    ORANGE,
+    PURPLE,
+    CYAN,
+    MAGENTA
+}
