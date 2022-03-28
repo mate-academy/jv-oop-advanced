f503f37cb0740e68de01227dc93cd509c71358d4
Pavel <pavelpm1971@gmail.com>
2022-03-28 11:11:54 +0200

    Test commit and push

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -6,6 +6,7 @@ package core.basesyntax;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("World");
     }
 
 }
