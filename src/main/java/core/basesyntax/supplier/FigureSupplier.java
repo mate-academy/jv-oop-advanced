6e1b0972ac403455f6da572fe80406a14ea0a78d
Alex <alexoriobel@gmail.com>
2022-04-08 19:01:09 +0300

    Fixed after auto review v12

--- src/main/java/core/basesyntax/supplier/FigureSupplier.java
+++ src/main/java/core/basesyntax/supplier/FigureSupplier.java
@@ -9,14 +9,14 @@ import core.basesyntax.figures.Square;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int DEF_RADIUS = 10;
+    private static final int FIG_RANDOM = 4;
+    private static final int RAN_COLOR = 8;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private static final int DEF_RADIUS = 10;
-    private static final int FIGURE_RANDOM = 4;
-    private static final int RANDOM_COLOR = 8;
 
     public Figure getRandomFigure() {
-        switch (random.nextInt(FIGURE_RANDOM) + 1) {
+        switch (random.nextInt(FIG_RANDOM) + 1) {
             case 1:
                 return getRendomeCircle();
             case 2:
@@ -37,27 +37,27 @@ public class FigureSupplier {
     }
 
     private Circle getRendomeCircle() {
-        return new Circle(colorSupplier.getRandomColor(), random.nextInt(RANDOM_COLOR) + 1);
+        return new Circle(colorSupplier.getRandomColor(), random.nextInt(RAN_COLOR) + 1);
     }
 
     private Square getRendomeSquare() {
-        return new Square(colorSupplier.getRandomColor(), random.nextInt(RANDOM_COLOR) + 1);
+        return new Square(colorSupplier.getRandomColor(), random.nextInt(RAN_COLOR) + 1);
     }
 
     private RightTriangle getRendomeRigthTriangle() {
         return new RightTriangle(colorSupplier.getRandomColor(),
-                random.nextInt(RANDOM_COLOR) + 1,
-                random.nextInt(RANDOM_COLOR) + 1);
+                random.nextInt(RAN_COLOR) + 1,
+                random.nextInt(RAN_COLOR) + 1);
     }
 
     private IsoscelesTrapezoid getRendomeIsoscelesTrapezoid() {
         return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                random.nextInt(RANDOM_COLOR) + 1, random.nextInt(RANDOM_COLOR) + 1);
+                random.nextInt(RAN_COLOR) + 1, random.nextInt(RAN_COLOR) + 1);
     }
 
     private Rectangle getRendomeRectangle() {
         return new Rectangle(colorSupplier.getRandomColor(),
-                random.nextInt(RANDOM_COLOR) + 1,
-                random.nextInt(RANDOM_COLOR) + 1);
+                random.nextInt(RAN_COLOR) + 1,
+                random.nextInt(RAN_COLOR) + 1);
     }
 }
