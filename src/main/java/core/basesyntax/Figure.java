25c63067dcf61da03188d12b70b626b352327ae5
Ihor Dmytrush <ihor.dm.a@gmail.com>
2022-04-12 15:09:48 +0300

    3th try: Figure implements AreaCalculator too;

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawable {
+public abstract class Figure implements AreaCalculator, Drawable {
     private String color;
 
     void setColor(String color) {
