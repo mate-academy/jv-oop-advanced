8d1ca7ff31964854d658d8abc4913e75744f0acc
Artem Bordichuk <artyombordichuk@gmail.com>
2022-04-13 15:21:01 +0300

    Imlemetned all your advises.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,12 +4,11 @@ import core.basesyntax.figure.Figure;
 import core.basesyntax.supplier.FigureSupplier;
 
 public class Main {
-    private static final int FIGURECOUNT = 5;
+    private static final int FIGURE_COUNT = 5;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[FIGURECOUNT];
-
+        Figure[] figures = new Figure[FIGURE_COUNT];
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
@@ -17,7 +16,6 @@ public class Main {
                 figures[i] = figureSupplier.getDefaultFigure();
             }
         }
-
         for (Figure figure : figures) {
             figure.draw();
         }
--- src/main/java/core/basesyntax/supplier/FigureSupplier.java
+++ src/main/java/core/basesyntax/supplier/FigureSupplier.java
@@ -7,35 +7,33 @@ import core.basesyntax.figure.Square;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int MAX_VALUE = 100;
-    private static final int BOUND = 3;
-    private static final int RADIUS = 10;
+    private static final int MAX_RANDOM_NUMBER = 100;
+    private static final int FIGURES_NUMBER = 3;
+    private static final int DEFAULT_RADIUS_CIRCLE = 10;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int index = random.nextInt(BOUND);
+        int index = random.nextInt(FIGURES_NUMBER);
+        String color = colorSupplier.getRandomColor();
         switch (index) {
-            case (0):
-                String color = colorSupplier.getRandomColor();
-                int width = random.nextInt(MAX_VALUE);
-                int height = random.nextInt(MAX_VALUE);
+            case 0:
+                int width = random.nextInt(MAX_RANDOM_NUMBER);
+                int height = random.nextInt(MAX_RANDOM_NUMBER);
                 return new Rectangle(color, width, height);
-            case (1):
-                color = colorSupplier.getRandomColor();
-                int side = random.nextInt(MAX_VALUE);
+            case 1:
+                int side = random.nextInt(MAX_RANDOM_NUMBER);
                 return new Square(color, side);
-            case (2):
-                color = colorSupplier.getRandomColor();
-                width = random.nextInt(MAX_VALUE);
-                height = random.nextInt(MAX_VALUE);
+            case 2:
+                width = random.nextInt(MAX_RANDOM_NUMBER);
+                height = random.nextInt(MAX_RANDOM_NUMBER);
                 return new Rectangle(color, width, height);
             default:
-                return new Square(colorSupplier.getRandomColor(),random.nextInt(MAX_VALUE));
+                return new Square(colorSupplier.getRandomColor(),random.nextInt(MAX_RANDOM_NUMBER));
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), RADIUS);
+        return new Circle(Color.WHITE.name(), DEFAULT_RADIUS_CIRCLE);
     }
 }
