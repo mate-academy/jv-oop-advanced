541dc270d55590d8763d68e10800da784c6d2bb6
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-27 14:43:54 +0300

    Changed variable numberOfColors in ColorSupplier class to local

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-    private final int numberOfColors = Color.values().length;
 
     public String getRandomColor() {
+        final int numberOfColors = Color.values().length;
         int colorIndex = random.nextInt(numberOfColors);
         return Color.values()[colorIndex].toString();
     }
