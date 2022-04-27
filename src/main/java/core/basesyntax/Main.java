a61707644dba270e61c544f629495d29fb6ed4a0
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-27 13:50:26 +0300

    Removed redundunt java.util from Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,5 @@
 package core.basesyntax;
 
-import java.util.Random;
-
-/**
- * Feel free to remove this class and create your own.
- */
 public class Main {
     public static void main(String[] args) {
         final int arrayLength = 6;
