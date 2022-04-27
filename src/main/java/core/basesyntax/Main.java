8a37997581d11d9465d2abbaef7fa5cb1d617207
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-27 14:04:30 +0300

    Fixed some variables problems

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Main {
-    public static void main(String[] args) {
-        final int arrayLength = 6;
+    private static final int ARRAY_LENGTH = 6;
 
+    public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[arrayLength];
+        Figure[] figures = new Figure[ARRAY_LENGTH];
 
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
