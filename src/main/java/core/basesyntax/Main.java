d93401960e56a60c8ee37a9a779f2e160dc5421d
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-27 14:56:10 +0300

    Removed empty line in Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[FIGURES_LENGTH];
-
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
                 figureSupplier.getRandomFigure().draw();
