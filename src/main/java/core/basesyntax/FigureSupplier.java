e28c40e700baea86c8f0a3db8158ca24d964ed27
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-04 12:36:28 +0300

    private methods were added

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,41 +4,55 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int FIGURE_TYPES = 5;
-    private static final int MAX_SIDE = 5;
+    private static final int MAX_SIDE = 12;
+    private static final String DEFAULT_COLOR = "white";
+    private static final int DEFAULT_SIDE = 10;
     private final Random random = new Random();
+    ColorSupplier randomColor = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int randomFigure = random.nextInt(FIGURE_TYPES);
-        ColorSupplier randomColor = new ColorSupplier();
-        String color = randomColor.getRandomColor();
         switch (randomFigure) {
             case (0):
-                Figure square = new Square((new Random().nextInt(MAX_SIDE)
-                        + 1), color);
-                return square;
+                return getRandomSquare();
             case (1):
-                Figure rectangle = new Rectangle((new Random().nextInt(MAX_SIDE)
-                        + 1), (new Random().nextInt(MAX_SIDE) + 1), color);
-                return rectangle;
+                return getRandomRectangle();
             case (2):
-                Figure rightTriangle = new RightTriangle((new Random().nextInt(MAX_SIDE)
-                        + 1), (new Random().nextInt(MAX_SIDE) + 1), color);
-                return rightTriangle;
+                return  getRandomRightTriangle();
             case (3):
-                Figure circle = new Circle((new Random().nextInt(MAX_SIDE)
-                        + 1), color);
-                return circle;
+                return getRandomCircle();
             default:
-                Figure isoscelesTrapezoid = new IsoscelesTrapezoid((new Random().nextInt(MAX_SIDE)
-                        + 1), (new Random().nextInt(MAX_SIDE) + 1),
-                        (new Random().nextInt(MAX_SIDE) + 1), color);
-                return isoscelesTrapezoid;
+                return getIsoscelesTrapezoid();
         }
-
     }
     
     public Figure getDefaultFigure() {
-        Figure defaultFigure = new Square(10, "white");
-        return defaultFigure;
+        return new Circle(DEFAULT_SIDE, DEFAULT_COLOR);
+    }
+
+    private Figure getRandomSquare() {
+        return new Square((new Random().nextInt(MAX_SIDE)
+                + 1), randomColor.getRandomColor());
+    }
+
+    private Figure getRandomRectangle() {
+        return new Rectangle((new Random().nextInt(MAX_SIDE)
+                + 1), (new Random().nextInt(MAX_SIDE) + 1), randomColor.getRandomColor());
+    }
+
+    private Figure getRandomRightTriangle() {
+        return new RightTriangle((new Random().nextInt(MAX_SIDE)
+                + 1), (new Random().nextInt(MAX_SIDE) + 1), randomColor.getRandomColor());
+    }
+
+    private Figure getRandomCircle() {
+        return new Circle((new Random().nextInt(MAX_SIDE)
+                + 1), randomColor.getRandomColor());
+    }
+
+    private Figure getIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid((new Random().nextInt(MAX_SIDE)
+                + 1), (new Random().nextInt(MAX_SIDE) + 1),
+                (new Random().nextInt(MAX_SIDE) + 1), randomColor.getRandomColor());
     }
 }
