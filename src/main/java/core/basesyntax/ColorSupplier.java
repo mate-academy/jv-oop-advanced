f45f5210b3237e1ea6b5e0233dd48fb8b82eb45d
VladimirShtukar <99186204+VladimirShtukar@users.noreply.github.com>
2022-05-14 18:38:59 +0300

    оновленння пов'язані з мавеном

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 import java.util.Random;
+
 public class ColorSupplier {
-public Color getRandomColor() {
+    public Color getRandomColor() {
         int randomIndex = new Random().nextInt(Color.values().length);
         return Color.values()[randomIndex];
-   }
+    }
 }
