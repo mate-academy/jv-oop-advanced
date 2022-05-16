24410032e0686f8e16f4141577480f6b1d524a97
Dima Povkh <dimapovkh@gmail.com>
2022-05-16 17:48:36 +0200

    hm-oop-advanced-fixed

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public static class Circle extends Figure {
+public class Circle extends Figure {
     private int radius;
 
     public Circle(int radius, String color) { // it was Color -> String
