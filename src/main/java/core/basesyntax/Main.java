acc71d8bccf469401542aff360c49ee3cc57df8f
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2022-05-19 15:09:00 +0300

    add for loop in to main method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,16 +5,17 @@ public class Main {
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[]{
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getDefaultFigure(),
-                figureSupplier.getDefaultFigure(),
-                figureSupplier.getDefaultFigure()
-        };
+        Figure[] figures = new Figure[COUNTER];
 
-        for (Figure figure: figures) {
+        for (int i = 0; i < COUNTER; i++) {
+            if (i < COUNTER / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
+
+        for (Figure figure : figures) {
             figure.draw();
         }
     }
