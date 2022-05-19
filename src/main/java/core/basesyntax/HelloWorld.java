ef6d9be50b33b67de0905bb133c88e25a2618d41
Ruslan Matiash <r7714007@gmail.com>
2022-05-19 23:50:45 +0300

    I'll do it tomorrow

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 /**
  * Feel free to remove this class and create your own.
  */
-public class HelloWorld {
+public class HelloWorld (soTiredToNight) {
+    Hello my dear friend!
+    Tonight I so tired, but I promise you I'll do it tomorrow!)
 
 }
