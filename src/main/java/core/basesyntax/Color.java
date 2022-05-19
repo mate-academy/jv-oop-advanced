716c0660a967dc06f3ccd985d3d1453e7a393723
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-05-19 17:47:59 +0300

    ColorSupplier Class was implemented

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum Color {
+    WHITE,
+    GREEN,
+    BLUE,
+    PINK,
+    BLACK,
+    ORANGE
+}
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier(random);
+        System.out.println(colorSupplier.getRandomColor());
     }
 }
