b684c7c362fefabdd498b195c3a8ff00ba00b009
Nata <natserbo@gmail.com>
2022-05-22 17:21:33 +0300

    add Draw interface, radius to constructor in Circle

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Circle extends Figure implements Area{
+public class Circle extends Figure implements Area, Draw{
     private int radius;
 
     public void setRadius(int radius) {
@@ -10,14 +10,20 @@ public class Circle extends Figure implements Area{
     public int getRadius() {
         return radius;
     }
-    public Circle(String color) {
+    public Circle(String color, int radius) {
         super(color);
+        this.radius =  radius;
     }
 
     @Override
     public double getArea() {
-        return 0;
+        return Math.PI * Math.pow(radius, getNumberTwo);
     }
 
-
+    @Override
+    public void draw() {
+        System.out.println("Figure: circle, area: " + getArea()
+                + " sq.units, " + "radius: " + radius
+                + " units, color: " + super.getColor());
+    }
 }
