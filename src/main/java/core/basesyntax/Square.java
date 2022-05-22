bfd7e9713a0afda10460e694c56717a24d3a0684
Nata <natserbo@gmail.com>
2022-05-22 17:30:20 +0300

    add side, getter,setter, constructor and Draw interface in Square

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -1,12 +1,30 @@
 package core.basesyntax;
 
-public class Square extends Figure implements Area{
-    public Square(String color, String type) {
-        super(color, type);
+public class Square extends Figure implements Area, Draw{
+    private int side;
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public Square(String color, int side) {
+        super(color);
+        this.side = side;
     }
 
     @Override
     public double getArea() {
-        return 0;
+        return Math.pow(side,getNumberTwo);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rightTriangle, area: " + getArea()
+                + " sq.units, " + "side: " + side
+                + " units, color: " + super.getColor());
     }
 }
