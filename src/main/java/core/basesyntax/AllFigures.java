2ba45bcce813c47bdafa1fb162412e9244db83b5
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-27 14:21:00 +0300

    created a separate enum and switch case

---
+++ src/main/java/core/basesyntax/AllFigures.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public enum AllFigures {
+    CIRCLE,
+    ISOSCELESTRAPEZOID,
+    RECTANGLE,
+    RIGHTTRIANGLE,
+    SQUARE
+}
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -8,10 +8,9 @@ import core.basesyntax.figures.Square;
 import java.util.Random;
 
 public class FigureSupplier {
-
-    private static final String COLOR = "WHITE";
-    private static final int RADIUS = 10;
-    private Random random;
+    private static final String DEFAULT_CIRCLE_COLOR = Color.WHITE.name();
+    private static final int DEFAULT_CIRCLE_RADIUS = 10;
+    private final Random random;
     private final ColorSupplier colorSupplier;
 
     public FigureSupplier(Random random, ColorSupplier colorSupplier) {
@@ -20,25 +19,32 @@ public class FigureSupplier {
     }
 
     public Figure getRandomFigure() {
-        String[] figures = {"circle", "isoscelesTrapezoid", "rectangle", "rightTriangle", "square"};
-        int index = random.nextInt(figures.length);
-        if ("circle".equals(figures[index])) {
-            return new Circle(colorSupplier.getRandomColor(), random.nextInt());
-        } else if ("isoscelesTrapezoid".equals(figures[index])) {
-            return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextInt(),
-                    random.nextInt(), random.nextInt());
-        } else if ("rectangle".equals(figures[index])) {
-            return new Rectangle(colorSupplier.getRandomColor(),
-                    random.nextInt(), random.nextInt());
-        } else if ("rightTriangle".equals(figures[index])) {
-            return new RightTriangle(colorSupplier.getRandomColor(),
-                    random.nextInt(), random.nextInt());
-        } else {
-            return new Square(colorSupplier.getRandomColor(), random.nextInt());
+        Figure figure;
+        int index = random.nextInt(AllFigures.values().length);
+        switch (AllFigures.values()[index]) {
+            case CIRCLE:
+                figure = new Circle(colorSupplier.getRandomColor(), random.nextInt());
+                break;
+            case ISOSCELESTRAPEZOID:
+                figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextInt(),
+                        random.nextInt(), random.nextInt());
+                break;
+            case RECTANGLE:
+                figure = new Rectangle(colorSupplier.getRandomColor(),
+                        random.nextInt(), random.nextInt());
+                break;
+            case RIGHTTRIANGLE:
+                figure = new RightTriangle(colorSupplier.getRandomColor(),
+                        random.nextInt(), random.nextInt());
+                break;
+            default:
+                figure = new Square(colorSupplier.getRandomColor(), random.nextInt());
+                break;
         }
+        return figure;
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(COLOR, RADIUS);
+        return new Circle(DEFAULT_CIRCLE_COLOR, DEFAULT_CIRCLE_RADIUS);
     }
 }
