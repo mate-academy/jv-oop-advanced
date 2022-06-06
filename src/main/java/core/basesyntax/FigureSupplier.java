f1e062d90d23f72cc667b1b4d552f87cea1e1532
Roman Voitalik <voitalik1@gmail.com>
2022-06-06 13:56:30 +0300

    make constants as class members

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,38 +3,39 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    public static final int FIGURE_COUNT = 5;
+    public static final int MAX_RANDOM_NUMBER = 20;
+    public static final double DEFAULT_RADIUS = 10.0;
+
     public Figure getRandomFigure() {
-        final int FigureCount = 5;
-        final int MaxRandomNumber = 20;
         Random random = new Random();
-        int figureNumber = random.nextInt(FigureCount);
+        int figureNumber = random.nextInt(FIGURE_COUNT);
         Color randomColor = new ColorSupplier().getRandomColor();
 
         switch (figureNumber) {
             case 1:
-                double radius = random.nextDouble() * MaxRandomNumber;
+                double radius = random.nextDouble() * MAX_RANDOM_NUMBER;
                 return new Circle(radius, randomColor);
             case 2:
-                double side = random.nextDouble() * MaxRandomNumber;
+                double side = random.nextDouble() * MAX_RANDOM_NUMBER;
                 return new Square(side, randomColor);
             case 3:
-                double length = random.nextDouble() * MaxRandomNumber;
-                double width = random.nextDouble() * MaxRandomNumber;
+                double length = random.nextDouble() * MAX_RANDOM_NUMBER;
+                double width = random.nextDouble() * MAX_RANDOM_NUMBER;
                 return new Rectangle(length, width, randomColor);
             case 4:
-                double firstLeg = random.nextDouble() * MaxRandomNumber;
-                double secondLeg = random.nextDouble() * MaxRandomNumber;
+                double firstLeg = random.nextDouble() * MAX_RANDOM_NUMBER;
+                double secondLeg = random.nextDouble() * MAX_RANDOM_NUMBER;
                 return new RightTriangle(firstLeg, secondLeg, randomColor);
             default:
-                double topSide = random.nextDouble() * MaxRandomNumber;
-                double downSide = random.nextDouble() * MaxRandomNumber;
-                double height = random.nextDouble() * MaxRandomNumber;
+                double topSide = random.nextDouble() * MAX_RANDOM_NUMBER;
+                double downSide = random.nextDouble() * MAX_RANDOM_NUMBER;
+                double height = random.nextDouble() * MAX_RANDOM_NUMBER;
                 return new IsoscelesTrapezoid(topSide, downSide, height, randomColor);
         }
     }
 
     public Figure getDefaultFigure() {
-        final double DefaultRadius = 10.0;
-        return new Circle(DefaultRadius, Color.WHITE);
+        return new Circle(DEFAULT_RADIUS, Color.WHITE);
     }
 }
