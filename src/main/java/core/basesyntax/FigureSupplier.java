7ed777953d140588bf9f24b826660e42624f2d76
Roman Voitalik <voitalik1@gmail.com>
2022-06-06 14:51:04 +0300

    make methods for each figure

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,39 +3,67 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    public static final int FIGURE_COUNT = 5;
-    public static final int MAX_RANDOM_NUMBER = 20;
-    public static final double DEFAULT_RADIUS = 10.0;
+    private static final int FIGURE_COUNT = 5;
+    private static final int MAX_RANDOM_NUMBER = 20;
+    private static final double DEFAULT_RADIUS = 10.0;
 
     public Figure getRandomFigure() {
         Random random = new Random();
         int figureNumber = random.nextInt(FIGURE_COUNT);
-        Color randomColor = new ColorSupplier().getRandomColor();
-
         switch (figureNumber) {
             case 1:
-                double radius = random.nextDouble() * MAX_RANDOM_NUMBER;
-                return new Circle(radius, randomColor);
+                return getRandomCircle();
             case 2:
-                double side = random.nextDouble() * MAX_RANDOM_NUMBER;
-                return new Square(side, randomColor);
+                return getRandomSquare();
             case 3:
-                double length = random.nextDouble() * MAX_RANDOM_NUMBER;
-                double width = random.nextDouble() * MAX_RANDOM_NUMBER;
-                return new Rectangle(length, width, randomColor);
+                return getRandomRectangle();
             case 4:
-                double firstLeg = random.nextDouble() * MAX_RANDOM_NUMBER;
-                double secondLeg = random.nextDouble() * MAX_RANDOM_NUMBER;
-                return new RightTriangle(firstLeg, secondLeg, randomColor);
+                return getRandomRightTriangle();
             default:
-                double topSide = random.nextDouble() * MAX_RANDOM_NUMBER;
-                double downSide = random.nextDouble() * MAX_RANDOM_NUMBER;
-                double height = random.nextDouble() * MAX_RANDOM_NUMBER;
-                return new IsoscelesTrapezoid(topSide, downSide, height, randomColor);
+                return getRandomIsoscelesTrapezoid();
         }
     }
 
     public Figure getDefaultFigure() {
         return new Circle(DEFAULT_RADIUS, Color.WHITE);
+    }
+
+    public Figure getRandomCircle() {
+        Color randomColor = new ColorSupplier().getRandomColor();
+        Random randomDouble = new Random();
+        double radius = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        return new Circle(radius, randomColor);
+    }
+
+    public Figure getRandomSquare() {
+        Color randomColor = new ColorSupplier().getRandomColor();
+        Random randomDouble = new Random();
+        double side = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        return new Square(side, randomColor);
+    }
+
+    public Figure getRandomRectangle() {
+        Color randomColor = new ColorSupplier().getRandomColor();
+        Random randomDouble = new Random();
+        double length = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        double width = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        return new Rectangle(length, width, randomColor);
+    }
+
+    public Figure getRandomRightTriangle() {
+        Color randomColor = new ColorSupplier().getRandomColor();
+        Random randomDouble = new Random();
+        double firstLeg = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        double secondLeg = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        return new RightTriangle(firstLeg, secondLeg, randomColor);
+    }
+
+    public Figure getRandomIsoscelesTrapezoid() {
+        Color randomColor = new ColorSupplier().getRandomColor();
+        Random randomDouble = new Random();
+        double topSide = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        double downSide = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        double height = randomDouble.nextDouble() * MAX_RANDOM_NUMBER;
+        return new IsoscelesTrapezoid(topSide, downSide, height, randomColor);
     }
 }
