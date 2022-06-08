eef16db96478e7b947959b6bbfc6d66f9f81e4f1
Roman Voitalik <voitalik1@gmail.com>
2022-06-08 17:27:24 +0300

    fix colorSupplier

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,7 +7,7 @@ public class FigureSupplier {
     private static final int MAX_RANDOM_NUMBER = 20;
     private static final double DEFAULT_RADIUS = 10.0;
     private Random random = new Random();
-    private ColorSupplier colorSupplier;
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int figureNumber = random.nextInt(FIGURE_COUNT);
@@ -31,27 +31,23 @@ public class FigureSupplier {
 
     public Figure getRandomCircle() {
         double radius = random.nextDouble() * MAX_RANDOM_NUMBER;
-        colorSupplier = new ColorSupplier();
         return new Circle(radius, colorSupplier.getRandomColor());
     }
 
     public Figure getRandomSquare() {
         double side = random.nextDouble() * MAX_RANDOM_NUMBER;
-        colorSupplier = new ColorSupplier();
         return new Square(side, colorSupplier.getRandomColor());
     }
 
     public Figure getRandomRectangle() {
         double length = random.nextDouble() * MAX_RANDOM_NUMBER;
         double width = random.nextDouble() * MAX_RANDOM_NUMBER;
-        colorSupplier = new ColorSupplier();
         return new Rectangle(length, width, colorSupplier.getRandomColor());
     }
 
     public Figure getRandomRightTriangle() {
         double firstLeg = random.nextDouble() * MAX_RANDOM_NUMBER;
         double secondLeg = random.nextDouble() * MAX_RANDOM_NUMBER;
-        colorSupplier = new ColorSupplier();
         return new RightTriangle(firstLeg, secondLeg, colorSupplier.getRandomColor());
     }
 
@@ -59,7 +55,6 @@ public class FigureSupplier {
         double topSide = random.nextDouble() * MAX_RANDOM_NUMBER;
         double downSide = random.nextDouble() * MAX_RANDOM_NUMBER;
         double height = random.nextDouble() * MAX_RANDOM_NUMBER;
-        colorSupplier = new ColorSupplier();
         return new IsoscelesTrapezoid(topSide, downSide, height, colorSupplier.getRandomColor());
     }
 }
