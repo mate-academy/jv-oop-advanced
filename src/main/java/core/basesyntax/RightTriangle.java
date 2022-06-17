73f8710af17b377712f8907ce3726a58d76e243e
Mykola Kutsyi <kytsij@gmail.com>
2022-06-17 12:37:11 +0300

    Project Java OOP advance. Create. Work wrong. Need testing and checking. Random get negative number.

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,34 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure {
+
+    private double firstLeg;
+    private double secondLeg;
+
+    public RightTriangle(String color, double firstLeg, double secondLeg) {
+        super(color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    public void setFirstLeg(double firstLeg) {
+        this.firstLeg = firstLeg;
+    }
+
+    public void setSecondLeg(double secondLeg) {
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.floor((firstLeg * secondLeg) / 2.0);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("right triangle, area: " + getArea()
+                + " sq.units, firstLeg: " + firstLeg
+                + " units, secondLeg: " + secondLeg
+                + " units, color: " + getColor());
+    }
+}
