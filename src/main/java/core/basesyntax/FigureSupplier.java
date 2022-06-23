1e15002b20e7d1f04d2d3230af4896e4525fbf89
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-06-23 05:15:17 +0300

    Created FigureSupplier and his methods

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,49 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    static final double DEFAULT_RADIUS = 10;
+    private static final Color DEFAULT_COLOR = Color.WHITE;
+    static final double MAX_LENGTH_OF_SIDE = 100.0;
+    static final int COUNT_OF_FIGURES = 5;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        String randomColor = colorSupplier.getRandomColor();
+        double randomSide = random.nextDouble() * MAX_LENGTH_OF_SIDE;
+        double secondRandomSide = random.nextDouble() * MAX_LENGTH_OF_SIDE;
+        int randomIndex = random.nextInt(COUNT_OF_FIGURES - 1);
+        Figure randomFigure;
+        switch (randomIndex) {
+            case 0:
+                randomFigure = new Circle(randomColor, randomSide);
+                break;
+            case 1:
+                randomFigure = new Square(randomColor, randomSide);
+                break;
+            case 2:
+                randomFigure = new Rectangle(randomColor, randomSide, secondRandomSide);
+                break;
+            case 3:
+                randomFigure = new RightTriangle(randomColor, randomSide, secondRandomSide);
+                break;
+            case 4:
+                double randomHeight = random.nextDouble() * MAX_LENGTH_OF_SIDE;
+                randomFigure = new IsoscelesTrapezoid(randomColor, randomSide, secondRandomSide, randomHeight);
+                break;
+            default:
+                randomFigure = getDefaultFigure();
+                break;
+        }
+        return randomFigure;
+    }
+
+    public Figure getDefaultFigure(){
+        String defaultColor = DEFAULT_COLOR.toString().toLowerCase();
+        final Circle DEFAULT_FIGURE;
+        DEFAULT_FIGURE = new Circle(defaultColor, DEFAULT_RADIUS);
+        return DEFAULT_FIGURE;
+    }
+}
