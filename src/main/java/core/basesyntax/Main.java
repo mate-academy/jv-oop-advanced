faf5fc2da178d986454eb404b48b5a3baca16fcb
Sasha Vradiy <alexvradii@gmail.com>
2022-06-28 11:33:21 +0300

    My solution of tusk jv-oop-advanced

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Main {
     private static FigureSupplier figureSupplier = new FigureSupplier();
+    private static FigureSupplier figureSupplierDefault = new FigureSupplier();
 
     public static void main(String[] args) {
         Figure[] figures = new Figure[new Random().nextInt(6)];
@@ -15,7 +16,6 @@ public class Main {
             figure.draw();
         }
         System.out.println(figures);
-        FigureSupplier figureSupplierDefault = new FigureSupplier();
         figureSupplierDefault.getDefaultFigure().draw();
     }
 }
