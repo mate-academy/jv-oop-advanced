0f6ec61690d4a7b6c37072c412a56344a7ec54ab
Misha Getsko <getsko.gil@gmail.com>
2022-07-04 12:35:55 +0300

    I fixed the names of the constants

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,15 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int NUMBERSFIGURE = 4;
-    private static final int FIGUREPARAMETERS = 15;
-    private static final int DEFAULTCIRCLERADIUS = 10;
-    private static final String DEFAULTCIRCLECOLOR = String.valueOf(Color.WHITE);
+    private static final int FIGURES_NUMBER = 4;
+    private static final int MAX_RANDOM_NUMBER = 15;
+    private static final int DEFAULT_CIRCLE_RADIUS = 10;
+    private static final String  DEFAULT_CIRCLE_COLOR = Color.WHITE.name();
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int number = random.nextInt(NUMBERSFIGURE);
+        int number = random.nextInt(FIGURES_NUMBER);
         switch (number) {
             case 0:
                 return getRandomSquare();
@@ -30,29 +30,29 @@ public class FigureSupplier {
 
     public Figure getDefaultFigure() {
         Circle circle = new Circle();
-        circle.setRadius(DEFAULTCIRCLERADIUS);
-        circle.setColor(DEFAULTCIRCLECOLOR);
+        circle.setRadius(DEFAULT_CIRCLE_RADIUS);
+        circle.setColor(DEFAULT_CIRCLE_COLOR);
         return circle;
     }
 
     private Circle getRandomCircle() {
         Circle circle = new Circle();
         circle.setColor(colorSupplier.getRandomColor());
-        circle.setRadius(random.nextInt(FIGUREPARAMETERS));
+        circle.setRadius(random.nextInt(MAX_RANDOM_NUMBER));
         return circle;
     }
 
     private Square getRandomSquare() {
         Square square = new Square();
         square.setColor(colorSupplier.getRandomColor());
-        square.setSide(random.nextInt(FIGUREPARAMETERS));
+        square.setSide(random.nextInt(MAX_RANDOM_NUMBER));
         return square;
     }
 
     private RightTriangle getRandomRightTriangle() {
         RightTriangle rightTriangle = new RightTriangle();
-        rightTriangle.setFirstCatet(random.nextInt(FIGUREPARAMETERS));
-        rightTriangle.setSecondCatet(random.nextInt(FIGUREPARAMETERS));
+        rightTriangle.setFirstCatet(random.nextInt(MAX_RANDOM_NUMBER));
+        rightTriangle.setSecondCatet(random.nextInt(MAX_RANDOM_NUMBER));
         rightTriangle.setColor(colorSupplier.getRandomColor());
         return rightTriangle;
     }
@@ -60,16 +60,16 @@ public class FigureSupplier {
     private Rectangle getRandomRectangle() {
         Rectangle rectangle = new Rectangle();
         rectangle.setColor(colorSupplier.getRandomColor());
-        rectangle.setHigth(random.nextInt(FIGUREPARAMETERS));
-        rectangle.setWidth(random.nextInt(FIGUREPARAMETERS));
+        rectangle.setHigth(random.nextInt(MAX_RANDOM_NUMBER));
+        rectangle.setWidth(random.nextInt(MAX_RANDOM_NUMBER));
         return rectangle;
     }
 
     private IsoscelesTrapezoid getRandomIsoscelesTrapezoid() {
         IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid();
-        isoscelesTrapezoid.setFirstGround(random.nextInt(FIGUREPARAMETERS));
-        isoscelesTrapezoid.setSecondGround(random.nextInt(FIGUREPARAMETERS));
-        isoscelesTrapezoid.setHigth(random.nextInt(FIGUREPARAMETERS));
+        isoscelesTrapezoid.setFirstGround(random.nextInt(MAX_RANDOM_NUMBER));
+        isoscelesTrapezoid.setSecondGround(random.nextInt(MAX_RANDOM_NUMBER));
+        isoscelesTrapezoid.setHigth(random.nextInt(MAX_RANDOM_NUMBER));
         isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
         return isoscelesTrapezoid;
     }
