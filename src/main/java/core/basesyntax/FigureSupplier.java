c6702133017d87082ec94e944850e9036d0fa1bc
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-07-08 22:31:47 +0300

    Created ColorSupplier & FigureSupplier classes

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,30 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    private static final int countOfFigures = 5;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
+    public Figure getRandomFigure() {
+        int figureNumber = random.nextInt(countOfFigures);
+        if (figureNumber == 0) {
+            return new Square(colorSupplier.getRandomColor(), getRandomDouble());
+        }
+        if (figureNumber == 1) {
+            return new Rectangle(colorSupplier.getRandomColor(), getRandomDouble(), getRandomDouble());
+        }
+        if (figureNumber == 2) {
+            return new RightTriangle(colorSupplier.getRandomColor(), getRandomDouble(), getRandomDouble());
+        }
+        if (figureNumber == 3) {
+            return new Circle(colorSupplier.getRandomColor(), getRandomDouble());
+        } else {
+            return  new IsoscelesTrapezoid(colorSupplier.getRandomColor() ,getRandomDouble(), getRandomDouble(), getRandomDouble());
+        }
+    }
+    public double getRandomDouble() {
+        return random.nextDouble();
+    }
+}
