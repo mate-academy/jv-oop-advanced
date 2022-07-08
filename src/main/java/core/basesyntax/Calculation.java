8b01088f7f89a369b899950467f0377a0751ad0a
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-07-08 21:50:56 +0300

    Created new classes of different figures and calculation interface

---
+++ src/main/java/core/basesyntax/Calculation.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public interface Calculation {
+    public abstract double getArea();
+
+    public abstract void printInfo();
+}
---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,38 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure implements Calculation {
+    private double firstLeg;
+    private double secondLeg;
+
+    public RightTriangle(String color, double firstLeg, double secondLeg) {
+        super(color);
+        this.setFirstLeg(firstLeg);
+        this.setSecondLeg(secondLeg);
+    }
+
+    public double getFirstLeg() {
+        return firstLeg;
+    }
+
+    public void setFirstLeg(double firstLeg) {
+        this.firstLeg = firstLeg;
+    }
+
+    public double getSecondLeg() {
+        return secondLeg;
+    }
+
+    public void setSecondLeg(double secondLeg) {
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double getArea() {
+        return firstLeg * secondLeg / 2;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println(getColor() + " right triangle with first leg: " + getFirstLeg() + ", second leg: " + getSecondLeg() + " and area: " + getArea());
+    }
+}
