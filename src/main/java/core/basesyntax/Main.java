a9849c42a210533845a22db09b85e935cae7e286
Alina Nikiforova <0500302717a@gmail.com>
2022-07-11 15:14:23 +0200

    added class Square

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Main {
-    public static final int SIZE_OF_ARRAY = 6;
+    public static final int NUMBER_OF_FIGURES = 6;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figures[] figure = new Figures[SIZE_OF_ARRAY];
+        Figure[] figure = new Figure[NUMBER_OF_FIGURES];
         for (int i = 0; i < figure.length; i++) {
             if (i < figure.length / 2) {
                 figureSupplier.getRandomFigure().draw();
