b579aa858311249c58ad614bcf218cd6b60faf79
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-18 12:00:30 +0200

    draw meth updated with public as for Interface

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -16,7 +16,7 @@ public class RightTriangle extends Figure {
     }
 
     @Override
-    void draw() {
+    public void draw() {
         System.out.println("Figure: right triangle, area: " + getArea() + " sq.units, firstLeg: "
                 + firstLeg + " units, secondLeg: " + secondLeg + " units, color: "
                 + super.getColor());
