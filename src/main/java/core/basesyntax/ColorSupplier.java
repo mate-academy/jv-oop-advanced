6eeb9e10f62c8f4adc25f647cf9f671f16cffd09
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-18 11:37:07 +0200

    Random field declared

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random;
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].name();
+        random = new Random();
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
