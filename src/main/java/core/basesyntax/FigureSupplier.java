d4815173b1fd4b55229ec29faa0e8644c825a4be
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-18 18:01:54 +0200

    private meth put after public

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -11,6 +11,36 @@ public class FigureSupplier {
     private String randomColor;
     private Figure figure;
 
+    public Figure getRandomFigure() {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        int randomIndex = random.nextInt(FIGURES_COUNT);
+        switch (randomIndex) {
+            case 0:
+                figure = figureSupplier.getRandomCircle();
+                break;
+            case 1:
+                figure = figureSupplier.getRandomSquare();
+                break;
+            case 2:
+                figure = figureSupplier.getRandomRightTriangle();
+                break;
+            case 3:
+                figure = figureSupplier.getRectangle();
+                break;
+            case 4:
+                figure = figureSupplier.getIsoscelesTrapezoid();
+                break;
+            default:
+                figure = figureSupplier.getIsoscelesTrapezoid();
+                break;
+        }
+        return figure;
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
+    }
+
     private Circle getRandomCircle() {
         randomColor = new ColorSupplier().getRandomColor();
         double randomRadius = random.nextInt(MAX_RANDOM_NUMBER);
@@ -44,35 +74,5 @@ public class FigureSupplier {
         double randomHeight = random.nextInt(MAX_RANDOM_NUMBER);
         return new IsoscelesTrapezoid(randomColor, randomFirstSideTr,
                 randomSecondSideTr, randomHeight);
-    }
-
-    public Figure getRandomFigure() {
-        FigureSupplier figureSupplier = new FigureSupplier();
-        int randomIndex = random.nextInt(FIGURES_COUNT);
-        switch (randomIndex) {
-            case 0:
-                figure = figureSupplier.getRandomCircle();
-                break;
-            case 1:
-                figure = figureSupplier.getRandomSquare();
-                break;
-            case 2:
-                figure = figureSupplier.getRandomRightTriangle();
-                break;
-            case 3:
-                figure = figureSupplier.getRectangle();
-                break;
-            case 4:
-                figure = figureSupplier.getIsoscelesTrapezoid();
-                break;
-            default:
-                figure = figureSupplier.getIsoscelesTrapezoid();
-                break;
-        }
-        return figure;
-    }
-
-    public Figure getDefaultFigure() {
-        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
