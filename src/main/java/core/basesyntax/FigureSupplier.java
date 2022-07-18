c6e4c38b682937a6d4f8f474a03bec494543f0ba
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-18 17:58:40 +0200

    const for random number created

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int MAX_RANDOM_NUMBER = 50;
     private static final int FIGURES_COUNT = 5;
     private static final String DEFAULT_COLOR = Color.WHITE.name();
     private static final int DEFAULT_RADIUS = 10;
@@ -12,35 +13,35 @@ public class FigureSupplier {
 
     private Circle getRandomCircle() {
         randomColor = new ColorSupplier().getRandomColor();
-        double randomRadius = random.nextInt(50);
+        double randomRadius = random.nextInt(MAX_RANDOM_NUMBER);
         return new Circle(randomColor, randomRadius);
     }
 
     private Square getRandomSquare() {
         randomColor = new ColorSupplier().getRandomColor();
-        double randomSide = random.nextInt(50);
+        double randomSide = random.nextInt(MAX_RANDOM_NUMBER);
         return new Square(randomColor, randomSide);
     }
 
     private RightTriangle getRandomRightTriangle() {
         randomColor = new ColorSupplier().getRandomColor();
-        double randomFirstLeg = random.nextInt(50);
-        double randomSecondLeg = random.nextInt(50);
+        double randomFirstLeg = random.nextInt(MAX_RANDOM_NUMBER);
+        double randomSecondLeg = random.nextInt(MAX_RANDOM_NUMBER);
         return new RightTriangle(randomColor, randomFirstLeg, randomSecondLeg);
     }
 
     private Rectangle getRectangle() {
         randomColor = new ColorSupplier().getRandomColor();
-        double randomFirstSide = random.nextInt(50);
-        double randomSecondSide = random.nextInt(50);
+        double randomFirstSide = random.nextInt(MAX_RANDOM_NUMBER);
+        double randomSecondSide = random.nextInt(MAX_RANDOM_NUMBER);
         return new Rectangle(randomColor, randomFirstSide, randomSecondSide);
     }
 
     private IsoscelesTrapezoid getIsoscelesTrapezoid() {
         randomColor = new ColorSupplier().getRandomColor();
-        double randomFirstSideTr = random.nextInt(50);
-        double randomSecondSideTr = random.nextInt(50);
-        double randomHeight = random.nextInt(50);
+        double randomFirstSideTr = random.nextInt(MAX_RANDOM_NUMBER);
+        double randomSecondSideTr = random.nextInt(MAX_RANDOM_NUMBER);
+        double randomHeight = random.nextInt(MAX_RANDOM_NUMBER);
         return new IsoscelesTrapezoid(randomColor, randomFirstSideTr,
                 randomSecondSideTr, randomHeight);
     }
