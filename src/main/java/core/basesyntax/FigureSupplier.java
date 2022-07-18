856104dde9d0458969ddeb5808f151b835734285
Serhii Davydov <serhii.davydov@gmail.com>
2022-07-18 15:08:56 +0300

    Remove redundant constants

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,16 +6,7 @@ public class FigureSupplier {
     private static final int AMOUNT_OF_FIGURES = 5;
     private static final int DEFAULT_CIRCLE_RADIUS = 10;
     private static final Color DEFAULT_COLOR = Color.WHITE;
-    private static final int MAX_CIRCLE_RADIUS = 20;
-    private static final int MAX_SQUARE_SIDE = 20;
-    private static final int MAX_RECTANGLE_WIDTH = 20;
-    private static final int MAX_RECTANGLE_HEIGHT = 10;
-    private static final int MAX_TRIANGLE_FIRST_LEG = 20;
-    private static final int MAX_TRIANGLE_SECOND_LEG = 10;
-    private static final int MAX_ISOSCELES_TRAPEZOID_HEIGHT = 10;
-    private static final int MAX_ISOSCELES_TRAPEZOID_BASE = 20;
-    private static final int MAX_ISOSCELES_TRAPEZOID_TOP = 20;
-
+    private static final int MAX_RANDOM_NUMBER = 20;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
@@ -37,27 +28,27 @@ public class FigureSupplier {
     }
 
     public Circle getRandomCircle() {
-        return new Circle(random.nextInt(MAX_CIRCLE_RADIUS), colorSupplier.getRandomColor());
+        return new Circle(random.nextInt(MAX_RANDOM_NUMBER), colorSupplier.getRandomColor());
     }
 
     public Square getRandomSquare() {
-        return new Square(random.nextInt(MAX_SQUARE_SIDE), colorSupplier.getRandomColor());
+        return new Square(random.nextInt(MAX_RANDOM_NUMBER), colorSupplier.getRandomColor());
     }
 
     public Rectangle getRandomRectangle() {
-        return new Rectangle(random.nextInt(MAX_RECTANGLE_WIDTH),
-                random.nextInt(MAX_RECTANGLE_HEIGHT), colorSupplier.getRandomColor());
+        return new Rectangle(random.nextInt(MAX_RANDOM_NUMBER),
+                random.nextInt(MAX_RANDOM_NUMBER), colorSupplier.getRandomColor());
     }
 
     public RightTriangle getRandomRightTriangle() {
-        return new RightTriangle(random.nextInt(MAX_TRIANGLE_FIRST_LEG),
-                random.nextInt(MAX_TRIANGLE_SECOND_LEG), colorSupplier.getRandomColor());
+        return new RightTriangle(random.nextInt(MAX_RANDOM_NUMBER),
+                random.nextInt(MAX_RANDOM_NUMBER), colorSupplier.getRandomColor());
     }
 
     public IsoscelesTrapezoid getRandomIsoscelesTrapezoid() {
-        return new IsoscelesTrapezoid(random.nextInt(MAX_ISOSCELES_TRAPEZOID_BASE),
-                random.nextInt(MAX_ISOSCELES_TRAPEZOID_TOP),
-                random.nextInt(MAX_ISOSCELES_TRAPEZOID_HEIGHT), colorSupplier.getRandomColor());
+        return new IsoscelesTrapezoid(random.nextInt(MAX_RANDOM_NUMBER),
+                random.nextInt(MAX_RANDOM_NUMBER),
+                random.nextInt(MAX_RANDOM_NUMBER), colorSupplier.getRandomColor());
     }
 
     public Figure getDefaultFigure() {
