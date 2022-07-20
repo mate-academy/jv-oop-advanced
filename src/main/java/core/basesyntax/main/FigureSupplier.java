86813e6a3125f19ae47f8c7285c7d7ba39200e97
Alexander <liero2D@gmail.com>
2022-07-20 20:04:50 +0300

    Added all fixes after review. I hope) Please, check again. Thanks.

--- src/main/java/core/basesyntax/main/FigureSupplier.java
+++ src/main/java/core/basesyntax/main/FigureSupplier.java
@@ -1,7 +1,6 @@
 package core.basesyntax.main;
 
 import core.basesyntax.figures.Circle;
-import core.basesyntax.figures.Color;
 import core.basesyntax.figures.Figure;
 import core.basesyntax.figures.IsoscelesTrapezoid;
 import core.basesyntax.figures.Rectangle;
@@ -10,44 +9,58 @@ import core.basesyntax.figures.Square;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int CIRCLE_RADIUS = 10;
+    private static final int FIGURE_RANDOM = 5;
+    private static final String CIRCLE_COLOR = "White";
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
-    private double getRandomDouble() {
-
-        return random.nextDouble();
-    }
-
     public Figure getRandomFigure() {
-        int indexRandomFigure = random.nextInt(5);
-
+        int indexRandomFigure = random.nextInt(FIGURE_RANDOM);
         switch (indexRandomFigure) {
             case 0:
                 return new Square(colorSupplier.getRandomColor(),
-                        getRandomDouble());
+                        getRandomSquare());
             case 1:
                 return new Circle(colorSupplier.getRandomColor(),
-                        getRandomDouble());
+                        getRandomCircle());
             case 2:
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        getRandomDouble(),
-                        getRandomDouble(),
-                        getRandomDouble());
+                        getRandomIsoscelesTrapezoid(),
+                        getRandomIsoscelesTrapezoid(),
+                        getRandomIsoscelesTrapezoid());
             case 3:
                 return new Rectangle(colorSupplier.getRandomColor(),
-                        getRandomDouble(),
-                        getRandomDouble());
+                        getRandomRectangle(),
+                        getRandomRectangle());
             default:
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        getRandomDouble(),
-                        getRandomDouble());
+                        getRandomRightTriangle(),
+                        getRandomRightTriangle());
         }
     }
 
-    public String getDefaultFigure() {
-        return ("Figure: " + Circle.class.getSimpleName()
-                + ", radius: " + 10 + ", color: " + Color.WHITE);
+    public Figure getDefaultFigure() {
+        return new Circle(CIRCLE_COLOR, CIRCLE_RADIUS);
+    }
+
+    private double getRandomRectangle() {
+        return random.nextDouble();
+    }
+
+    private double getRandomSquare() {
+        return random.nextDouble();
+    }
+
+    private double getRandomCircle() {
+        return random.nextDouble();
+    }
+
+    private double getRandomIsoscelesTrapezoid() {
+        return random.nextDouble();
+    }
+
+    private double getRandomRightTriangle() {
+        return random.nextDouble();
     }
 }
-
-
--- src/main/java/core/basesyntax/main/Main.java
+++ src/main/java/core/basesyntax/main/Main.java
@@ -1,22 +1,23 @@
 package core.basesyntax.main;
 
 import core.basesyntax.figures.Figure;
-import java.util.Random;
 
 /**
  * Feel free to remove this class and create your own.
  */
 public class Main {
-    private static final FigureSupplier figureSupplier = new FigureSupplier();
-    private static final Random random = new Random();
+    private static final int FIGURE_NUMBERS = 6;
 
     public static void main(String[] args) {
-        FigureSupplier figureSupplier1 = new FigureSupplier();
-        System.out.println(figureSupplier1.getDefaultFigure());
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[FIGURE_NUMBERS];
 
-        Figure[] figures = new Figure[random.nextInt(10)];
-        for (int i = 0; i < figures.length; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
+        for (int i = 0; i < FIGURE_NUMBERS; i++) {
+            if (i < FIGURE_NUMBERS / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
         }
 
         for (Figure figure : figures) {
