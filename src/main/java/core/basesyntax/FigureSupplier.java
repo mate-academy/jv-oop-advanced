688448cbcda6eaf744b22e03e6ddbb5b3a673641
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2022-07-20 19:13:33 +0300

    fixed solution (renamed constants and variables, edited block switch-sase)

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,23 +3,23 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int FIGURE_COUNT = 5;
+    private static final int FIGURES_COUNT = 5;
     private static final int MAX_RANDOM_NUMBER = 100;
     private static final int DEFAULT_RADIUS = 10;
-    private static final String DEFAULT_COLOR = "white";
+    private static final String DEFAULT_COLOR = Color.WHITE.name().toLowerCase();
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int figureNumber = random.nextInt(FIGURE_COUNT);
-        switch (figureNumber) {
-            case 1:
+        int randomIndex = random.nextInt(FIGURES_COUNT);
+        switch (randomIndex) {
+            case 0:
                 return getRandomSquare();
-            case 2:
+            case 1:
                 return getRandomRectangle();
-            case 3:
+            case 2:
                 return getRandomRightTriangle();
-            case 4:
+            case 3:
                 return getRandomCircle();
             default:
                 return getIsoscelesTrapezoid();
