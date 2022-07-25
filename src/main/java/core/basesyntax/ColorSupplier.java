90309a3fa922309ff6df6c4b44424bda16e0c916
Benatti <ssunica0@gmail.com>
2022-07-25 11:50:25 +0300

    fixed common mist

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     public static final int COLORS_COUNT = 6;
+
     public static Colors getRandomColor() {
         return Colors.values()[new Random().nextInt(COLORS_COUNT)];
     }
