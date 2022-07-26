332c37e538a7a0850d694c94d91771ea35518dc0
Mikhail Maiorov <majorov.michail@gmail.com>
2022-07-26 13:07:38 +0300

    class was refactored

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,23 +4,26 @@ import java.util.Random;
 
 public class FigureSupplier {
     public Figure getDefaultFigure() {
-        Circle circle = new Circle(10);
+        int defaultSideSizeValue = 10;
+        Circle circle = new Circle(defaultSideSizeValue);
         circle.setColor(Colors.WHITE.toString());
         return circle;
     }
 
     public Figure getRandomFigure() {
-        switch (new Random().nextInt(Figures.values().length)) {
+        switch (new Random().nextInt(Shape.values().length)) {
             case 1 : return new Square(getRandomNumber());
             case 2 : return new RightTriangle(getRandomNumber(), getRandomNumber());
             case 3 : return new Rectangle(getRandomNumber(), getRandomNumber());
             case 4 : return new IsoscelesTrapezoid(getRandomNumber(),
-                    getRandomNumber() + 1, getRandomNumber());
+                    getRandomNumber(), getRandomNumber());
             default: return new Circle(getRandomNumber());
         }
     }
 
     private int getRandomNumber() {
-        return new Random().nextInt(10) + 1;
+        int maxRandomSideSize = 20;
+        // maxRandomSideSize + 1 to avoid zero value
+        return new Random().nextInt(maxRandomSideSize) + 1;
     }
 }
