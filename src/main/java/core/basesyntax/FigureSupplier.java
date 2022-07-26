f0796a2e7e90eeb662e94506f860aa328536dd6f
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-07-26 13:58:20 +0300

    added solution to hw-jv-oop-advanced (fixed switch and naming)

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,29 +3,45 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int DefaultCircleRadius = 10;
-    private static final int LimitValue = 20;
-    private static final int AmountOfFigures = 5;
+    private static final int DEFAULT_CIRCLE_RADIUS = 10;
+    private static final int LIMIT_VALUE = 20;
+    private static final int AMOUNT_OF_FIGURES = 5;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int numberOfFigures = new Random().nextInt(AmountOfFigures);
-        return numberOfFigures == 1
-                ? new Circle(colorSupplier.getRandomColor(), random.nextInt(LimitValue))
-                : numberOfFigures == 2
-                ? new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextInt(LimitValue),
-                random.nextInt(LimitValue), random.nextInt(LimitValue))
-                : numberOfFigures == 3
-                ? new Rectangle(colorSupplier.getRandomColor(), random.nextInt(LimitValue),
-                random.nextInt(LimitValue))
-                : numberOfFigures == 4
-                ? new RightTriangle(colorSupplier.getRandomColor(), random.nextInt(LimitValue))
-                : new Square(colorSupplier.getRandomColor(), random.nextInt(LimitValue));
-
+        int numberOfFigures = random.nextInt(AMOUNT_OF_FIGURES);
+        Figure figure = null;
+        switch (numberOfFigures) {
+            case 0:
+                figure = new Circle(colorSupplier.getRandomColor(),
+                        random.nextInt(LIMIT_VALUE));
+                break;
+            case 1:
+                figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                        random.nextInt(LIMIT_VALUE),
+                        random.nextInt(LIMIT_VALUE),
+                        random.nextInt(LIMIT_VALUE));
+                break;
+            case 2:
+                figure = new Rectangle(colorSupplier.getRandomColor(),
+                        random.nextInt(LIMIT_VALUE),
+                        random.nextInt(LIMIT_VALUE));
+                break;
+            case 3:
+                figure = new RightTriangle(colorSupplier.getRandomColor(),
+                        random.nextInt(LIMIT_VALUE));
+                break;
+            case 4:
+                figure = new Square(colorSupplier.getRandomColor(), random.nextInt(LIMIT_VALUE));
+                break;
+            default:
+                break;
+        }
+        return figure;
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name().toLowerCase(), DefaultCircleRadius);
+        return new Circle(Color.WHITE.name().toLowerCase(), DEFAULT_CIRCLE_RADIUS);
     }
 }
