e2d3f055511df43fbbad61410ae9aed4ace3d738
X3NON-135 <immort4ld3momo@gmail.com>
2022-07-26 12:51:23 +0100

    some patch fixes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,12 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Figure[] figures = new Figure[FIGURE_COUNT];
-
-
-            for (int i = 0; i < figures.length; i++) {
-                figures[i] = figureSupplier.getRandomFigure();
-            }
-
+        for (int i = 0; i < figures.length; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
 
         for (Figure figure : figures) {
             figure.drawFigure();
