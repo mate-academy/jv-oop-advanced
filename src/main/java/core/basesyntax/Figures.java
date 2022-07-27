b032706007726815e93cd1036927d7952f516372
Benatti <ssunica0@gmail.com>
2022-07-27 13:19:36 +0300

    create new Drawable interface

--- src/main/java/core/basesyntax/Figures.java
+++ src/main/java/core/basesyntax/Figures.java
@@ -7,8 +7,6 @@ public abstract class Figures implements AreaCalculation, Drawable {
         this.color = color;
     }
 
-    public abstract String draw();
-
     public Color getColor() {
         return color;
     }
