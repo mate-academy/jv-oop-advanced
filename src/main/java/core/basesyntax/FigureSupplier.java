be1d8d718feadee48280cf9be1bb9e9c35ee8fc6
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2022-07-27 10:00:22 +0300

    Second commit

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,21 +7,21 @@ public class FigureSupplier {
 
     public Figure getRandomFigure() {
         int figure = random.nextInt(5);
-        int firstLag = random.nextInt(60);
-        int secondLag = random.nextInt(70);
-        int thirdLag = random.nextInt(80);
+        int firstParametr = random.nextInt(60) + 1;
+        int secondParametr = random.nextInt(70) + 1;
+        int thirdParametr = random.nextInt(80) + 1;
         String color = new ColorSupplier().getRandomColor();
         switch (figure) {
             case (1):
-                return new Circle(firstLag, color);
+                return new Circle(firstParametr, color);
             case (2):
-                return new IsoscelesTrapezoid(firstLag, secondLag, thirdLag, color);
+                return new IsoscelesTrapezoid(firstParametr, secondParametr, thirdParametr, color);
             case (3):
-                return new Rectangle(firstLag, secondLag,color);
+                return new Rectangle(firstParametr, secondParametr,color);
             case (4):
-                return new RightTriangle(firstLag, secondLag,color);
+                return new RightTriangle(firstParametr, secondParametr,color);
             default:
-                return new Square(firstLag, color);
+                return new Square(firstParametr, color);
         }
     }
 
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         Figure[] figures = new Figure[6];
+        FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < 6; i++) {
-            if (i > 2) figures[i] = new FigureSupplier().getDefaultFigure();
-            else figures[i]= new FigureSupplier().getRandomFigure();
+            if (i > 2) figures[i] =figureSupplier.getDefaultFigure();
+            else figures[i]= figureSupplier.getRandomFigure();
             System.out.println(figures[i]);
         }
     }
