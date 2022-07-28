a1cbbe94229cb4b23ba7956682652cee482a0d7a
Vasyl Pankiv <wasilius2@gmail.com>
2022-07-28 15:44:27 +0300

    add FigureType enum and FigureSupplier. Implement method getRandomFigure in FigureSupplier

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,38 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    public static final int MAX_RANDOM_VALUE = 50;
+    public static final int FIGURE_COUNT = FigureType.values().length;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        int randFigureIndex = random.nextInt(FIGURE_COUNT);
+        switch (FigureType.values()[randFigureIndex]) {
+            case CIRCLE:
+                return new Circle(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_RANDOM_VALUE));
+            case SQUARE:
+                return new Square(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_RANDOM_VALUE));
+            case RECTANGLE:
+                return new Rectangle(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_RANDOM_VALUE),
+                        random.nextInt(MAX_RANDOM_VALUE));
+            case RIGHT_TRIANGLE:
+                return new RightTriangle(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_RANDOM_VALUE),
+                        random.nextInt(MAX_RANDOM_VALUE));
+            default:
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_RANDOM_VALUE),
+                        random.nextInt(MAX_RANDOM_VALUE),
+                        random.nextInt(MAX_RANDOM_VALUE));
+        }
+    }
+
+
+}
+
