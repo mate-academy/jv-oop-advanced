cc63c0ad73a6c7511b0cbf3f2b718de1cfdb6303
Nazar <litvik.12@gmail.com>
2022-07-28 11:36:36 +0300

    test

--- src/main/java/auxiliary/ColorSupplier.java
+++ src/main/java/auxiliary/ColorSupplier.java
@@ -5,11 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     private String color;
 
-    public void setRandomColor() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        this.color = colorSupplier.getRandomColor();
-    }
-
     public String getRandomColor() {
         Random random = new Random();
         return Color.values()[random.nextInt(Color.values().length)].name();
