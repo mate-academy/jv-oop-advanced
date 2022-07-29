ccf788d397b7a1a23367695b23cbcc8c32a90a7c
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-30 00:52:19 +0300

    implementation main method in Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,26 @@
 package core.basesyntax;
 
+import core.basesyntax.model.Drawable;
+import core.basesyntax.model.Figure;
+import core.basesyntax.service.FigureSupplier;
+
 public class Main {
+    public static final int NUMBER_FIGURES = 12;
+    public static final int MAXIMUM_LENGTH = 100;
+
     public static void main(String[] args) {
+        Figure[] figures = new Figure[NUMBER_FIGURES];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < figures.length; i++) {
+            if (i % 2 == 0) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
+
+        for (Drawable figure : figures) {
+            figure.draw();
+        }
     }
 }
