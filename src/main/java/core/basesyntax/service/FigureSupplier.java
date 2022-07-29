99cf9e34c8c84a5f98ef3b6e6caee71425b4f48d
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-30 00:49:37 +0300

    implementation FigureSupplier

--- src/main/java/core/basesyntax/service/FigureSupplier.java
+++ src/main/java/core/basesyntax/service/FigureSupplier.java
@@ -1,23 +1,58 @@
 package core.basesyntax.service;
 
+import static core.basesyntax.Main.MAXIMUM_LENGTH;
+
 import core.basesyntax.model.Circle;
 import core.basesyntax.model.Color;
 import core.basesyntax.model.Figure;
+import core.basesyntax.model.FigureType;
+import core.basesyntax.model.IsoscelesTrapezoid;
+import core.basesyntax.model.Rectangle;
+import core.basesyntax.model.RightTriangle;
+import core.basesyntax.model.Square;
 import java.util.Random;
 
 public class FigureSupplier {
-    public static final int MAXIMUM_RADIUS = 100;
-    private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
-
     public Figure getRandomFigure() {
-        /** generate a specific figure based on the `figureNumber` value*/
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
         Figure figure = null;
+        int numberFigureTypes = FigureType.values().length;
+        int randomFigureTypeIndex = random.nextInt(numberFigureTypes);
+        FigureType randomFigureType = FigureType.values()[randomFigureTypeIndex];
+        Color randomColor = colorSupplier.getRandomColor();
+        switch (randomFigureType) {
+            case CIRCLE:
+                figure = new Circle(randomColor,
+                        random.nextInt(MAXIMUM_LENGTH));
+                break;
+            case ISOSCELES_TRAPEZOID:
+                figure = new IsoscelesTrapezoid(randomColor,
+                        random.nextInt(MAXIMUM_LENGTH),
+                        random.nextInt(MAXIMUM_LENGTH),
+                        random.nextInt(MAXIMUM_LENGTH));
+                break;
+            case RECTANGLE:
+                figure = new Rectangle(randomColor,
+                        random.nextInt(MAXIMUM_LENGTH),
+                        random.nextInt(MAXIMUM_LENGTH));
+                break;
+            case RIGHT_TRIANGLE:
+                figure = new RightTriangle(randomColor,
+                        random.nextInt(MAXIMUM_LENGTH),
+                        random.nextInt(MAXIMUM_LENGTH));
+                break;
+            case SQUARE:
+                figure = new Square(randomColor,
+                        random.nextInt(MAXIMUM_LENGTH));
+                break;
+            default:
+                break;
+        }
         return figure;
     }
 
     public Figure getDefaultFigure() {
-        //return white circle with radius 10
         return new Circle(Color.WHITE, 10);
     }
 }
