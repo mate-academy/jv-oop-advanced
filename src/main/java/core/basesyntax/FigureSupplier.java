bf357e4f41f3e504b79c3b7d1465c6041da11655
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-07-29 10:56:35 +0300

    add constant DEFAULT_RADIUS

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int AMOUNT_OF_FIGURE = 5;
     private static final double MAX_PARAMETER = 10.0;
+    private static final double DEFAULT_RADIUS = 10.0;
 
     public Figure getRandomFigure() {
         switch (new Random().nextInt(AMOUNT_OF_FIGURE)) {
@@ -35,6 +36,6 @@ public class FigureSupplier {
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Colors.WHITE, 10);
+        return new Circle(Colors.WHITE, DEFAULT_RADIUS);
     }
 }
