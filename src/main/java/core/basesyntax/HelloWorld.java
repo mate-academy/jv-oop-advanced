04027d2b83a8cd5218f2248439669fc0dc952dc0
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-07-29 08:43:21 +0300

    added solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,5 +4,21 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[6];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        ColorSupplier colorSupplier = new ColorSupplier();
+        for (int i = 0; i < 6; i++) {
+            if (i < 3) {
+                figures[i] = figureSupplier.getRandomFigure();
+                figures[i].setColor(colorSupplier.getRandomColor());
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
+        for (Figure figure: figures) {
+            figure.getInfo();
+        }
+    }
 
 }
