0c1afca8ffeae482616d8d35d87bec107046b699
IraHurko <iragurko19@gmail.com>
2022-07-29 17:44:19 +0300

    add Class Rectangle

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private int width;
+    private int height;
+
+    public Rectangle(String color, int width, int height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: circle, area: " + getArea()
+                + " sq.units, radius: " + width + " units, " + height + " units, color: " + color);
+    }
+}
