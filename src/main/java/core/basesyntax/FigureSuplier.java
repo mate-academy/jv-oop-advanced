aeb47ea8710ef23e09e61fe500a3af1f522a0e93
Misha Brovkin <haasiakira@gmail.com>
2022-07-29 17:04:20 +0300

    fixed all checkstyle isseus

--- src/main/java/core/basesyntax/FigureSuplier.java
+++ src/main/java/core/basesyntax/FigureSuplier.java
@@ -6,26 +6,26 @@ public class FigureSuplier {
     private final ColorSuplier color = new ColorSuplier();
 
     public Figure getRandomFigure() {
-        switch (new Random().nextInt(4)) {
+        switch (new Random().nextInt(5)) {
             case 0:
                 return new Circle(color.getRandomColor(),
-                        new Random().nextDouble(20));
+                        new Random().nextInt(20));
             case 1:
-                return new Square(new Random().nextDouble(20),
+                return new Square(new Random().nextInt(20),
                         color.getRandomColor());
             case 2:
-                return new Rectangle(new Random().nextDouble(20),
-                        new Random().nextDouble(20),
+                return new Rectangle(new Random().nextInt(20),
+                        new Random().nextInt(20),
                         color.getRandomColor());
             case 3:
                 return new RightTriangle(color.getRandomColor(),
-                        new Random().nextDouble(20),
-                        new Random().nextDouble(20));
+                        new Random().nextInt(20),
+                        new Random().nextInt(20));
             default:
                 return new IsoscelesTrapezoid(color.getRandomColor(),
-                        new Random().nextDouble(20),
-                        new Random().nextDouble(20),
-                        new Random().nextDouble(20));
+                        new Random().nextInt(20),
+                        new Random().nextInt(20),
+                        new Random().nextInt(20));
         }
     }
 
