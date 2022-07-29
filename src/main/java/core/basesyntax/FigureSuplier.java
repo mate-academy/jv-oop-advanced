1da3c2f9a7298fe8a528984bcc34b380c5ae501f
Misha Brovkin <haasiakira@gmail.com>
2022-07-29 17:12:06 +0300

    fixed all checkstyle issues

--- src/main/java/core/basesyntax/FigureSuplier.java
+++ src/main/java/core/basesyntax/FigureSuplier.java
@@ -5,28 +5,29 @@ import java.util.Random;
 public class FigureSuplier {
     private final ColorSuplier color = new ColorSuplier();
     private static final int MAX_VALUE = 5;
+    private static final int SIZE_LIMIT = 20;
 
     public Figure getRandomFigure() {
         switch (new Random().nextInt(MAX_VALUE)) {
             case 0:
                 return new Circle(color.getRandomColor(),
-                        new Random().nextInt(20));
+                        new Random().nextInt(SIZE_LIMIT));
             case 1:
-                return new Square(new Random().nextInt(20),
+                return new Square(new Random().nextInt(SIZE_LIMIT),
                         color.getRandomColor());
             case 2:
-                return new Rectangle(new Random().nextInt(20),
-                        new Random().nextInt(20),
+                return new Rectangle(new Random().nextInt(SIZE_LIMIT),
+                        new Random().nextInt(SIZE_LIMIT),
                         color.getRandomColor());
             case 3:
                 return new RightTriangle(color.getRandomColor(),
-                        new Random().nextInt(20),
-                        new Random().nextInt(20));
+                        new Random().nextInt(SIZE_LIMIT),
+                        new Random().nextInt(SIZE_LIMIT));
             default:
                 return new IsoscelesTrapezoid(color.getRandomColor(),
-                        new Random().nextInt(20),
-                        new Random().nextInt(20),
-                        new Random().nextInt(20));
+                        new Random().nextInt(SIZE_LIMIT),
+                        new Random().nextInt(SIZE_LIMIT),
+                        new Random().nextInt(SIZE_LIMIT));
         }
     }
 
