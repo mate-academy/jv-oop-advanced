4758f0407b5ced6959b6d5c791ef9f5b74cc206b
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-30 00:28:38 +0300

    add class Rectangle

---
+++ src/main/java/core/basesyntax/model/Rectangle.java
@@ -0,0 +1,49 @@
+package core.basesyntax.model;
+
+public class Rectangle extends Figure {
+    private int firstLeg;
+    private int secondLeg;
+
+    public int getFirstLeg() {
+        return firstLeg;
+    }
+
+    public void setFirstLeg(int firstLeg) {
+        this.firstLeg = firstLeg;
+    }
+
+    public int getSecondLeg() {
+        return secondLeg;
+    }
+
+    public void setSecondLeg(int secondLeg) {
+        this.secondLeg = secondLeg;
+    }
+
+    public Rectangle() {
+    }
+
+    public Rectangle(Color color) {
+        super(color);
+    }
+
+    public Rectangle(Color color, int firstLeg, int secondLeg) {
+        this(color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double calculateArea() {
+        return firstLeg * secondLeg;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: "
+                + calculateArea() + " sq.units, firstLeg = "
+                + getFirstLeg() + " units, secondLeg = "
+                + getSecondLeg() + " units, color: "
+                + getColor().toString().toLowerCase());
+    }
+}
