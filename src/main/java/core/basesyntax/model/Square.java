f2e79076c8bca242d5bdfa497936f2f523dd0fa1
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-30 00:18:41 +0300

    add class Square

---
+++ src/main/java/core/basesyntax/model/Square.java
@@ -0,0 +1,36 @@
+package core.basesyntax.model;
+
+public class Square extends Figure {
+    private int leg;
+
+    public int getLeg() {
+        return leg;
+    }
+
+    public void setLeg(int leg) {
+        this.leg = leg;
+    }
+
+    public Square() {
+    }
+
+    public Square(Color color) {
+        super(color);
+    }
+
+    public Square(Color color, int leg) {
+        this(color);
+        this.leg = leg;
+    }
+
+    @Override
+    public double calculateArea() {
+        return leg * leg;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: square, area: " + calculateArea() + " sq.units, leg = "
+                + getLeg() + " units, color: " + getColor().toString().toLowerCase());
+    }
+}
