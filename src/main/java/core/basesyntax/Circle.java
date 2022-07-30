998778e3f78f0c5aedadbfc9f6b7c6bc78aa19fc
Aptypio13 <aptypio668@gmail.com>
2022-07-30 16:08:30 +0300

    Create Class Circle.java with parms radius and color Circle extends Figure and have constructor

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private final double radius;
+    private final Color color;
+
+    public Circle(double radius, Color color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String getFigureInfo() {
+        return "Figure: circle Area: " + getArea() + " sq.units"
+                + " Radius: " + radius + " units"
+                + " Color: " + color + "\n";
+    }
+
+}
