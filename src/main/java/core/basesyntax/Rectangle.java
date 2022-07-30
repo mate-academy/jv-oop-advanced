732199db40694324a30fb9de9b5534b339229938
Aptypio13 <aptypio668@gmail.com>
2022-07-30 16:09:57 +0300

    Create Class Rectangle.java with parms length,width,color Circle extends Figure and have constructor

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+
+    private final double length;
+    private final double width;
+    private final Color color;
+
+    public Rectangle(double length, double width, Color color) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public String getFigureInfo() {
+        return "Figure: rectangle Area: " + getArea() + " sq.units"
+                + " Length: " + length
+                + " Width: " + width
+                + " Color " + color + "\n";
+
+    }
+}
