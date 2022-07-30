ebfa6bb397b2ff8e029854771aa9b5d518d8041a
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2022-07-30 21:19:53 +0300

    delete implements from figures class

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class IsoscelesTrapezoid extends Figure implements AreaCalculator, Drawable {
+public class IsoscelesTrapezoid extends Figure {
     private final int firstSide;
     private final int secondSide;
     private final int height;
