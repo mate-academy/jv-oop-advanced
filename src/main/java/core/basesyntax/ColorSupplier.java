19397276c073fe771e436b2499cda8565c1d61d1
UmyaFamilia <umyafamilya1974@gmail.com>
2022-07-31 18:14:45 +0300

    create interfases and enum

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,4 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final int INDEX_COLOR=9;
+    public String getRandomColor(){
+        Random random=new Random();
+        int index=random.nextInt(INDEX_COLOR);
+        String result=Color.values()[index].toString();
+        return result;
+    }
+
 }
