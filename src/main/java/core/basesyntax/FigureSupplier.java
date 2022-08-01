9f9ce5936337ffce3fc1cbf1910d94044cd6c830
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-08-01 14:45:31 +0300

    rename constant Ð to AMOUNT_OF_FIGURES

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int AMOUNT_OF_FIGURE = 5;
+    private static final int AMOUNT_OF_FIGURES = 5;
     private static final double MAX_PARAMETER = 10.0;
     private static final double DEFAULT_RADIUS = 10.0;
 
     public Figure getRandomFigure() {
-        switch (new Random().nextInt(AMOUNT_OF_FIGURE)) {
+        switch (new Random().nextInt(AMOUNT_OF_FIGURES)) {
             case 0:
                 return new Square(Color.values()[new Random().nextInt(Color.values().length)],
                     new Random().nextDouble() * MAX_PARAMETER);
