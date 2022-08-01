f3b4bc62115d41a359d6073a7783c55ccdb15190
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-08-01 12:39:34 +0300

    constants are renamed

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class HelloWorld {
-    private static final int MAXVALUE = 6;
-    private static final int MINVALUE = 3;
+    private static final int MAX_VALUE = 6;
+    private static final int MIN_VALUE = 3;
 
     public static void main(String[] args) {
-        Figure [] figures = new Figure[MAXVALUE];
+        Figure [] figures = new Figure[MAX_VALUE];
         for (int i = 0; i < figures.length; i++) {
-            if (i <= MINVALUE) {
+            if (i <= MIN_VALUE) {
                 figures[i] = new FigureSupplier().getRandomFigure();
                 figures[i].draw();
             } else {
