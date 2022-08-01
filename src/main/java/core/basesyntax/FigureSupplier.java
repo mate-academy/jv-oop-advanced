c41987c1ace4685d657509c15b3078841f581838
UmyaFamilia <umyafamilya1974@gmail.com>
2022-08-01 17:35:21 +0300

    this progtam fourth try

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,25 +6,26 @@ public class FigureSupplier {
     private static final int MAX_PROPERTY_VALUE = 100;
     private static final int NUMBER_OF_FIGURES = 5;
     private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int figureNumber = random.nextInt(NUMBER_OF_FIGURES);
         switch (figureNumber) {
             case 1:
-                return new IsoscelesTrapezoid(new ColorSupplier().getRandomColor(),
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_PROPERTY_VALUE),
                         random.nextInt(MAX_PROPERTY_VALUE),
                         random.nextInt(MAX_PROPERTY_VALUE));
             case 2:
-                return new RightTriangle(new ColorSupplier().getRandomColor(),
+                return new RightTriangle(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_PROPERTY_VALUE),
                         random.nextInt(MAX_PROPERTY_VALUE));
             case 3:
-                return new Rectangle(new ColorSupplier().getRandomColor(),
+                return new Rectangle(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_PROPERTY_VALUE),
                         random.nextInt(MAX_PROPERTY_VALUE));
             case 4:
-                return new Square(new ColorSupplier().getRandomColor(),
+                return new Square(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_PROPERTY_VALUE));
             default:
                 return getDefaultFigure();
