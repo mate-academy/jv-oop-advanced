3b3d55a35175f9222e2fe2f0f75abd6dd8aef9ef
Aptypio13 <aptypio668@gmail.com>
2022-08-01 14:54:06 +0300

    Generate a random values and random colors out of method,

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,33 +4,35 @@ import java.util.Random;
 
 public class FigureSupplier {
 
-    private static final double DEFAULT_VALUE = 3.0;
+    Random random = new Random();
+    ColorSupplier colorSupplier = new ColorSupplier();
+    int index = random.nextInt(FigureNames.values().length);
+    double randomValue = random.nextDouble(100.0);
 
     public Figure getRandomFigure() {
-        int index = new Random().nextInt(FigureNames.values().length);
         switch (FigureNames.values()[index]) {
             case CIRCLE:
                 return new Circle(
-                        DEFAULT_VALUE, new ColorSupplier().getRandomColor());
+                        randomValue, colorSupplier.getRandomColor());
             case ISOSCELES_TRAPEZOID:
                 return new IsoscelesTrapezoid(
-                        DEFAULT_VALUE,
-                        DEFAULT_VALUE,
-                        DEFAULT_VALUE, new ColorSupplier().getRandomColor());
+                        randomValue,
+                        randomValue,
+                        randomValue, colorSupplier.getRandomColor());
             case SQUARE:
                 return new Square(
-                        DEFAULT_VALUE, new ColorSupplier().getRandomColor());
+                        randomValue, colorSupplier.getRandomColor());
             case RECTANGLE:
                 return new Rectangle(
-                        DEFAULT_VALUE, DEFAULT_VALUE, new ColorSupplier().getRandomColor());
+                        randomValue, randomValue, colorSupplier.getRandomColor());
             case RIGHT_TRIANGLE:
                 return new RightTriangle(
-                        DEFAULT_VALUE, new ColorSupplier().getRandomColor());
+                        randomValue, colorSupplier.getRandomColor());
             default: return getDefaultFigure();
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(DEFAULT_VALUE, Color.WHITE);
+        return new Circle(randomValue, Color.WHITE);
     }
 }
