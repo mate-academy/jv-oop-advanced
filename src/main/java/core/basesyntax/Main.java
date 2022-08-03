f3ca22832fe53adebcae32d5c93c8966e847bb97
Andriii Shevchuk <zovten156@gmail.com>
2022-08-03 20:26:11 +0300

    Update src/main/java/core/basesyntax/Main.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
 
-        Figures defaultFigure = figureSupplier.getDefaultFigure();
 
         Figures[] figureArray = new Figures[6];
         for (int i = 0; i < figureArray.length; i++) {
