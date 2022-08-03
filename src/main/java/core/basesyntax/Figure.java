114e392b9d37504f07766afd98756651d96625e7
victoriadobryden <vikidobryden@gmail.com>
2022-08-03 20:50:28 +0300

    refactored code

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
-public abstract class Figures implements Behavior {
+public abstract class Figures implements Drawable, AreaInfo {
     public static final int MAX_SIDE = 100;
     protected String color;
 
     protected Figures(String color) {
         this.color = color;
     }
-
-    public abstract double getArea();
 }
