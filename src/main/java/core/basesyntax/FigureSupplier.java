b14a773ad6809504c02a90297957cde0ee580ccd
Oleksandr Fedchik <fed4iksasha@gmail.com>
2022-08-15 22:30:32 +0300

    rewrote "switch-case" via methods

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,45 +3,56 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-
     public static final int numberOfFigures = 4; // start count from zero
     public static final int maxValuesOfUnits = 10;
     public static final int minValuesOfUnits = 1;
     private Random random = new Random();
 
     public Figure getRandomFigure() {
-        Figure figure;
         switch (random.nextInt(numberOfFigures)) {
             case (0):
-                figure = new Square((random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        new ColorSupplier().getRandomColor());
-                break;
+                return getSquare();
             case (1):
-                figure = new Rectangle((random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        new ColorSupplier().getRandomColor());
-                break;
+                return getRectangle();
             case (2):
-                figure = new RightTriangle(
-                        (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        new ColorSupplier().getRandomColor());
-                break;
+                return getRightTriangle();
             case (3):
-                figure = new Circle((random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        new ColorSupplier().getRandomColor());
-                break;
+                return getCircle();
             case (4):
-                figure = new IsoscelesTrapezoid(
-                        (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
-                        new ColorSupplier().getRandomColor());
-                break;
+                return getIsoscelesTrapezoid();
             default:
-                figure = new Circle(10, "WHITE");
-                break;
+                return getDefaultFigure();
         }
-        return figure;
+    }
+
+    public Figure getSquare() {
+        return new Square((random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                new ColorSupplier().getRandomColor());
+    }
+
+    public Figure getRectangle() {
+        return new Rectangle((random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                new ColorSupplier().getRandomColor());
+    }
+
+    public Figure getRightTriangle() {
+        return new RightTriangle(
+                (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                new ColorSupplier().getRandomColor());
+    }
+
+    public Figure getCircle() {
+        return new Circle((random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                new ColorSupplier().getRandomColor());
+    }
+
+    public Figure getIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(
+                (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                (random.nextInt(maxValuesOfUnits - minValuesOfUnits) + 1),
+                new ColorSupplier().getRandomColor());
     }
 
     public Figure getDefaultFigure() {
