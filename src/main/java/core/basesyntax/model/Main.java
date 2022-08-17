78cf25a80738a6952acb6c7b51828e1b8738b210
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-18 00:28:00 +0300

    Random figure method is used

---
+++ src/main/java/core/basesyntax/model/Main.java
@@ -0,0 +1,24 @@
+package core.basesyntax.model;
+
+import core.basesyntax.model.Figure;
+import core.basesyntax.suppliers.FigureSupplier;
+
+import java.util.Random;
+
+public class Main {
+    private static FigureSupplier figureSupplier = new FigureSupplier();
+
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[new Random().nextInt(10)];
+
+        for (int i = 0; i < figures.length; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
+}
+
+
