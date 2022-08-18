4af70011f6934f3df98e80a3805019e0ee29004d
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-08-18 20:59:22 +0300

    Update Main.java

--- src/main/java/core/basesyntax/model/Main.java
+++ src/main/java/core/basesyntax/model/Main.java
@@ -4,12 +4,12 @@ import core.basesyntax.suppliers.FigureSupplier;
 import java.util.Random;
 
 public class Main {
-    private static FigureSupplier figureSupplier = new FigureSupplier();
+    private static FigureSupplier FIGURE_SUPPLIER = new FigureSupplier();
 
     public static void main(String[] args) {
         Figure[] figures = new Figure[new Random().nextInt(10)];
         for (int i = 0; i < figures.length; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
+            figures[i] = FIGURE_SUPPLIER.getRandomFigure();
         }
         for (Figure figure : figures) {
             figure.draw();
