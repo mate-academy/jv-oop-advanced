ab0f8aa3fec61b4a50c716434accad5007fb9f1e
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-08-18 20:10:49 +0300

    Update Main.java

--- src/main/java/core/basesyntax/model/Main.java
+++ src/main/java/core/basesyntax/model/Main.java
@@ -8,11 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         Figure[] figures = new Figure[new Random().nextInt(10)];
-
         for (int i = 0; i < figures.length; i++) {
             figures[i] = figureSupplier.getRandomFigure();
         }
-
         for (Figure figure : figures) {
             figure.draw();
         }
