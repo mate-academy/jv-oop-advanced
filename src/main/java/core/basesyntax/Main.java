fdc6687afbeda92429048a170c42ba3370464727
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-08-20 22:19:12 +0300

    Create Main.java

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+import core.basesyntax.model.Figure;
+import core.basesyntax.suppliers.FigureSupplier;
+import java.util.Random;
+
+public class Main {
+    private static FigureSupplier FIGURE_SUPPLIER = new FigureSupplier();
+
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[new Random().nextInt(10)];
+        for (int i = 0; i < figures.length / 2; i++) {
+            figures[i] = FIGURE_SUPPLIER.getRandomFigure();
+            figures[i].draw();
+        }
+        for (int i = 0; i < figures.length / 2; i++) {
+            figures[i] = FIGURE_SUPPLIER.getDefaultFigure();
+            figures[i].draw();
+        }
+    }
+}
