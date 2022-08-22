f321f78607c5defe7228a6df5c498c0657976d24
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-08-22 17:24:48 +0300

    Update FigureSupplier.java

--- src/main/java/core/basesyntax/suppliers/FigureSupplier.java
+++ src/main/java/core/basesyntax/suppliers/FigureSupplier.java
@@ -1,7 +1,6 @@
 package core.basesyntax.suppliers;
 
 import core.basesyntax.model.Circle;
-import core.basesyntax.model.Color;
 import core.basesyntax.model.Figure;
 import core.basesyntax.model.IsoscelesTrapezoid;
 import core.basesyntax.model.Rectangle;
@@ -13,7 +12,7 @@ public class FigureSupplier {
     private static final int FIGURES_NUMBER = 5;
     private static final int MAX_RANDOM_NUMBER = 100;
     private static final int DEFAULT_RADIUS = 10;
-    private static final String DEFAULT_COLOR = Color.WHITE;    
+    private static final String DEFAULT_COLOR = "WHITE";    
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
     
