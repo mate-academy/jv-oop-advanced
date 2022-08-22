8518f65ad3287a6037940006fee303d6f780237e
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-08-22 13:25:20 +0300

    Update Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,16 +5,17 @@ import core.basesyntax.suppliers.FigureSupplier;
 import java.util.Random;
 
 public class Main {
-    private static FigureSupplier FIGURE_SUPPLIER = new FigureSupplier();
-
     public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[new Random().nextInt(10)];
-        for (int i = 0; i < figures.length / 2; i++) {
-            figures[i] = FIGURE_SUPPLIER.getRandomFigure();
-            figures[i].draw();
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+            figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
         }
-        for (int i = 0; i < figures.length / 2; i++) {
-            figures[i] = FIGURE_SUPPLIER.getDefaultFigure();
+        for (int i = 0; i < figures.length; i++) {
             figures[i].draw();
         }
     }
