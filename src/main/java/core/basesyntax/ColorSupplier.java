5e8662f568e5c53517a4447fa756e9a2d0e9022a
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-08-24 17:34:54 +0300

    Made edits at random

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random random = new Random();
     public static String getRandomColor() {
-        Random random = new Random();
         int number = random.nextInt(Colors.values().length);
         return Colors.values()[number].name();
     }
