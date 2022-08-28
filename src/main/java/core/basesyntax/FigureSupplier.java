abaf28285efe9d65812dfb6e8b587edb82d72c36
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-08-28 18:35:33 +0300

    Change random figure creation methods

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,74 +6,68 @@ public class FigureSupplier {
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
+
     public Figure getRandomFigure() {
+
         int index = this.random.nextInt(FigureTypes.values().length);
         FigureTypes figureType = FigureTypes.values()[index];
-        String color = this.colorSupplier.getRandomColor();
 
         switch (figureType) {
             case CIRCLE:
-                return this.createCircle(
-                        color,
-                        this.random.nextDouble()
-                );
+                return this.createRandomCircle();
             case ISOSCELES_TRAPEZOID:
-                return this.createIsoscelesTrapezoid(
-                        color,
-                        this.random.nextDouble(),
-                        this.random.nextDouble(),
-                        this.random.nextDouble()
-                );
+                return this.createRandomIsoscelesTrapezoid();
             case RECTANGLE:
-                return this.createRectangle(
-                        color,
-                        this.random.nextDouble(),
-                        this.random.nextDouble()
-                );
+                return this.createRandomRectangle();
             case RIGHT_TRIANGLE:
-                return this.createRightTriangle(
-                        color,
-                        this.random.nextDouble(),
-                        this.random.nextDouble()
-                );
+                return this.createRandomRightTriangle();
             case SQUARE:
-                return this.createSquare(
-                        color,
-                        this.random.nextDouble()
-                );
+                return this.createRandomSquare();
             default:
                 return null;
         }
     }
 
     public Figure getDefaultFigure() {
-        return this.createCircle("WHITE", 10);
+        return new Circle("WHITE", 10);
     }
 
-    private Circle createCircle(final String color, final double radius) {
-        return new Circle(color, radius);
+    private Circle createRandomCircle() {
+        return new Circle(
+                this.colorSupplier.getRandomColor(),
+                this.random.nextDouble()
+        );
     }
 
-    private IsoscelesTrapezoid createIsoscelesTrapezoid(final String color,
-                                                        final double upperBasis,
-                                                        final double lowerBasis,
-                                                        final double side) {
-        return new IsoscelesTrapezoid(color, upperBasis, lowerBasis, side);
+    private IsoscelesTrapezoid createRandomIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(
+                this.colorSupplier.getRandomColor(),
+                this.random.nextDouble(),
+                this.random.nextDouble(),
+                this.random.nextDouble()
+        );
     }
 
-    private Rectangle createRectangle(final String color,
-                                      final double firstSide,
-                                      final double secondSide) {
-        return new Rectangle(color, firstSide, secondSide);
+    private Rectangle createRandomRectangle() {
+        return new Rectangle(
+                this.colorSupplier.getRandomColor(),
+                this.random.nextDouble(),
+                this.random.nextDouble()
+        );
     }
 
-    private RightTriangle createRightTriangle(final String color,
-                                              final double firstLeg,
-                                              final double secondLeg) {
-        return new RightTriangle(color, firstLeg, secondLeg);
+    private RightTriangle createRandomRightTriangle() {
+        return new RightTriangle(
+                this.colorSupplier.getRandomColor(),
+                this.random.nextDouble(),
+                this.random.nextDouble()
+        );
     }
 
-    private Square createSquare(final String color, final double side) {
-        return new Square(color, side);
+    private Square createRandomSquare() {
+        return new Square(
+                this.colorSupplier.getRandomColor(),
+                this.random.nextDouble()
+        );
     }
 }
