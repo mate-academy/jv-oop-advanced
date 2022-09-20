26e6558814484b917b9dc7aa18a8ce978104c69e
SomniumDZ <mshveda28@gmail.com>
2022-09-20 11:15:01 +0300

    Create Square class

---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class Square extends Figure {
+    private double side;
+
+    public Square(String color, double side) {
+        super(color);
+        this.side = side;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: square; Side: %f, Area: %f; Color: %s\n", side, getArea(), getColor());
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+}
