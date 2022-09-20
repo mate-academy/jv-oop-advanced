98af72c074293b4aeecb821add970a9af5b57239
SomniumDZ <mshveda28@gmail.com>
2022-09-20 12:05:34 +0300

    Create FigureSupplier

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,43 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    private static final double MAX_PARAMETER = 30d;
+
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    private enum Figures {
+        SQUARE, RECTANGLE, RIGHT_TRIANGLE, CIRCLE, ISOSCELES_TRAPEZOID
+    }
+
+    public Figure getRandomFigure() {
+        var figureIndex = random.nextInt(Figures.values().length);
+        switch (Figures.values()[figureIndex]) {
+            case SQUARE:
+                return new Square(colorSupplier.getRandomColor(), random.nextDouble(MAX_PARAMETER));
+            case RECTANGLE:
+                return new Rectangle(
+                        colorSupplier.getRandomColor(),
+                        random.nextDouble(MAX_PARAMETER), random.nextDouble(MAX_PARAMETER)
+                );
+            case RIGHT_TRIANGLE:
+                return new RightTriangle(
+                        colorSupplier.getRandomColor(),
+                        random.nextDouble(MAX_PARAMETER),
+                        random.nextDouble(MAX_PARAMETER)
+                );
+            case CIRCLE:
+                return new Circle(colorSupplier.getRandomColor(), random.nextDouble(MAX_PARAMETER));
+            case ISOSCELES_TRAPEZOID:
+                return new IsoscelesTrapezoid(
+                        colorSupplier.getRandomColor(),
+                        random.nextDouble(MAX_PARAMETER), random.nextDouble(MAX_PARAMETER),
+                        random.nextDouble(MAX_PARAMETER)
+                );
+            default:
+                throw new IllegalStateException("Unexpected value: " + Figures.values()[figureIndex]);
+        }
+    }
+}
