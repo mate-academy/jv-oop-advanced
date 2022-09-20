44a14316617d66f1aafa1cfa050b10f738af5ecd
SomniumDZ <mshveda28@gmail.com>
2022-09-20 11:19:51 +0300

    Create Rectangle figure class

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private final double width;
+    private final double height;
+
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf(
+                "Figure: square; Width: %f, Height:%f, Area: %f; Color: %s\n",
+                width, height, getArea(), getColor()
+        );
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+}
