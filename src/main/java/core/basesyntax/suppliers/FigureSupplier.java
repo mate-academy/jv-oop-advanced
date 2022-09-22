9c0b7e006a93d52163db532b2be63502a73b9ac9
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-22 19:47:01 +0200

    Final version

--- src/main/java/core/basesyntax/suppliers/FigureSupplier.java
+++ src/main/java/core/basesyntax/suppliers/FigureSupplier.java
@@ -1,10 +1,6 @@
 package core.basesyntax.suppliers;
 
-import core.basesyntax.model.RightTriangle;
-import core.basesyntax.model.IsoscelesTrapezoid;
-import core.basesyntax.model.Rectangle;
-import core.basesyntax.model.Circle;
-import core.basesyntax.model.Square;
+import core.basesyntax.model.*;
 import core.basesyntax.model.Figure;
 import java.util.Random;
 
