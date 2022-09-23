c6a9d541e280b177b65bdd793d2488b80e7cc2e7
Victoria Kharlanchuk <kaplabetty@gmail.com>
2022-09-23 21:00:43 +0200

    local variable

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private int colorNumber;
     private Random random = new Random();
     
     public String getRandomColor() {
+        int colorNumber;
         colorNumber = random.nextInt((Color.values().length));
         return Color.values()[colorNumber].name();
     }
