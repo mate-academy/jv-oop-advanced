d5deda55574565d1eb3705840cd7ccfedeb3ff63
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-09-24 18:19:44 +0300

    solution jv-oop-advanced fixed fixes of figures array

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,14 +2,18 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        int arrayNumber = 7;
+        int arrayNumber = 6;
         Figure[] figures = new Figure[arrayNumber];
         FigureSupplier figureSupplier = new FigureSupplier();
-        for (int i = 0; i < figures.length / 2; i++) {
-            System.out.println(figureSupplier.getRandomFigure().draw());
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
         }
-        for (int i = figures.length / 2; i < figures.length; i++) {
-            System.out.println(figureSupplier.getDefaultFigure().draw());
+        for (Figure figure : figures) {
+            System.out.println(figure.draw());
         }
     }
 }
