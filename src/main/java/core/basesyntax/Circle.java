e36c0a503d7ac115ba92227d9645a01bac6d79b0
maksim-untilov <maksimuntilov7@gmail.com>
2022-09-27 20:59:55 +0300

    fixed bugs in previous version

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Circle extends Figure {
-    private int radius;
+    private int radius ;
 
     public Circle(int radius, String color) {
         super(color);
