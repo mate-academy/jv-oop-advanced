61e71386f9aa9a1b9a4c962c4c29d05b29ffe2a7
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-09-27 20:09:57 +0300

    correction of mistakes from previous push

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
-public abstract class Figure implements CommonInformation, FigureListToDo {
+public abstract class Figure implements CommonInformation, FigureArea {
     private final String color;
 
     public Figure(String color) {
         this.color = color;
     }
+
+    public abstract String getName();
 
     public String getColor() {
         return color;
--- src/main/java/core/basesyntax/FigureListToDo.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public interface FigureListToDo {
-
-    String getName();
-
-    double area();
-}
