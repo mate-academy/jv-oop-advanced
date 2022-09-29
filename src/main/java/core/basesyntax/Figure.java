8435d13f34e75ef6f58ac3607a4727619f5f6089
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-09-29 14:14:29 +0300

    I hope its last commit in this project

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public abstract class Figure implements AreaCalculator, PrintInfo {
+    private Colors color;
 
-    public Figure(Colors color){
+    public Figure(Colors color) {
         this.color = color;
     }
-    private Colors color;
 
     public Colors getColor() {
         return color;
