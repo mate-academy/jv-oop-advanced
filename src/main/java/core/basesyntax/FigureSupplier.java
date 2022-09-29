5327e58d974ceb387ecd03e7f0225b5c34e6087d
Zoreslav Todorenko <wzxzzx@gmail.com>
2022-09-29 19:30:18 +0300

    solve checkstyle mistaces

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,31 +6,37 @@ public class FigureSupplier {
     private static final int AMOUNT_OF_FIGURES = 4;
     private final ColorSupplier color;
     private final Random random;
-    Figure figure = null;
+    private final DimesionSupplier dimesion = new DimesionSupplier();
+    private Figure figure = null;
 
     public FigureSupplier(ColorSupplier color, Random random) {
         this.color = color;
         this.random = random;
     }
-    private final DimesionSupplier dimesion = new DimesionSupplier();
+
     public Figure getRandomFigure() {
         switch (new Random().nextInt(AMOUNT_OF_FIGURES)) {
             case 0:
-                figure = new Rectangle("Rectangle", color.getRandomColour(), dimesion.getRandomDimesion(), dimesion.getRandomDimesion());
+                figure = new Rectangle("Rectangle", color.getRandomColour(),
+                        dimesion.getRandomDimesion(), dimesion.getRandomDimesion());
                 break;
             case 1:
                 figure = new IsoscelesTrapezoid("IsoscelesTrapezoid", color.getRandomColour(),
-                        dimesion.getRandomDimesion(),dimesion.getRandomDimesion(), dimesion.getRandomDimesion());
+                        dimesion.getRandomDimesion(),dimesion.getRandomDimesion(),
+                        dimesion.getRandomDimesion());
                 break;
             case 2:
                 figure = new RightTriangle("RightTriangle",
-                        color.getRandomColour(), dimesion.getRandomDimesion(), dimesion.getRandomDimesion());
+                        color.getRandomColour(), dimesion.getRandomDimesion(),
+                        dimesion.getRandomDimesion());
                 break;
             case 3:
-                figure = new Circle("Circle" ,color.getRandomColour(),dimesion.getRandomDimesion());
+                figure = new Circle("Circle", color.getRandomColour(),
+                        dimesion.getRandomDimesion());
                 break;
             default:
-                figure = new Square("Square", color.getRandomColour(), dimesion.getRandomDimesion());
+                figure = new Square("Square", color.getRandomColour(),
+                        dimesion.getRandomDimesion());
                 break;
         }
         return figure;
