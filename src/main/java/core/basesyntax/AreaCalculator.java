4f5627fdbaba34d3f4bab4700012db8fc979163e
Zoreslav Todorenko <wzxzzx@gmail.com>
2022-09-30 16:34:17 +0300

    Magic Number and redundand and extra lines fixe

--- src/main/java/core/basesyntax/AreaCalculator.java
+++ src/main/java/core/basesyntax/AreaCalculator.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public interface AreaCalculator {
-    String ROUNDING_PATTERN = "#0.0";
+    public static final String ROUNDING_PATTERN = "#0.0";
 
     double calculateArea();
 }
