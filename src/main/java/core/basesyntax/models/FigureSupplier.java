161cbdc184b7ddd9e3f4d55d858a9843c452cc6e
NSay0 <krvipa@gmail.com>
2022-09-30 22:47:41 +0300

    Deleted reduntant seperated methods create<Figurename>. Deleted reduntant randomize method.

--- src/main/java/core/basesyntax/models/FigureSupplier.java
+++ src/main/java/core/basesyntax/models/FigureSupplier.java
@@ -7,93 +7,47 @@ import java.util.Random;
 public class FigureSupplier {
     private static final double PROPERTY_MULTIPLIER = 10;
     private static final Color DEFAULT_COLOR = Color.WHITE;
-    private static final Figures DEFAULT_FIGURE = Figures.CIRCLE;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier(random);
 
     public Figure getDefaultFigure() {
-        return createFigure(DEFAULT_FIGURE);
+        return new Circle(DEFAULT_COLOR.name(), 10);
     }
 
     public Figure getRandomFigure() {
         int randFigureIndex = random.nextInt(Figures.values().length);
         Figure figure = createFigure(Figures.values()[randFigureIndex]);
-        return randomizeProperties(figure);
+        return figure;
     }
 
     private Figure createFigure(Figures newFigure) {
         switch (newFigure) {
             case CIRCLE:
-                return createCircle();
+                return new Circle(colorSupplier.getRandomColor(),
+                        random.nextDouble() * PROPERTY_MULTIPLIER
+                );
             case ISOSCELESTRAPEZOID:
-                return createIsoscelesTrapezoid();
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                        random.nextDouble() * PROPERTY_MULTIPLIER,
+                        random.nextDouble() * PROPERTY_MULTIPLIER,
+                        random.nextDouble() * PROPERTY_MULTIPLIER
+                );
             case RECTANGLE:
-                return createRectangle();
+                return new Rectangle(colorSupplier.getRandomColor(),
+                        random.nextDouble() * PROPERTY_MULTIPLIER,
+                        random.nextDouble() * PROPERTY_MULTIPLIER
+                );
             case RIGHTTRIANGLE:
-                return createRightTriangle();
+                return new RightTriangle(colorSupplier.getRandomColor(),
+                        random.nextDouble() * PROPERTY_MULTIPLIER,
+                        random.nextDouble() * PROPERTY_MULTIPLIER
+                );
             case SQUARE:
-                return createSquare();
+                return new Square(colorSupplier.getRandomColor(),
+                        random.nextDouble() * PROPERTY_MULTIPLIER
+                );
             default:
                 return getDefaultFigure();
-        }
-    }
-
-    private Circle createCircle() {
-        return new Circle(DEFAULT_COLOR.name(), PROPERTY_MULTIPLIER);
-    }
-
-    private IsoscelesTrapezoid createIsoscelesTrapezoid() {
-        return new IsoscelesTrapezoid(
-                DEFAULT_COLOR.name(), PROPERTY_MULTIPLIER, PROPERTY_MULTIPLIER, PROPERTY_MULTIPLIER
-        );
-    }
-
-    private Rectangle createRectangle() {
-        return new Rectangle(DEFAULT_COLOR.name(), PROPERTY_MULTIPLIER, PROPERTY_MULTIPLIER);
-    }
-
-    private RightTriangle createRightTriangle() {
-        return new RightTriangle(DEFAULT_COLOR.name(), PROPERTY_MULTIPLIER, PROPERTY_MULTIPLIER);
-    }
-
-    private Square createSquare() {
-        return new Square(DEFAULT_COLOR.name(), PROPERTY_MULTIPLIER);
-    }
-
-    private Figure randomizeProperties(Figure figure) {
-        Figures figureType = Figures.valueOf(figure.getClass().getSimpleName().toUpperCase());
-        switch (figureType) {
-            case CIRCLE:
-                Circle circle = (Circle) figure;
-                circle.setRadius(random.nextDouble() * PROPERTY_MULTIPLIER);
-                circle.setColor(colorSupplier.getRandomColor());
-                return circle;
-            case ISOSCELESTRAPEZOID:
-                IsoscelesTrapezoid isoscelesTrapezoid = (IsoscelesTrapezoid) figure;
-                isoscelesTrapezoid.setTopSide(random.nextDouble() * PROPERTY_MULTIPLIER);
-                isoscelesTrapezoid.setHeight(random.nextDouble() * PROPERTY_MULTIPLIER);
-                isoscelesTrapezoid.setBottomSide(random.nextDouble() * PROPERTY_MULTIPLIER);
-                isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
-                return isoscelesTrapezoid;
-            case RECTANGLE:
-                Rectangle rectangle = (Rectangle) figure;
-                rectangle.setHeight(random.nextDouble() * PROPERTY_MULTIPLIER);
-                rectangle.setBottom(random.nextDouble() * PROPERTY_MULTIPLIER);
-                rectangle.setColor(colorSupplier.getRandomColor());
-                return rectangle;
-            case RIGHTTRIANGLE:
-                RightTriangle rightTriangle = (RightTriangle) figure;
-                rightTriangle.setBase(random.nextDouble() * PROPERTY_MULTIPLIER);
-                rightTriangle.setHeight(random.nextDouble() * PROPERTY_MULTIPLIER);
-                rightTriangle.setColor(colorSupplier.getRandomColor());
-                return rightTriangle;
-            case SQUARE:
-                Square square = (Square) figure;
-                square.setSide(random.nextDouble() * PROPERTY_MULTIPLIER);
-                square.setColor(colorSupplier.getRandomColor());
-                return square;
-            default:
-                throw new RuntimeException("Unable to randomize");
         }
     }
 }
