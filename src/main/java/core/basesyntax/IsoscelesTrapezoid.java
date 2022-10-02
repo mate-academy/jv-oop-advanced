fa8289b1a8e40843a600931ef4b869777c938698
Pavlo Hryhorash <pavlo.hryhorash@gmail.com>
2022-10-03 00:41:09 +0300

    Deleted some getters and setters

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -12,30 +12,6 @@ public class IsoscelesTrapezoid extends Figure {
         this.height = height;
     }
 
-    public int getFirstSide() {
-        return firstSide;
-    }
-
-    public void setFirstSide(int firstSide) {
-        this.firstSide = firstSide;
-    }
-
-    public int getSecondSide() {
-        return secondSide;
-    }
-
-    public void setSecondSide(int secondSide) {
-        this.secondSide = secondSide;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     @Override
     public double getArea() {
         return (firstSide + secondSide) / 2 * height;
