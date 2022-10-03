994a2b2215ad54bf9046a3c33dc7007dd02dfda7
yehbreus <yeh.breus@gmail.com>
2022-10-03 13:42:02 +0300

    rename interface Draw-Drawable

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements FigureOperations, Draw {
+public abstract class Figure implements FigureOperations, Drawable {
     private final String color;
 
     public Figure(String color) {
