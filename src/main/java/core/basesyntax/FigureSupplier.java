7206557fe5c07ebd12e668995fe8554135f93735
Serhii <romanchuk552@gmail.com>
2022-10-03 16:37:26 +0300

    jv-oop-advanced pass code quality test

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,23 +7,28 @@ public class FigureSupplier {
     private final int randomInd = new Random().nextInt(Shape.values().length);
     private final String randomShape = Shape.values()[randomInd].toString();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final String color = colorSupplier.getRandomColor();
 
     public Figure getRandomFigure() {
         switch (randomShape) {
             case "CIRCLE":
-                return new Circle(color, new Random().nextInt(MAX_RANG));
+                return new Circle(colorSupplier.getRandomColor(),
+                        new Random().nextInt(MAX_RANG));
             case "ISOSCELES_TRAPEZOID":
-                return new IsoscelesTrapezoid(color, new Random().nextInt(MAX_RANG),
-                        new Random().nextInt(MAX_RANG), new Random().nextInt(MAX_RANG));
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                        new Random().nextInt(MAX_RANG),
+                        new Random().nextInt(MAX_RANG),
+                        new Random().nextInt(MAX_RANG));
             case "RECTANGLE":
-                return new Rectangle(color, new Random().nextInt(MAX_RANG),
+                return new Rectangle(colorSupplier.getRandomColor(),
+                        new Random().nextInt(MAX_RANG),
                         new Random().nextInt(MAX_RANG));
             case "RIGHT_TRIANGLE":
-                return new RightTriangle(color, new Random().nextInt(MAX_RANG),
+                return new RightTriangle(colorSupplier.getRandomColor(),
+                        new Random().nextInt(MAX_RANG),
                         new Random().nextInt(MAX_RANG));
             case "SQUARE":
-                return new Square(color, new Random().nextInt(MAX_RANG));
+                return new Square(colorSupplier.getRandomColor(),
+                        new Random().nextInt(MAX_RANG));
             default:
                 return null;
         }
