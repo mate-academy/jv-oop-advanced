c9f4cb54c14b0be4e7d28ac5a3dacf1f9a279799
Andrii Matveev <matveev.tesla@gmail.com>
2022-10-03 11:40:29 +0300

    comments has been removed

--- src/main/java/core/basesyntax/ColourSupplier.java
+++ src/main/java/core/basesyntax/ColourSupplier.java
@@ -7,18 +7,6 @@ public class ColourSupplier {
     private Random random = new Random();
 
     public String getRandomColour() {
-        /*switch (random.nextInt(COLOUR_COUNT)) {
-            case 1:
-                return Colours.RED.name().toLowerCase();
-            case 2:
-                return Colours.BLACK.name().toLowerCase();
-            case 3:
-                return Colours.GREEN.name().toLowerCase();
-            case 4 :
-                return Colours.BLUE.name().toLowerCase();
-            default:
-                return Colours.WHITE.name().toLowerCase();
-        }*/
         return Colour.values()[random.nextInt(COLOUR_COUNT)].name();
     }
 }
