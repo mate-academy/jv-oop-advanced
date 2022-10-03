ecfeffd1252c535e74d80a3888f64c9f3dbaa331
Andrew Miroshnikov <giggle2594@gmail.com>
2022-10-03 17:07:43 +0300

    Final correction

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,23 +4,25 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int FIGURE_LENGTH = 5;
+    private static final int MAX_RANDOM_INT = 100;
     private static final Random random = new Random();
+    private int randomSide1 = random.nextInt(MAX_RANDOM_INT);
+    private int randomSide2 = random.nextInt(MAX_RANDOM_INT);
     private final ColorSupplier supplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int index = random.nextInt(FIGURE_LENGTH);
-        int randomSide = random.nextInt(100);
         switch (index) {
             case 0 :
-                return new Circle(randomSide, supplier.getRandomColor());
+                return new Circle(randomSide1, supplier.getRandomColor());
             case 1 :
-                return new IsoscelesTrapezoid(randomSide, randomSide, supplier.getRandomColor());
+                return new IsoscelesTrapezoid(randomSide1, randomSide2, supplier.getRandomColor());
             case 2 :
-                return new RightTriangle(randomSide, randomSide, supplier.getRandomColor());
+                return new RightTriangle(randomSide1, randomSide2, supplier.getRandomColor());
             case 3 :
-                return new Rectangle(randomSide, randomSide, supplier.getRandomColor());
+                return new Rectangle(randomSide1, randomSide2, supplier.getRandomColor());
             default:
-                return new Square(randomSide, supplier.getRandomColor());
+                return new Square(randomSide1, supplier.getRandomColor());
         }
     }
 
