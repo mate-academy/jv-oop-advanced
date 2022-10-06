a8e275c66a95d331a66a8bbc923feff618b29ccb
Ilya Samilyak <samilyakilya@gmail.com>
2022-10-06 21:02:29 +0300

    Fixed after review 2, except one

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -8,28 +8,27 @@ public class FigureSupplier {
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure(int value) {
-        int rndInt = random.nextInt(MAX_VALUE);
 
-        switch (value) {
+        switch (4) {
             case 1:
-                return new Circle(colorSupplier.getRandomColor(), random.nextInt(rndInt));
+                return new Circle(colorSupplier.getRandomColor(), random.nextInt(MAX_VALUE));
             case 2:
-                return new Square(colorSupplier.getRandomColor(), random.nextInt(rndInt));
+                return new Square(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_VALUE));
             case 3:
                 return new Rectangle(colorSupplier.getRandomColor(),
-                        random.nextInt(rndInt), random.nextInt(rndInt));
+                        random.nextInt(MAX_VALUE), random.nextInt(MAX_VALUE));
             case 4:
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextInt(rndInt), random.nextInt(rndInt));
+                        random.nextInt(MAX_VALUE), random.nextInt(MAX_VALUE));
             default:
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(rndInt), random.nextInt(rndInt));
+                        random.nextInt(MAX_VALUE), random.nextInt(MAX_VALUE));
         }
     }
 
     public Figure getDefaultFigure() {
-
-        return getRandomFigure(1);
+        return new Circle("white", 10);
     }
 }
 
