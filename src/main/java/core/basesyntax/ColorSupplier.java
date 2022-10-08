07729e756b4ea7b7c88d8f8af2ce09d27ef84456
Katya Humeniuk <kapelka.boo@gmail.com>
2022-10-08 16:47:17 +0300

    Added fixes after review.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+
     public String getRandomColor() {
-    int index = random.nextInt(Color.values().length - 1);
-    return Color.values()[index].name().toLowerCase();
+        int index = random.nextInt(Color.values().length - 1);
+        return Color.values()[index].name().toLowerCase();
     }
 }
