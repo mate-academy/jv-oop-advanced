df27ad762378b9c10ddc38491271808c93bd6590
Dmytro Busyhin <zza4etik@gmail.com>
2022-10-10 23:05:22 +0300

    Created constants, fixed errors in methods getRandom

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,14 +5,14 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 5;
     private static final int MAX_VALUE = 10;
+    private static final int CIRCLE_RADIUS = 10;
+    private static final String COLOR = Color.WHITE.name().toLowerCase();
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getDefaultFigure() {
-        final int figureParameter = 10;
-        final String color = Color.WHITE.name().toLowerCase();
-        Figure figure = new Circle(figureParameter);
-        figure.setColor(color);
+        Figure figure = new Circle(CIRCLE_RADIUS);
+        figure.setColor(COLOR);
         return figure;
     }
 
@@ -38,24 +38,24 @@ public class FigureSupplier {
     }
 
     private Figure getRandomCircle() {
-        return new Circle(random.nextInt(MAX_VALUE + 1));
+        return new Circle(random.nextInt(MAX_VALUE) + 1);
     }
 
     private Figure getRandomSquare() {
-        return new Square(random.nextInt(MAX_VALUE + 1));
+        return new Square(random.nextInt(MAX_VALUE) + 1);
     }
 
     private Figure getRandomRectangle() {
-        return new Rectangle(random.nextInt(MAX_VALUE + 1), random.nextInt(MAX_VALUE + 1));
+        return new Rectangle(random.nextInt(MAX_VALUE) + 1, random.nextInt(MAX_VALUE) + 1);
     }
 
     private Figure getRandomIsoscelesTrapezoid() {
-        return new IsoscelesTrapezoid(random.nextInt(MAX_VALUE + 1), random.nextInt(MAX_VALUE + 1),
-                random.nextInt(MAX_VALUE + 1));
+        return new IsoscelesTrapezoid(random.nextInt(MAX_VALUE) + 1, random.nextInt(MAX_VALUE) + 1,
+                random.nextInt(MAX_VALUE) + 1);
     }
 
     private Figure getRandomIsoscelesRightTriangle() {
-        return new IsoscelesTrapezoid(random.nextInt(MAX_VALUE + 1), random.nextInt(MAX_VALUE + 1),
-                random.nextInt(MAX_VALUE + 1));
+        return new IsoscelesTrapezoid(random.nextInt(MAX_VALUE) + 1, random.nextInt(MAX_VALUE) + 1,
+                random.nextInt(MAX_VALUE) + 1);
     }
 }
