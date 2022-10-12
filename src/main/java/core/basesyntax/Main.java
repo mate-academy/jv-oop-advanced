d0fc26ca7b7dfa4735819d95c15376b49a50efbf
Krupii Oleh <oleg2003andriy@gmail.com>
2022-10-12 23:45:20 +0300

    supercommit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class Main {
     private static FigureSupplier figureSupplier = new FigureSupplier();
-    private static final int COUNTFIGURES = 6;
+    private static final int NUMBER_OF_FIGURES = 6;
 
     public static void main(String[] args) {
-        Figure[] figures = new Figure[COUNTFIGURES];
+        Figure[] figures = new Figure[NUMBER_OF_FIGURES];
         for (int i = 0; i < figures.length; i++) {
             figures[i] = i % 2 == 0 ? figureSupplier.getDefaultFigure()
                     : figureSupplier.getRandomFigure();
