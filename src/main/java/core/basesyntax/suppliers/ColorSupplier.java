abc508a9b3fffaa8e6938bb28455452898783235
Tetiana B <tbabenko0909@gmail.com>
2022-10-19 15:32:49 +0200

    delete a row

--- src/main/java/core/basesyntax/suppliers/ColorSupplier.java
+++ src/main/java/core/basesyntax/suppliers/ColorSupplier.java
@@ -3,6 +3,7 @@ package core.basesyntax.suppliers;
 import java.util.Random;
 
 public class ColorSupplier {
+
     private Random random = new Random();
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
