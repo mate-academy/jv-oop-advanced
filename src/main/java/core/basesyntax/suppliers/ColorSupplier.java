69d9ef62740c1f2609d868cc30807ff3ff848113
Tetiana B <tbabenko0909@gmail.com>
2022-10-19 13:59:38 +0200

    change random to private.

--- src/main/java/core/basesyntax/suppliers/ColorSupplier.java
+++ src/main/java/core/basesyntax/suppliers/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax.suppliers;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
+    private Random random = new Random();
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return String.valueOf(Color.values()[index]);
