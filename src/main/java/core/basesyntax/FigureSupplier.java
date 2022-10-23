a34c97697ee9f6c459298692cf05d49acc619f78
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2022-10-23 21:34:47 +0300

    added constants for default figure

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,11 +5,13 @@ import java.util.Random;
 public class FigureSupplier {
     public static final int FIGURE_COUNT = 5;
     public static final int MAX_UNITS = 50;
+    public static final String DEFAULT_COLOR = "WHITE";
+    public static final int DEFAULT_RADIUS = 10;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getDefaultFigure() {
-        return new Circle("WHITE", 10);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 
     public Figure getRandomFigure() {
