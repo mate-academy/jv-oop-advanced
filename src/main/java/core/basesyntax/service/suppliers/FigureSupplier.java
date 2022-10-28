eecca5a16b5827d9da6625d764e5332966358ba6
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-28 18:44:54 +0200

    order

--- src/main/java/core/basesyntax/service/suppliers/FigureSupplier.java
+++ src/main/java/core/basesyntax/service/suppliers/FigureSupplier.java
@@ -9,11 +9,11 @@ import core.basesyntax.Square;
 import java.util.Random;
 
 public class FigureSupplier {
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private final Random random = new Random();
     private static final int DEFAULT_NUMBER = 50;
     private static final int DEFAULT_RADIUS = 10;
     private static final String DEFAULT_COLOR = "white";
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Figure getRandomFigure() {
         int figureIndex = random.nextInt(4);
