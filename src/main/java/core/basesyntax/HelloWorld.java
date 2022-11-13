844439cb6881b9790850a9b4e785d91dedd880e4
Igor Puhach <igorpuhach21@gmail.com>
2022-11-13 14:40:29 +0200

    fixed bugs # 7

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,6 +4,7 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
 
