edbccb84ea176b5b0cfd5c672dfebecff4d4bef4
Denys Yastrebov <programmer_p@outlook.com>
2022-11-18 10:27:09 +0200

    fixed syntax errors

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int MAX_SIDE = 1000;
     private Random random = new Random();
-    private int randomSide = random.nextInt(MAX_SIDE);
+    private int side = random.nextInt(MAX_SIDE);
     private ColorSupplier color = new ColorSupplier();
 
     public Figures defaultFigure() {
@@ -27,22 +27,22 @@ public class FigureSupplier {
                 figureInstance = circle;
                 break;
             case (1):
-                IsoscelesTrapezoid trapezoid = new IsoscelesTrapezoid(randomSide, randomSide, randomSide);
+                IsoscelesTrapezoid trapezoid = new IsoscelesTrapezoid(side, side, side);
                 trapezoid.setColor(color.getRandomColor());
                 figureInstance = trapezoid;
                 break;
             case (2):
-                Rectangle rectangle = new Rectangle(randomSide, randomSide);
+                Rectangle rectangle = new Rectangle(side, side);
                 rectangle.setColor(color.getRandomColor());
                 figureInstance = rectangle;
                 break;
             case (3):
-                RightTriangle triangle = new RightTriangle(randomSide, randomSide);
+                RightTriangle triangle = new RightTriangle(side, side);
                 triangle.setColor(color.getRandomColor());
                 figureInstance = triangle;
                 break;
             case (4):
-                Square square = new Square(randomSide);
+                Square square = new Square(side);
                 square.setColor(color.getRandomColor());
                 figureInstance = square;
                 break;
