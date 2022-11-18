fd3b5e571928c9f21f5932994bc12b51fd6c66cb
Nikita Akosnov <nikitaua4226@gmail.com>
2022-11-18 18:05:54 +0200

    fixed next.random()

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -8,34 +8,33 @@ public class FigureSupplier {
     public static final int DEFAULT_RADIUS = 10;
     public static final int FIGURE_COUNT = 5;
     public static final int UPPER_BOUND = 31;
-    public static final int ORIGIN = 1;
 
     public Figure getRandomFigure() {
         int figureIndex = random.nextInt(FIGURE_COUNT);
         Figure figure;
         switch (figureIndex) {
             case 0: 
-                int randomRadius = random.nextInt(ORIGIN, UPPER_BOUND);
+                int randomRadius = random.nextInt(UPPER_BOUND);
                 figure = new Circle(colorSupplier.getRandomColor(), randomRadius);
                 break;
-            case ORIGIN:
-                int randomShortBase = random.nextInt(ORIGIN, UPPER_BOUND);
-                int randomLongBase = random.nextInt(ORIGIN, UPPER_BOUND);
-                int randomHeight = random.nextInt(ORIGIN, UPPER_BOUND);
+            case 1:
+                int randomShortBase = random.nextInt(UPPER_BOUND);
+                int randomLongBase = random.nextInt(UPPER_BOUND);
+                int randomHeight = random.nextInt(UPPER_BOUND);
                 figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(), randomShortBase, randomLongBase, randomHeight);
                 break;
             case 2:
-                int firstSide = random.nextInt(ORIGIN, UPPER_BOUND);
-                int secondSide = random.nextInt(ORIGIN, UPPER_BOUND);
+                int firstSide = random.nextInt(UPPER_BOUND);
+                int secondSide = random.nextInt(UPPER_BOUND);
                 figure = new Rectangle(colorSupplier.getRandomColor(), firstSide, secondSide);
                 break;
             case 3:
-                int firstLeg = random.nextInt(ORIGIN, UPPER_BOUND);
-                int secondLeg = random.nextInt(ORIGIN, UPPER_BOUND);
+                int firstLeg = random.nextInt(UPPER_BOUND);
+                int secondLeg = random.nextInt(UPPER_BOUND);
                 figure = new RightTriangle(colorSupplier.getRandomColor(), firstLeg, secondLeg);
                 break;  
             default:
-                int randomSide = random.nextInt(ORIGIN, UPPER_BOUND);
+                int randomSide = random.nextInt(UPPER_BOUND);
                 figure = new Square(colorSupplier.getRandomColor(), randomSide);               
         }
 
