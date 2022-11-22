fd8d3930ec6138ddf4a2ea4d66160135a6f250b8
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2022-11-22 20:32:35 +0200

    Update src/main/java/core/basesyntax/ColorSupplier.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,19 +7,7 @@ public class ColorSupplier {
 
     public String getRandomColor() {
         int colorNumber = random.nextInt(Color.values().length);
-        switch (colorNumber) {
-            case 0:
-                return Color.WHITE.name();
-            case 1:
-                return Color.PURPLE.name();
-            case 2:
-                return Color.RED.name();
-            case 3:
-                return Color.BLUE.name();
-            case 4:
-                return Color.GREEN.name();
-            default:
-                return Color.YELLOW.name();
-        }
+        return Color.values()[colorNumber].name();
+
     }
 }
