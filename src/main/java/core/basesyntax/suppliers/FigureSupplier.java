cec5b187b8d5d404083ad4ee3bc68abf33bd5565
AndriiBohun <poikka69@gmail.com>
2022-11-23 15:42:37 +0200

    C006_jv-oop-advanced

--- src/main/java/core/basesyntax/suppliers/FigureSupplier.java
+++ src/main/java/core/basesyntax/suppliers/FigureSupplier.java
@@ -1,10 +1,10 @@
 package core.basesyntax.suppliers;
 
+import core.basesyntax.models.Figure;
 import core.basesyntax.models.Circle;
 import core.basesyntax.models.IsoscelesTrapezoid;
 import core.basesyntax.models.Rectangle;
 import core.basesyntax.models.RightTriangle;
-import core.basesyntax.models.Figure;
 import core.basesyntax.models.Square;
 import java.util.Random;
 
