2b21aacd7275a795d3336165ba87e8bc5d1b75a0
Stepan Peknyi <stepan_peknyy@ukr.net>
2022-11-28 19:02:53 +0200

    Update src/main/java/core/basesyntax/Figure.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,15 +1,17 @@
 package core.basesyntax;
 
 public abstract class Figure implements Drawable, AreaCalculator {
-    private ColorSupplier colorSupplier = new ColorSupplier();
     private String color;
+    
+    public Figure(String color) {
+        this.color = color;
+    }
 
     public void setColor(String color) {
         this.color = color;
     }
 
     public String getColor() {
-        color = colorSupplier.getRandomColor();
         return color;
     }
 
