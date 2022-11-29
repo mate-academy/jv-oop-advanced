76722d430d3935d41eac627cb0349b6c98ba8de5
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2022-11-29 16:38:58 +0200

    initial RightTriangle class implementation updated

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -1,4 +1,23 @@
 package core.basesyntax;
 
-public class RightTriangle {
+public class RightTriangle extends Figure {
+    private static final String FIGURE_NAME = "right triangle";
+    private final int firstLeg;
+    private final int secondLeg;
+
+    public RightTriangle(String color, int firstLeg, int secondLeg) {
+        super(color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    protected double getArea() {
+        return firstLeg * secondLeg / 2;
+    }
+
+    @Override
+    protected String draw() {
+        return "Figure: " + FIGURE_NAME + ", area: " + getArea() + " sq.units, first leg: " +  firstLeg + " units, second leg: " + secondLeg + " units, color: " + color;
+    }
 }
