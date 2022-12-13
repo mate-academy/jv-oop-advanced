acf5dcf21a496456a28201df898818a8bafb94d8
chumpaq <119128499+chumpaq@users.noreply.github.com>
2022-12-13 09:47:39 +0200

    Update src/main/java/core/basesyntax/Main.java
    Co-authored-by: Konstantin <www.kosta.kon@gmail.com>

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,10 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         Figure[] figures = new Figure[6];
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
         }
-        for (int i = 3; i < figures.length; i++) {
+        for (int i = figures.length / 2; i < figures.length; i++) {
             figures[i] = figureSupplier.getDefaultFigure();
         }
 
