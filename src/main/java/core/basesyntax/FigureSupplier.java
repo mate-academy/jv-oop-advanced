af54333ce849b9d752a8dcbb55a76b87ea62bf39
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2022-12-18 19:06:25 +0200

    fixed all reviews problems

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,53 +3,75 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final String COLOR = "White";
-    private Figure figure;
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private Random random = new Random();
+    private static final String DEFAULT_COLOR = "White";
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private static final Random RANDOM = new Random();
+    int randomNumber = RANDOM.nextInt(5);
+    double figuresSidesNumber = RANDOM.nextInt(100);
+
+    private Square createSquare(){
+        return new Square(COLOR_SUPPLIER.getRandomColor(),
+                figuresSidesNumber);
+    }
+
+    private Rectangle createRectangle(){
+        return new Rectangle(COLOR_SUPPLIER.getRandomColor(),
+                figuresSidesNumber,
+                figuresSidesNumber);
+    }
+
+    private RightTriangle createRightTriangle(){
+        return new RightTriangle(COLOR_SUPPLIER.getRandomColor(),
+                figuresSidesNumber,
+                figuresSidesNumber);
+    }
+
+    private Circle createCircle(){
+        return new Circle(COLOR_SUPPLIER.getRandomColor(),
+                figuresSidesNumber);
+    }
+
+    private IsoscelesTrapezoid createIsoscelesTrapezoid(){
+        return new IsoscelesTrapezoid(COLOR_SUPPLIER.getRandomColor(),
+                figuresSidesNumber,
+                figuresSidesNumber,
+                figuresSidesNumber);
+    }
 
     public Figure getRandomFigure() {
-        int randomNumber = random.nextInt(5);
+        Figure figure;
+
 
         switch (randomNumber) {
             case 0 : {
-                figure = new Square(colorSupplier.getRandomColor(),
-                        randomNumber + 10);
+                figure = createSquare();
                 break;
             }
             case 1 : {
-                figure = new Rectangle(colorSupplier.getRandomColor(),
-                        (double)randomNumber + 10,
-                        (double)randomNumber + 11);
+                figure = createRectangle();
                 break;
             }
             case 2 : {
-                figure = new RightTriangle(colorSupplier.getRandomColor(),
-                        (double)randomNumber + 10,
-                        (double)randomNumber + 11);
+                figure = createRightTriangle();
                 break;
             }
             case 3 : {
-                figure = new Circle(colorSupplier.getRandomColor(),
-                        (double)randomNumber + 10);
+                figure = createCircle();
                 break;
             }
             case 4 : {
-                figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        (double)randomNumber + 10,
-                        (double)randomNumber + 11,
-                        (double)randomNumber + 12);
+                figure = createIsoscelesTrapezoid();
                 break;
             }
             default: {
-                figure = new Square(colorSupplier.getRandomColor(),
-                        randomNumber + 10);
+                figure = new Square(COLOR_SUPPLIER.getRandomColor(),
+                        figuresSidesNumber);
             }
         }
         return figure;
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(COLOR, 10.0);
+        return new Circle(DEFAULT_COLOR, 10.0);
     }
 }
