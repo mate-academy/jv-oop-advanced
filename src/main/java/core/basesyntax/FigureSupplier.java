5b7cae810f2987420bf0cbb162c3aa4666869890
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-18 10:09:14 +0200

    1. Added getColor().name(). 2. Renamed figure1 with numberOfFigure. 3. Created own method for making each figure.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -32,20 +32,25 @@ public class FigureSupplier {
                 return createIsoscelesTrapezoid(color);
         }
     }
+
     private Figure createSquare(Color color) {
         return new Square(color, random.nextInt(MAX_SIDE_VALUE));
     }
+
     private Figure createRectangle(Color color) {
         return new Rectangle(color, random.nextInt(MAX_SIDE_VALUE),
                 random.nextInt(MAX_SIDE_VALUE));
     }
+
     private Figure createRightTriangle(Color color) {
         return new RightTriangle(color, random.nextInt(MAX_SIDE_VALUE),
                 random.nextInt(MAX_SIDE_VALUE));
     }
+
     private Figure createCircle(Color color) {
         return new Circle(color, random.nextInt(MAX_SIDE_VALUE));
     }
+
     private Figure createIsoscelesTrapezoid(Color color) {
         return new IsoscelesTrapezoid(color, random.nextInt(MAX_SIDE_VALUE),
                 random.nextInt(MAX_SIDE_VALUE), random.nextInt(MAX_SIDE_VALUE));
