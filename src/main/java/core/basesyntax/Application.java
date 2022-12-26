851163daa3e95f4c8dde0bc0f8b4e1b2ec1f3d70
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2022-12-27 00:55:18 +0200

    HW oop advanced next mistake fixed v2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
 public class Application {
+    static final int ARRAY_COUNT = 6;
 
     public static void main(String[] args) {
-        final int ARRAY_COUNT = 6;
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[ARRAY_COUNT];
         for (int i = 0; i < ARRAY_COUNT; i++) {
