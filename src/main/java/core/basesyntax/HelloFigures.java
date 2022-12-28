8acf3fca3f574acdf15d0167a4f01cbf31b157e7
gordgelin <gordgelin@s69a.su>
2022-12-28 21:07:42 +0200

    Implemented psvm logic. Ready to review?

--- src/main/java/core/basesyntax/HelloFigures.java
+++ src/main/java/core/basesyntax/HelloFigures.java
@@ -1,8 +1,23 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloFigures {
+import core.basesyntax.figure.Figure;
+import core.basesyntax.figure.FigureSupplier;
 
+public class HelloFigures {
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[10];
+
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
 }
--- src/main/java/core/basesyntax/color/ColorSupplier.java
+++ src/main/java/core/basesyntax/color/ColorSupplier.java
@@ -5,6 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     private static final Random RANDOM = new Random(System.currentTimeMillis());
     public String getRandomColor(){
-        return Color.values()[RANDOM.nextInt(Color.values().length)].name();
+        return Color.values()[RANDOM.nextInt(Color.values().length)].name().toLowerCase();
     }
 }
