ab19414b4018e7bb2fa6a63dabdface5e5b21db8
gordgelin <gordgelin@s69a.su>
2022-12-28 07:22:21 +0200

    Added Messages class with some String constants

---
+++ src/main/java/core/basesyntax/constants/Messages.java
@@ -0,0 +1,10 @@
+package core.basesyntax.constants;
+
+public final class Messages {
+    public static final String ILLEGAL_FIGURE_PARAMS = "Figure dimensions should be greater than zero!";
+    public static final String ILLEGAL_FIGURE_COLOR = "Figure color can't be null or empty!";
+
+    private Messages(){
+
+    }
+}
