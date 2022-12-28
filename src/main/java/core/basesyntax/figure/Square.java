3333b604aad0933000b6cff4e3e13da10d87046d
gordgelin <gordgelin@s69a.su>
2022-12-28 05:33:02 +0200

    Implemented Square class

--- src/main/java/core/basesyntax/figure/Square.java
+++ src/main/java/core/basesyntax/figure/Square.java
@@ -1,4 +1,33 @@
 package core.basesyntax.figure;
 
 public class Square extends Figure{
+    private double side;
+
+    public Square(double side){
+        this.side = side;
+    }
+
+    public Square(String color, double side){
+        this.color = color;
+        this.side = side;
+    }
+    @Override
+    public void draw() {
+        String str = "Figure: square, area: " + getArea() + " sq.units, side: " + side;
+
+        System.out.println(str);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(side, 2);
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
 }
