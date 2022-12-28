d7cd36cdd945d1a827fda1445f5f9c9c7c519ec9
gordgelin <gordgelin@s69a.su>
2022-12-28 21:00:34 +0200

    Finished implementation of FigureSupplier

--- src/main/java/core/basesyntax/figure/FigureSupplier.java
+++ src/main/java/core/basesyntax/figure/FigureSupplier.java
@@ -1,4 +1,48 @@
 package core.basesyntax.figure;
 
+import core.basesyntax.color.Color;
+import core.basesyntax.color.ColorSupplier;
+
+import java.util.Random;
+
 public class FigureSupplier {
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
+    private static final int RANDOM_MAX_VALUE = 100;
+    public Figure getDefaultFigure(){
+        return new Circle(Color.WHITE.name().toLowerCase(), 10);
+    }
+
+    public Figure getRandomFigure(){
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String color = colorSupplier.getRandomColor();
+
+        Shape shape = Shape.values()[RANDOM.nextInt(Shape.values().length)];
+
+        Figure figure = null;
+
+        switch (shape) {
+            case CIRCLE: {
+                figure = new Circle(color, RANDOM.nextInt(RANDOM_MAX_VALUE));
+                break;
+            }
+            case SQUARE:{
+                figure = new Square(color, RANDOM.nextInt(RANDOM_MAX_VALUE));
+                break;
+            }
+            case RECTANGLE:{
+                figure = new Rectangle(color,RANDOM.nextInt(RANDOM_MAX_VALUE), RANDOM.nextInt(RANDOM_MAX_VALUE));
+                break;
+            }
+            case RIGHT_TRIANGLE:{
+                figure = new RightTriangle(color, RANDOM.nextInt(RANDOM_MAX_VALUE), RANDOM.nextInt(RANDOM_MAX_VALUE));
+                break;
+            }
+            case ISOSCELES_TRAPEZOID:{
+                figure = new IsoscelesTrapezoid(RANDOM.nextInt(RANDOM_MAX_VALUE), RANDOM.nextInt(RANDOM_MAX_VALUE), RANDOM.nextInt(RANDOM_MAX_VALUE));
+                break;
+            }
+        }
+
+        return figure;
+    }
 }
