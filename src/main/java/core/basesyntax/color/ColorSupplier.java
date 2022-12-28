49c58648bfdf7cb7151b4bfea6b9a3740d61ee4c
gordgelin <gordgelin@s69a.su>
2022-12-28 06:28:43 +0200

    Codestyle improvements

--- src/main/java/core/basesyntax/color/ColorSupplier.java
+++ src/main/java/core/basesyntax/color/ColorSupplier.java
@@ -3,10 +3,8 @@ package core.basesyntax.color;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final static Random RANDOM = new Random(System.currentTimeMillis());
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
     public String getRandomColor(){
-        String randomColor = Color.values()[RANDOM.nextInt(Color.values().length)].name();
-
-        return randomColor;
+        return Color.values()[RANDOM.nextInt(Color.values().length)].name();
     }
 }
