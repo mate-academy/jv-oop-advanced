8184d41df4276a593698a139e01b49797cec2267
gordgelin <gordgelin@s69a.su>
2022-12-28 04:41:34 +0200

    Added color field and its setter/getter to Figure class

--- src/main/java/core/basesyntax/figure/Figure.java
+++ src/main/java/core/basesyntax/figure/Figure.java
@@ -1,4 +1,15 @@
 package core.basesyntax.figure;
 
+import core.basesyntax.color.Color;
+
 public abstract class Figure implements DrawableFigure, GeometricFigure {
+    private Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
