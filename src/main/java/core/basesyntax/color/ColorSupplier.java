a8ca425bedf4486a55acd8085fb96ab19863c88c
gordgelin <gordgelin@s69a.su>
2022-12-28 05:02:20 +0200

    Implemented ColorSupplier class

--- src/main/java/core/basesyntax/color/ColorSupplier.java
+++ src/main/java/core/basesyntax/color/ColorSupplier.java
@@ -1,4 +1,12 @@
 package core.basesyntax.color;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final static Random random = new Random(System.currentTimeMillis());
+    public String getRandomColor(){
+        String randomColor = Color.values()[random.nextInt(Color.values().length)].name();
+
+        return randomColor;
+    }
 }
