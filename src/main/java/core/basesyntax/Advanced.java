0fd716c337caca5495cf74c0fd0b23315b525590
Mik911 <911Sokol@gmail.com>
2022-12-28 19:42:27 +0200

    jc-oop-advanced.  9 check

--- src/main/java/core/basesyntax/Advanced.java
+++ src/main/java/core/basesyntax/Advanced.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Advanced {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-       // Random rd = new Random();
+        // Random rd = new Random();
         Figure[] figures = new Figure[6];
+
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
                 figures[i] = figureSupplier.getRandomFigure(i);
