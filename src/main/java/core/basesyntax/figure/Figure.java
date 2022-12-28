1f87eb482718e08d3c9d42e819c2ed22d7e7fe85
gordgelin <gordgelin@s69a.su>
2022-12-28 06:33:15 +0200

    Added getName method to Figure

--- src/main/java/core/basesyntax/figure/Figure.java
+++ src/main/java/core/basesyntax/figure/Figure.java
@@ -1,9 +1,12 @@
 package core.basesyntax.figure;
 
-import core.basesyntax.color.Color;
-
 public abstract class Figure implements DrawableFigure, GeometricFigure {
+    private static final String FIGURE_NAME = "figure";
     protected String color = "blank";
+
+    public static String getName() {
+        return FIGURE_NAME;
+    }
 
     public String getColor() {
         return color;
