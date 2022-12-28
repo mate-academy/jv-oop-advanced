0e56bdc062f5e9a08dc5226cb6943d9c7d880cf8
gordgelin <gordgelin@s69a.su>
2022-12-28 06:37:20 +0200

    Improved setColor method safety. Changed argument type from String to Color

--- src/main/java/core/basesyntax/figure/Figure.java
+++ src/main/java/core/basesyntax/figure/Figure.java
@@ -1,5 +1,7 @@
 package core.basesyntax.figure;
 
+import core.basesyntax.color.Color;
+
 public abstract class Figure implements DrawableFigure, GeometricFigure {
     private static final String FIGURE_NAME = "figure";
     protected String color = "blank";
@@ -12,7 +14,7 @@ public abstract class Figure implements DrawableFigure, GeometricFigure {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setColor(Color color) {
+        this.color = color.name().toLowerCase();
     }
 }
