488afa8d4586dfa04c34a759c37166518f1d3835
Denys Dominskyi <dominskyj@gmail.com>
2023-01-02 22:21:21 +0200

    Solution5

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Figure[] arr = {new FigureSupplier().getRandomFigure(), new FigureSupplier().getRandomFigure(),
-                new FigureSupplier().getRandomFigure(), FigureSupplier.getDefaultFigure(),
-                FigureSupplier.getDefaultFigure(), FigureSupplier.getDefaultFigure()};
+        Figure[] arr = {new FigureSupplier().getRandomFigure(),
+                new FigureSupplier().getRandomFigure(), new FigureSupplier().getRandomFigure(),
+                FigureSupplier.getDefaultFigure(), FigureSupplier.getDefaultFigure(),
+                FigureSupplier.getDefaultFigure()};
     }
 }
