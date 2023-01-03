46461742b78b26d04cf16130a44daa2b25ca0e14
Kateryna <katerynachi5@gmail.com>
2023-01-03 22:06:51 +0100

    Create separate methods to generate figures, create additional constants, change work of getDefaultFigure

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,33 +4,55 @@ import java.util.Random;
 
 public class FigureSupplier {
     public static final int NUMBER_OF_FIGURES = 5;
-    private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private Circle defaultCircle = new Circle(Color.WHITE.name().toLowerCase(), 10);
+    public static final int NEXT_INT_BOUND = 10;
+    public static final int DEFAULT_RADIUS = 10;
+    public static final Color DEFAULT_COLOR = Color.WHITE;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final String randomColor = colorSupplier.getRandomColor();
+    private final int randomValueX = random.nextInt(NEXT_INT_BOUND) + 1;
+    private final int randomValueY = random.nextInt(NEXT_INT_BOUND) + 1;
+    private final int randomValueZ = random.nextInt(NEXT_INT_BOUND) + 1;
 
     public Figure getRandomFigure() {
         int index = random.nextInt(NUMBER_OF_FIGURES);
-        String randomColor = colorSupplier.getRandomColor();
-        int randomValueX = random.nextInt(10) + 1;
-        int randomValueY = random.nextInt(10) + 1;
-        int randomValueZ = random.nextInt(10) + 1;
 
         switch (index) {
             case 0:
-                return new Circle(randomColor, randomValueX);
+                return getCircle();
             case 1:
-                return new IsoscelesTrapezoid(randomColor, randomValueX, randomValueY,
-                        randomValueZ);
+                return getIsoscelesTrapezoid();
             case 2:
-                return new Rectangle(randomColor, randomValueX, randomValueY);
+                return getRectangle();
             case 3:
-                return new RightTriangle(randomColor, randomValueX, randomValueY);
+                return getRightTriangle();
             default:
-                return new Square(randomColor, randomValueX);
+                return getSquare();
         }
     }
 
     public Figure getDefaultFigure() {
-        return defaultCircle;
+        return new Circle(DEFAULT_COLOR.name().toLowerCase(), DEFAULT_RADIUS);
+    }
+
+    private Figure getCircle() {
+        return new Circle(randomColor, randomValueX);
+    }
+
+    private Figure getIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(randomColor, randomValueX, randomValueY,
+                randomValueZ);
+    }
+
+    private Figure getRectangle() {
+        return new Rectangle(randomColor, randomValueX, randomValueY);
+    }
+
+    private Figure getRightTriangle() {
+        return new RightTriangle(randomColor, randomValueX, randomValueY);
+    }
+
+    private Figure getSquare() {
+        return new Square(randomColor, randomValueX);
     }
 }
