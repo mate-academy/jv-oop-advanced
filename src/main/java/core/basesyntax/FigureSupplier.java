10001da15f3e2a132b5248958cd81f9d6bab1e9b
Artur Muliukov <arthur.muliukov@gmail.com>
2023-01-04 22:39:43 +0500

    Version 1.8

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -9,21 +9,21 @@ public class FigureSupplier {
 
     public Figure getRandomFigure() {
         int index = new Random().nextInt(5);
-        int Side = new Random().nextInt(10);
+        int side = new Random().nextInt(10);
 
         if (index == 0) {
-            return new Circle(Side, color.getRandomColor());
+            return new Circle(side, color.getRandomColor());
         }
         if (index == 1) {
-            return new IsoscelesTrapezoid(Side, Side, Side, color.getRandomColor());
+            return new IsoscelesTrapezoid(side, side, side, color.getRandomColor());
         }
         if (index == 2) {
-            return new Rectangle(Side, Side, color.getRandomColor());
+            return new Rectangle(side, side, color.getRandomColor());
         }
         if (index == 3) {
-            return new RightTriangle(Side, Side, color.getRandomColor());
+            return new RightTriangle(side, side, color.getRandomColor());
         } else {
-            return new Square(Side, color.getRandomColor());
+            return new Square(side, color.getRandomColor());
         }
     }
 
