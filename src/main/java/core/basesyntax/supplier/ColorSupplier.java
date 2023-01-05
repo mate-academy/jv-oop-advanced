054ca05258f3265b6144150d4248cb1c3df503d3
yermilov.m <yermilov.m@hotmail.com>
2023-01-06 00:56:49 +0100

    implemented another mentor's requests

--- src/main/java/core/basesyntax/supplier/ColorSupplier.java
+++ src/main/java/core/basesyntax/supplier/ColorSupplier.java
@@ -3,8 +3,13 @@ package core.basesyntax.supplier;
 import java.util.Random;
 
 public class ColorSupplier {
+    public static final Random random;
+
+    static {
+        random = new Random();
+    }
 
     public String getRandomColor() {
-        return Color.values()[new Random().nextInt(Color.values().length)].name();
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
--- src/main/java/core/basesyntax/supplier/FigureSupplier.java
+++ src/main/java/core/basesyntax/supplier/FigureSupplier.java
@@ -9,32 +9,43 @@ import core.basesyntax.template.Figure;
 import java.util.Random;
 
 public class FigureSupplier {
+    public static final Random random;
+    public static final ColorSupplier colorSupplier;
+    public static final int FIGURE_COUNT;
+    public static final int MAX_BALL_NUMBER;
+
+    static {
+        random = new Random();
+        colorSupplier = new ColorSupplier();
+        FIGURE_COUNT = 5;
+        MAX_BALL_NUMBER = 10;
+    }
 
     public Figure getRandomFigure() {
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        switch (random.nextInt(5)) {
+        switch (random.nextInt(FIGURE_COUNT)) {
             case 1:
-                return new Square(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
+                return new Square(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_BALL_NUMBER) + 1);
             case 2:
                 return new Rectangle(colorSupplier.getRandomColor(),
-                        random.nextInt(10) + 1,
-                        random.nextInt(10) + 1);
+                        random.nextInt(MAX_BALL_NUMBER) + 1,
+                        random.nextInt(MAX_BALL_NUMBER) + 1);
             case 3:
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextInt(10) + 1,
-                        random.nextInt(10) + 1);
+                        random.nextInt(MAX_BALL_NUMBER) + 1,
+                        random.nextInt(MAX_BALL_NUMBER) + 1);
             case 4:
-                return new Circle(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
+                return new Circle(colorSupplier.getRandomColor(),
+                        random.nextInt(MAX_BALL_NUMBER) + 1);
             default:
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(10) + 1,
-                        random.nextInt(10) + 1,
-                        random.nextInt(10) + 1);
+                        random.nextInt(MAX_BALL_NUMBER) + 1,
+                        random.nextInt(MAX_BALL_NUMBER) + 1,
+                        random.nextInt(MAX_BALL_NUMBER) + 1);
         }
     }
 
     public static Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), 10);
+        return new Circle(Color.WHITE.name(), MAX_BALL_NUMBER);
     }
 }
