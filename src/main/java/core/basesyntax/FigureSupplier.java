d43da9fa899fde7690e8e2830b1bfdcec0f30600
Malinskyi Daniil <danmalina2707@gmail.com>
2023-01-06 16:12:35 +0200

    all magic numbers were replaced by constants

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,6 +4,9 @@ import java.util.Random;
 
 public class FigureSupplier {
     public static final int FIGURE_COUNT = 5;
+    public static final int MAX_BOUND = 100;
+    public static final int DEFAULT_RADIUS = 10;
+    public static final Color WHITE = Color.WHITE;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
@@ -11,26 +14,26 @@ public class FigureSupplier {
         int randNum = random.nextInt(FIGURE_COUNT);
         switch (randNum) {
             case (0):
-                return new Circle(colorSupplier.getRandomColor(), random.nextInt(100));
+                return new Circle(colorSupplier.getRandomColor(), random.nextInt(MAX_BOUND));
             case (1):
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(100), random.nextInt(100));
+                        random.nextInt(MAX_BOUND), random.nextInt(MAX_BOUND));
             case (2):
                 return new Rectangle(colorSupplier.getRandomColor(),
-                        random.nextInt(100), random.nextInt(100));
+                        random.nextInt(MAX_BOUND), random.nextInt(MAX_BOUND));
             case (3):
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextInt(100), random.nextInt(100));
+                        random.nextInt(MAX_BOUND), random.nextInt(MAX_BOUND));
             case (4):
                 return new Square(colorSupplier.getRandomColor(),
-                        random.nextInt(100));
+                        random.nextInt(MAX_BOUND));
             default:
                 return getDefaultFigure();
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE, 10);
+        return new Circle(WHITE, DEFAULT_RADIUS);
     }
 }
 
