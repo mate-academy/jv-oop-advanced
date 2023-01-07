fd4589a5a9826fd2bd9c00d865178c33ffc59ad2
Yaroslav Ometiukh <yaroslav.ometiukh@outlook.com>
2023-01-07 18:57:26 +0200

    +Changed&renamed constants that describe size of the figure in FigureSupplier

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int DEFAULT_SIZE = 10;
     private static final Color DEFAULT_COLOR = Color.WHITE;
-    private static final int MAX_LENGHT = 30;
-    private static final int MIN_LENGHT = 1;
+    private static final int DEFAULT_SIZE = 10;
+    private static final int MIN_SIZE = 1;
+    private static final int MAX_SIZE = 30 - MIN_SIZE;
+
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
@@ -19,28 +20,28 @@ public class FigureSupplier {
             case CIRCLE:
                 return new Circle(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT);
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
             case RECTANGLE:
                 return new Rectangle(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT,
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT);
+                        random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
             case ISOSCELES_TRAPEZOID:
                 return new IsoscelesTrapezoid(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT,
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT,
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT);
+                        random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
             case RIGHT_TRIANGLE:
                 return new RightTriangle(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT,
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT,
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT);
+                        random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
             case SQUARE:
                 return new Square(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(MAX_LENGHT - MIN_LENGHT) + MIN_LENGHT);
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
             default:
                 return getDefaultFigure();
         }
