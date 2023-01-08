ed226faee64f3a1f01ba23920020eb394264d1fb
Aleksandr Zahorodnii <git.hub.zagar@gmail.com>
2023-01-08 22:02:15 +0100

    fixed after review

--- src/main/java/core/basesyntax/figure/FigureSupplier.java
+++ src/main/java/core/basesyntax/figure/FigureSupplier.java
@@ -1,5 +1,6 @@
 package core.basesyntax.figure;
 
+import core.basesyntax.figure.color.Color;
 import core.basesyntax.figure.color.ColorSupplier;
 import core.basesyntax.figure.name.FigureNameSupplier;
 import java.util.Random;
@@ -7,11 +8,32 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int BOUND_NUMBER = 10;
     private static final int DEFAULT_RADIUS = 10;
-    private static final String DEFAULT_COLOR = "white";
+    private static final String DEFAULT_COLOR = Color.WHITE.name().toLowerCase();
     private ColorSupplier colorSupplier = new ColorSupplier();
     private FigureNameSupplier figureNameSupplier = new FigureNameSupplier();
     private Random random = new Random();
     private String color;
+
+    public Figure getRandomFigure() {
+        color = colorSupplier.getRandomColor();
+        switch (figureNameSupplier.getRandomName()) {
+            case SQUARE:
+                return getSquare();
+            case TRIANGLE:
+                return getRightTriangle();
+            case RECTANGLE:
+                return getRectangle();
+            case CIRCLE:
+                return getCircle();
+            case TRAPEZOID:
+                return getIsoscelesTrapezoid();
+            default: return getDefaultFigure();
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
+    }
 
     private Figure getCircle() {
         int radius = random.nextInt(BOUND_NUMBER);
@@ -40,26 +62,5 @@ public class FigureSupplier {
         int downSide = random.nextInt(BOUND_NUMBER);
         int height = random.nextInt(BOUND_NUMBER);
         return new IsoscelesTrapezoid(color, upperSide, downSide, height);
-    }
-
-    public Figure getRandomFigure() {
-        color = colorSupplier.getRandomColor();
-        switch (figureNameSupplier.getRandomName()) {
-            case "SQUARE":
-                return getSquare();
-            case "TRIANGLE":
-                return getRightTriangle();
-            case "RECTANGLE":
-                return getRectangle();
-            case "CIRCLE":
-                return getCircle();
-            case "TRAPEZOID":
-                return getIsoscelesTrapezoid();
-            default: return getDefaultFigure();
-        }
-    }
-
-    public Figure getDefaultFigure() {
-        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
--- src/main/java/core/basesyntax/figure/name/FigureNameSupplier.java
+++ src/main/java/core/basesyntax/figure/name/FigureNameSupplier.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class FigureNameSupplier {
     private Random random = new Random();
 
-    public String getRandomName() {
+    public FigureName getRandomName() {
         int index = random.nextInt(FigureName.values().length);
-        return FigureName.values()[index].name();
+        return FigureName.values()[index];
     }
 }
