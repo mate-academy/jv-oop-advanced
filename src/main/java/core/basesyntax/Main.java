a888970822a910d6aaacb505ea601973e1eb2c2f
Oleksii Velykyi <velykyi.lesha@gmail.com>
2023-01-11 21:08:34 +0200

    initial commit

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Square square = new Square("blue", 2.0);
+        square.draw();
+    }
+}
---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Square extends Figure implements Drawing {
+    private double sideSize;
+
+    public Square(String color, double sideSize) {
+        super(color);
+        this.sideSize = sideSize;
+    }
+    @Override
+    public void draw() {
+        System.out.println(this);
+    }
+    @Override
+    public double getArea() {
+        return sideSize * sideSize;
+    }
+    @Override
+    public String toString() {
+        return "Figure: Square, " + "Color: " + this.getColor() + ", Side Size: " + sideSize + ", Area: " + this.getArea();
+    }
+}
