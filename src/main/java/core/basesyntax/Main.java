f99d8c6bb3a1b70b028567dbd2f0415390a4564d
Maxim Skory <devisrush@gmail.com>
2023-01-13 03:10:46 +0200

    Class Main was created, psvm was added, Figure array was added

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Main {
+    public static final int NUMBER_OF_FIGURES = 6;
+
+    public static void main(String[] args) {
+        Figure [] figures = new Figure[NUMBER_OF_FIGURES];
+    }
+}
