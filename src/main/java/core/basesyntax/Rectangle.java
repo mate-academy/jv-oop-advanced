e0cddcfb51d89a731d5ab76cc31688ec2d5e5c32
Melnychuk <melnychukmyhailo@gmail.com>
2023-01-16 15:13:57 +0200

    figures added

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private double sideA;
+    private double sideB;
+
+    public Rectangle(double sideA, double sideB, String color) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    @Override
+    public double getArea() {
+        return sideA * sideB;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: " + getArea()
+                + " sq.units, sideA: " + sideA
+                + " sq.units, sideB: " + sideB
+                + " units, color: " + getColor());
+    }
+}
