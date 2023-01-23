e6de1aa1d2f45f68ed994b7f4f0056c096dbb4a6
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-23 17:44:02 +0200

    Restore after deleted class Square

---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Square extends Figure {
+    private int side;
+
+    public Square(int side, String color) {
+        this.side = side;
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: square, area: " + area() + " units, side: " + side + " units, color: " + color);
+    }
+
+    @Override
+    public double area() {
+        return (double)(side * side);
+    }
+}
