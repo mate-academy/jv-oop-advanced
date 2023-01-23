edadd86b5f718b09b9d20a9edaaa8fd547c1a9a4
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-23 21:18:14 +0200

    Fixed whitespaces in class Main

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class IsoscelesTrapezoid extends Figure{
+public class IsoscelesTrapezoid extends Figure {
     private int upperBase;
     private int lowerBase;
     private int height;
@@ -15,11 +15,12 @@ public class IsoscelesTrapezoid extends Figure{
     @Override
     public void draw() {
         System.out.println("Figure: trapezoid, area: " + area() + " units, upperBase: " + upperBase
-                + " units, lowerBase: " + lowerBase + " units, height: " + height + " units, color: " + color);
+                + " units, lowerBase: " + lowerBase + " units, height: " + height
+                + " units, color: " + color);
     }
 
     @Override
     public double area() {
-        return  (double)((upperBase + lowerBase) / 2) * height;
+        return (double)((upperBase + lowerBase) / 2) * height;
     }
 }
