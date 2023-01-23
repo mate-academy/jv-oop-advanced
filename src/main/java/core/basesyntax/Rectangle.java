3d5dff993e9fffcc0e832e0890175c0c7e98aff8
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-23 17:44:26 +0200

    Restore after deleted class Rectangle

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private int height;
+    private int width;
+
+    public Rectangle(int height, int width, String color) {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: " + this.area() + " units, height: " + height
+                + " units, width: " + width + " units, color: " + color);
+    }
+
+    @Override
+    public double area() {
+        return  (double)height * width;
+    }
+}
