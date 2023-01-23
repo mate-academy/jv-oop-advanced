b4e77a8e0cf730bbb512ffb1a2f9540d230041d2
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-23 21:28:11 +0200

    Fixed characters in line in class Square

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -10,7 +10,8 @@ public class Square extends Figure {
 
     @Override
     public void draw() {
-        System.out.println("Figure: square, area: " + area() + " units, side: " + side + " units, color: " + color);
+        System.out.println("Figure: square, area: " + area() + " units, side: " + side
+                + " units, color: " + color);
     }
 
     @Override
