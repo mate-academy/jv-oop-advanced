07b6bab15e357461221f86c3c8fef9a98b16a724
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-23 22:05:19 +0200

    Fixed abstract class Figure, added constructor and getter

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -2,4 +2,12 @@ package core.basesyntax;
 
 abstract class Figure implements AreaCalculator, PaintFigure {
     private String color;
+
+    public Figure(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
