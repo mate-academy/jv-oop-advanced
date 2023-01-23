dde0994fe020244f113c150a8afab9296e163986
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-23 21:14:06 +0200

    Fixed whitespace in class Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Figure[] figures = new Figure[6];
         FigureSupplier figureSupplier = new FigureSupplier();
 
-        for (int i = 0; i < figures.length; i++ ) {
+        for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
             } else {
