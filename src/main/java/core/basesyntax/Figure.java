143005eb7bf3ac74b877874f8496a04f8883b3b0
Denys Dominskyi <dominskyj@gmail.com>
2023-02-07 00:18:52 +0200

    Solution6 fixed some mistakes

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Figure implements Drawable, AreaCalculator {
-    protected Color color = new ColorSupplier().getRandomColor();
-    public static final int SITE_MAX_LENGTH = 100;
+    static final int SITE_MAX_LENGTH = 100;
     protected static final double SIDE = new Random().nextInt(SITE_MAX_LENGTH);
+    protected Color color = new ColorSupplier().getRandomColor();
 
     @Override
     public double area() {
