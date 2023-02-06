6c406b0a7df8602755652439e62c4447410ee7a4
potrap <potrapmax@gmail.com>
2023-02-06 16:09:14 +0200

    added class-level variable random

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random random = new Random();
+
     public static Color getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         return Color.values()[index];
