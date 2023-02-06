e3e6d5f14e38ef064fd3ef6f636f8510096ea9bd
WallRun <dimonty2003@gmail.com>
2023-02-06 14:21:56 +0200

    Added AreaCalculator interface and reverted changes to pom.xml

---
+++ src/main/java/core/basesyntax/base/AreaCalculator.java
@@ -0,0 +1,5 @@
+package core.basesyntax.base;
+
+public interface AreaCalculator {
+    double area();
+}
--- src/main/java/core/basesyntax/base/Figure.java
+++ src/main/java/core/basesyntax/base/Figure.java
@@ -1,11 +1,9 @@
 package core.basesyntax.base;
 
-public abstract class Figure implements Drawable {
+public abstract class Figure implements Drawable, AreaCalculator {
     protected String color;
 
     public Figure(String color) {
         this.color = color;
     }
-
-    public abstract double area();
 }
