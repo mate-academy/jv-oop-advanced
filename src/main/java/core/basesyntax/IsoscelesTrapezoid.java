9d9e894e6228fc46041dc9ad6ccf60788ae06623
Yaroslav Chornyi <chorniy.j@gmail.com>
2023-02-09 01:05:48 +0200

    Create class IsoscelesTrapezoid with two override methods

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure implements Information {
+    private final double longBase;
+    private final double shortBase;
+    private final double height;
+
+    public IsoscelesTrapezoid(Color color, double longBase, double shortBase, double height) {
+        this.setColor(color);
+        this.longBase = longBase;
+        this.shortBase = shortBase;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+
+        return ((longBase + shortBase) / 2) * height;
+    }
+
+    @Override
+    public void printInformation() {
+        System.out.println("Figure: isosceles trapezoid, area: "
+                + area() + " sq.units, longBase: "
+                + longBase + " units, shortBase: "
+                + shortBase + " units, height: "
+                + height + " units, color: " + getColor().name());
+    }
+}
