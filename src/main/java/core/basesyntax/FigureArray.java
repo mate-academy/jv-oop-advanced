cadd0de140796e34eb6c7904c658f59d1e161a5c
Yaroslav Chornyi <chorniy.j@gmail.com>
2023-02-09 01:24:10 +0200

    Change class HelloWorlds to class FigureArray when create array with half of random figure and half of default figure

---
+++ src/main/java/core/basesyntax/FigureArray.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureArray {
+    private static final int ARRAY_LENGTH = 14;
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] array = new Figure[random.nextInt(ARRAY_LENGTH)];
+
+        for (int i = 0; i < array.length; i++) {
+            if (i < array.length / 2) {
+                array[i] = figureSupplier.getRandomFigure();
+                array[i].printInformation();
+            } else {
+                array[i] = figureSupplier.getDefaultFigure();
+                array[i].printInformation();
+            }
+        }
+    }
+}
