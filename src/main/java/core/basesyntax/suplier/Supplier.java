32bdeb9baeac9c74c8ca2dba59f36687320114fd
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-02-08 15:57:19 +0200

    Made random field like usual private field and added getter to this.

--- src/main/java/core/basesyntax/suplier/Supplier.java
+++ src/main/java/core/basesyntax/suplier/Supplier.java
@@ -3,7 +3,11 @@ package core.basesyntax.suplier;
 import java.util.Random;
 
 public abstract class Supplier {
-    public static final Random RANDOM = new Random();
     public static final int MIN_VALUE = 1;
     public static final int MAX_VALUE = 20;
+    private Random random = new Random();
+
+    public Random getRandom() {
+        return random;
+    }
 }
