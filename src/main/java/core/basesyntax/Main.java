4da71be8d10190f99676023bab396ec67fee6f8c
Ilko Good <ilko.engineering@gmail.com>
2023-02-08 18:15:31 +0200

    Main fields updated once more

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Main {
     private static final int AMOUNT = 10;
-    private static final FigureSupplier figureSupplier = new FigureSupplier();
 
     public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < AMOUNT; i++) {
             if (i < AMOUNT / 2) {
                 System.out.println(figureSupplier.getRandomFigure().draw());
