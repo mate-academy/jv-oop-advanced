c0636ccfb3fced6b332f9b480473fb60ab386978
Ilko Good <ilko.engineering@gmail.com>
2023-02-08 18:11:58 +0200

    Main fields updated

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 public class Main {
     private static final int AMOUNT = 10;
-    private static final FigureSupplier FIGURE_SUPPLIER = new FigureSupplier();
+    private static final FigureSupplier figureSupplier = new FigureSupplier();
 
     public static void main(String[] args) {
         for (int i = 0; i < AMOUNT; i++) {
             if (i < AMOUNT / 2) {
-                System.out.println(FIGURE_SUPPLIER.getRandomFigure().draw());
+                System.out.println(figureSupplier.getRandomFigure().draw());
             } else {
-                System.out.println("Default_" + FIGURE_SUPPLIER.getDefaultFigure().draw());
+                System.out.println("Default_" + figureSupplier.getDefaultFigure().draw());
             }
         }
     }
