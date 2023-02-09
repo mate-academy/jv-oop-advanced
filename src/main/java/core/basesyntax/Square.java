6782c4a1d79c40e1857b96535c624b9892ed8a04
kromch <94765048+kromch@users.noreply.github.com>
2023-02-09 12:05:57 +0200

    Create Square.java

---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+class Square extends Figure {
+    private double side;
+
+    public Square(double side, ColorSupplier cs) {
+        super(cs);
+        this.side = side;
+        calcSquare();
+    }
+
+    public void draw() {
+        System.out.println(this.getClass().getSimpleName() + ": square, area: "
+                + String.format("%.2f", square) + " sq.units, side: "
+                + String.format("%.2f", side) + " units, color: " + color);
+	}
+		
+	private void calcSquare() {
+	    this.square  = this.side * this.side;
+	}
+}
