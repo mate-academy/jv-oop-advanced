aa341bfd2338f594a06eeb2bd24f7d4d4ff7d829
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-09 23:06:59 +0200

    Update RightTriangle.java

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -5,20 +5,20 @@ public class RightTriangle extends Figure {
     private final double secondLeg;
 
     public RightTriangle(Color color, double firstLeg, double secondLeg) {
-        this.setColor(color);
+        setColor(color);
         this.firstLeg = firstLeg;
         this.secondLeg = secondLeg;
     }
 
     @Override
-    public double area() {
+    public double calculateArea() {
         return 0.5 * (firstLeg * secondLeg);
     }
 
     @Override
     public void printInformation() {
         System.out.println("Figure: right triangle, area: "
-                + area() + " sq.units, firstLeg and secondLeg: "
+                + calculateArea() + " sq.units, firstLeg and secondLeg: "
                 + firstLeg + " and " + secondLeg
                 + " units, color: " + getColor().name());
     }
