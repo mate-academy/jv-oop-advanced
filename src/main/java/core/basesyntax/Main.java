6f96e3b521e1cdab4b1838e7bf697d8ae23ab110
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-02-09 19:46:44 +0200

    add main method with iteration

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final int ARRAY_LENGTH = 6;
 
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (i < ARRAY_LENGTH / 2) {
+                System.out.println(figureSupplier.getRandomFigure().draw());
+            } else {
+                System.out.println(figureSupplier.getDefaultFigure().draw());
+            }
+        }
     }
 }
