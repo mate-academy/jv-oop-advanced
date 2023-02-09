da464b40ef3eff6b62a53896d98abf73d7a3e38f
kromch <94765048+kromch@users.noreply.github.com>
2023-02-09 13:48:13 +0200

    Update Circle.java

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Circle extends Figure {
     private double radius;
-    private static final double PI = 3.14159265359;
+    static private final double PI = 3.14159265359;
 
     public Circle(double radius, ColorSupplier colorSuppliers) {
         super(colorSuppliers);
