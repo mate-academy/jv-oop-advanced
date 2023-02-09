ac17220f18b722c61261023ee0ac7fdf3ffa2fbc
kromch <94765048+kromch@users.noreply.github.com>
2023-02-09 16:43:20 +0200

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class HelloWorld {
+    private static final int ARRAY_SIZE = 6;
+
     public static void main(String[] args) {
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[ARRAY_SIZE];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length; i++) {
             figures[i] = i < figures.length / 2 ? figureSupplier.getRandomFigure()
