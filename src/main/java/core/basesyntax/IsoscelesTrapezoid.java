13ff3922b7c493e8704f315d7597b0a765092d6d
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-09 17:07:01 +0200

    Update IsoscelesTrapezoid.java

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class IsoscelesTrapezoid extends Figure implements Information {
+public class IsoscelesTrapezoid extends Figure {
     private final double longBase;
     private final double shortBase;
     private final double height;
