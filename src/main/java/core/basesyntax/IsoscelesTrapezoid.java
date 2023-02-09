7dcf98b553e04d2054cfc8c2b9824760e5b44fb1
kromch <94765048+kromch@users.noreply.github.com>
2023-02-09 12:28:51 +0200

    Create IsoscelesTrapezoid.java

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private double firstLeg;
+    private double secondLeg;
+    private double height;
+
+    public IsoscelesTrapezoid(double firstLeg, double secondLeg, 
+                              double height, ColorSupplier colorSupplier) {
+        super(ColorSupplier);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+        this.height = height;
+        calcSquare();
+    }
+
+    public void draw() {
+        System.out.println(this.getClass().getSimpleName() + ": square, area: "
+                + String.format("%.2f", square) + " sq.units, firstLeg: "
+                + String.format("%.2f", firstLeg)
+                + ", secondLeg: " + String.format("%.2f", secondLeg) + ", height: "
+                + String.format("%.2f", height) + " units, color: " + color);
+    }
+
+    private void calcSquare() {
+        this.square = ((firstLeg + secondLeg) / 2) * height;
+    }
+}
