46ea8706ff4ca1c69687aede10e92fa960e92607
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-02-09 18:09:31 +0200

    Renamed local variables in default case of switch.

--- src/main/java/core/basesyntax/suplier/FigureSupplier.java
+++ src/main/java/core/basesyntax/suplier/FigureSupplier.java
@@ -1,6 +1,6 @@
 package core.basesyntax.suplier;
 
-import core.basesyntax.Figures;
+import core.basesyntax.FigureName;
 import core.basesyntax.figure.Circle;
 import core.basesyntax.figure.Figure;
 import core.basesyntax.figure.IsoscelesTrapezoid;
@@ -8,35 +8,36 @@ import core.basesyntax.figure.Rectangle;
 import core.basesyntax.figure.RightTriangle;
 import core.basesyntax.figure.Square;
 
-public class FigureSupplier extends Supplier {
+public class FigureSupplier extends RandomSupplier {
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         Figure figure;
-        switch (Figures.values()[getRandom().nextInt(Figures.values().length)]) {
+        switch (FigureName.values()[getRandom().nextInt(FigureName.values().length)]) {
             case CIRCLE:
-                int radius = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
+                int radius = getRandomValue();
                 figure = new Circle(colorSupplier.getRandomColor(), radius);
                 break;
             case SQUARE:
-                int side = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
+                int side = getRandomValue();
                 figure = new Square(colorSupplier.getRandomColor(), side);
                 break;
             case RECTANGLE:
-                int width = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
-                int length = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
+                int width = getRandomValue();
+                int length = getRandomValue();
                 figure = new Rectangle(colorSupplier.getRandomColor(), width, length);
                 break;
             case RIGHT_TRIANGLE:
-                int firstLeg = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
-                int secondLeg = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
+                int firstLeg = getRandomValue();
+                int secondLeg = getRandomValue();
                 figure = new RightTriangle(colorSupplier.getRandomColor(), firstLeg, secondLeg);
                 break;
             default:
-                int a = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
-                int b = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
-                int h = getRandom().nextInt(Supplier.MAX_VALUE) + Supplier.MIN_VALUE;
-                figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(), a, b, h);
+                int upSide = getRandomValue();
+                int lowSide = getRandomValue();
+                int height = getRandomValue();
+                figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                        upSide, lowSide, height);
                 break;
         }
         return figure;
