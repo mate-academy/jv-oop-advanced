bf7146075db42622e341af2aafd4b750a22d2ccb
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-09 23:05:35 +0200

    Update IsoscelesTrapezoid.java

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -6,22 +6,21 @@ public class IsoscelesTrapezoid extends Figure {
     private final double height;
 
     public IsoscelesTrapezoid(Color color, double longBase, double shortBase, double height) {
-        this.setColor(color);
+        setColor(color);
         this.longBase = longBase;
         this.shortBase = shortBase;
         this.height = height;
     }
 
     @Override
-    public double area() {
-
+    public double calculateArea() {
         return ((longBase + shortBase) / 2) * height;
     }
 
     @Override
     public void printInformation() {
         System.out.println("Figure: isosceles trapezoid, area: "
-                + area() + " sq.units, longBase: "
+                + calculateArea() + " sq.units, longBase: "
                 + longBase + " units, shortBase: "
                 + shortBase + " units, height: "
                 + height + " units, color: " + getColor().name());
