873b1b3b14ad1f37b430ce683c92b92d055ff00c
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-09 23:00:33 +0200

    Update FigureArray.java

--- src/main/java/core/basesyntax/FigureArray.java
+++ src/main/java/core/basesyntax/FigureArray.java
@@ -6,18 +6,16 @@ public class FigureArray {
     private static final int ARRAY_LENGTH = 14;
 
     public static void main(String[] args) {
-
         Random random = new Random();
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] array = new Figure[random.nextInt(ARRAY_LENGTH)];
-
         for (int i = 0; i < array.length; i++) {
             if (i < array.length / 2) {
                 array[i] = figureSupplier.getRandomFigure();
-                array[i].printInformation();
+                array[i].draw();
             } else {
                 array[i] = figureSupplier.getDefaultFigure();
-                array[i].printInformation();
+                array[i].draw();
             }
         }
     }
