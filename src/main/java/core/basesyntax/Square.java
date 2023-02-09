3c1232d140911364f506ded12080b32d21860381
kromch <94765048+kromch@users.noreply.github.com>
2023-02-09 13:45:07 +0200

    Update Square.java

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -13,9 +13,9 @@ public class Square extends Figure {
         System.out.println(this.getClass().getSimpleName() + ": square, area: "
                 + String.format("%.2f", square) + " sq.units, side: "
                 + String.format("%.2f", side) + " units, color: " + color);
-	}
-		
+    }
+	
     private void calcSquare() {
-        this.square  = this.side * this.side;
+        this.square = this.side * this.side;
     }
 }
