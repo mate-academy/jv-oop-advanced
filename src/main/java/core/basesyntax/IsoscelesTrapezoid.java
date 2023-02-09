e1ec1c74f74c04234e45436128f913405dc2237c
kromch <94765048+kromch@users.noreply.github.com>
2023-02-09 17:17:03 +0200

    Update IsoscelesTrapezoid.java

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -11,18 +11,17 @@ public class IsoscelesTrapezoid extends Figure {
         this.firstLeg = firstLeg;
         this.secondLeg = secondLeg;
         this.height = height;
-        calcSquare();
     }
 
     public void draw() {
         System.out.println(this.getClass().getSimpleName() + ": square, area: "
-                + String.format("%.2f", square) + " sq.units, firstLeg: "
+                + String.format("%.2f", calculateArea()) + " sq.units, firstLeg: "
                 + String.format("%.2f", firstLeg)
                 + ", secondLeg: " + String.format("%.2f", secondLeg) + ", height: "
                 + String.format("%.2f", height) + " units, color: " + color);
     }
 
-    private void calcSquare() {
-        this.square = ((firstLeg + secondLeg) / 2) * height;
+    private double calculateArea() {
+        return ((firstLeg + secondLeg) / 2) * height;
     }
 }
