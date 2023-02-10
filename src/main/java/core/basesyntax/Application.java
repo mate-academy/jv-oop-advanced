18c443afbfb232ad52231245f8859a5aa149c88b
Artem Grunin <artem.grunin.2006@gmail.com>
2023-02-10 18:38:26 +0200

    removed redundant empty lines

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -9,15 +9,12 @@ public class Application {
     public static void main(String[] args) {
         final FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[NUMBER_OF_FIGURES];
-
         for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
         }
-
         for (int i = figures.length / 2; i < NUMBER_OF_FIGURES; i++) {
             figures[i] = figureSupplier.getDefaultFigure();
         }
-
         for (Figure figure : figures) {
             figure.printInfo();
         }
