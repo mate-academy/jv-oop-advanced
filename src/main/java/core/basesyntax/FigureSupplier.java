6428a1ae8b21b718ff6ecf0fa4763249d6f55e64
kromch <94765048+kromch@users.noreply.github.com>
2023-02-10 15:39:51 +0200

    Update FigureSupplier.java

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,46 +3,40 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final double DEFAULT_VALUE = 10.0;
-    private static final int MULTIPLAYER = 100;
+    private static final String DEFAULT_COLOR = "WHITE";
+    private static final double DEFAULT_RADIUS = 10.0;
+    private static final int NUMBER_RANGE = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Figure getRandomFigure() {
         FiguresShape[] figureshape = FiguresShape.values();
-        Figure fg = null;
         switch (figureshape[random.nextInt(figureshape.length)].name()) {
             case "SQUARE": {
-                fg = new Square(random.nextDouble() * MULTIPLAYER, colorSupplier.getRandomColor());
-                break;
+                return new Square(random.nextDouble() * MULTIPLAYER, colorSupplier.getRandomColor());
             }
             case "RECTANGLE": {
-                fg = new Rectangle(random.nextDouble() * MULTIPLAYER, random.nextDouble()
+                return new Rectangle(random.nextDouble() * MULTIPLAYER, random.nextDouble()
                     * MULTIPLAYER, colorSupplier.getRandomColor());
-                break;
             }
             case "RIGHTTRIANGLE": {
-                fg = new RightTriangle(random.nextDouble() * MULTIPLAYER, random.nextDouble()
+                retun new RightTriangle(random.nextDouble() * MULTIPLAYER, random.nextDouble()
                     * MULTIPLAYER, colorSupplier.getRandomColor());
-                break;
             }
             case "CIRCLE": {
-                fg = new Circle(random.nextDouble() * MULTIPLAYER, colorSupplier.getRandomColor());
-                break;
+                return new Circle(random.nextDouble() * MULTIPLAYER, colorSupplier.getRandomColor());
             }
             case "ISOSCELESTRAPEZOID": {
-                fg = new IsoscelesTrapezoid(random.nextDouble() * MULTIPLAYER, random.nextDouble()
+                return new IsoscelesTrapezoid(random.nextDouble() * MULTIPLAYER, random.nextDouble()
                     * MULTIPLAYER, random.nextDouble() * MULTIPLAYER,
                     colorSupplier.getRandomColor());
-                break;
             }
             default: {
             }
         }
-        return fg;
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(DEFAULT_VALUE);
+        return new Circle(DEFAULT_RADIUS, DEFAULT_COLOR);
     }
 }
