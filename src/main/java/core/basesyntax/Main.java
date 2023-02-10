5febedf632a476e11e9c842767777790dfe12f85
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-02-10 17:02:39 +0200

    fixed solution hw-4-jv-oop-advanced

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int NUMBER_OF_FIGURES = 6;
     public static void main(String[] args) {
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[NUMBER_OF_FIGURES];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
--- src/main/java/core/basesyntax/RandomDoubleGenerator.java
+++ src/main/java/core/basesyntax/RandomDoubleGenerator.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class RandomDoubleGenerator {
-    //private Random random = new Random();
+    private Random random = new Random();
     private static final double MAX_FOR_RANDOM_DOUBLE = 100.0;
     private static final double MIN_FOR_RANDOM_DOUBLE = 0.0;
 
     public Double getRandomDouble() {
-        return (new Random().nextDouble() * (MAX_FOR_RANDOM_DOUBLE - MIN_FOR_RANDOM_DOUBLE))
+        return (random.nextDouble() * (MAX_FOR_RANDOM_DOUBLE - MIN_FOR_RANDOM_DOUBLE))
                 + MIN_FOR_RANDOM_DOUBLE;
     }
 }
