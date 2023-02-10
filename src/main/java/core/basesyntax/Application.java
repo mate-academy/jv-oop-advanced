8e527e8366c474dfec7e83baa79f142c1ebc0b9e
Artem Grunin <artem.grunin.2006@gmail.com>
2023-02-10 11:34:05 +0200

    replaced block if in main

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -10,12 +10,12 @@ public class Application {
         final FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[NUMBER_OF_FIGURES];
 
-        for (int i = 0; i < figures.length; i++) {
-            if (i < figures.length / 2) {
-                figures[i] = figureSupplier.getRandomFigure();
-            } else {
-                figures[i] = figureSupplier.getDefaultFigure();
-            }
+        for (int i = 0; i < figures.length / 2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (int i = figures.length / 2; i < NUMBER_OF_FIGURES; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
         }
 
         for (Figure figure : figures) {
