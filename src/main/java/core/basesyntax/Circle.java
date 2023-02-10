e5f0aee36cfb1f2e7a0c890627f75dd518962749
kromch <94765048+kromch@users.noreply.github.com>
2023-02-10 17:28:39 +0200

    Update Circle.java

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -8,12 +8,14 @@ public class Circle extends Figure {
         this.radius = radius;
     }
 
+    @Override
     public void draw() {
         System.out.println(this.getClass().getSimpleName() + ": square, area: "
                 + String.format("%.2f", calculateArea()) + " sq.units, radius: "
                 + String.format("%.2f", radius) + " units, color: " + color);
     }
-    
+
+    @Override
     public double calculateArea() {
         return Math.PI * radius * radius;
     }
