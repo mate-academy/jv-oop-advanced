8ca4189dae4a46a64d5322404e49faac6fff55af
kromch <94765048+kromch@users.noreply.github.com>
2023-02-10 14:35:54 +0200

    Update Figure.java

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -3,15 +3,12 @@ package core.basesyntax;
 public abstract class Figure implements AreaCalculator, Drawable {
     protected final String defaultColor = "WHITE";
     protected String color;
-    private ColorSupplier colorSuppliers;
 
-    public Figure(ColorSupplier colorSuppliers) {
-        this.colorSuppliers = colorSuppliers;
-        this.color = this.colorSuppliers.getRandomColor();
+    public Figure() {
+        this.color = defaultColor;
     }
 
     public Figure(String color) {
-        this.colorSuppliers = new ColorSupplier();
         this.color = color;
     }
 }
