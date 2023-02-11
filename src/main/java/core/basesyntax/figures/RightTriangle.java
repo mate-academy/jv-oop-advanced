d6ea1923d2ada758d59342e09d8ad3c1827b533c
Maksym <maxim.yashyn@gmail.com>
2023-02-11 14:42:56 +0200

    override draw() and getArea() methods in RightTriangle class

--- src/main/java/core/basesyntax/figures/RightTriangle.java
+++ src/main/java/core/basesyntax/figures/RightTriangle.java
@@ -1,4 +1,22 @@
 package core.basesyntax.figures;
 
 public class RightTriangle extends Figure {
+    private int lenght;
+    private int width;
+
+    public RightTriangle(String color, int lenght, int width) {
+        super(color);
+        this.lenght = lenght;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return lenght * width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: right triangle, area: " + getArea() + ", length: " + lenght + ", width: " + width + ", color: " + getColor());
+    }
 }
