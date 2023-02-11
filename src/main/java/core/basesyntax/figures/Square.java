268bef07fdda453f311ce9e54461939374927d28
Maksym <maxim.yashyn@gmail.com>
2023-02-11 14:38:05 +0200

    override draw() and getArea() methods in Square class

--- src/main/java/core/basesyntax/figures/Square.java
+++ src/main/java/core/basesyntax/figures/Square.java
@@ -1,4 +1,20 @@
 package core.basesyntax.figures;
 
 public class Square extends Figure {
+    private int side;
+
+    public Square(String color, int side) {
+        super(color);
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public void draw() {
+
+    }
 }
