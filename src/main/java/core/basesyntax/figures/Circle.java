b7173faaceb8d2f4c91904b0027135592e32b936
Maksym <maxim.yashyn@gmail.com>
2023-02-11 14:38:26 +0200

    override draw() and getArea() methods in Circle class

--- src/main/java/core/basesyntax/figures/Circle.java
+++ src/main/java/core/basesyntax/figures/Circle.java
@@ -1,4 +1,19 @@
 package core.basesyntax.figures;
 
 public class Circle extends Figure {
+    private int radius;
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: circle, area: " + getArea() + ", radius: " + radius + ", color: " + getColor());
+    }
 }
