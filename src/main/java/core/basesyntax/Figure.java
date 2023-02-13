11837d3d4432d8aaacc44872d3e5cd44e115f00f
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-02-13 12:41:57 +0200

    added CONST for def.color, added setter and rewrote random color

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -10,4 +10,8 @@ public abstract class Figure implements AreaCalculator, Printable {
     public String getColor() {
         return color;
     }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int FIGURE_COUNTER = 5;
-    private static final int VALUE_COUNTER = 100;
+    private static final int MAX_VALUE = 100;
     private static final int DEFAULT_VALUE = 10;
+    private static final String DEFAULT_COLOR = "WHITE";
 
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
@@ -13,23 +14,23 @@ public class FigureSupplier {
     public Figure getRandomFigure() {
         switch (random.nextInt(FIGURE_COUNTER)) {
             case 0:
-                return new Circle(colorSupplier.getRandomColor(), random.nextInt(VALUE_COUNTER));
+                return new Circle(colorSupplier.getRandomColor(), random.nextInt(MAX_VALUE));
             case 1:
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(VALUE_COUNTER), random.nextInt(VALUE_COUNTER),
-                        random.nextInt(VALUE_COUNTER));
+                        random.nextInt(MAX_VALUE), random.nextInt(MAX_VALUE),
+                        random.nextInt(MAX_VALUE));
             case 2:
                 return new Rectangle(colorSupplier.getRandomColor(),
-                        random.nextInt(VALUE_COUNTER), random.nextInt(VALUE_COUNTER));
+                        random.nextInt(MAX_VALUE), random.nextInt(MAX_VALUE));
             case 3:
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextInt(VALUE_COUNTER), random.nextInt(VALUE_COUNTER));
+                        random.nextInt(MAX_VALUE), random.nextInt(MAX_VALUE));
             default:
-                return new Square(colorSupplier.getRandomColor(), random.nextInt(VALUE_COUNTER));
+                return new Square(colorSupplier.getRandomColor(), random.nextInt(MAX_VALUE));
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), DEFAULT_VALUE);
+        return new Circle(DEFAULT_COLOR, DEFAULT_VALUE);
     }
 }
