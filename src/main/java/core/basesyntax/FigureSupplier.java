5d33595b44aaf025c62dacc5c6e9588cc3096922
Gleb Chebotarov <taronassvolt@gmail.com>
2023-02-13 13:46:50 +0200

    Changes

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,13 +5,9 @@ import java.security.SecureRandom;
 public class FigureSupplier {
     private static final double DEFAULT_RADIUS = 10;
     private static final Color DEFAULT_COLOR = Color.WHITE;
-    private static final double BOUND_OF_RANDOM = 100;
     private final ColorSupplier randomColor = new ColorSupplier();
     private final SecureRandom random = new SecureRandom();
 
-    public double getRandomNumber() {
-        return random.nextDouble(BOUND_OF_RANDOM);
-    }
 
     public TypeFigure getRandomTypeFigure() {
         int randomFigure = random.nextInt(TypeFigure.values().length);
@@ -22,23 +18,23 @@ public class FigureSupplier {
         switch (getRandomTypeFigure()) {
             case CIRCLE:
                 return new Circle(randomColor.getRandomColor(),
-                        getRandomNumber() * Circle.MAX_RADIUS);
+                        random.nextDouble() * Circle.MAX_RADIUS);
             case SQUARE:
                 return new Square(randomColor.getRandomColor(),
-                        getRandomNumber() * Square.MAX_SIDE);
+                        random.nextDouble() * Square.MAX_SIDE);
             case TRIANGLE:
                 return new RightTriangle(randomColor.getRandomColor(),
-                        getRandomNumber() * RightTriangle.MAX_FIRST_LEG,
-                        getRandomNumber() * RightTriangle.MAX_SECOND_LEG);
+                        random.nextDouble() * RightTriangle.MAX_FIRST_LEG,
+                        random.nextDouble() * RightTriangle.MAX_SECOND_LEG);
             case RECTANGLE:
                 return new Rectangle(randomColor.getRandomColor(),
-                        getRandomNumber()* Rectangle.MAX_FIRST_SIDE,
-                        getRandomNumber() * Rectangle.MAX_SECOND_SIDE);
+                        random.nextDouble() * Rectangle.MAX_FIRST_SIDE,
+                        random.nextDouble() * Rectangle.MAX_SECOND_SIDE);
             case TRAPEZOID:
                 return new IsoscelesTrapezoid(randomColor.getRandomColor(),
-                        getRandomNumber() * IsoscelesTrapezoid.MAX_HEIGHT,
-                        getRandomNumber() * IsoscelesTrapezoid.MAX_UPPER_SIDE,
-                        getRandomNumber() * IsoscelesTrapezoid.MAX_LOWER_SIDE);
+                        random.nextDouble() * IsoscelesTrapezoid.MAX_HEIGHT,
+                        random.nextDouble() * IsoscelesTrapezoid.MAX_UPPER_SIDE,
+                        random.nextDouble() * IsoscelesTrapezoid.MAX_LOWER_SIDE);
             default:
                 return getDefaultFigure();
         }
