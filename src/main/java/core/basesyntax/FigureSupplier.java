ca69d2123dd27258aa29b5cefb578489c7a61d24
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-02-13 11:43:09 +0200

    fixed-3 solution hw-4-jv-oop-advanced

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,10 +4,32 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final String DEFAULT_COLOR = Color.WHITE.name();
-    private static final double RADIUS = 10;
+    private static final double DEFAULT_RADIUS = 10;
     private ColorSupplier color = new ColorSupplier();
     private RandomDoubleGenerator randomDouble = new RandomDoubleGenerator();
     private Random random = new Random();
+
+    public Circle getDefaultFigure() {
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
+    }
+
+    public Figure getRandomFigure() {
+        int randomNumber = random.nextInt(FigureName.values().length);
+        switch (FigureName.values()[randomNumber]) {
+            case CIRCLE :
+                return getRandomCircle();
+            case SQUARE :
+                return getRandomSquare();
+            case RECTANGLE :
+                return getRandomRectangle();
+            case RIGHT_TRIANGLE :
+                return getRandomRightTriangle();
+            case ISOSCELES_TRAPEZOID :
+                return getRandomIsoscelesTrapezoid();
+            default:
+                return getDefaultFigure();
+        }
+    }
 
     private Circle getRandomCircle() {
         return new Circle(color.getRandomColor(), randomDouble.getRandomDouble());
@@ -30,30 +52,5 @@ public class FigureSupplier {
     private IsoscelesTrapezoid getRandomIsoscelesTrapezoid() {
         return new IsoscelesTrapezoid(color.getRandomColor(), randomDouble.getRandomDouble(),
                 randomDouble.getRandomDouble(), randomDouble.getRandomDouble());
-    }
-
-    public Circle getDefaultFigure() {
-        return new Circle(DEFAULT_COLOR, RADIUS);
-    }
-
-    public Figure getRandomFigure() {
-        FigureName[] figureNames = FigureName.values();
-        for (FigureName figureName : figureNames) {
-            switch (figureName) {
-                case CIRCLE :
-                    return getRandomCircle();
-                case SQUARE :
-                    return getRandomSquare();
-                case RECTANGLE :
-                    return getRandomRectangle();
-                case RIGHT_TRIANGLE :
-                    return getRandomRightTriangle();
-                case ISOSCELES_TRAPEZOID :
-                    return getRandomIsoscelesTrapezoid();
-                default:
-                    return getDefaultFigure();
-            }
-        }
-        return getDefaultFigure();
     }
 }
