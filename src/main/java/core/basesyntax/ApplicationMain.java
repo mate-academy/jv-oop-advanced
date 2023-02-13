e28e98777213bc24628d37205b30e0f107b92ef2
Maksym <maxim.yashyn@gmail.com>
2023-02-13 14:32:26 +0200

    deleted random object

--- src/main/java/core/basesyntax/ApplicationMain.java
+++ src/main/java/core/basesyntax/ApplicationMain.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 public class ApplicationMain {
     public static void main(String[] args) {
-        Random random = new Random();
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[8];
         for (int i = 0; i < figures.length; i++) {
