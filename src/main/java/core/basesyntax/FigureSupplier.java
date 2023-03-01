c93841c3ff23bc3ddcd73834d4bb2909138e6b46
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2023-02-28 22:03:38 -0500

    added changes to class FigureSupplier according comments

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,45 +6,66 @@ public class FigureSupplier {
     private static final int MAX_VALUE = 10;
     private static final int DEFAULT_SIZE = 10;
     private static final String DEFAULT_COLOR = String.valueOf(Color.WHITE).toLowerCase();
-    private static final String DEFAULT_FIGURE = String.valueOf(ExistFigure.CIRCLE).toLowerCase();
+    private static final String DEFAULT_FIGURE = String.valueOf(FigureType.CIRCLE).toLowerCase();
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
-    private ExistFigure randomFigure;
+    private FigureType randomFigure;
 
     public Figure getRandomFigure() {
-        randomFigure = ExistFigure.values()[random.nextInt(ExistFigure.values().length)];
+        randomFigure = FigureType.values()[random.nextInt(FigureType.values().length)];
         switch (randomFigure) {
             case SQUARE:
-                return new Square(randomFigure.name().toLowerCase(),
-                        colorSupplier.getRandomColor().toLowerCase(),
-                        random.nextInt(MAX_VALUE));
+                return getSquare();
             case RECTANGLE:
-                return new Rectangle(randomFigure.name().toLowerCase(),
-                        colorSupplier.getRandomColor().toLowerCase(),
-                        random.nextInt(MAX_VALUE),
-                        random.nextInt(MAX_VALUE));
+                return getRectangle();
             case RIGHT_TRIANGLE:
-                return new RightTriangle(randomFigure.name()
-                        .substring(randomFigure.name().indexOf("_") + 1)
-                        .toLowerCase(),
-                        colorSupplier.getRandomColor().toLowerCase(),
-                        random.nextInt(MAX_VALUE),
-                        random.nextInt(MAX_VALUE));
+                return getRightTriangle();
             case CIRCLE:
-                return new Circle(randomFigure.name().toLowerCase(),
-                        colorSupplier.getRandomColor().toLowerCase(),
-                        random.nextInt(MAX_VALUE));
+                return getCircle();
+            case ISOSCELES_TRAPEZOID:
+                return getIsoscelesTrapezoid();
             default:
-                return new IsoscelesTrapezoid(randomFigure.name()
-                        .substring(randomFigure.name().indexOf("_") + 1)
-                        .toLowerCase(),
-                        colorSupplier.getRandomColor().toLowerCase(),
-                        random.nextInt(MAX_VALUE),
-                        random.nextInt(MAX_VALUE),
-                        random.nextInt(MAX_VALUE));
+                return getRandomFigure();
         }
     }
 
+    private Figure getSquare() {
+        return new Square(randomFigure.name().toLowerCase(),
+                colorSupplier.getRandomColor().toLowerCase(),
+                random.nextInt(MAX_VALUE));
+    }
+
+    private Figure getRectangle () {
+        return new Rectangle(randomFigure.name().toLowerCase(),
+                colorSupplier.getRandomColor().toLowerCase(),
+                random.nextInt(MAX_VALUE),
+                random.nextInt(MAX_VALUE));
+    }
+
+    private Figure getRightTriangle() {
+        return new RightTriangle(randomFigure.name()
+                .substring(randomFigure.name().indexOf("_") + 1)
+                .toLowerCase(),
+                colorSupplier.getRandomColor().toLowerCase(),
+                random.nextInt(MAX_VALUE),
+                random.nextInt(MAX_VALUE));
+    }
+
+    private Figure getCircle() {
+        return new Circle(randomFigure.name().toLowerCase(),
+                colorSupplier.getRandomColor().toLowerCase(),
+                random.nextInt(MAX_VALUE));
+    }
+
+    private Figure getIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(randomFigure.name()
+                .substring(randomFigure.name().indexOf("_") + 1)
+                .toLowerCase(),
+                colorSupplier.getRandomColor().toLowerCase(),
+                random.nextInt(MAX_VALUE),
+                random.nextInt(MAX_VALUE),
+                random.nextInt(MAX_VALUE));
+    }
     public Figure getDefaultFigure() {
         return new Circle(DEFAULT_FIGURE, DEFAULT_COLOR, DEFAULT_SIZE);
     }
