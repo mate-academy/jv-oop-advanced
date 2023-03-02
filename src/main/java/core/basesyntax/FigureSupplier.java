87dd93ad79dff11395e3212e93b1b63ca64c835a
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-03-02 23:32:43 +0200

    make changes to the code after analyzed comments from mentor

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,37 +5,65 @@ import core.basesyntax.model.Color;
 import core.basesyntax.model.Figure;
 import core.basesyntax.model.IsoscelesTrapezoid;
 import core.basesyntax.model.Rectangle;
-import core.basesyntax.model.RightTriangle;
-import core.basesyntax.model.Square;
 import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAX_COUNT_FIGURES = 5;
-    private static final int MAX_RANDOM_VALUE = 20;
-    private final Figure circle = new Circle(10, Color.WHITE.name());
+    private static final int CIRCLE_RADIUS = 10;
+    private static final String CIRCLE_COLOR = Color.WHITE.name();
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Figure[] figures;
-
-    {
-        figures = new Figure[] {
-                new Circle(random.nextInt(MAX_RANDOM_VALUE), colorSupplier.getRandomColor()),
-                new IsoscelesTrapezoid(random.nextInt(MAX_RANDOM_VALUE),
-                        random.nextInt(MAX_RANDOM_VALUE),
-                        random.nextInt(MAX_RANDOM_VALUE), colorSupplier.getRandomColor()),
-                new RightTriangle(random.nextInt(MAX_RANDOM_VALUE),
-                        random.nextInt(MAX_RANDOM_VALUE),
-                        colorSupplier.getRandomColor()),
-                new Rectangle(random.nextInt(MAX_RANDOM_VALUE),
-                        random.nextInt(MAX_RANDOM_VALUE), colorSupplier.getRandomColor()),
-                new Square(random.nextInt(MAX_RANDOM_VALUE), colorSupplier.getRandomColor())};
-    }
+    private final NumberSupplier numberSupplier = new NumberSupplier();
 
     public Figure getRandomFigure() {
-        return figures[random.nextInt(MAX_COUNT_FIGURES)];
+        int figureNumber = random.nextInt(MAX_COUNT_FIGURES);
+
+        switch (figureNumber) {
+            case 0:
+                return getRandomCircle();
+            case 1:
+                return getRandomSquare();
+            case 2:
+                return getRandomIsoscelesTrapezoid();
+            case 3:
+                return getRandomRectangle();
+            case 4:
+                return getRandomRightTriangle();
+            default:
+                return null;
+        }
     }
 
     public Figure getDefaultFigure() {
-        return circle;
+        return new Circle(CIRCLE_RADIUS, CIRCLE_COLOR);
+    }
+
+    public Figure getRandomCircle() {
+        return new Circle(numberSupplier.getMaxRandomValue(),
+                colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomSquare() {
+        return new Circle(numberSupplier.getMaxRandomValue(),
+                colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(numberSupplier.getMaxRandomValue(),
+                numberSupplier.getMaxRandomValue(),
+                numberSupplier.getMaxRandomValue(),
+                colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomRectangle() {
+        return new Rectangle(numberSupplier.getMaxRandomValue(),
+                numberSupplier.getMaxRandomValue(),
+                colorSupplier.getRandomColor());
+    }
+
+    public Figure getRandomRightTriangle() {
+        return new Rectangle(numberSupplier.getMaxRandomValue(),
+                numberSupplier.getMaxRandomValue(),
+                colorSupplier.getRandomColor());
     }
 }
