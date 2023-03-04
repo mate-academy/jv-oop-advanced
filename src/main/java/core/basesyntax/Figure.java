f6da763d236c73fb77d9eb0722bca8b2e411f17a
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-03-04 15:58:34 +0100

    create separate INTERFACE Drawable with drawFigure() method

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Figure implements AreaCalculator {
+public class Figure implements AreaCalculator, Drawable{
     private String color;
 
     public String getColor() {
