c4f139617c6b496a97dc59747a5c47eee3e0daa2
AVykhovanok <58370326+AVykhovanok@users.noreply.github.com>
2023-03-07 19:37:46 +0200

    Update Figure.java

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -3,9 +3,6 @@ package core.basesyntax;
 public abstract class Figure implements Drawable,CalcArea {
     protected Color color;
 
-    public Figure() {
-    }
-
     public Figure(Color color) {
         this.color = color;
     }
