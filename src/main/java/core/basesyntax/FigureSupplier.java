7fb67ac5909a8f4797f3e4f45f74a0f4bf733f00
Ivan Mosiur <mosiurivsn@gmail.com>
2023-03-11 17:52:36 +0200

    In the FigureSupplier class changed the order of the method, added a permanent change DEFAULT_COLOR. In the Drawable interface deleted the empty line.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,37 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final Color DEFAULT_COLOR = Color.WHITE;
     private static final int FIGURE_COUNT = 5;
-    private static final int SIZE = 10;
+    private static final int DEFAULT_SIZE = 10;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-
-    private Figure getRandomCircle() {
-        return new Circle(colorSupplier.getRandomColor(),
-                random.nextInt(SIZE));
-    }
-
-    private Figure getRandomSquare() {
-        return new Square(colorSupplier.getRandomColor(),
-                random.nextInt(SIZE));
-    }
-
-    private Figure getRandomRectangle() {
-        return new Rectangle(colorSupplier.getRandomColor(),
-                random.nextInt(SIZE),
-                random.nextInt(SIZE));
-    }
-
-    private Figure getRandomRightTriangle() {
-        return new RightTriangle(colorSupplier.getRandomColor(),
-                random.nextInt(SIZE),
-                random.nextInt(SIZE));
-    }
-
-    private Figure getRandomIsoscelesTrapezoid() {
-        return new IsoscelesTrapezoid(random.nextInt(SIZE),
-                random.nextInt(SIZE),colorSupplier.getRandomColor());
-    }
 
     public Figure getRandomFigure() {
         int number = random.nextInt(FIGURE_COUNT);
@@ -54,6 +28,33 @@ public class FigureSupplier {
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE, SIZE);
+        return new Circle(DEFAULT_COLOR, DEFAULT_SIZE);
+    }
+
+    private Figure getRandomCircle() {
+        return new Circle(colorSupplier.getRandomColor(),
+                random.nextInt(DEFAULT_SIZE));
+    }
+
+    private Figure getRandomSquare() {
+        return new Square(colorSupplier.getRandomColor(),
+                random.nextInt(DEFAULT_SIZE));
+    }
+
+    private Figure getRandomRectangle() {
+        return new Rectangle(colorSupplier.getRandomColor(),
+                random.nextInt(DEFAULT_SIZE),
+                random.nextInt(DEFAULT_SIZE));
+    }
+
+    private Figure getRandomRightTriangle() {
+        return new RightTriangle(colorSupplier.getRandomColor(),
+                random.nextInt(DEFAULT_SIZE),
+                random.nextInt(DEFAULT_SIZE));
+    }
+
+    private Figure getRandomIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(random.nextInt(DEFAULT_SIZE),
+                random.nextInt(DEFAULT_SIZE),colorSupplier.getRandomColor());
     }
 }
