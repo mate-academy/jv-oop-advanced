6958a5187c443ae6351f36f373005c57cb0aa2ea
testSouth <southparaplan@gmail.com>
2023-03-14 17:08:27 +0200

    creat MAX_INT_FOR_FIGURE, NUMBER_OF_FIGURES , changed the implementation of the method getRandomFigure, Fixed the errors indicated by the mentor

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,27 +3,33 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    public static final int CIRCLE_RADIUS = 10;
+    public static final int DEFAULT_CIRCLE_RADIUS = 10;
+    public static final int MAX_INT_FOR_FIGURE = 100;
+    public static final int NUMBER_OF_FIGURES = 5;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Figure getRandomFigure() {
-        Circle circle = new Circle(colorSupplier.getRandomColor(), random.nextInt(100));
-        Square square = new Square(colorSupplier.getRandomColor(), random.nextInt(100));
-        RightTriangle rightTriangle = new RightTriangle(colorSupplier.getRandomColor(),
-                random.nextInt(100), random.nextInt(100));
-        Rectangle rectangle = new Rectangle(colorSupplier.getRandomColor(), random.nextInt(100),
-                random.nextInt(100));
-        IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid(colorSupplier
-                .getRandomColor(),
-                random.nextInt(100), random.nextInt(100), random
-                .nextInt(100));
-        Figure[] figures = new Figure[]{circle, square, rightTriangle,
-                rectangle, isoscelesTrapezoid};
-        return figures[random.nextInt(figures.length)];
+        int result = random.nextInt(NUMBER_OF_FIGURES);
+        if (result == 1) {
+            return new Circle(colorSupplier.getRandomColor(), random.nextInt(MAX_INT_FOR_FIGURE));
+        } else if (result == 2) {
+            return new Square(colorSupplier.getRandomColor(), random.nextInt(MAX_INT_FOR_FIGURE));
+        } else if (result == 3) {
+            return new RightTriangle(colorSupplier.getRandomColor(),
+                    random.nextInt(MAX_INT_FOR_FIGURE), random.nextInt(MAX_INT_FOR_FIGURE));
+        } else if (result == 4) {
+            return new Rectangle(colorSupplier.getRandomColor(), random.nextInt(MAX_INT_FOR_FIGURE),
+                    random.nextInt(MAX_INT_FOR_FIGURE));
+        } else {
+            return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                    random.nextInt(MAX_INT_FOR_FIGURE), random.nextInt(MAX_INT_FOR_FIGURE),
+                    random.nextInt(MAX_INT_FOR_FIGURE));
+        }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.toString(), CIRCLE_RADIUS);
+        return new Circle(Color.WHITE.toString(), DEFAULT_CIRCLE_RADIUS);
     }
 }
+
