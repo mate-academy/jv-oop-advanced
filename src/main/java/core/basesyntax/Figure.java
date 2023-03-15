0ac35934321f175fb00da43cac0d37259412cc9a
Ihor Tkachuk <iono19031@gmail.com>
2023-03-15 19:59:42 +0200

    canceled drawFigure() method

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements AreaCalculator {
+public abstract class Figure implements AreaCalculator, FigureDrawer {
     private String color;
 
     public String getColor() {
@@ -10,6 +10,4 @@ public abstract class Figure implements AreaCalculator {
     public void setColor(String color) {
         this.color = color;
     }
-
-    public abstract void drawFigure();
 }
