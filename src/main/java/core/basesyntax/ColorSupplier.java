1bca7470cec8c3de7bb456d38eff1a0a45a1c50d
Ihor Tkachuk <iono19031@gmail.com>
2023-03-15 19:57:24 +0200

    changed getRandomColor() method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final String[] colors = {"white", "black", "red", "blue", "yellow", "green"};
     private final Random random = new Random();
 
     public String getRandomColor() {
-        return colors[random.nextInt(colors.length)];
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
