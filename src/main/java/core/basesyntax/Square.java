f48775cc5439fbc17a8eeaa506faa6abfadf6aec
z1row <pmeysarosh2004@gmail.com>
2023-03-27 00:10:26 +0200

    Created Square class

---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Square extends Figure {
+    private final int side;
+
+    public Square(int side, String color) {
+        this.side = side;
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: square, area: " + String.format("%.2f", getArea()) + ", side: " + side + ", color: " + color);
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+}
