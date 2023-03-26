90bc0e502301c0a75083b16c3c3918c9da5f2760
z1row <pmeysarosh2004@gmail.com>
2023-03-27 00:09:21 +0200

    Created IsoscelesTrapezoid class

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private final int side;
+    private final int firstBase;
+    private final int secondBase;
+
+    public IsoscelesTrapezoid(int side, int firstBase, int secondBase, String color) {
+        this.side = side;
+        this.firstBase = firstBase;
+        this.secondBase = secondBase;
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, area: " + String.format("%.2f", getArea()) + ", side: " + side +
+                ", first Base: " + firstBase + ", secondBase: " + secondBase + ", color: " + color);
+    }
+
+    @Override
+    public double getArea() {
+        return (double) (firstBase + secondBase) / 4 * Math.sqrt(4 * Math.pow(side, 2) - Math.pow(firstBase - secondBase, 2));
+    }
+}
