8bca7149e13d1dc5678767289da802141045be02
z1row <pmeysarosh2004@gmail.com>
2023-03-27 00:10:17 +0200

    Created RightTriangle class

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure {
+    private final int side;
+
+    public RightTriangle(int side, String color) {
+        this.side = side;
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: right triangle, area: " + String.format("%.2f", getArea()) + ", side: " + side + ", color: " + color);
+    }
+
+    @Override
+    public double getArea() {
+        return (Math.sqrt(3) / 4) * Math.pow(side, 2);
+    }
+}
