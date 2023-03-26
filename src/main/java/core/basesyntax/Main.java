fc7f16ed0bd9af4afa835b9e55e9f5bbdd31f0e3
z1row <pmeysarosh2004@gmail.com>
2023-03-27 00:09:54 +0200

    Created Main class

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[6];
+        for (int i = 0; i < 6; i++) {
+            if (i < 6 / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+            figures[i].draw();
+        }
+    }
+}
