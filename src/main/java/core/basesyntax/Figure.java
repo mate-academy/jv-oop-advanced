517b4c5ffa9302562e025de9c40629b693322455
z1row <pmeysarosh2004@gmail.com>
2023-03-27 00:44:50 +0200

    added access mod.

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
 public abstract class Figure implements Drawing, GetArea {
-    String color;
+    protected String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
