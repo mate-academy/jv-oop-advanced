b6eb551bd279bfc843122ee64eb48edf0d61f7fb
z1row <pmeysarosh2004@gmail.com>
2023-03-27 00:10:09 +0200

    Created Rectangle class

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private final int length;
+    private final int width;
+
+    public Rectangle(int length, int width, String color) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: " + String.format("%.2f", getArea()) + ", length: " + length + ", width: " + width + ", color: " + color);
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+}
