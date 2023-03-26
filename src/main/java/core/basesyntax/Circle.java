7ec0076f510cecd4936452e3a4cab1fbe76612ab
z1row <pmeysarosh2004@gmail.com>
2023-03-27 00:06:09 +0200

    Created Circle class

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private final int radius;
+
+    public Circle(int radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: circle, area: " + String.format("%.2f", getArea()) + ", radius: " + radius + ", color: " + color);
+    }
+}
