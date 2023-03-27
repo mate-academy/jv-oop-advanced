72546520d0430b6b089b1b155ba91cea18e9b6ca
z1row <pmeysarosh2004@gmail.com>
2023-03-27 20:29:12 +0200

    Fixed syntax

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,16 +4,16 @@ import java.util.Random;
 
 public class FigureSupplier {
     private final Random random = new Random();
-    private final int FIGURES_BOUND = 5;
-    private final int LENGTH_BOUND = 100;
+    private final int FIGURESBOUND = 5;
+    private final int LENGTHBOUND = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         final String color = colorSupplier.getRandomColor();
-        final int index = random.nextInt(FIGURES_BOUND);
-        final int firstRandom = random.nextInt(LENGTH_BOUND);
-        final int secondRandom = random.nextInt(LENGTH_BOUND);
-        final int thirdRandom = random.nextInt(LENGTH_BOUND);
+        final int index = random.nextInt(FIGURESBOUND);
+        final int firstRandom = random.nextInt(LENGTHBOUND);
+        final int secondRandom = random.nextInt(LENGTHBOUND);
+        final int thirdRandom = random.nextInt(LENGTHBOUND);
         switch (index) {
             case 0:
                 return new Circle(firstRandom, color);
