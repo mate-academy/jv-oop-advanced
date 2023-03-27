96f35873477bc6a210f413682e0bf8bcc98f6997
z1row <pmeysarosh2004@gmail.com>
2023-03-27 14:02:28 +0200

    Made magic number final

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,24 +7,28 @@ public class FigureSupplier {
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        final int index = random.nextInt(5);
         final String color = colorSupplier.getRandomColor();
-        switch (index) {
+        final int INDEX = random.nextInt(5);
+        final int RANDOM_LENGTH_1 = random.nextInt(100);
+        final int RANDOM_LENGTH_2 = random.nextInt(100);
+        final int RANDOM_LENGTH_3 = random.nextInt(100);
+
+        switch (INDEX) {
             case 0:
-                return new Circle(random.nextInt(100), color);
+                return new Circle(RANDOM_LENGTH_1, color);
             case 1:
-                return new IsoscelesTrapezoid(random.nextInt(100),
-                        random.nextInt(100), random.nextInt(100), color);
+                return new IsoscelesTrapezoid(RANDOM_LENGTH_1, RANDOM_LENGTH_2,
+                        RANDOM_LENGTH_3, color);
             case 2:
-                return new Rectangle(random.nextInt(100), random.nextInt(100), color);
+                return new Rectangle(RANDOM_LENGTH_1, RANDOM_LENGTH_2, color);
             case 3:
-                return new RightTriangle(random.nextInt(100), color);
+                return new RightTriangle(RANDOM_LENGTH_1, RANDOM_LENGTH_2, color);
             default:
-                return new Square(random.nextInt(100), color);
+                return new Square(RANDOM_LENGTH_1, color);
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(10, Color.white.toString());
+        return new Circle(10, Color.WHITE.toString());
     }
 }
