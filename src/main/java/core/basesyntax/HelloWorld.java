ed2d921543a314acd14c19a2dc10fcaaa4118b69
Artem Kostenko <a_kostenco@yahoo.com>
2023-03-30 14:47:24 +0300

    make realization

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,24 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorld {
+import core.basesyntax.figures.Circle;
+import core.basesyntax.figures.Figure;
+import core.basesyntax.figures.RightTriangle;
+import core.basesyntax.figures.Square;
 
+public class HelloWorld {
+    private static FigureSupplier figureSupplier = new FigureSupplier();
+
+    public static void main(String[] args) {
+        Figure[] figures = {
+                figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(),
+                new Circle(27),
+                new Square(15),
+                new RightTriangle(25,30)
+        };
+        for (Figure figure: figures) {
+            figure.draw();
+        }
+    }
 }
