20ac6cc825561c8c9ab41e196d07aa610afff18e
Stipaxa <petrivs572@gmail.com>
2023-04-05 18:53:44 +0300

    Moved the creation of each shape into a separate private method.Moved value for default figure of class constant. Now, first half array of figures generated with random parameters, second - with default parameters

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         FigureSupplier supplier = new FigureSupplier();
-        Figure[] figures = new Figure[3];
+        Figure[] figures = new Figure[6];
         for (int i = 0; i < figures.length; i++) {
-            figures[i] = supplier.getRandomFigure();
+            if (i <= figures.length / 2 - 1) {
+                figures[i] = supplier.getRandomFigure();
+            } else {
+                figures[i] = supplier.getDefaultFigure();
+            }
             figures[i].draw();
         }
     }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,30 +4,52 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 5;
+    private static final String DEFAULT_COLOR = "white";
+    private static final int DEFAULT_RADIUS = 10;
 
     private Random random = new Random();
     private ColorSupplier randomColor = new ColorSupplier();
 
+    private Figure createRandomSquare() {
+        return new Square(randomColor.getRandomColor(), random.nextInt(101));
+    }
+
+    private Figure createRandomRectangle() {
+        return new Rectangle(randomColor.getRandomColor(),
+                random.nextInt(101), random.nextInt(101));
+    }
+
+    private Figure createRandomRightTriangle() {
+        return new RightTriangle(randomColor.getRandomColor(),
+                random.nextInt(101), random.nextInt(101));
+    }
+
+    private Figure createRandomCircle() {
+        return new Circle(randomColor.getRandomColor(), random.nextInt(101));
+    }
+
+    private Figure createRandomIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(randomColor.getRandomColor(), random.nextInt(101),
+                random.nextInt(101), random.nextInt(101));
+    }
+
     public Figure getDefaultFigure() {
-        return new Circle("white", 10);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 
     public Figure getRandomFigure() {
-        int figureNumber = random.nextInt(FIGURE_COUNT);
+        int figureNumber = random.nextInt(FIGURE_COUNT + 1);
         switch (figureNumber) {
             case 1:
-                return new Square(randomColor.getRandomColor(), random.nextInt(101));
+                return createRandomSquare();
             case 2:
-                return new Rectangle(randomColor.getRandomColor(),
-                        random.nextInt(101), random.nextInt(101));
+                return createRandomRectangle();
             case 3:
-                return new RightTriangle(randomColor.getRandomColor(),
-                        random.nextInt(101), random.nextInt(101));
+                return createRandomRightTriangle();
             case 4:
-                return new Circle(randomColor.getRandomColor(), random.nextInt(101));
+                return createRandomCircle();
             case 5:
-                return new IsoscelesTrapezoid(randomColor.getRandomColor(), random.nextInt(101),
-                        random.nextInt(101), random.nextInt(101));
+                return createRandomIsoscelesTrapezoid();
             default:
                 return getDefaultFigure();
         }
