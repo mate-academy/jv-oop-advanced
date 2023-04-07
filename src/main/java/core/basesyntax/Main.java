e9c647a7f945c12ef9352df92b7260c2a9f9b75c
Sieryi D <hunterst@ukr.net>
2023-04-07 12:41:28 +0300

    add changes to Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int NUMBER_FIGURE = 4;
+
     public static void main(String[] args) {
-        final int NUMBER_FIGURE = 4;
         Figures[] figures = new Figures[NUMBER_FIGURE];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int a = 0; a < figures.length; a++) {
