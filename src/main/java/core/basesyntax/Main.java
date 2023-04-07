7d0701d80b8b18f364b7225d36b0d5f134b1fe15
rus1980707 <rus1980707@gmail.com>
2023-04-07 12:31:04 +0300

    jv-oop-advanced changed all

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,17 +1,18 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int FIGURES = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[6];
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
-                figures[i].draw();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
-                figures[i].draw();
             }
+            figures[i].draw();
         }
     }
 }
