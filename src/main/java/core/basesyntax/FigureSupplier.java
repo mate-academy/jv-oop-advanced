83030334978a9a8c46197c8020805edddc8cc996
Stipaxa <petrivs572@gmail.com>
2023-04-07 20:14:20 +0300

    Remove comments

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,11 +4,33 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 5;
-    private static final String DEFAULT_COLOR = "white";
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
     private static final int DEFAULT_RADIUS = 10;
 
     private Random random = new Random();
     private ColorSupplier randomColor = new ColorSupplier();
+
+    public Figure getDefaultFigure() {
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
+    }
+
+    public Figure getRandomFigure() {
+        int figureNumber = random.nextInt(FIGURE_COUNT + 1);
+        switch (figureNumber) {
+            case 1:
+                return createRandomSquare();
+            case 2:
+                return createRandomRectangle();
+            case 3:
+                return createRandomRightTriangle();
+            case 4:
+                return createRandomCircle();
+            case 5:
+                return createRandomIsoscelesTrapezoid();
+            default:
+                return getDefaultFigure();
+        }
+    }
 
     private Figure createRandomSquare() {
         return new Square(randomColor.getRandomColor(), random.nextInt(101));
@@ -31,27 +53,5 @@ public class FigureSupplier {
     private Figure createRandomIsoscelesTrapezoid() {
         return new IsoscelesTrapezoid(randomColor.getRandomColor(), random.nextInt(101),
                 random.nextInt(101), random.nextInt(101));
-    }
-
-    public Figure getDefaultFigure() {
-        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
-    }
-
-    public Figure getRandomFigure() {
-        int figureNumber = random.nextInt(FIGURE_COUNT + 1);
-        switch (figureNumber) {
-            case 1:
-                return createRandomSquare();
-            case 2:
-                return createRandomRectangle();
-            case 3:
-                return createRandomRightTriangle();
-            case 4:
-                return createRandomCircle();
-            case 5:
-                return createRandomIsoscelesTrapezoid();
-            default:
-                return getDefaultFigure();
-        }
     }
 }
