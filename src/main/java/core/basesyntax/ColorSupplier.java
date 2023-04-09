2ae701a8e89cedb5f6f8da453a6a9322d32b4bff
Pavlo Polezhay <pasha200417@gmail.com>
2023-04-09 12:41:40 +0300

    added ColorSupplier

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class ColorSupplier {
+    private static final Random RANDOM = new Random();
+
+    public static Color getRandomColor() {
+        Color[] colors = Color.values();
+        int index = RANDOM.nextInt(colors.length);
+        return colors[index];
+    }
+}
