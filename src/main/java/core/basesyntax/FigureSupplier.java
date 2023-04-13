f810c797072b6d24be8de75dc1ce1c563469ac9a
Vlad Gryshkow <128516158+shaman1437@users.noreply.github.com>
2023-04-13 10:40:39 +0200

    Update FigureSupplier.java
     created private method in this class which will create all figures

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -1,31 +1,21 @@
 package core.basesyntax;
 import java.util.Random;
 public class FigureSupplier {
-    private static final Random random = new Random();
-
-    public static Figure getRandomFigure() {
+    private final Random random = new Random();
+    public Figure getRandomFigure() {
         String color = ColorSupplier.getRandomColor();
 
         switch (random.nextInt(5)) {
             case 0:
-                double side = random.nextDouble() * 10 + 1;
-                return new Square(side, color);
+                return createSquare(color);
             case 1:
-                double width = random.nextDouble() * 10 + 1;
-                double height = random.nextDouble() * 10 + 1;
-                return new Rectangle(width, height, color);
+                return createRectangle(color);
             case 2:
-                double sideA = random.nextDouble() * 10 + 1;
-                double sideB = random.nextDouble() * 10 + 1;
-                return new RightTriangle(sideA, sideB, color);
+                return createRightTriangle(color);
             case 3:
-                double radius = random.nextDouble() * 10 + 1;
-                return new Circle(radius, color);
+                return createCircle(color);
             case 4:
-                double sideT = random.nextDouble() * 10 + 1;
-                double sideY = random.nextDouble() * 10 + 1;
-                double height1 = random.nextDouble() * 10 + 1;
-                return new IsoscelesTrapezoid(sideT, sideY, height1, color);
+                return createIsoscelesTrapezoid(color);
             default:
                 throw new RuntimeException("Unexpected figure type");
         }
