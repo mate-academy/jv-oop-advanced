c69d2ed5742d4829ff73fad1a9ef86588eeda5c9
Ros <rospsix@gmail.com>
2023-04-14 13:20:06 +0100

    added 2 constant variable for using in getDefaultFigure() method,and created private method which create figures

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,34 +3,54 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final String DEFAULT_COLOR = "White";
+    private static final int DEFAULT_RADIUS = 10;
     private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int index = random.nextInt(6);
-        String color = new ColorSupplier().getRandomColor();
+        String color = colorSupplier.getRandomColor();
         switch (index) {
             case 0:
-                return new Square(color, getRandomNumber(1, 14));
+                return createRandomSquare(color);
             case 1:
-                return new Rectangle(color, getRandomNumber(1, 15), getRandomNumber(1, 18));
+                return createRandomRectangle(color);
             case 2:
-                return new RightTriangle(
-            color,
-            getRandomNumber(3, 5),
-            getRandomNumber(6, 15),
-            getRandomNumber(1, 20));
+                return createRandomTriangle(color);
             case 3:
-                return new Circle(color, getRandomNumber(0, 12));
+                return createRandomCircle(color);
             case 4:
-                return new IsoscelesTrapezoid(
-            color, getRandomNumber(1, 15), getRandomNumber(1, 15), getRandomNumber(1, 20));
+                return createRandomTrapezoid(color);
             default:
                 return null;
         }
     }
 
+    private Square createRandomSquare(String color) {
+        return new Square(color, getRandomNumber(1, 15));
+    }
+
+    private RightTriangle createRandomTriangle(String color) {
+        return new RightTriangle(
+        color, getRandomNumber(1, 15), getRandomNumber(2, 14), getRandomNumber(2, 16));
+    }
+
+    private Rectangle createRandomRectangle(String color) {
+        return new Rectangle(color, getRandomNumber(1, 15), getRandomNumber(2, 16));
+    }
+
+    private IsoscelesTrapezoid createRandomTrapezoid(String color) {
+        return new IsoscelesTrapezoid(
+        color, getRandomNumber(1, 15), getRandomNumber(2, 16), getRandomNumber(2, 16));
+    }
+
+    private Circle createRandomCircle(String color) {
+        return new Circle(color, getRandomNumber(1, 15));
+    }
+
     public Figure getDefaultFigure() {
-        return new Circle("white", 10);
+        return new Circle(DEFAULT_COLOR,DEFAULT_RADIUS);
     }
 
     private int getRandomNumber(int min, int max) {
