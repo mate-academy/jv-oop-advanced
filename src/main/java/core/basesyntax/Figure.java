9d2c8ae099d576bb30058d5eb7205243ff64ce57
Ros <rospsix@gmail.com>
2023-04-14 20:00:35 +0100

    Added second one interface Drawable

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements AreaCalculator {
+public abstract class Figure implements AreaCalculator, Drawable {
     private final String color;
 
     public Figure(String color) {
