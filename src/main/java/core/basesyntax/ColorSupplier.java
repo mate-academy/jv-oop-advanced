0c7cb0ed3e99a43e2e6661aa698cf21703ba64e3
Ros <rospsix@gmail.com>
2023-04-14 13:21:05 +0100

    In this class random use enum class for get colors from list

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final String[] colors = {"Red", "Green", "Blue", "White", "Black", "Orange"};
     private final Random random = new Random();
 
     public String getRandomColor() {
+        Color[] colors = Color.values();
         int index = random.nextInt(colors.length);
-        return colors[index];
+        return colors[index].name();
     }
 }
