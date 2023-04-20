b12478611f44490f56bfed770fc7668b04320f9c
Ihor <igor.doggy.2010@icloud.com>
2023-04-20 16:16:09 +0300

    Added fixes to solution: 1. Deleted field figure. 2. Added true random methods in FigureSupplier. 3. Changed the loop in main method. 4. Renamed areaCalculator() method to calculateArea().

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int FIGURES_AMOUNT = 5;
-    private static final double RANDOM_PROPERTIES = 25;
-    private static final double DEFAULT_CIRCLE_RADIUS = 10;
+    private static final double RANDOM_PROPERTIES = 25D;
+    private static final double DEFAULT_CIRCLE_RADIUS = 10D;
     private static final String DEFAULT_CIRCLE_COLOR = Color.WHITE.name();
     private final Random random = new Random();
     private final ColorSupplier supplier = new ColorSupplier();
