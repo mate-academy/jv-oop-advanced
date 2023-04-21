bee8cb324312e19203ad03b0e540680b76960751
MykytaKat <129322235+MykytaKat@users.noreply.github.com>
2023-04-21 11:45:45 +0200

    Update Rectangle.java

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -4,20 +4,20 @@ public class Rectangle extends Figure {
     private final int length;
     private final int width;
 
-    public Rectangle(String color, int length, int width) {
+    public Rectangle(Color color, int length, int width) {
         this.width = width;
         this.length = length;
-        setColor(Colors.valueOf(color));
+        setColor(Color.valueOf(String.valueOf(color)));
     }
 
     @Override
-    public double getArea() {
+    public double calculateArea() {
         return length * width;
     }
 
     @Override
-    public void getInfo() {
-        System.out.println("Figure: rectangle, area: " + getArea()
+    public void printInfo() {
+        System.out.println("Figure: rectangle, area: " + calculateArea()
                 + " sq.units, length: " + length
                 + " units, width: " + width
                 + " units, color: " + getColor().name().toLowerCase());
