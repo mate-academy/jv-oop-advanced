7b1cf79f70541393c2d1b880457c15eb726b9277
MykytaKat <129322235+MykytaKat@users.noreply.github.com>
2023-04-21 11:46:08 +0200

    Update RightTriangle.java

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -4,20 +4,20 @@ public class RightTriangle extends Figure {
     private final int height;
     private final int hypotenuse;
 
-    public RightTriangle(String color, int height, int hypotenuse) {
+    public RightTriangle(Color color, int height, int hypotenuse) {
         this.height = height;
         this.hypotenuse = hypotenuse;
-        setColor(Colors.valueOf(color));
+        setColor(Color.valueOf(String.valueOf(color)));
     }
 
     @Override
-    public double getArea() {
+    public double calculateArea() {
         return (hypotenuse * height) / 2;
     }
 
     @Override
-    public void getInfo() {
-        System.out.println("Figure: right triangle, area: " + getArea()
+    public void printInfo() {
+        System.out.println("Figure: right triangle, area: " + calculateArea()
                 + " sq.units, height: " + height
                 + " units, hypotenuse: " + hypotenuse
                 + " units, color: " + getColor().name().toLowerCase());
