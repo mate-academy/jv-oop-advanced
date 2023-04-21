18c7bd6ef264a73c98676ec312d9a7a28364ba69
Olena Protsailo <olenaprotsailo@gmail.com>
2023-04-21 18:58:45 +0300

    added style

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,9 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
-        String value = Colors.values() [index].toString();
-        return value;
-
+        return Colors.values() [index].toString();
     }
 }
 
