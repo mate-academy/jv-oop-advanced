f7ae4a20f3846598dd90c6397138ab1b628a5b65
Vladyslav Krutyus <kijdron@gmail.com>
2023-04-24 10:56:38 +0300

    task solution added after second review

--- src/main/java/core/basesyntax/arraygenerator/FigureArrayGenerator.java
+++ src/main/java/core/basesyntax/arraygenerator/FigureArrayGenerator.java
@@ -5,23 +5,23 @@ import core.basesyntax.suppliers.figuresuppliers.FigureSupplier;
 import java.util.Random;
 
 public class FigureArrayGenerator {
-    private final int figureCount = 5;
-    private final int arraySize = 6;
+    private static final int ARRAY_SIZE = 6;
+    private static final int FIGURE_COUNT = 5;
     private final FigureSupplier figureSupplier = new FigureSupplier();
     private final Random random = new Random();
 
     public Figure[] generateArray() {
-        Figure[] figures = new Figure[arraySize];
-        for (int i = 0; i < arraySize / 2; i++) {
+        Figure[] figures = new Figure[ARRAY_SIZE];
+        for (int i = 0; i < ARRAY_SIZE / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure(generateIndex());
         }
-        for (int i = arraySize / 2; i < arraySize; i++) {
+        for (int i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
             figures[i] = figureSupplier.getDefualtFigure();
         }
         return figures;
     }
 
     private int generateIndex() {
-        return random.nextInt(figureCount);
+        return random.nextInt(FIGURE_COUNT);
     }
 }
--- src/main/java/core/basesyntax/suppliers/figuresuppliers/FigureSupplier.java
+++ src/main/java/core/basesyntax/suppliers/figuresuppliers/FigureSupplier.java
@@ -3,46 +3,29 @@ package core.basesyntax.suppliers.figuresuppliers;
 import core.basesyntax.enums.Color;
 import core.basesyntax.figures.Circle;
 import core.basesyntax.figures.Figure;
-import core.basesyntax.figures.IsoscelesTrapezoid;
-import core.basesyntax.figures.Rectangle;
-import core.basesyntax.figures.RightTriangle;
-import core.basesyntax.figures.Square;
-import core.basesyntax.suppliers.colorsuppliers.ColorSupplier;
-import java.util.Random;
 
 public class FigureSupplier {
-    private final Color defaultColor = Color.WHITE;
-    private final int defaultSideLength = 10;
-    private final int minSideLength = 5;
-    private final int maxSideLength = 25;
-    private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Random random = new Random();
+    private static final int DEFAULT_SIDE_LENGTH = 10;
+    private static final Color DEFAULT_COLOR = Color.WHITE;
+    private final FigureGenerator figureGenerator = new FigureGenerator();
 
     public Figure getRandomFigure(int type) {
-        int[] figureParameters = generateParameters();
-        int firstParameter = figureParameters[0];
-        int secondParameter = figureParameters[1];
-        int thirdParameter = figureParameters[2];
         Figure figure;
         switch (type) {
-            case 1: figure = new Circle(getRandomColor(),
-                    firstParameter);
-            break;
-            case 2: figure = new IsoscelesTrapezoid(getRandomColor(),
-                    firstParameter,
-                    secondParameter,
-                    thirdParameter);
-            break;
-            case 3: figure = new Rectangle(getRandomColor(),
-                    firstParameter,
-                    secondParameter);
-            break;
-            case 4: figure = new RightTriangle(getRandomColor(),
-                    firstParameter,
-                    secondParameter);
-            break;
-            case 5: figure = new Square(getRandomColor(),
-                    firstParameter);
+            case 1:
+                figure = figureGenerator.getCircle();
+                break;
+            case 2:
+                figure = figureGenerator.getIsoscelesTrapezoid();
+                break;
+            case 3:
+                figure = figureGenerator.getRectangle();
+                break;
+            case 4:
+                figure = figureGenerator.getRightTriangle();
+                break;
+            case 5:
+                figure = figureGenerator.getSquare();
                 break;
             default:
                 figure = getDefualtFigure();
@@ -51,27 +34,6 @@ public class FigureSupplier {
     }
 
     public Figure getDefualtFigure() {
-        return new Circle(defaultColor.name(), defaultSideLength);
-    }
-
-    private String getRandomColor() {
-        return colorSupplier.getRandomColor();
-    }
-
-    private int[] generateParameters() {
-        int first = generateRandomNumber();
-        int second = generateRandomNumber();
-
-        while (second > first) {
-            second = generateRandomNumber();
-        }
-
-        int third = generateRandomNumber();
-
-        return new int[]{first, second, third};
-    }
-
-    private int generateRandomNumber() {
-        return minSideLength + random.nextInt(maxSideLength);
+        return new Circle(DEFAULT_COLOR.name(), DEFAULT_SIDE_LENGTH);
     }
 }
