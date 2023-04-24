84f6e3a5aea81ba67c4ee86cd1e8615656309433
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-04-24 17:47:11 +0300

    Check list errors have been removed

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -9,10 +9,10 @@ import core.basesyntax.figures.Square;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int MAX_NUMBER_OF_FIGURES = 5;
-    private static final double MAX_SIZE = 100d;
-    private static final double DEFAULT_RADIUS_OF_CIRCLE = 10d;
-    private static final String DEFAULT_COLOR = "white";
+    public static final int MAX_NUMBER_OF_FIGURES = 5;
+    public static final double MAX_SIZE = 100d;
+    public static final double DEFAULT_RADIUS_OF_CIRCLE = 10d;
+    public static final String DEFAULT_COLOR = "white";
 
     private final Random random;
     private final ColorSupplier colorSupplier;
@@ -25,25 +25,33 @@ public class FigureSupplier {
     public Figure getRandomFigure() {
 
         switch (random.nextInt(MAX_NUMBER_OF_FIGURES)) {
-            case 0 :
-                return new Circle(colorSupplier.getRandomColor(),
-                        random.nextDouble() * MAX_SIZE);
-            case 1 :
+            case 0:
+                double radius = random.nextDouble() * MAX_SIZE;
+                return new Circle(colorSupplier.getRandomColor(), radius);
+            case 1:
+                double topSide = random.nextDouble() * MAX_SIZE;
+                double bottomSide = random.nextDouble() * MAX_SIZE;
+                double hight = random.nextDouble() * MAX_SIZE;
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextDouble() * MAX_SIZE,
-                        random.nextDouble() * MAX_SIZE,
-                        random.nextDouble() * MAX_SIZE);
-            case 2 :
+                        topSide,
+                        bottomSide,
+                        hight);
+            case 2:
+                double sideA = random.nextDouble() * MAX_SIZE;
+                double sideB = random.nextDouble() * MAX_SIZE;
                 return new Rectangle(colorSupplier.getRandomColor(),
-                        random.nextDouble() * MAX_SIZE,
-                        random.nextDouble() * MAX_SIZE);
-            case 3 :
+                        sideA,
+                        sideB);
+            case 3:
+                double firstLeg = random.nextDouble() * MAX_SIZE;
+                double secondLeg = random.nextDouble() * MAX_SIZE;
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextDouble() * MAX_SIZE,
-                        random.nextDouble() * MAX_SIZE);
-            default :
+                        firstLeg,
+                        secondLeg);
+            default:
+                double side = random.nextDouble() * MAX_SIZE;
                 return new Square(colorSupplier.getRandomColor(),
-                        random.nextDouble() * MAX_SIZE);
+                        side);
         }
     }
 
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,12 +5,11 @@ import java.util.Random;
 
 public class Main {
     private static final int NUMBER_OF_FIGURES = 6;
+    private static final Random random = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier(random);
+    private static final FigureSupplier figureSupplier = new FigureSupplier(random, colorSupplier);
 
     public static void main(String[] args) {
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier(random);
-        FigureSupplier figureSupplier = new FigureSupplier(random, colorSupplier);
-
         Figure[] figures = new Figure[NUMBER_OF_FIGURES];
         for (int i = 0; i < NUMBER_OF_FIGURES / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
