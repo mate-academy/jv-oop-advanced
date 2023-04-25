9b5f53d26d19520f2e4014670bcab64986c0946f
Chris <tynkastya@gmail.com>
2023-04-25 13:33:57 +0300

    final implemented task

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,40 +3,40 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int FIGURES_COUNT = 5;
     private static final Color DEFAULT_COLOR = Color.WHITE;
     private static final int DEFAULT_RADIUS = 10;
     private static final int MAX_RANGE = 20;
     private final Random random = new Random();
-    private final Circle circle = new Circle();
-    private final Square square = new Square();
-    private final RightTriangle rightTriangle = new RightTriangle();
-    private final Rectangle rectangle = new Rectangle();
-    private final IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid();
     private final ColorSupplier colorSupplier = new ColorSupplier(random);
-    private final Figure[] figures = {circle, square, rightTriangle, rectangle, isoscelesTrapezoid};
 
     public Figure getRandomFigure() {
-        int randomNameFigure = random.nextInt(figures.length);
+        int randomNameFigure = random.nextInt(FIGURES_COUNT);
         switch (randomNameFigure) {
             case 0:
+                Circle circle = new Circle();
                 circle.setColor(colorSupplier.getRandomColor());
                 circle.setRadius(random.nextInt(MAX_RANGE) + 1);
                 return circle;
             case 1:
+                Square square = new Square();
                 square.setColor(colorSupplier.getRandomColor());
                 square.setSide(random.nextInt(MAX_RANGE) + 1);
                 return square;
             case 2:
+                RightTriangle rightTriangle = new RightTriangle();
                 rightTriangle.setColor(colorSupplier.getRandomColor());
                 rightTriangle.setFirstLeg(random.nextInt(MAX_RANGE) + 1);
                 rightTriangle.setSecondLeg(random.nextInt(MAX_RANGE) + 1);
                 return rightTriangle;
             case 3:
+                Rectangle rectangle = new Rectangle();
                 rectangle.setColor(colorSupplier.getRandomColor());
                 rectangle.setWidth(random.nextInt(MAX_RANGE) + 1);
                 rectangle.setLength(random.nextInt(MAX_RANGE) + 1);
                 return rectangle;
             default:
+                IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid();
                 isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
                 isoscelesTrapezoid.setHeight(random.nextInt(MAX_RANGE) + 1);
                 isoscelesTrapezoid.setFirstSide(random.nextInt(MAX_RANGE) + 1);
@@ -46,6 +46,7 @@ public class FigureSupplier {
     }
 
     public Figure getDefaultFigure() {
+        Circle circle = new Circle();
         circle.setColor(DEFAULT_COLOR);
         circle.setRadius(DEFAULT_RADIUS);
         return circle;
