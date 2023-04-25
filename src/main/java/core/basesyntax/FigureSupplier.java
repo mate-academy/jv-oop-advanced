6fc9e614acbbc8a1b2a1defdc39cc2302afbaf4d
Serhii Dudko <nechupara@gmail.com>
2023-04-25 16:34:03 +0300

    making random figures moves from switch to separate funcs

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class FigureSupplier {
     public static final int TOTAL_AMOUNT_OF_FIGURES = 5;
-    public static final String DEFAULT_FIGURE_COLOR = "white";
+    public static final String DEFAULT_FIGURE_COLOR = Colors.WHITE.name().toLowerCase();
     public static final double DEFAULT_FIGURE_SIZE = 10;
     public static final int MAX_FIGURE_SIZE = 100;
     public static final double MIN_DECIMAL_PART = 0.001;
@@ -14,11 +14,44 @@ public class FigureSupplier {
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     private double getRandomSize() {
-
         double randomNumber = random.nextDouble() * MAX_FIGURE_SIZE + MIN_DECIMAL_PART;
         String formattedRandomNumber = df.format(randomNumber);
 
         return Double.parseDouble(formattedRandomNumber);
+    }
+
+    private Figure getRandomSquare() {
+        String color = colorSupplier.getRandomColor();
+        double side = getRandomSize();
+        return new Square(color, side);
+    }
+
+    private Figure getRandomRectangle() {
+        String color = colorSupplier.getRandomColor();
+        double width = getRandomSize();
+        double length = getRandomSize();
+        return new Rectangle(color, width, length);
+    }
+
+    private Figure getRandomRightTriangle() {
+        String color = colorSupplier.getRandomColor();
+        double firstLeg = getRandomSize();
+        double secondLeg = getRandomSize();
+        return new RightTriangle(color, firstLeg, secondLeg);
+    }
+
+    private Figure getRandomCircle() {
+        String color = colorSupplier.getRandomColor();
+        double radius = getRandomSize();
+        return new Circle(color, radius);
+    }
+
+    private Figure getRandomIsoscelesTrapezoid() {
+        String color = colorSupplier.getRandomColor();
+        double topSide = getRandomSize();
+        double bottomSide = getRandomSize();
+        double height = getRandomSize();
+        return new IsoscelesTrapezoid(color, topSide, bottomSide, height);
     }
 
     public Figure getRandomFigure() {
@@ -26,29 +59,15 @@ public class FigureSupplier {
 
         switch (randomFigureNumber) {
             case 0:
-                String color = colorSupplier.getRandomColor();
-                double side = getRandomSize();
-                return new Square(color, side);
+                return getRandomSquare();
             case 1:
-                color = colorSupplier.getRandomColor();
-                double width = getRandomSize();
-                double length = getRandomSize();
-                return new Rectangle(color, width, length);
+                return getRandomRectangle();
             case 2:
-                color = colorSupplier.getRandomColor();
-                double firstLeg = getRandomSize();
-                double secondLeg = getRandomSize();
-                return new RightTriangle(color, firstLeg, secondLeg);
+                return getRandomRightTriangle();
             case 3:
-                color = colorSupplier.getRandomColor();
-                double radius = getRandomSize();
-                return new Circle(color, radius);
+                return getRandomCircle();
             default:
-                color = colorSupplier.getRandomColor();
-                double topSide = getRandomSize();
-                double bottomSide = getRandomSize();
-                double height = getRandomSize();
-                return new IsoscelesTrapezoid(color, topSide, bottomSide, height);
+                return getRandomIsoscelesTrapezoid();
         }
     }
 
