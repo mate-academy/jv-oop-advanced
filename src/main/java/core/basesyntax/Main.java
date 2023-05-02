eb18a5617c13a7e97996c736e6b4dc0ab530c033
Nikita <kaptetos@gmail.com>
2023-05-02 19:20:41 +0300

    changed to one cycle

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         DefaultFigureSupplier figureSupplier = new DefaultFigureSupplier();
         Figure[] figures = new Figure[6];
-        for (int i = 0; i < figures.length / 2; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-        for (int i = figures.length / 2; i < figures.length; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
         }
         for (Figure figure : figures) {
             figure.draw();
