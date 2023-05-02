5e1a8d72fd97c9616c109b65a7ee067add6d7bea
Nikita <kaptetos@gmail.com>
2023-05-02 16:44:18 +0300

    changed according to the recommendations and requirements

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
+        DefaultFigureSupplier figureSupplier = new DefaultFigureSupplier();
         Figure[] figures = new Figure[6];
         for (int i = 0; i < figures.length / 2; i++) {
-            figures[i] = FigureSupplier.getRandomFigure();
+            figures[i] = figureSupplier.getRandomFigure();
         }
         for (int i = figures.length / 2; i < figures.length; i++) {
-            figures[i] = FigureSupplier.getDefaultFigure();
+            figures[i] = figureSupplier.getDefaultFigure();
         }
         for (Figure figure : figures) {
             figure.draw();
