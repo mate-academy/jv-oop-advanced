2ca68d0fb5b9b2339fa96fec0cc5a422122d8f8b
Olga M <axiomkaonline@gmail.com>
2023-05-08 10:47:10 +0300

    fixed after review

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int FIGURES_COUNT = 5;
+    private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 30;
     private static final int DEFAULT_SIZE = 10;
+    private static final String DEFAULT_COLOR = "WHITE";
     private static final Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
@@ -27,34 +29,34 @@ public class FigureSupplier {
 
     private Square createRandomSquare() {
         return new Square(colorSupplier.getRandomColor(),
-        random.nextInt(MAX_NUMBER));
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
     }
 
     private Circle createRandomCircle() {
         return new Circle(colorSupplier.getRandomColor(),
-        random.nextInt(MAX_NUMBER));
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
     }
 
     private RightTriangle createRandomRightTriangle() {
         return new RightTriangle(colorSupplier.getRandomColor(),
-        random.nextInt(MAX_NUMBER),
-        random.nextInt(MAX_NUMBER));
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER,
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
     }
 
     private IsoscelesTrapezoid createRandomIsoscelesTrapezoid() {
         return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-        random.nextInt(MAX_NUMBER),
-        random.nextInt(MAX_NUMBER),
-        random.nextInt(MAX_NUMBER));
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER,
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER,
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
     }
 
     private Rectangle createRandomRectangle() {
         return new Rectangle(colorSupplier.getRandomColor(),
-        random.nextInt(MAX_NUMBER),
-        random.nextInt(MAX_NUMBER));
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER,
+        random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
     }
 
     public Figure createDefaultFigure() {
-        return new Circle(Color.WHITE.name(),DEFAULT_SIZE);
+        return new Circle(DEFAULT_COLOR,DEFAULT_SIZE);
     }
 }
