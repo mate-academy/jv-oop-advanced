fecc889e3fc0d92a5b732f7d0134dbcb2c5da30b
OLYA <Ole4kastar79@gmail.com>
2023-05-24 22:56:15 +0300

    fixed 16 add emptyLineSeparator ( CI build )

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int FIGURES_COUNT = 6;
+
     public static void main(String[] args) {
         Figure[] figures = new Figure[FIGURES_COUNT];
         FigureSupplier figureSupplier = new FigureSupplier();
