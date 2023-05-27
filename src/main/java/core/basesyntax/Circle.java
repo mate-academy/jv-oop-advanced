77398d479c8657f0c4ed0593972dd7ca597b13d5
Bohdan Poliienko <mignatov756@gmail.com>
2023-05-27 12:09:48 +0300

    oop advanced task solution

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+
+    private final int radius;
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return (double) Math.round(Math.PI * radius * radius);
+    }
+
+    @Override
+    public void draw() {
+        String name = "circle";
+        System.out.println("Figure: " + name + ", area: " + getArea() + " sq.units, radius: "
+                + radius + " units, color: " + getColor());
+
+    }
+}
---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        FigureSupplier newFigure = new FigureSupplier();
+        Figure[] figures = new Figure[4];
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = newFigure.getRandomFigure();
+            } else {
+                figures[i] = newFigure.getDefaultFigure();
+            }
+            figures[i].draw();
+        }
+    }
+}
