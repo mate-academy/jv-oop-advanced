c02121d8b8d5b0aac56cffad36542cb2cac884aa
happytroya <happytroya2001@gmail.com>
2023-05-31 13:59:04 +0300

    Figure class Maven mistakes fixed

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
-public abstract class Figure implements FigureMaths{
+public abstract class Figure implements FigureMaths {
     protected Color color;
+    
     public abstract void draw();
 }
