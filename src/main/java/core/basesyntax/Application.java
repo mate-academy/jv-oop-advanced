3a3cbaab1147b38002c97ae900696dd178382ef5
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-07 22:22:11 +0300

    make figureSupplier class variable

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Application {
     private static final Random RANDOM = new Random();
     private static final int FIGURES_COUNT = 10;
+    private static FigureSupplier figureSupplier = new FigureSupplier();
 
     public static void main(String[] args) {
         int randomNumber = RANDOM.nextInt(FIGURES_COUNT);
         int number = 0;
-        FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < randomNumber; i++) {
             Figure figure = figureSupplier.getRandomFigure();
             figure.draw();
