73883f04e730dd04bc790d9b9463c7296fee9306
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-07 22:20:49 +0300

    change constant variable names

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Application {
-    private static final Random random = new Random();
-    private static final int figuresCount = 10;
+    private static final Random RANDOM = new Random();
+    private static final int FIGURES_COUNT = 10;
 
     public static void main(String[] args) {
-        int randomNumber = random.nextInt(figuresCount);
+        int randomNumber = RANDOM.nextInt(FIGURES_COUNT);
         int number = 0;
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < randomNumber; i++) {
