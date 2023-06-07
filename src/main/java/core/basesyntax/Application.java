24fad4c9fd4152cc0df84f2e7a6985d4814f4341
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-07 22:53:16 +0300

    make one for loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,22 +4,21 @@ import java.util.Random;
 
 public class Application {
     private static final Random RANDOM = new Random();
-    private static final int FIGURES_COUNT = 10;
+    private static final int FIGURES_COUNT = 20;
     private static FigureSupplier figureSupplier = new FigureSupplier();
 
     public static void main(String[] args) {
         int randomNumber = RANDOM.nextInt(FIGURES_COUNT);
-        int number = 0;
+        int halfOfRandomNumber = randomNumber / 2;
         for (int i = 0; i < randomNumber; i++) {
-            Figure figure = figureSupplier.getRandomFigure();
-            figure.draw();
-            if (i == randomNumber / 2) {
-                number = i;
+            Figure figure;
+            if (i < halfOfRandomNumber) {
+                figure = figureSupplier.getRandomFigure();
+            } else {
+                figure = figureSupplier.getDefaultFigure();
             }
-        }
-        for (int i = number; i < randomNumber; i++) {
-            Figure figure = figureSupplier.getDefaultFigure();
             figure.draw();
+
         }
     }
 }
