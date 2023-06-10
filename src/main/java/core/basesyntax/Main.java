d64261578a1fac65c1d63c7107b29e7cdfbe2d6c
barbequeue <p99kab00@gmail.com>
2023-06-10 14:51:07 +0300

    Draw random and default shapes in Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,23 @@
 package core.basesyntax;
 
+import core.basesyntax.shape.Shape;
+import core.basesyntax.supplier.ShapeSupplier;
+
 public class Main {
     public static void main(String[] args) {
+        ShapeSupplier shapeSupplier = new ShapeSupplier();
 
+        Shape[] shapes = new Shape[] {
+                shapeSupplier.getRandom(),
+                shapeSupplier.getRandom(),
+                shapeSupplier.getRandom(),
+                shapeSupplier.getDefault(),
+                shapeSupplier.getDefault(),
+                shapeSupplier.getDefault()
+        };
+
+        for (Shape shape : shapes) {
+            shape.draw();
+        }
     }
 }
