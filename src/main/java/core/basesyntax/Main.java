1e522050cff18ba7f672742a2822e044e76ee0fa
barbequeue <p99kab00@gmail.com>
2023-06-11 16:07:21 +0300

    Create random and default shapes in a loop

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,17 +4,20 @@ import core.basesyntax.shape.Shape;
 import core.basesyntax.supplier.ShapeSupplier;
 
 public class Main {
+    private static final byte SHAPES_COUNT = 10;
+
     public static void main(String[] args) {
         ShapeSupplier shapeSupplier = new ShapeSupplier();
 
-        Shape[] shapes = new Shape[] {
-                shapeSupplier.getRandom(),
-                shapeSupplier.getRandom(),
-                shapeSupplier.getRandom(),
-                shapeSupplier.getDefault(),
-                shapeSupplier.getDefault(),
-                shapeSupplier.getDefault()
-        };
+        Shape[] shapes = new Shape[SHAPES_COUNT];
+
+        for (byte i = 0; i < SHAPES_COUNT / 2; i++) {
+            shapes[i] = shapeSupplier.getRandom();
+        }
+
+        for (byte i = SHAPES_COUNT / 2; i < SHAPES_COUNT; i++) {
+            shapes[i] = shapeSupplier.getDefault();
+        }
 
         for (Shape shape : shapes) {
             shape.draw();
