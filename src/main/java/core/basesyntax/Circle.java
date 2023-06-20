d43a059fb0cd807f17a7ee46f6a9dc8fcc45994c
I <dovgopolkir@gmail.com>
2023-06-20 21:50:45 +0300

    created class Circle; extended class Figure; overrided getArea(); created public void draw()

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -1,2 +1,25 @@
-package core.basesyntax;public class Circle {
+package core.basesyntax;
+
+import java.text.DecimalFormat;
+
+public class Circle extends Figure {
+    private double radius;
+    private Color color;
+
+    public Circle(double radius, Color color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+
+    public void draw() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        System.out.println("Figure: circle, area: " + decimalFormat.format(getArea()) + " sq.units, radius: " + (int) radius +
+                " units, color: " + color);
+    }
 }
