567d7f99a6922b110f69883ef8ae1beb7ced3677
I <dovgopolkir@gmail.com>
2023-06-20 21:47:30 +0300

    created class Rectangle; this class extended class Figure; overrided getArea() and created public void draw()

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,2 +1,25 @@
-package core.basesyntax;public class Rectangle {
+package core.basesyntax;
+
+import java.text.DecimalFormat;
+
+public class Rectangle extends Figure {
+    private double length;
+    private double width;
+    private Color color;
+
+    public Rectangle(double length, double width, Color color) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    public void draw() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        System.out.println("Figure: rectangle, area: " + decimalFormat.format(getArea()) + " sq.units, length: " + (int) length + " units, width: " + (int) width + " units, color: " + color);
+    }
 }
