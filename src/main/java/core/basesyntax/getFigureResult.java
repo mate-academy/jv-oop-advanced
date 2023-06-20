808ce7bb806d04f832654e722f9c24926b8c5bd7
I <dovgopolkir@gmail.com>
2023-06-20 21:52:04 +0300

    created Main class getFigureResult; released methods where we getArea and get draw

--- src/main/java/core/basesyntax/getFigureResult.java
+++ src/main/java/core/basesyntax/getFigureResult.java
@@ -3,8 +3,19 @@ package core.basesyntax;
 /**
  * Feel free to remove this class and create your own.
  */
-public class HelloWorld {
+public class getFigureResult {
     public static void main(String[] args) {
-
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[6];
+        for (int i = 0; i < 3; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+        for (int i = 3; i < 6; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
+        for (Figure figure: figures) {
+            figure.draw();
+            System.out.println();
+        }
     }
 }
