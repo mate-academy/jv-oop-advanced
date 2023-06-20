a8ae4bf42800c1c2907f23e30b60cc083888684c
I <dovgopolkir@gmail.com>
2023-06-20 21:45:02 +0300

    created class Square and this class extended figure class; overrided getArea() and created public void draw()

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -1,2 +1,23 @@
-package core.basesyntax;public class Square {
+package core.basesyntax;
+
+import java.text.DecimalFormat;
+
+public class Square extends Figure {
+    private double side;
+    private Color color;
+
+    public Square(double side, Color color) {
+        this.side = side;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    public void draw() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        System.out.println("Figure: square, area: " + decimalFormat.format(getArea()) + " sq.units, side: " + (int) side + " units, color: " + color);
+    }
 }
