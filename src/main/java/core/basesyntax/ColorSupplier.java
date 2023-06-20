ad09dceb6a60730e3eba68ae4399515456bac769
I <dovgopolkir@gmail.com>
2023-06-20 22:18:57 +0300

    replaced on the right place private static final int COLOR_COUNT

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random random = new Random();
     private static final int COLOR_COUNT = 9;
+    private Random random = new Random();
+
     public Color getRandomColor() {
         int colorIndex = random.nextInt(COLOR_COUNT);
         return Color.values()[colorIndex];
