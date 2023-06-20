77bb917c19296b64ff5bbb55102d91e2cc120e0e
I <dovgopolkir@gmail.com>
2023-06-20 21:53:40 +0300

    created class FigureSupplier; created method for getting Random Color

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -1,2 +1,33 @@
-package core.basesyntax;public class FigureSupplier {
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    private Random random = new Random();
+    private static final int NUMBER_OF_FIGURES_COUNT = 5;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        Color color = colorSupplier.getRandomColor();
+        int figureIndex = random.nextInt(NUMBER_OF_FIGURES_COUNT);
+        switch (figureIndex) {
+            case 0:
+                return new Square(random.nextDouble() * 10 + 1, color);
+            case 1:
+                return new Rectangle(random.nextDouble() * 10 + 1, random.nextDouble() * 10 + 1, color);
+            case 2:
+                return new RightTriangle(random.nextDouble() * 10 + 1, random.nextDouble() * 10 + 1, color);
+            case 3:
+                return new Circle(random.nextDouble() * 10 + 1, color);
+            case 4:
+                return new IsoscelesTrapezoid(random.nextDouble() * 10 + 1, random.nextDouble() * 10 + 1,
+                        random.nextDouble() * 10 + 1, color);
+            default:
+                throw new IllegalArgumentException("Invalid figure number: " + figureIndex);
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(10, Color.WHITE);
+    }
 }
