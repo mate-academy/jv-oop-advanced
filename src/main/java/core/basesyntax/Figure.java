dc6dd774e7e38f6e71f7a09df01f9262ae49274b
I <dovgopolkir@gmail.com>
2023-06-20 21:39:44 +0300

    created Figure class and implemented there interfaces AreaCalculator and Drawable; also Overrided these two implements

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Figure implements AreaCalculator, Drawable{
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
--- src/main/java/core/basesyntax/getFigureResult.java
+++ src/main/java/core/basesyntax/getFigureResult.java
@@ -4,5 +4,7 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
 
+    }
 }
