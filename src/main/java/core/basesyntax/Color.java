5904043ff73ff1757de6e26fc92e14997b960f1f
I <dovgopolkir@gmail.com>
2023-06-20 21:56:16 +0300

    created ENUM Color

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -1,2 +1,13 @@
-package core.basesyntax;public enum Color {
+package core.basesyntax;
+
+public enum Color {
+    BLACK,
+    YELLOW,
+    GREEN,
+    WHITE,
+    RED,
+    PURPLE,
+    BROWN,
+    GREY,
+    BLUE
 }
