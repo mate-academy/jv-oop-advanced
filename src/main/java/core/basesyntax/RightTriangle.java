013402d8b89c4f8767dbbd68d1b2c0814445ed2c
I <dovgopolkir@gmail.com>
2023-06-20 21:48:36 +0300

    created class RightTriangle; this class exteneded class Figure; overrided getArea(); created public void draw()

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -1,2 +1,27 @@
-package core.basesyntax;public class RightTriangle {
+package core.basesyntax;
+
+import java.text.DecimalFormat;
+
+public class RightTriangle extends Figure {
+    private double base;
+    private double height;
+    private Color color;
+
+    public RightTriangle(double base, double height, Color color) {
+        this.base = base;
+        this.height = height;
+        this.color = color;
+    }
+
+
+    @Override
+    public double getArea() {
+        return (base * height) / 2;
+    }
+
+    public void draw() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        System.out.println("Figure: right triangle, area: " + decimalFormat.format(getArea()) + " sq.units, base: " + (int) base +
+                " units, height: " + (int) height + " units, color: " + color);
+    }
 }
