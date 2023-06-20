0028bb35d14440a866ccce94fa0978f310447ca5
I <dovgopolkir@gmail.com>
2023-06-20 22:17:04 +0300

    added whitespace on the line 3 after implementation of the Drawable

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Figure implements AreaCalculator, Drawable{
+public class Figure implements AreaCalculator, Drawable {
     @Override
     public double getArea() {
         return 0;
