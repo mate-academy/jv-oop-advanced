38a86989fee96c458cced79e7e204a244bee31fe
I <dovgopolkir@gmail.com>
2023-06-21 20:55:00 +0300

    added changes in the class FigureSupplier; added constants for 10 and color.WHITE in the method public Figure getDefaultFigure; case 4 was made default added private methods in this class for figures

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int CIRCLE_RADIUS = 10;
+    private static final Color CIRCLE_COLOR = Color.WHITE;
     private static final int NUMBER_OF_FIGURES_COUNT = 5;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
@@ -12,29 +14,48 @@ public class FigureSupplier {
         int figureIndex = random.nextInt(NUMBER_OF_FIGURES_COUNT);
         switch (figureIndex) {
             case 0:
-                return new Square(random.nextDouble() * 10 + 1, color);
+                return getRandomSquare(color);
             case 1:
-                return new Rectangle(random.nextDouble() * 10
-                        + 1, random.nextDouble() * 10
-                        + 1, color);
+                return getRandomRectangle(color);
             case 2:
-                return new RightTriangle(random.nextDouble() * 10
-                        + 1, random.nextDouble() * 10
-                        + 1, color);
+                return getRandomRightTriangle(color);
             case 3:
-                return new Circle(random.nextDouble() * 10 + 1, color);
-            case 4:
-                return new IsoscelesTrapezoid(random.nextDouble() * 10
-                        + 1, random.nextDouble() * 10
-                        + 1,
-                        random.nextDouble() * 10
-                                + 1, color);
+                return getRandomCircle(color);
             default:
-                throw new IllegalArgumentException("Invalid figure number: " + figureIndex);
+                return getRandomIsoscelesTrapezoid(color);
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(10, Color.WHITE);
+        return new Circle(CIRCLE_RADIUS, CIRCLE_COLOR);
+    }
+
+    private Circle getRandomCircle(Color color) {
+        double radius = random.nextDouble() * 10 + 1;
+        return new Circle(radius, color);
+    }
+
+    private Square getRandomSquare(Color color) {
+        double side = random.nextDouble() * 10 + 1;
+        return new Square(side, color);
+    }
+
+    private Rectangle getRandomRectangle(Color color) {
+        double length = random.nextDouble() * 10 + 1;
+        double width = random.nextDouble() * 10 + 1;
+        return new Rectangle(length, width, color);
+    }
+
+    private RightTriangle getRandomRightTriangle(Color color) {
+        double base = random.nextDouble() * 10 + 1;
+        double height = random.nextDouble() * 10 + 1;
+        return new RightTriangle(base, height, color);
+    }
+
+    private IsoscelesTrapezoid getRandomIsoscelesTrapezoid(Color color) {
+        double baseA = random.nextDouble() * 10 + 1;
+        double baseB = random.nextDouble() * 10 + 1;
+        double height = random.nextDouble() * 10 + 1;
+        return new IsoscelesTrapezoid(baseA, baseB, height, color);
     }
 }
