25bf519852b45e2e72f672729a91877363a02648
I <dovgopolkir@gmail.com>
2023-06-21 22:07:52 +0300

    added changes in the class Figure; added getter and public Figure(Color color)

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,13 +1,15 @@
 package core.basesyntax;
 
 public abstract class Figure implements AreaCalculator, Drawable {
-    public Color color;
+    private Color color;
 
     public Figure(Color color) {
         this.color = color;
-    }
+    } //public Figure() {}
 
-    public Figure() {
-    }
+    public Color getColor() {
+        return color;
+    } //public abstract void draw();
 }
+
 
