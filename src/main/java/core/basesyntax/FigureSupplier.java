3104f3f71415c0bd4f7d8ffcdb5bf238fc7f0bc6
Sergei Kudelin <chertterner686@gmail.com>
2023-06-22 17:57:50 +0300

    fixed after mentor check

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int BOUND_100 = 100;
+    private static final int BOUND_10 = 10;
     private static final double DEFAULT_RADIUS = 10;
     private static final String DEFAULT_COLOR = Color.WHITE.name();
     private static final Shape[] SHAPES = Shape.values();
-    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Figure getDefaultFigure() {
@@ -16,20 +18,18 @@ public class FigureSupplier {
     public Figure getRandomFigure() {
         switch (SHAPES[random.nextInt(SHAPES.length)]) {
             case CIRCLE:
-                return new Circle(random.nextInt(100), COLOR_SUPPLIER.getRandomColor());
+                return new Circle(random.nextInt(BOUND_100), colorSupplier.getRandomColor());
             case ISOSCELES_TRAPEZOID:
-                return new IsoscelesTrapezoid(random.nextInt(100),
-                        random.nextInt(100), random.nextInt(10), COLOR_SUPPLIER.getRandomColor());
+                return new IsoscelesTrapezoid(random.nextInt(BOUND_100),
+                        random.nextInt(BOUND_100), random.nextInt(BOUND_10), colorSupplier.getRandomColor());
             case SQUARE:
-                return new Square(random.nextInt(100), COLOR_SUPPLIER.getRandomColor());
+                return new Square(random.nextInt(BOUND_100), colorSupplier.getRandomColor());
             case RECTANGLE:
-                return new Rectangle(random.nextInt(100),
-                        random.nextInt(100), COLOR_SUPPLIER.getRandomColor());
-            case RIGHT_TRIANGLE:
-                return new RightTriangle(random.nextInt(100),
-                        random.nextInt(100), COLOR_SUPPLIER.getRandomColor());
-            default: return null;
+                return new Rectangle(random.nextInt(BOUND_100),
+                        random.nextInt(BOUND_100), colorSupplier.getRandomColor());
+            default:
+                return new RightTriangle(random.nextInt(BOUND_100),
+                        random.nextInt(BOUND_100), colorSupplier.getRandomColor());
         }
     }
-
 }
--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -6,11 +6,14 @@ import java.util.Arrays;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    private static final int  FIGURE_ARRAY_LENGTH = 6;
+    private static final int MIDDLE_FIGURE_ARRAY_LENGTH = 3;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[FIGURE_ARRAY_LENGTH];
         for (int index = 0; index < figures.length; ++index) {
-            if (index < 3) {
+            if (index < MIDDLE_FIGURE_ARRAY_LENGTH) {
                 figures[index] = figureSupplier.getDefaultFigure();
             } else {
                 figures[index] = figureSupplier.getRandomFigure();
