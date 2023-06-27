21813bddf387d2799ada71b5d162de486f5ea98f
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-06-27 17:40:22 +0300

    changed FigureSupplier to class-level variable

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,9 +4,10 @@ import core.basesyntax.figures.Figure;
 import core.basesyntax.suppliers.FigureSupplier;
 
 public class Main {
+    private static final FigureSupplier figureSupplier = new FigureSupplier();
+
     public static void main(String[] args) {
         Figure[] figures = new Figure[6];
-        FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
