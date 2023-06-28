4248a6a443dd0280938b6cb2be67b82f73321f66
Visqe <Veetee@tutanota.com>
2023-06-28 20:53:46 +0300

    implemented main() method that creates array of figures and "draws" them

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,24 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorld {
+import core.basesyntax.figures.Figure;
 
+public class HelloWorld {
+    public static final int FIGURE_COUNT = 6;
+
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[FIGURE_COUNT];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        //fill first half of array
+        for (int i = 0; i < FIGURE_COUNT / 2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+        //fill second half of array
+        for (int i = FIGURE_COUNT / 2; i < FIGURE_COUNT; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
+        //draw each figure
+        for (int i = 0; i < FIGURE_COUNT; i++) {
+            figures[i].draw();
+        }
+    }
 }
