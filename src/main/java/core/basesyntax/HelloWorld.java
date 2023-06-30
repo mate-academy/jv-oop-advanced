d33d8924a76ab5e51faf04e4bf31faa5031bc010
Oleksandr Savenets <savenvoin@gmail.com>
2023-06-30 16:42:43 +0300

    my solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,20 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class HelloWorld {
+    private static final int NUMBER_OF_FIGURES = 6;
 
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[NUMBER_OF_FIGURES];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefoultFigure();
+            }
+        }
+        for (Figure figure: figures) {
+            figure.draw();
+        }
+    }
 }
