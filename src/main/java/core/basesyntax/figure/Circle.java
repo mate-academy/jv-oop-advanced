218c0c041a63e4bac2bf5ce8fda1dd51a2e27c08
yermilov.m <yermilov.m@hotmail.com>
2023-07-04 15:44:06 +0200

    added Circle class

---
+++ src/main/java/core/basesyntax/figure/Circle.java
@@ -0,0 +1,20 @@
+package core.basesyntax.figure;
+
+import core.basesyntax.template.Figure;
+
+public class Circle extends Figure {
+    private double radius;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public void draw() {
+        System.out.println("Figure: circle, area: " + getArea() + " sq.units, radius: " + radius + " units, color: " + getColor());
+    }
+}
