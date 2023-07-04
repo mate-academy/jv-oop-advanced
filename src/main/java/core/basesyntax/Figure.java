27112ae662f6c52399c6d4697172cc1c5419aeb9
yermilov.m <yermilov.m@hotmail.com>
2023-07-04 15:34:41 +0200

    added Figure class

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public abstract class Figure {
+    private String color;
+
+    public Figure(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract double getArea();
+}
