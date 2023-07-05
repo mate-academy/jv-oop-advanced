7980767f50569028cd3dbbb6916af88cfb9d8d24
yermilov.m <yermilov.m@hotmail.com>
2023-07-05 15:40:01 +0200

    added AreaCalculator interface, implemented by Figure class

--- src/main/java/core/basesyntax/template/Figure.java
+++ src/main/java/core/basesyntax/template/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax.template;
 
-public abstract class Figure implements Drawable {
+public abstract class Figure implements Drawable, AreaCalculator {
     private String color;
 
     public Figure(String color) {
