1ce250ca4e54693c51e2ea4da7ad4ffafa30ec95
Andrii Riaboshapka <postagama@gmail.com>
2023-07-09 01:03:23 +0300

    created new class IsoscelesTrapezoid

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private double base1;
+    private double base2;
+    private double height;
+
+    public IsoscelesTrapezoid(String color, double base1, double base2, double height) {
+        super(color);
+        this.base1 = base1;
+        this.base2 = base2;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return ((base1 + base2) / 2) * height;
+    }
+
+    @Override
+    public String getAdditionalInfo() {
+        return "base1: " + base1 + " units, base2: " + base2
+                + " units, height: " + height + " units";
+    }
+}
