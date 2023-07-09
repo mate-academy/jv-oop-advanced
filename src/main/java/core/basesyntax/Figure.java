12729ec7f8401dedd4029654981e397babe71045
Andrii Riaboshapka <postagama@gmail.com>
2023-07-09 14:57:34 +0300

    added new renamed implement AreaCalculable

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawable, Calculable {
+public abstract class Figure implements Drawable, AreaCalculable {
     private String color;
 
     public Figure(String color) {
