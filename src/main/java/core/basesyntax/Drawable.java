c7e103795a9cfc82b7038a4bd811b7f940657ecf
oKukHtina <olakukhtina95@gmail.com>
2023-07-11 23:44:09 +0300

    rename Interface

--- src/main/java/core/basesyntax/Drawable.java
+++ src/main/java/core/basesyntax/Drawable.java
@@ -1,5 +1,5 @@
 package core.basesyntax;
 
-public interface BehaviourFigures {
+public interface Drawable {
     void draw();
 }
--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements BehaviourFigures {
+public abstract class Figure implements Drawable {
     private String color;
 
     public Figure(String color) {
