00fec8bb9503adc7881f2c5975303f14f2e25d77
OSlovachevska <slovachevska.ok@gmail.com>
2023-07-11 19:12:55 +0300

    fixed_4

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private static final int RANDOM_SIZE_OF_ARRAY = 6;
     private static final FigureSupplier figureSupplier = new FigureSupplier();
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Figure[] figures = new Figure[RANDOM_SIZE_OF_ARRAY];
         for (int i = 0; i < RANDOM_SIZE_OF_ARRAY; i++) {
             if (i < RANDOM_SIZE_OF_ARRAY / 2) {
