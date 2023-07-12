a682f0536d7144548da6871710d4bbe4b5d0a7f5
Andrii Hryniuk <grinuk0706@gmail.com>
2023-07-12 11:06:27 +0300

    added fixes in mvn clean package

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final Random RANDOM = new Random();
+
     public String getRandomColor() {
         int indexOfColor = RANDOM.nextInt(Color.values().length - 1);
         return Color.values()[indexOfColor].name();
