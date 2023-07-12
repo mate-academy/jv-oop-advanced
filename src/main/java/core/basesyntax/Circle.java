ef4d62a8ad36131aeb7bbfcccd923d2eec97106c
L-181_5501 <slovachevska.ok@gmail.com>
2023-07-12 14:48:46 +0300

    fixed in 'hw-solution-fixed' branch

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -15,7 +15,7 @@ public class Circle extends Figure {
 
     @Override
     public void draw() {
-        System.out.println("Figure: circle, area: " + getArea() + " sq.units, side: "
-                + radius + " units, color: " + getColor());
+        System.out.println("Figure: circle, area: " + getArea()
+                + " sq.units, side: " + radius + " units, color: " + getColor());
     }
 }
