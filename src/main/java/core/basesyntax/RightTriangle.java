8cb4a9b6ff097f74a7968173ffb6d9008b2d33d5
breakingtumble <yaroslavglavatsky@gmail.com>
2023-07-14 20:16:23 +0300

    created RightTriangle class which extends abstract Figure class

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure {
+    private final int firstLeg;
+    private final int secondLeg;
+
+    public RightTriangle(String color, int firstLeg, int secondLeg) {
+        super(color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * firstLeg * secondLeg;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: right triangle, area: %.1f sq.units, firstLeg: %d units, secondLeg: %d units, color: %s%n", calculateArea(), firstLeg, secondLeg, getColor());
+    }
+}
