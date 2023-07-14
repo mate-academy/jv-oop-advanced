6ec72e55e02bea2a632b0048174e75b3cb11d96d
breakingtumble <yaroslavglavatsky@gmail.com>
2023-07-14 20:15:36 +0300

    created abstract Figure class which has 2 abstract parameters and 1 field

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public abstract class Figure {
+    private final String color;
+
+    public Figure(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract double calculateArea();
+
+    public abstract void draw();
+}
