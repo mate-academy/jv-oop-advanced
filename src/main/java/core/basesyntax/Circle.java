c7d42f7f5adbd335b5f6c17b9129fdef4e898644
breakingtumble <yaroslavglavatsky@gmail.com>
2023-07-14 20:16:02 +0300

    created Circle class which extends abstract Figure class

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private final int radius;
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: circle, area: %.1f sq.units, radius: %d units, color: %s%n", calculateArea(), radius, getColor());
+    }
+}
