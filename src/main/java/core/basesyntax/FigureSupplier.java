d639f3add34550042b278493ffb17dcde4148663
breakingtumble <yaroslavglavatsky@gmail.com>
2023-07-15 12:56:52 +0300

    renamed AreaCalculable to AreaCalculator, added constants for default values, added private methods to get each random figure

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,31 +5,51 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int FIGURE_COUNT = 5;
     private static final int UPPER_INTEGER_BOUND = 10;
+    private static final String WHITE_COLOR = Color.WHITE.name().toLowerCase();
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         switch (random.nextInt(FIGURE_COUNT)) {
             case 0:
-                return new Circle(colorSupplier.getRandomColor(), getRandomIntValue());
+                return getRandomCircle();
             case 1:
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), getRandomIntValue(),
-                        getRandomIntValue(), getRandomIntValue());
+                return getRandomTrapezoid();
             case 2:
-                return new Rectangle(colorSupplier.getRandomColor(), getRandomIntValue(),
-                        getRandomIntValue());
+                return getRandomRectangle();
             case 3:
-                return new RightTriangle(colorSupplier.getRandomColor(), getRandomIntValue(),
-                        getRandomIntValue());
+                return getRandomTriangle();
             case 4:
-                return new Square(colorSupplier.getRandomColor(), getRandomIntValue());
+                return getRandomSquare();
             default:
-                return new Circle(Color.WHITE.name(), getRandomIntValue());
+                return getDefaultFigure();
         }
+    }
+    private Figure getRandomCircle() {
+        return new Circle(colorSupplier.getRandomColor(), getRandomIntValue());
+    }
+
+    private Figure getRandomTrapezoid() {
+        return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), getRandomIntValue(),
+                getRandomIntValue(), getRandomIntValue());
+    }
+
+    private Figure getRandomRectangle() {
+        return new Rectangle(colorSupplier.getRandomColor(), getRandomIntValue(),
+                getRandomIntValue());
+    }
+
+    private Figure getRandomTriangle() {
+        return new RightTriangle(colorSupplier.getRandomColor(), getRandomIntValue(),
+                getRandomIntValue());
+    }
+
+    private Figure getRandomSquare() {
+        return new Square(colorSupplier.getRandomColor(), getRandomIntValue());
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name().toLowerCase(), 10);
+        return new Circle(WHITE_COLOR, 10);
     }
 
     private int getRandomIntValue() {
