4ad1841d7d309e66f56b6e3506a351776ee24737
Kolinko Oleksii <alexkolinko22@gmail.com>
2023-07-19 14:36:54 +0200

    jv-oop-advanced-HW4-fixed-getRandomFigure4

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,32 +6,38 @@ public class FigureSupplier {
     public static final int MAX_SIDE_LENGTH = 20;
     public static final double DEFAULT_RADIUS = 10;
     public static final String DEFAULT_COLOR = String.valueOf(Color.WHITE);
+    public static final int FIGURES_COUNT = 5;
     private final Random random = new Random();
     private final String color = new ColorSupplier().getRandomColor();
 
     public Figure getRandomFigure() {
         // Square getRandomFigure
         double side = random.nextInt(MAX_SIDE_LENGTH);
-        Figure square = new Square(side, color);
         // Rectangle getRandomFigure
         double width = random.nextInt(MAX_SIDE_LENGTH);
         double heigth = random.nextInt(MAX_SIDE_LENGTH);
-        Figure rectangle = new Rectangle(width, heigth, color);
         // Circle getRandomFigure
         double radius = random.nextInt(MAX_SIDE_LENGTH);
-        Figure circle = new Circle(radius, color);
         // RightTriangle getRandomFigure
         double firstLeg = random.nextInt(MAX_SIDE_LENGTH);
         double secondLeg = random.nextInt(MAX_SIDE_LENGTH);
-        Figure rightTriangle = new RightTriangle(firstLeg, secondLeg, color);
         // IsoscelesTrapezoid getRandomFigure
         double bottomBase = random.nextInt(MAX_SIDE_LENGTH);
         double upperBase = random.nextInt(MAX_SIDE_LENGTH);
         double equalSides = random.nextInt(MAX_SIDE_LENGTH);
-        Figure isoscelesTrapezoid = new IsoscelesTrapezoid(bottomBase,
-                upperBase, equalSides, color);
-        Figure[] figurs = {isoscelesTrapezoid, rectangle, circle, rightTriangle, square};
-        return figurs[random.nextInt(figurs.length)];
+        int randomNumber = random.nextInt(FIGURES_COUNT);
+        switch (randomNumber) {
+            case 0:
+                return new Rectangle(width, heigth, color);
+            case 1:
+                return new Circle(radius, color);
+            case 2:
+                return new RightTriangle(firstLeg, secondLeg, color);
+            case 3:
+                return new IsoscelesTrapezoid(bottomBase, upperBase, equalSides, color);
+            default:
+                return new Square(side, color);
+        }
     }
 
     public Figure getDefaultFigure() {
