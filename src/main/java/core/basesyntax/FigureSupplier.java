21f67af094890c6184a781ab1fb85e6be44468ff
Alexander Karpenko <karalexander@gmail.com>
2023-07-22 16:39:41 +0300

    all issues were corrected 2 Classes were modified

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,13 +6,40 @@ public class FigureSupplier {
     public static final int FIGURE_COUNT = 5;
     private final Random random = new Random();
 
-    public FigureList getRandomFigure() {
-        int figureNumber = random.nextInt(FIGURE_COUNT);
+    public Figure getRandomFigure() {
+        int figureCount = random.nextInt(FIGURE_COUNT);
         FigureList[] values = FigureList.values();
-        return values[figureNumber];
+        FigureList randomFigureType = values[figureCount];
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String randomColor = colorSupplier.getRandomColor();
+
+        switch (randomFigureType) {
+            case CIRCLE:
+                double radius = random.nextInt(100);
+                return new Circle(radius, randomColor);
+            case SQUARE:
+                double side = random.nextInt(100);
+                return new Square(side, randomColor);
+            case RECTANGLE: double length = random.nextInt(100);
+                double width = random.nextInt(100);
+                return new Rectangle(length, width, randomColor);
+            case ISOSCELES_TRAPEZOID:
+                double sideA = random.nextInt(100);
+                double sideB = random.nextInt(100);
+                double height = random.nextInt(100);
+                return new IsoscelesTrapezoid(sideA, sideB, height, randomColor);
+            case RIGHT_TRIANGLE:
+                double firstLeg = random.nextInt(100);
+                double secondLeg = random.nextInt(100);
+                return new RightTriangle(firstLeg, secondLeg, randomColor);
+            default:
+                throw new IllegalArgumentException("Unknown figure type: " + randomFigureType);
+        }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(10, "WHITE");
+        double radius = 10;
+        String color = "WHITE";
+        return new Circle(radius, color);
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,48 +5,20 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        //creating random number of figures to be displayed
         Random random = new Random();
-        FigureSupplier figureSupplier = new FigureSupplier();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Figure[] figures = new Figure[6];
+        int figureNumber = random.nextInt(20);
 
-        for (int i = 0; i < 6; i++) {
-            if (i < 3) {
-                String randomColor = colorSupplier.getRandomColor();
-                switch (figureSupplier.getRandomFigure()) {
-                    case CIRCLE:
-                        double radius = random.nextInt(100);
-                        figures[i] = new Circle(radius, randomColor);
-                        break;
-                    case SQUARE:
-                        double side = random.nextInt(100);
-                        figures[i] = new Square(side, randomColor);
-                        break;
-                    case RECTANGLE:
-                        double length = random.nextInt(100);
-                        double width = random.nextInt(100);
-                        figures[i] = new Rectangle(length, width, randomColor);
-                        break;
-                    case ISOSCELES_TRAPEZOID:
-                        double sideA = random.nextInt(100);
-                        double sideB = random.nextInt(100);
-                        double height = random.nextInt(100);
-                        figures[i] = new IsoscelesTrapezoid(sideA, sideB, height, randomColor);
-                        break;
-                    case RIGHT_TRIANGLE:
-                        double firstLeg = random.nextInt(100);
-                        double secondLeg = random.nextInt(100);
-                        figures[i] = new RightTriangle(firstLeg, secondLeg, randomColor);
-                        break;
-                    default:
-                        break;
-                }
+        //creating and printing an array of random figures
+        Figure[] figure = new Figure[figureNumber];
+        for (int i = 0; i < figure.length; i++) {
+            if (i < figure.length / 2) {
+                figure[i] = new FigureSupplier().getRandomFigure();
             } else {
-                figures[i] = new FigureSupplier().getDefaultFigure();
+                figure[i] = new FigureSupplier().getDefaultFigure();
             }
-            System.out.println(figures[i].draw());
+            System.out.println(figure[i].draw());
         }
-
     }
 }
 
