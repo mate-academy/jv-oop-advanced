f85ce6eb9250eebfa5a56154e6b1c745f149fdfc
Ololenko Oleksandr <sahkaa2009@gmail.com>
2023-07-31 18:59:44 +0300

    There are some figures of the following types: square, rectangle, right triangle, circle, isosceles trapezoid. You need to create corresponding classes for them Square, Rectangle, RightTriangle, Circle, IsoscelesTrapezoid

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class Rectangle extends IsoscelesTrapezoid {
+    public Rectangle(final String color, final int base, final int height) {
+        super(color, base, height);
+    }
+
+    @Override
+    public double getArea() {
+        return bottomBase * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: "
+                + "rectangle,"
+                + " area: "
+                + getArea()
+                + " sq.units,"
+                + " base: "
+                + bottomBase
+                + " units, "
+                + " height: "
+                + height
+                + " units, "
+                + "color: "
+                + getColor());
+    }
+}
