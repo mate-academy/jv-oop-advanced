74324a18034759564a27cc6799be0f263f3d1b7c
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-07-31 15:50:11 -0500

    Expressions in constructors were replaced with local variables, added constants for default figure to avoid 'magic numbers'

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final double CIRCLE_RADIUS = 10;
+    private static final String CIRCLE_COLOR = Color.WHITE.name();
     private Random random = new Random();
     private Figures[] figures = Figures.values();
     private ColorSupplier colorSupplier = new ColorSupplier();
@@ -12,20 +14,29 @@ public class FigureSupplier {
         String randomColor = colorSupplier.getRandomColor();
         switch (figures[randomIndex]) {
             case CIRCLE:
-                return new Circle(randomColor, random.nextDouble());
+                double randomRadius = random.nextDouble();
+                return new Circle(randomColor, randomRadius);
             case SQUARE:
-                return new Square(randomColor, random.nextDouble());
+                double randomSideSize = random.nextDouble();
+                return new Square(randomColor, randomSideSize);
             case RECTANGLE:
-                return new Rectangle(randomColor, random.nextDouble(), random.nextDouble());
+                double randomSide1 = random.nextDouble();
+                double randomSide2 = random.nextDouble();
+                return new Rectangle(randomColor, randomSide1, randomSide2);
             case RIGHT_TRIANGLE:
-                return new RightTriangle(randomColor, random.nextDouble(), random.nextDouble());
+                double randomFirstLeg = random.nextDouble();
+                double randomSecondLeg = random.nextDouble();
+                return new RightTriangle(randomColor, randomFirstLeg, randomSecondLeg);
             default:
-                return new IsoscelesTrapezoid(randomColor, random.nextDouble(), random.nextDouble(), random.nextDouble());
+                double randomLowerBaseSide = random.nextDouble();
+                double randomUpperBaseSide = random.nextDouble();
+                double height = random.nextDouble();
+                return new IsoscelesTrapezoid(randomColor, randomLowerBaseSide, randomUpperBaseSide, height);
         }
 
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), 10);
+        return new Circle(CIRCLE_COLOR, CIRCLE_RADIUS);
     }
 }
