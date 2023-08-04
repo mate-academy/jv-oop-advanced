90854cc5adb9c0b3353aacd43290fa1d4e05b9cb
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-08-04 19:11:27 +0300

    first version

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,4 +5,16 @@ package core.basesyntax;
  */
 public class HelloWorld {
 
+    public static void main(String[] args) {
+
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[6];
+        for (int i = 0; i < figures.length; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+            figures[i].print();
+
+        }
+
+    }
+
 }
