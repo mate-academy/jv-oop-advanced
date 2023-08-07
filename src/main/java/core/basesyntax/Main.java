4c65982507e4fd61384d44d9dc4ab4235622538a
Andreyepam <epameducation@gmail.com>
2023-08-07 15:29:11 +0300

    fixed main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,15 +4,16 @@ public class Main {
     public static void main(String[] args) {
         ColorSupplier colorSupplier = new ColorSupplier();
         FigureSupplier figureSupplier = new FigureSupplier(colorSupplier);
-
         Figure[] figures = new Figure[6];
-        for (int i = 0; i < figures.length; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-        figures[5] = new IsoscelesTrapezoid(colorSupplier.getRandomColor(), 10, 20, 15);
 
-        for (Figure figure : figures) {
-            figure.draw();
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+                figures[i].draw();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+                figures[i].draw();
+            }
         }
     }
 }
