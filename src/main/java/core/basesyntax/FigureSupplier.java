9f295577aadf0aa8a0840263a23c5ea80b6c963a
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-07 18:06:23 +0300

    Replaced code blocks in switch with methids,

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -12,44 +12,58 @@ public class FigureSupplier {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
+    private Figure getRandomCircle() {
+        int radius = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        return new Circle(colorSupplier.getRandomColor(), radius);
+    }
+
+    private Figure getRandomIsoscelesTrapezoid() {
+        int sideA = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        int sideB = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        int height = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                sideA, sideB, height);
+    }
+
+    private Figure getRandomRectangle() {
+        int sideA = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        int sideB = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        return new Rectangle(colorSupplier.getRandomColor(), sideA, sideB);
+    }
+
+    private Figure getRandomRightTriangle() {
+        int firstLeg = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        int secondLeg = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        return new RightTriangle(colorSupplier.getRandomColor(), firstLeg, secondLeg);
+    }
+
+    private Figure getRandomSquare() {
+        int side = random.nextInt(
+                (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
+        return new Square(colorSupplier.getRandomColor(), side);
+    }
+
     public Figure getRandomFigure() {
         switch (random.nextInt(FIGURE_COUNT)) {
-            case 0: {
-                int radius = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                return new Circle(colorSupplier.getRandomColor(), radius);
-            }
-            case 1: {
-                int sideA = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                int sideB = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                int height = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        sideA, sideB, height);
-            }
-            case 2: {
-                int sideA = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                int sideB = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                return new Rectangle(colorSupplier.getRandomColor(), sideA, sideB);
-            }
-            case 3: {
-                int firstLeg = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                int secondLeg = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                return new RightTriangle(colorSupplier.getRandomColor(), firstLeg, secondLeg);
-            }
-            case 4: {
-                int side = random.nextInt(
-                        (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
-                return new Square(colorSupplier.getRandomColor(), side);
-            }
+            case 0:
+                return getRandomCircle();
+            case 1:
+                return getRandomIsoscelesTrapezoid();
+            case 2:
+                return getRandomRectangle();
+            case 3:
+                return getRandomRightTriangle();
+            case 4:
             default:
-                return getDefaultFigure();
+                return getRandomSquare();
         }
     }
 
