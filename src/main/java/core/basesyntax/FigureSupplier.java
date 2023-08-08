a1cc595087fff6eed8ac8f3f3826fe9d521fd34d
Yana Bilyk <bilyk.yn@gmail.com>
2023-08-07 20:13:45 -0400

    fixed common mistakes and new Random

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,30 +6,30 @@ public class FigureSupplier {
     private static final double DEFAULT_RADIUS = 10;
     private static final Color DEFAULT_COLOR = Color.WHITE;
     private static final int FIGURES_AMOUNT = 5;
-    private static final int MAX_NUMBER = 25;
+    private static final int MAX_LENGTH = 25;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
-    private int getRandomNumber() {
-        return new Random().nextInt((int) MAX_NUMBER + 1);
+    private int getRandomLength() {
+        return random.nextInt((int) MAX_LENGTH + 1);
     }
 
     public Figure getRandomFigure() {
         int value = random.nextInt(FIGURES_AMOUNT) + 1;
         switch (value) {
             case 1:
-                return new Circle(colorSupplier.getRandomColor(), getRandomNumber());
+                return new Circle(colorSupplier.getRandomColor(), getRandomLength());
             case 2:
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        getRandomNumber(), getRandomNumber(), getRandomNumber());
+                        getRandomLength(), getRandomLength(), getRandomLength());
             case 3:
-                return new Rectangle(colorSupplier.getRandomColor(),getRandomNumber(),
-                        getRandomNumber());
+                return new Rectangle(colorSupplier.getRandomColor(), getRandomLength(),
+                        getRandomLength());
             case 4:
-                return new Square(colorSupplier.getRandomColor(),getRandomNumber());
+                return new Square(colorSupplier.getRandomColor(), getRandomLength());
             default:
-                return new RightTriangle(colorSupplier.getRandomColor(), getRandomNumber(),
-                        getRandomNumber());
+                return new RightTriangle(colorSupplier.getRandomColor(), getRandomLength(),
+                        getRandomLength());
         }
     }
 
