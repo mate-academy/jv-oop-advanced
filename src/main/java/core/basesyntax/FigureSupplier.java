78105e3ae610a68dc09fb7384a5697a45770f92d
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-08-08 17:29:55 +0300

    added separete methods to create figures

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -10,16 +10,36 @@ public class FigureSupplier {
 
     public Figure getRandomFigure() {
         switch (random.nextInt(5)) {
-            case 0: return new Square(random.nextDouble(), colorSupplier.getRandomColor());
-            case 1: return new Rectangle(random.nextDouble(), random.nextDouble(),
-                    colorSupplier.getRandomColor());
-            case 2: return new Circle(random.nextDouble(), colorSupplier.getRandomColor());
-            case 3: return new RightTriangle(random.nextDouble(), random.nextDouble(),
-                    colorSupplier.getRandomColor());
-            case 4: return new IsoscelesTrapezoid(random.nextDouble(), random.nextDouble(),
-                    random.nextDouble(), colorSupplier.getRandomColor());
+            case 0: return createSquare();
+            case 1: return createRectangle();
+            case 2: return createCircle();
+            case 3: return createRightTriangle();
+            case 4: return createIsoscelesTrapezoid();
             default: return getDefaultFigure();
         }
+    }
+
+    private Figure createSquare() {
+        return new Square(random.nextDouble(), colorSupplier.getRandomColor());
+    }
+
+    private Figure createRectangle() {
+        return new Rectangle(random.nextDouble(), random.nextDouble(),
+                colorSupplier.getRandomColor());
+    }
+
+    private Figure createCircle() {
+        return new Circle(random.nextDouble(), colorSupplier.getRandomColor());
+    }
+
+    private Figure createRightTriangle() {
+        return new RightTriangle(random.nextDouble(), random.nextDouble(),
+                colorSupplier.getRandomColor());
+    }
+
+    private Figure createIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(random.nextDouble(), random.nextDouble(),
+                random.nextDouble(), colorSupplier.getRandomColor());
     }
 
     public Figure getDefaultFigure() {
