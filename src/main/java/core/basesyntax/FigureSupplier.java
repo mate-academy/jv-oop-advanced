14fc0953a537d865810ff6c01ffd0e0f0d160824
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-08 11:04:39 +0300

    Change place for private methods.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -12,6 +12,26 @@ public class FigureSupplier {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
+    public Figure getRandomFigure() {
+        switch (random.nextInt(FIGURE_COUNT)) {
+            case 0:
+                return getRandomCircle();
+            case 1:
+                return getRandomIsoscelesTrapezoid();
+            case 2:
+                return getRandomRectangle();
+            case 3:
+                return getRandomRightTriangle();
+            case 4:
+            default:
+                return getRandomSquare();
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
+    }
+
     private Figure getRandomCircle() {
         int radius = random.nextInt(
                 (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
@@ -49,25 +69,5 @@ public class FigureSupplier {
         int side = random.nextInt(
                 (RANDOM_RANGE_MAX - RANDOM_RANGE_MIN) + 1) + RANDOM_RANGE_MIN;
         return new Square(colorSupplier.getRandomColor(), side);
-    }
-
-    public Figure getRandomFigure() {
-        switch (random.nextInt(FIGURE_COUNT)) {
-            case 0:
-                return getRandomCircle();
-            case 1:
-                return getRandomIsoscelesTrapezoid();
-            case 2:
-                return getRandomRectangle();
-            case 3:
-                return getRandomRightTriangle();
-            case 4:
-            default:
-                return getRandomSquare();
-        }
-    }
-
-    public Figure getDefaultFigure() {
-        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
