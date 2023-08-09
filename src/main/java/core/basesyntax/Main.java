e1c82f5efceb48b172befe070006f243fd2eff2a
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-09 13:12:39 +0300

    add const in Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int LENGTH_ARRAy = 4;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[4];
+        Figure[] figures = new Figure[LENGTH_ARRAy];
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length) {
                 figures[i] = figureSupplier.getRandomFigure();
