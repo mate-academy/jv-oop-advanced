6d6d96aa6167c838605daedf86a163089e41a7fb
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-08-11 12:39:45 +0300

    fixed checkstyle with constant

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class Main {
+    public static final int FIGURES_COUNT = 6;
+
     public static void main(String[] args) {
-        final int FIGURES_COUNT = 6;
+
         Figure[] figuresArray = new Figure[FIGURES_COUNT];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figuresArray.length; i++) {
