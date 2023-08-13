ee9b37758988575e2ebb42b13a9c42ab1df4a828
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-08-13 22:02:13 +0300

    I have created classes, an enum, and an interface for the implementation of figure generation

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private final double firstSide;
+    private final double secondSide;
+
+    public Rectangle(String color, int firstSide, int secondSide) {
+        super(color);
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+
+    @Override
+    public double getArea() {
+        return this.firstSide * secondSide;
+    }
+
+    @Override
+    public void toDraw() {
+        System.out.println("Figure: rectangle, area: "
+                + String.format("%.1f", getArea())
+                + " sq.units, first side: "
+                + firstSide
+                + " units, second side: "
+                + secondSide
+                + " units, color: "
+                + getColor());
+    }
+}
