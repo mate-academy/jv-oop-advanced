04772bff75f7cc24c1b0b07e8ad31b780ba84116
LilyH <lilygrebenik@gmail.com>
2023-08-18 22:51:06 +0300

    build: Square class was added, it's derives from Figure class

---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Square extends Figure {
+    private int side;
+
+    public Square(String name, String color, int side) {
+        super(name, color);
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + getName() + ", area: " + getArea() + " sq. units, side: " + side + " units, color: " + getColor() + ".");
+    }
+}
