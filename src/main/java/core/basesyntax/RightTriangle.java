3c1ecde960c58d91660b91d7a0ab7b382498407d
LilyH <lilygrebenik@gmail.com>
2023-08-18 22:50:30 +0300

    build: RightTriangle class was added, it's derives from Figure class

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure {
+    private int firstLeg;
+    private int secondLeg;
+
+    public RightTriangle(String name, String color, int firstLeg, int secondLeg) {
+        super(name, color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double getArea() {
+        return firstLeg * secondLeg / 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + getName() + ", area: " + getArea() + " sq. units, first leg: " + firstLeg + " units, second leg: " + secondLeg + " units, color: " + getColor() + ".");
+
+    }
+}
