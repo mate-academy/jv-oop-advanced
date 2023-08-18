cf53de46cdf9552fefc008f1075e10cb9d29dd4a
LilyH <lilygrebenik@gmail.com>
2023-08-18 22:49:40 +0300

    build: Circle class was added, it's derives from Figure class

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private int radius;
+    public Circle(String name, String color,int radius) {
+        super(name, color);
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + getName() + ", area: " + getArea() + " sq. units, radius: " + radius + " units, color: " + getColor() + ".");
+
+    }
+
+    @Override
+    public double getArea() {
+        return Math.round((Math.PI * radius * radius) * 100.0) / 100.0;
+    }
+
+}
