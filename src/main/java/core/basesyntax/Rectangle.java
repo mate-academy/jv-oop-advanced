85e2df1174208143b8f7388269100779981aa2e1
LilyH <lilygrebenik@gmail.com>
2023-08-18 22:50:17 +0300

    build: Rectangle class was added, it's derives from Figure class

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private int firstSide;
+    private int secondSide;
+
+    public Rectangle(String name, String color, int firstSide, int secondSide) {
+        super(name, color);
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+
+    @Override
+    public double getArea() {
+        return firstSide * secondSide;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + getName() + ", area: " + getArea() + " sq. units, first side: " + firstSide + " units, second side: " + secondSide + " units, color: " + getColor() + ".");
+
+    }
+}
