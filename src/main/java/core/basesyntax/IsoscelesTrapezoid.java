9b3f8b8032f5f9548db99a8bd53998d9e43d2f8b
LilyH <lilygrebenik@gmail.com>
2023-08-18 22:50:01 +0300

    build: IsoscelesTrapezoid class was added, it's derives from Figure class

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private int leg;
+    private int height;
+
+    public IsoscelesTrapezoid(String name, String color, int leg, int height) {
+        super(name, color);
+        this.leg = leg;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        double height2 = height;
+        return (height2 / 2) * leg * 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + getName() + ", area: " + getArea() + " sq. units, leg: " + leg + " units, height: " + height + " units, color: " + getColor() + ".");
+    }
+}
