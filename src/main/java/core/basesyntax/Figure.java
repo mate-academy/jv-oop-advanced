3a92bab3abc189e62c1ae9407e78ddbb35a7772b
LilyH <lilygrebenik@gmail.com>
2023-08-18 22:46:00 +0300

    build: Figure abstract class was added, AreaCalculator and Drawable interfaces were implemented into it

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public abstract class Figure implements Drawable, AreaCalculator {
+    private String color;
+    private String name;
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+}
