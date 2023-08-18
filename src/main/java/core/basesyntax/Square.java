c2a7f1191d966cc903a38a76deea028d376d1f13
LilyH <lilygrebenik@gmail.com>
2023-08-18 23:31:30 +0300

    fix: errors were fixed

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -7,10 +7,12 @@ public class Square extends Figure {
         super(name, color);
         this.side = side;
     }
+
     @Override
     public double getArea() {
         return side * side;
     }
+
     @Override
     public void draw() {
         System.out.println("Figure: " + getName() + ", area: " + getArea() + " sq. units, side: "
