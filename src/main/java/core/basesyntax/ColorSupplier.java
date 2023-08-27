525d2f3079273f0a307fb5aeed00a35f613ae15c
oleksii-lytvynenko <oleksii.lytvynenko@vaimo.com>
2023-08-27 19:33:43 +0300

    fix issues in ColorSupplier class(v2)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
+    private Random random = new Random();
+
     public String getRandomColor() {
         Color [] color = Color.values();
         int index = random.nextInt(color.length);
