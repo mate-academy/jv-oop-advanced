8a465fa3e40dad91e57a28355336da4bfc65d7e6
Egor Reshetnik <mwm3335@gmail.com>
2023-08-29 00:26:27 +0300

    create cicle with getArea() and draw() methods

---
+++ src/main/java/core/basesyntax/figures/Circle.java
@@ -0,0 +1,22 @@
+package core.basesyntax.figures;
+
+import core.basesyntax.Figure;
+
+public class Circle extends Figure {
+    private static final double PI = 3.14;
+    private int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return PI * Math.pow(radius, 2);
+    }
+
+    public void draw() {
+        System.out.println("Figure: " + getName() + ", area: " + getArea()
+                + " sq.units, radius: " + radius + " units, color: " + getColor());
+    }
+}
