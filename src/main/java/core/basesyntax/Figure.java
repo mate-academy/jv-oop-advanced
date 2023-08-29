567405dd1920fe065c14f21e3380c818c6d97260
Egor Reshetnik <mwm3335@gmail.com>
2023-08-29 17:00:29 +0300

    remove field name,add constructor

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -4,15 +4,10 @@ import core.basesyntax.interfaces.Calculable;
 import core.basesyntax.interfaces.Drawable;
 
 public abstract class Figure implements Drawable, Calculable {
-    private String name;
     private String color;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public Figure(String color) {
+        this.color = color;
     }
 
     public String getColor() {
