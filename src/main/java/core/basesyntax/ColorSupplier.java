b575654e693309d9f0b7400f703cb42520ec5b4e
Petro Smoliar <smp22pl@gmail.com>
2023-08-30 21:52:20 +0200

    corrected

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final int MAX_NUMBER = 6;
     private final Random random = new Random();
 
     public String getRandomColor() {
-        return Color.values()[random.nextInt(MAX_NUMBER)].name();
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,35 +3,35 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-
-    public static final int FIX_RADIUS = 10;
+    public static final int DEFAULT_RADIUS = 10;
     private static final int MAX_NUMBER = 5;
     private static final int MAX_PARAMETR = 10;
-    private static final String COLOR_WHITE = Color.WHITE.name();
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         String color = colorSupplier.getRandomColor();
-        int firstLeg = random.nextInt(MAX_PARAMETR) + 1;
-        int secondLeg = random.nextInt(MAX_PARAMETR) + 1;
-        int radius = random.nextInt(MAX_PARAMETR) + 1;
-        int height = random.nextInt(MAX_PARAMETR) + 1;
         switch (random.nextInt(MAX_NUMBER)) {
             case 0:
-                return new Circle(color, radius);
+                return new Circle(color, getRandomValue());
             case 1:
-                return new IsoscelesTrapezoid(color, firstLeg, secondLeg, height);
+                return new IsoscelesTrapezoid(color, getRandomValue(),
+                        getRandomValue(), getRandomValue());
             case 2:
-                return new Rectangle(color, firstLeg, secondLeg);
+                return new Rectangle(color, getRandomValue(), getRandomValue());
             case 3:
-                return new RightTriangle(color, firstLeg, secondLeg);
+                return new RightTriangle(color, getRandomValue(), getRandomValue());
             default:
-                return new Square(color, firstLeg);
+                return new Square(color, getRandomValue());
         }
     }
 
+    private int getRandomValue() {
+        return random.nextInt(MAX_PARAMETR) + 1;
+    }
+
     public Figure getDefaultFigure() {
-        return new Circle(COLOR_WHITE, FIX_RADIUS);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
