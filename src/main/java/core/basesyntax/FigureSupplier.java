d47b77bf279de4192dc03eba9531b8699e6604ef
Eugene Bormotov <bormotovjenya2510@gmail.com>
2023-08-30 18:39:04 +0200

    Deleted an empty line in the Circle class, added a constructor to the parent class, deleted an empty line in the Color enum, deleted an empty line in the RightTriangle class, deleted an empty line in the Rectangle class, removed the empty line in IsoscelesTrapezoid, created constants in the FigereSupplier class, made a correction in the FigureSupplier class, after the default line, created constant.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,26 +3,29 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private final String defaultColor = Color.WHITE.name();
+    private static final int LIMIT_NUMBER_FIGURE = 5;
+    private static final int SIDE_LIMIT = 10;
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
+    private static final int DEFAULT_RADIUS = 10;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int figureIndex = random.nextInt(0, 5);
+        int figureIndex = random.nextInt(LIMIT_NUMBER_FIGURE);
         switch (figureIndex) {
             case 0:
-                return new Circle(colorSupplier.getRandomColor(), random.nextInt(0, 10));
+                return new Circle(colorSupplier.getRandomColor(), random.nextInt(SIDE_LIMIT));
             case 1:
-                return new Square(colorSupplier.getRandomColor(), random.nextInt(0, 10));
+                return new Square(colorSupplier.getRandomColor(), random.nextInt(SIDE_LIMIT));
             case 2:
-                return new Rectangle(colorSupplier.getRandomColor(), random.nextInt(0, 10),
+                return new Rectangle(colorSupplier.getRandomColor(), random.nextInt(SIDE_LIMIT),
                         random.nextInt(0, 10));
             case 3:
-                return new RightTriangle(colorSupplier.getRandomColor(), random.nextInt(0, 10),
+                return new RightTriangle(colorSupplier.getRandomColor(), random.nextInt(SIDE_LIMIT),
                         random.nextInt(0, 10));
             case 4:
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextInt(0,10),
-                        random.nextInt(0, 10));
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextInt(SIDE_LIMIT),
+                        random.nextInt(SIDE_LIMIT));
             default:
                 return getDefaultFigure();
         }
@@ -30,7 +33,7 @@ public class FigureSupplier {
 
     public Figure getDefaultFigure() {
 
-        int defaultRadius = 10;
-        return new Circle(defaultColor, defaultRadius);
+
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
