28b42550054b9aaeb18a74faf96972512eae9072
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-09-01 16:32:19 +0300

    Created private methods

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int FIGURES = 5;
     private static final double MAX_VALUE = 40.0;
-    private static final int DEFAULT_RADIUS = 15;
+    private static final int DEFAULT_RADIUS = 10;
     private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private String randomColor = new ColorSupplier().getRandomColor();
+
 
     public Figure getDefaultFigure() {
         return new Circle(Color.WHITE.name(), DEFAULT_RADIUS);
@@ -15,29 +16,46 @@ public class FigureSupplier {
 
     public Figure getRandomFigure() {
         int randomFigureIndex = random.nextInt(FIGURES);
-        double randomRadius = getRandomUnits();
-        double randomSide = getRandomUnits();
-        double randomFirstSide = getRandomUnits();
-        double randomSecondSide = getRandomUnits();
-        double randomFirstLeg = getRandomUnits();
-        double randomSecondLeg = getRandomUnits();
-        double randomUpperBase = getRandomUnits();
-        double randomLowBase = getRandomUnits();
-        double randomSidePart = getRandomUnits();
-        String color = colorSupplier.getRandomColor();
-
         switch (randomFigureIndex) {
-            case 0: return new Circle(color, randomRadius);
-            case 1: return new IsoscelesTrapezoid(color, randomLowBase,
-                    randomUpperBase, randomSidePart);
-            case 2: return new Rectangle(color, randomFirstSide, randomSecondSide);
-            case 3: return new RightTriangle(color, randomFirstLeg, randomSecondLeg);
-            case 4: return new Square(color, randomSide);
+            case 0: return createCircle();
+            case 1: return createIsoscelesTrapezoid();
+            case 2: return createRectangle();
+            case 3: return createRightTriangle();
+            case 4: return createSquare();
             default: return getDefaultFigure();
         }
     }
 
     private double getRandomUnits() {
         return random.nextDouble() * MAX_VALUE;
+    }
+
+    private Circle createCircle() {
+        double randomRadius = getRandomUnits();
+        return new Circle(randomColor, randomRadius);
+    }
+
+    private IsoscelesTrapezoid createIsoscelesTrapezoid() {
+        double randomLowBase = getRandomUnits();
+        double randomUpperBase = getRandomUnits();
+        double randomSidePart = getRandomUnits();
+        return new IsoscelesTrapezoid(randomColor, randomLowBase, randomUpperBase, randomSidePart);
+    }
+
+    private Rectangle createRectangle() {
+        double randomFirstSide = getRandomUnits();
+        double randomSecondSide = getRandomUnits();
+        return new Rectangle(randomColor, randomFirstSide, randomSecondSide);
+    }
+
+    private RightTriangle createRightTriangle() {
+        double randomFirstLeg = getRandomUnits();
+        double randomSecondLeg = getRandomUnits();
+        return new RightTriangle(randomColor, randomFirstLeg, randomSecondLeg);
+    }
+
+    private Square createSquare() {
+        double randomSide = getRandomUnits();
+        return new Square(randomColor, randomSide);
     }
 }
