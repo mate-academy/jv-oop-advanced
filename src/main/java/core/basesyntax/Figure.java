0015a0610e4bbeb4364e3cad781f81846a0cb66e
Nazar <nazarmos13@gmail.com>
2023-09-09 12:04:37 +0300

    redid my homework

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -3,10 +3,6 @@ package core.basesyntax;
 public abstract class Figure implements AreaCalculator, Drawable {
     private String color;
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public Figure(String color) {
         this.color = color;
     }
