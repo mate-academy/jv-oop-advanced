934385abeaba7b4cf4d86e4472fd7c5f07b6510d
Vadym Stanishevskyi <mainvadym@gmail.com>
2023-09-10 16:12:00 +0200

    added enum Color with colors, implemented method getRandomColor in class ColorSupplier

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class Main {
+    private final int figureCount= 6;
     public static void main(String[] args){
           Figure rightTriangle = new RightTriangle(4, 4, 5, "green");
           rightTriangle.printFigure();
