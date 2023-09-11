eade7b824f7fa68c9355cbd0daebfe9d70ad07d4
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-09-11 12:44:27 +0300

    added the сcorrect solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,5 +4,19 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        Figure [] figures = new Figure[4];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        int middle = figures.length / 2;
+        for (int i = 0; i < 4;i++) {
+            if (i < middle) {
+                figures[i] = figureSupplier.getRandomFigure();
+                figures[i].draw();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+                figures[i].draw();
+            }
 
+        }
+    }
 }
