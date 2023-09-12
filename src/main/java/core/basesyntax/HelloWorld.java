e5ce61c68edb1d32a739a96144ea57113492d58a
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-09-12 11:29:41 +0300

    fixed all mistakes with fields

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,19 +4,19 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    private static final FigureSupplier FIGURE_SUPPLIER = new FigureSupplier();
+
     public static void main(String[] args) {
         Figure [] figures = new Figure[4];
-        FigureSupplier figureSupplier = new FigureSupplier();
         int middle = figures.length / 2;
         for (int i = 0; i < 4;i++) {
             if (i < middle) {
-                figures[i] = figureSupplier.getRandomFigure();
+                figures[i] = FIGURE_SUPPLIER.getRandomFigure();
                 figures[i].draw();
             } else {
-                figures[i] = figureSupplier.getDefaultFigure();
+                figures[i] = FIGURE_SUPPLIER.getDefaultFigure();
                 figures[i].draw();
             }
-
         }
     }
 }
