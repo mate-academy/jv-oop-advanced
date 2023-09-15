9981e37e2d6f65818b11c6dcebebac850a6da2aa
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-09-15 10:40:57 +0300

    added final int range

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,30 +5,31 @@ import java.util.Random;
 public class FigureSupplier {
     private static final Random RANDOM = new Random();
     private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private static final int RANGE = 101;
 
     public Figure getRandomFigure() {
         Type figure = Type.values()[RANDOM.nextInt(Type.values().length)];
         switch (figure) {
             case SQUARE: {
-                return new Square(RANDOM.nextInt(101),
+                return new Square(RANDOM.nextInt(RANGE),
                         COLOR_SUPPLIER.getRandomColor());
             }
             case ISOSCELES_TRAPEZOID: {
-                return new IsoscelesTrapezoid(RANDOM.nextInt(101),
-                        RANDOM.nextInt(101), RANDOM.nextInt(101),
+                return new IsoscelesTrapezoid(RANDOM.nextInt(RANGE),
+                        RANDOM.nextInt(RANGE), RANDOM.nextInt(RANGE),
                         COLOR_SUPPLIER.getRandomColor());
             }
             case CIRCLE: {
-                return new Circle(RANDOM.nextInt(101),
+                return new Circle(RANDOM.nextInt(RANGE),
                         COLOR_SUPPLIER.getRandomColor());
             }
             case RESTANGLE: {
-                return new Rectangle(RANDOM.nextInt(101),RANDOM.nextInt(101),
+                return new Rectangle(RANDOM.nextInt(RANGE),RANDOM.nextInt(RANGE),
                         COLOR_SUPPLIER.getRandomColor());
             }
             default: {
-                return new RightTriangle(RANDOM.nextInt(101),
-                        RANDOM.nextInt(101),
+                return new RightTriangle(RANDOM.nextInt(RANGE),
+                        RANDOM.nextInt(RANGE),
                         COLOR_SUPPLIER.getRandomColor());
             }
         }
