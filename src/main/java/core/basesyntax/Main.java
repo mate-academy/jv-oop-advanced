44c283df863c3a48fe3e00ef5f00324e8c3053c4
Taras <tazik1232@gmail.com>
2023-09-15 17:38:05 +0300

    add fixes jv-oop-advanced

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,20 +4,18 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class Main {
+    public static final int ARRAY_SIZE = 6;
 
     public static void main(String[] args) {
-        Figure[] figures = new Figure[6];
-        for (int i = 0; i < figures.length; i++) {
-            if (i < figures.length / 2) {
-
-                figures[i] = new FigureSupplier().getRandomFigure();
-
+        Figure[] figures = new Figure[ARRAY_SIZE];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            if (ARRAY_SIZE / 2 > i) {
+                figures[i] = figureSupplier.getRandomFigure();
             } else {
-                figures[i] = new FigureSupplier().getDefaultFigure();
+                figures[i] = figures[i - ARRAY_SIZE / 2];
             }
-
             figures[i].draw();
         }
     }
-
 }
