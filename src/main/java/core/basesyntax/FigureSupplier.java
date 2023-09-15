8c82f193f9bdd4b7f8440082a57c84ee2535e07b
Vladyslav Druhov <space.drugov@gmail.com>
2023-09-15 16:15:15 +0300

    rework FigureSupplier

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,26 +3,29 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final Random RANDOM = new Random();
+    private static final int NUM_FIGURE_TYPES = 5;
+    private static final double SIDE_LIMIT = 10.0;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int randomType = RANDOM.nextInt(5);
+        int randomType = random.nextInt(NUM_FIGURE_TYPES);
         String randomColor = new ColorSupplier().getRandomColor();
         switch (randomType) {
             case 0:
-                return new Square(RANDOM.nextDouble() * 10, randomColor);
+                return new Square(getR, randomColor);
             case 1:
-                return new Rectangle(RANDOM.nextDouble() * 10,
-                        RANDOM.nextDouble() * 10, randomColor);
+                return new Rectangle(random.nextDouble() * 10,
+                        random.nextDouble() * 10, randomColor);
             case 2:
-                return new RightTriangle(RANDOM.nextDouble() * 10 + 1,
-                        RANDOM.nextDouble() * 10 + 1, randomColor);
+                return new RightTriangle(random.nextDouble() * 10 + 1,
+                        random.nextDouble() * 10 + 1, randomColor);
             case 3:
-                return new Circle(RANDOM.nextDouble() * 10, randomColor);
+                return new Circle(random.nextDouble() * 10, randomColor);
             default:;
                 return new IsoscelesTrapezoid(
-                        RANDOM.nextDouble() * 10, RANDOM.nextDouble() * 10,
-                        RANDOM.nextDouble() * 10, randomColor);
+                        random.nextDouble() * 10, random.nextDouble() * 10,
+                        random.nextDouble() * 10, randomColor);
         }
     }
 
