30a14e99dc0453f5a12ab09cde0bd3317cae5786
Mateusz Rostek <17rostek17@gmail.com>
2023-09-18 18:25:54 +0200

    Created ColorSupplier class and Colors enum

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class ColorSupplier {
+    private static final Random random = new Random();
+
+    public String getRandomColor() {
+        Colors[] colors = Colors.values();
+
+        return colors[random.nextInt(colors.length)].name();
+    }
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,6 @@
+package core.basesyntax;
+
+public enum Colors {
+
+    BLACK, BLUE, RED, GREEN, WHITE, YELLOW
+}
