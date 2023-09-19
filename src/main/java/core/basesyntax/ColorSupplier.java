dd82d8b5dcd890aed7642acbba812f44773b3131
Vladyslav Druhov <space.drugov@gmail.com>
2023-09-19 11:13:33 +0300

    According to the edit, enum replaced the names of

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Colors[] COLORS = Colors.values();
+    private static final Color[] COLORS = Color.values();
     private final Random random = new Random();
 
     public String getRandomColor() {
