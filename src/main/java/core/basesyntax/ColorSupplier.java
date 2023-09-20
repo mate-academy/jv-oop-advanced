21542f68ee8de25b3abf7ab041ffae86555b69dd
Jakub Sekowski <sekowskijakub@gmail.com>
2023-09-20 09:11:45 +0200

    Fixes made in 3 classes: - ColorSupplier: corrected variable to class-level - Drawable: created interface for drawing - Figure: now implements Drawable interface, draw method deleted

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
-        Random random = new Random();
         Colors[] color = Colors.values();
         int randomIndex = random.nextInt(color.length);
         return color[randomIndex].name();
--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements AreaCalculator {
+public abstract class Figure implements AreaCalculator, Drawable {
     private String color;
 
     public Figure(String color) {
@@ -10,6 +10,4 @@ public abstract class Figure implements AreaCalculator {
     public String getColor() {
         return color.toLowerCase();
     }
-
-    public abstract void draw();
 }
