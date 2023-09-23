a1a95d3a38b2bbcec9a35ab71e0354fd5ba58b26
AdamMudrak <mudrets2000@gmail.com>
2023-09-24 00:06:12 +0300

    Add figure supplier to generate random figures and a default figure

---
+++ src/main/java/core/basesyntax/services/FigureSupplier.java
@@ -0,0 +1,33 @@
+package core.basesyntax.services;
+
+import core.basesyntax.Colors;
+import core.basesyntax.figures.Circle;
+import core.basesyntax.model.Figure;
+
+public class FigureSupplier extends Figure {
+
+    public int getFigureCountFromModel() {
+        return getFigureCount();
+    }
+
+    public double getRandomFigure() {
+        return getRandom().nextInt(getFigureCount()) + 1;
+    }
+
+    public Figure getDefaultFigure() {
+        Figure figure = new Circle();
+        figure.setArea(getFigureCountFromModel());
+        figure.setColor(Colors.WHITE.name());
+        return figure;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
