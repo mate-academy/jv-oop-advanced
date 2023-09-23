4bb12c6564056b12ce82514ce1de0800cea9f8dc
AdamMudrak <mudrets2000@gmail.com>
2023-09-24 00:04:36 +0300

    Add abstract model of a figure

---
+++ src/main/java/core/basesyntax/model/Figure.java
@@ -0,0 +1,36 @@
+package core.basesyntax.model;
+
+import core.basesyntax.services.AreaCalculator;
+import core.basesyntax.services.Drawable;
+import java.util.Random;
+
+public abstract class Figure implements AreaCalculator, Drawable {
+    private final int figureCount = 10;
+    private final Random random = new Random();
+    private String color;
+    private double area;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public int getFigureCount() {
+        return figureCount;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+}
