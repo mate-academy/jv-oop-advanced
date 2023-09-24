fda1a3dcabfd1d946e4e8e2e74d027936e5a62a4
AdamMudrak <mudrets2000@gmail.com>
2023-09-25 00:07:04 +0300

    Delete redundant imports, modify method not to eat so much memory by creating a method field instead of initializing new FigureSupplier

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,27 +1,22 @@
 package core.basesyntax;
 
-import core.basesyntax.figures.Circle;
-import core.basesyntax.figures.IsoscelesTrapezoid;
-import core.basesyntax.figures.Rectangle;
-import core.basesyntax.figures.RightTriangle;
-import core.basesyntax.figures.Square;
 import core.basesyntax.model.Figure;
 import core.basesyntax.services.FigureSupplier;
 
 public class Application {
     public static void main(String[] args) {
-
+        FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[]{
-                new Circle(), new IsoscelesTrapezoid(),
-                new Rectangle(),
-                new RightTriangle(),
-                new Square(),
-                new FigureSupplier().getDefaultFigure(),
-                new FigureSupplier().getDefaultFigure(),
-                new FigureSupplier().getDefaultFigure()
-        };
+                figureSupplier.getRandomFigure("new Circle()"),
+                figureSupplier.getRandomFigure("new IsoscelesTrapezoid()"),
+                figureSupplier.getRandomFigure("new Rectangle()"),
+                figureSupplier.getRandomFigure("new RightTriangle()"),
+                figureSupplier.getRandomFigure("new Square()"),
+                figureSupplier.getRandomFigure("If not a figure name, returns a default figure"),
+                figureSupplier.getDefaultFigure()};
         for (Figure figure : figures) {
             figure.draw();
+            System.out.println();
         }
     }
 }
