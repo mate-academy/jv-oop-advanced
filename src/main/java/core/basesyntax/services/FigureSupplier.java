5e759eea469e73b2c84dfd3cad1c3cc25fd9d0b9
AdamMudrak <mudrets2000@gmail.com>
2023-09-25 00:04:45 +0300

    Add sufficient imports, rebuild methods to use only this class for building both default and random figures

--- src/main/java/core/basesyntax/services/FigureSupplier.java
+++ src/main/java/core/basesyntax/services/FigureSupplier.java
@@ -1,33 +1,59 @@
 package core.basesyntax.services;
 
-import core.basesyntax.Colors;
+import core.basesyntax.Color;
 import core.basesyntax.figures.Circle;
+import core.basesyntax.figures.IsoscelesTrapezoid;
+import core.basesyntax.figures.Rectangle;
+import core.basesyntax.figures.RightTriangle;
+import core.basesyntax.figures.Square;
 import core.basesyntax.model.Figure;
+import java.util.Random;
 
 public class FigureSupplier {
-
-    public int getFigureCountFromModel() {
-        return getFigureCount();
-    }
-
-    public double getRandomFigure() {
-        return getRandom().nextInt(getFigureCount()) + 1;
-    }
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final int figureCeilingParam = 10;
+    private final Random random = new Random();
 
     public Figure getDefaultFigure() {
-        Figure figure = new Circle();
-        figure.setArea(getFigureCountFromModel());
-        figure.setColor(Colors.WHITE.name());
-        return figure;
+        Circle defaultFigure = new Circle();
+        defaultFigure.setRadius(10);
+        defaultFigure.setColor(Color.WHITE.name().toLowerCase());
+        return defaultFigure;
     }
 
-    @Override
-    public double calculateArea() {
-        return 0;
-    }
-
-    @Override
-    public void draw() {
-
+    public Figure getRandomFigure(String figure) {
+        switch (figure) {
+            case "new Circle()":
+                Circle circle = new Circle();
+                circle.setRadius(random.nextInt(figureCeilingParam) + 1);
+                circle.setColor(colorSupplier.getRandomColor());
+                return circle;
+            case "new IsoscelesTrapezoid()":
+                IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid();
+                isoscelesTrapezoid.setHeight(random.nextInt(figureCeilingParam) + 1);
+                isoscelesTrapezoid.setLowerBase(random.nextInt(figureCeilingParam) + 1);
+                isoscelesTrapezoid.setUpperBase(random.nextInt(figureCeilingParam) + 1);
+                isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
+                return isoscelesTrapezoid;
+            case "new Rectangle()":
+                Rectangle rectangle = new Rectangle();
+                rectangle.setLength(random.nextInt(figureCeilingParam) + 1);
+                rectangle.setWidth(random.nextInt(figureCeilingParam) + 1);
+                rectangle.setColor(colorSupplier.getRandomColor());
+                return rectangle;
+            case "new RightTriangle()":
+                RightTriangle rightTriangle = new RightTriangle();
+                rightTriangle.setFirstLeg(random.nextInt(figureCeilingParam) + 1);
+                rightTriangle.setSecondLeg(random.nextInt(figureCeilingParam) + 1);
+                rightTriangle.setColor(colorSupplier.getRandomColor());
+                return rightTriangle;
+            case "new Square()":
+                Square square = new Square();
+                square.setSide(random.nextInt(figureCeilingParam) + 1);
+                square.setColor(colorSupplier.getRandomColor());
+                return square;
+            default:
+                return getDefaultFigure();
+        }
     }
 }
