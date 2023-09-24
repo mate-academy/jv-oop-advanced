463c6a99d7d4db222cfe27196b913f8d18dab7e9
AdamMudrak <mudrets2000@gmail.com>
2023-09-25 00:02:27 +0300

    Delete redundant imports, fields, getters & setters

--- src/main/java/core/basesyntax/model/Figure.java
+++ src/main/java/core/basesyntax/model/Figure.java
@@ -2,11 +2,9 @@ package core.basesyntax.model;
 
 import core.basesyntax.services.AreaCalculator;
 import core.basesyntax.services.Drawable;
-import java.util.Random;
 
 public abstract class Figure implements AreaCalculator, Drawable {
     private String color;
-    private double area;
 
     public String getColor() {
         return color;
@@ -14,21 +12,5 @@ public abstract class Figure implements AreaCalculator, Drawable {
 
     public void setColor(String color) {
         this.color = color;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
-    }
-
-    public int getFigureCount() {
-        return figureCount;
-    }
-
-    public Random getRandom() {
-        return random;
     }
 }
