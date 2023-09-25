258df1b2be37b38213d6c4ea3293a6a1a338f238
AdamMudrak <mudrets2000@gmail.com>
2023-09-25 16:21:21 +0300

    Use only one for loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,16 +6,15 @@ import core.basesyntax.services.FigureSupplier;
 public class Application {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[]{
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getRandomFigure(),
-                figureSupplier.getDefaultFigure()};
-        for (Figure figure : figures) {
-            figure.draw();
+        Figure[] figures = new Figure[8];
+        for (int i = 0; i < figures.length; i++) {
+            if(i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            }
+            else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+            figures[i].draw();
             System.out.println();
         }
     }
