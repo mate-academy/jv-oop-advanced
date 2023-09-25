5225f044e92b34d1a604cfe69c359a129c8d3df2
AdamMudrak <mudrets2000@gmail.com>
2023-09-25 13:33:52 +0300

    Change method according to the recommendations to both generate a random figure and random properties for it.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -7,12 +7,12 @@ public class Application {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[]{
-                figureSupplier.getRandomFigure("new Circle()"),
-                figureSupplier.getRandomFigure("new IsoscelesTrapezoid()"),
-                figureSupplier.getRandomFigure("new Rectangle()"),
-                figureSupplier.getRandomFigure("new RightTriangle()"),
-                figureSupplier.getRandomFigure("new Square()"),
-                figureSupplier.getRandomFigure("If not a figure name, returns a default figure"),
+                figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(),
+                figureSupplier.getRandomFigure(),
                 figureSupplier.getDefaultFigure()};
         for (Figure figure : figures) {
             figure.draw();
--- src/main/java/core/basesyntax/services/FigureSupplier.java
+++ src/main/java/core/basesyntax/services/FigureSupplier.java
@@ -12,44 +12,47 @@ import java.util.Random;
 public class FigureSupplier {
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private static final int MAX_NUMBER = 10;
+    private static final int MAX_AMOUNT_OF_FIGURES = 6;
+    private final int DEFAULT_RADIUS = 10;
     private final Random random = new Random();
 
     public Figure getDefaultFigure() {
         Circle defaultFigure = new Circle();
-        defaultFigure.setRadius(10);
+        defaultFigure.setRadius(DEFAULT_RADIUS);
         defaultFigure.setColor(Color.WHITE.name().toLowerCase());
         return defaultFigure;
     }
 
     public Figure getRandomFigure() {
+        int figure = random.nextInt(random.nextInt(MAX_AMOUNT_OF_FIGURES) + 1);
         switch (figure) {
-            case "new Circle()":
+            case 1:
                 Circle circle = new Circle();
-                circle.setRadius(random.nextInt(figureCeilingParam) + 1);
+                circle.setRadius(random.nextInt(MAX_NUMBER) + 1);
                 circle.setColor(colorSupplier.getRandomColor());
                 return circle;
-            case "new IsoscelesTrapezoid()":
+            case 2:
                 IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid();
-                isoscelesTrapezoid.setHeight(random.nextInt(figureCeilingParam) + 1);
-                isoscelesTrapezoid.setLowerBase(random.nextInt(figureCeilingParam) + 1);
-                isoscelesTrapezoid.setUpperBase(random.nextInt(figureCeilingParam) + 1);
+                isoscelesTrapezoid.setHeight(random.nextInt(MAX_NUMBER) + 1);
+                isoscelesTrapezoid.setLowerBase(random.nextInt(MAX_NUMBER) + 1);
+                isoscelesTrapezoid.setUpperBase(random.nextInt(MAX_NUMBER) + 1);
                 isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
                 return isoscelesTrapezoid;
-            case "new Rectangle()":
+            case 3:
                 Rectangle rectangle = new Rectangle();
-                rectangle.setLength(random.nextInt(figureCeilingParam) + 1);
-                rectangle.setWidth(random.nextInt(figureCeilingParam) + 1);
+                rectangle.setLength(random.nextInt(MAX_NUMBER) + 1);
+                rectangle.setWidth(random.nextInt(MAX_NUMBER) + 1);
                 rectangle.setColor(colorSupplier.getRandomColor());
                 return rectangle;
-            case "new RightTriangle()":
+            case 4:
                 RightTriangle rightTriangle = new RightTriangle();
-                rightTriangle.setFirstLeg(random.nextInt(figureCeilingParam) + 1);
-                rightTriangle.setSecondLeg(random.nextInt(figureCeilingParam) + 1);
+                rightTriangle.setFirstLeg(random.nextInt(MAX_NUMBER) + 1);
+                rightTriangle.setSecondLeg(random.nextInt(MAX_NUMBER) + 1);
                 rightTriangle.setColor(colorSupplier.getRandomColor());
                 return rightTriangle;
-            case "new Square()":
+            case 5:
                 Square square = new Square();
-                square.setSide(random.nextInt(figureCeilingParam) + 1);
+                square.setSide(random.nextInt(MAX_NUMBER) + 1);
                 square.setColor(colorSupplier.getRandomColor());
                 return square;
             default:
