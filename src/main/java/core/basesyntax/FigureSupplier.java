617064f87f26910567daa91177080ed84a5abd75
Tetiana Koval <tetyanak0102@gmail.com>
2023-09-26 10:35:37 +0300

    edited switch block, added constant with radius

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAX_SIDE_LENGTH = 101;
-    private final Figure defaultFigure = new Circle(Color.WHITE, 10);
+    private static final int DEFAULT_RADIUS = 10;
+    private final Figure defaultFigure = new Circle(Color.WHITE, DEFAULT_RADIUS);
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
@@ -15,32 +16,25 @@ public class FigureSupplier {
     private final Figures[] figures = Figures.values();
 
     public Figure getRandomFigure() {
-        Figure figure = defaultFigure;
         switch (figures[random.nextInt(figures.length)]) {
             case CIRCLE:
-                figure = new Circle(colorSupplier.getRandomColor(),
+                return new Circle(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_SIDE_LENGTH));
-                break;
             case RECTANGLE:
-                figure = new Rectangle(colorSupplier.getRandomColor(),
+                return new Rectangle(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_SIDE_LENGTH), random.nextInt(MAX_SIDE_LENGTH));
-                break;
             case SQUARE:
-                new Square(colorSupplier.getRandomColor(), random.nextInt(MAX_SIDE_LENGTH));
-                break;
+                return new Square(colorSupplier.getRandomColor(), random.nextInt(MAX_SIDE_LENGTH));
             case RIGHT_TRIANGLE:
-                figure = new RightTriangle(colorSupplier.getRandomColor(),
+                return new RightTriangle(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_SIDE_LENGTH), random.nextInt(MAX_SIDE_LENGTH));
-                break;
             case ISOSCELES_TRAPEZOID:
-                figure = new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
                         random.nextInt(MAX_SIDE_LENGTH), random.nextInt(MAX_SIDE_LENGTH),
                         random.nextInt(MAX_SIDE_LENGTH));
-                break;
             default:
-                break;
+                return defaultFigure;
         }
-        return figure;
     }
 
     public Figure getDefaultFigure() {
