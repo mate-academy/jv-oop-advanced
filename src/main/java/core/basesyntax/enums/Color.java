f2c61f891e75b28949ea110de0a5dacec5c42071
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-09-27 20:40:18 +0300

    Create Enum Color to storage constants representation of colors strings.

---
+++ src/main/java/core/basesyntax/enums/Color.java
@@ -0,0 +1,19 @@
+package core.basesyntax.enums;
+
+public enum Color {
+    BLACK("black"),
+    BLUE("blue"),
+    GREEN("green"),
+    RED("red"),
+    WHITE("white"),
+    YELLOW("yellow");
+
+    private final String color;
+    Color(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
