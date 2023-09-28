5adf789ed5fa4566aa73221f0ca6c482b613b95c
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-09-28 17:00:22 +0300

    Remove constants from interface.

--- src/main/java/core/basesyntax/interfaces/AreaCalculator.java
+++ src/main/java/core/basesyntax/interfaces/AreaCalculator.java
@@ -1,8 +1,5 @@
 package core.basesyntax.interfaces;
 
 public interface AreaCalculator {
-    double ZERO_POINT_FIVE = 0.5;
-    double TWO = 2.0;
-    double FOUR = 4.0;
     double getArea();
 }
--- src/main/java/core/basesyntax/interfaces/Drawable.java
+++ src/main/java/core/basesyntax/interfaces/Drawable.java
@@ -1,7 +1,5 @@
 package core.basesyntax.interfaces;
 
 public interface Drawable {
-    String WORD_SEPARATOR = " ";
-    String COMMA = ",";
     void draw();
 }
