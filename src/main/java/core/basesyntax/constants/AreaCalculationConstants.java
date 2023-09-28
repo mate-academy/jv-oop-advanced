cc92d2a593ff32d6e918c23a3df94d45efed733a
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-09-28 17:00:58 +0300

    Create classes to store constants, which were removed from interfaces.

---
+++ src/main/java/core/basesyntax/constants/AreaCalculationConstants.java
@@ -0,0 +1,12 @@
+package core.basesyntax.constants;
+
+public class AreaCalculationConstants {
+
+    public static final double ZERO_POINT_FIVE = 0.5;
+    public static final double TWO = 2.0;
+    public static final double FOUR = 4.0;
+
+    private AreaCalculationConstants() {
+
+    }
+}
