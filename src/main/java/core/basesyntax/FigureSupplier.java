84e057b342afedf0b684ce71c6c4c5131ed78e9f
Danil Ponomarenko <akame.dante13@gmail.com>
2023-09-29 18:43:43 +0300

    Removed parameter Random random in getRandomSize() because we already have random.nextDouble() in usage

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -16,23 +16,23 @@ public class FigureSupplier {
 
         switch (randomChoice) {
             case 0:
-                double squareSide = getRandomSize(random);
+                double squareSide = getRandomSize();
                 return new Square(squareSide, color);
             case 1:
-                double length = getRandomSize(random);
-                double width = getRandomSize(random);
+                double length = getRandomSize();
+                double width = getRandomSize();
                 return new Rectangle(length, width, color);
             case 2:
-                double base = getRandomSize(random);
-                double height = getRandomSize(random);
+                double base = getRandomSize();
+                double height = getRandomSize();
                 return new RightTriangle(base, height, color);
             case 3:
-                double radius = getRandomSize(random);
+                double radius = getRandomSize();
                 return new Circle(radius, color);
             default:
-                double topBase = getRandomSize(random);
-                double bottomBase = getRandomSize(random);
-                double trapezoidSide = getRandomSize(random);
+                double topBase = getRandomSize();
+                double bottomBase = getRandomSize();
+                double trapezoidSide = getRandomSize();
                 return new IsoscelesTrapezoid(topBase, bottomBase, trapezoidSide, color);
         }
     }
@@ -41,7 +41,7 @@ public class FigureSupplier {
         return new Circle(DEFAULT_RADIUS, Colors.WHITE.name());
     }
 
-    private double getRandomSize(Random random) {
+    private double getRandomSize() {
         return random.nextDouble() * (MAX_SIZE - MIN_SIZE) + MIN_SIZE;
     }
 
