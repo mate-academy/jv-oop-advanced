12d251e8705563d1bfbcd2844e365847646b38ee
Danil Ponomarenko <akame.dante13@gmail.com>
2023-09-29 11:16:17 +0300

    Remove redundant methods implementation in parent class Figure. Make Figure class abstract, according to Abstraction principle.

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,14 +1,5 @@
 package core.basesyntax;
 
-public class Figure implements Measurable, Drawable {
+public abstract class Figure implements Measurable, Drawable {
     protected String color;
-
-    @Override
-    public double getArea() {
-        return 0;
-    }
-
-    @Override
-    public void draw() {
-    }
 }
