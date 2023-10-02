70cb7bb6f885ac491ed9d0817507ae9546899841
Влад <vladlesn86@gmail.com>
2023-10-02 16:35:23 +0300

    did fixes with test checker

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Rectangle extends Figure implements Area {
+public class Rectangle extends Figure {
     private int length;
     private int width;
 
--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class RightTriangle extends Figure implements Area {
+public class RightTriangle extends Figure {
     private int firstLeg;
     private int secondLeg;
 
