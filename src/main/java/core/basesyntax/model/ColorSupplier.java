80c94c3f76aa3b5aa99d74342b4860e36f519972
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-10-03 18:00:03 +0300

    added colors variable

--- src/main/java/core/basesyntax/model/ColorSupplier.java
+++ src/main/java/core/basesyntax/model/ColorSupplier.java
@@ -6,6 +6,7 @@ public class ColorSupplier {
     private Random randomIndex = new Random();
 
     public String getRandomColor() {
-        return Color.values()[randomIndex.nextInt(Color.values().length)].name();
+        Color[] colors = Color.values();
+        return colors[randomIndex.nextInt(colors.length)].name();
     }
 }
