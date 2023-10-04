f71b043e94da1a63c9e23d0e3f956672767c8fe3
almanac1914 <144943174+almanac1914@users.noreply.github.com>
2023-10-04 12:46:12 -0600

    added "public" and getRandomLength method

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-abstract class Figure implements Drawable, Measurement {
+public abstract class Figure implements Drawable, Measurement {
     protected String color;
 
     public Figure(String color) {
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,54 +4,38 @@ import java.util.Random;
 
 class FigureSupplier {
     private static final int FIGURE_COUNT = FigureType.values().length;
+    private static final String DEFAULT_COLOR = "white"; 
+    private static final double DEFAULT_RADIUS = 10.0; 
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Figure getRandomFigure() {
         FigureType figureType = FigureType.values()[random.nextInt(FIGURE_COUNT)];
-        double[] properties = generateRandomProperties(figureType);
-        String color = colorSupplier.getRandomColor();
-        return createFigure(figureType, properties, color);
+        return createFigure(figureType);
     }
 
     public Figure getDefaultFigure() {
-        return createFigure(FigureType.CIRCLE, new double[]{10.0}, "white");
+        return new Circle(DEFAULT_RADIUS, DEFAULT_COLOR);
     }
 
-    private double[] generateRandomProperties(FigureType figureType) {
-        double[] properties;
-        switch (figureType) {
-            case SQUARE:
-            case CIRCLE:
-                properties = new double[]{random.nextDouble() * 10};
-                break;
-            case RECTANGLE:
-            case RIGHT_TRIANGLE:
-                properties = new double[]{random.nextDouble() * 10, random.nextDouble() * 10};
-                break;
-            case ISOSCELES_TRAPEZOID:
-                properties = new double[]{random.nextDouble() * 10, random.nextDouble() 
-                    * 10, random.nextDouble() * 5};
-                break;
-            default:
-                properties = new double[]{};
-                break;
-        }
-        return properties;
+    private double getRandomLength() {
+        return random.nextDouble() * 10;
     }
 
-    private Figure createFigure(FigureType figureType, double[] properties, String color) {
+    private Figure createFigure(FigureType figureType) {
+        String color = colorSupplier.getRandomColor();
         switch (figureType) {
             case SQUARE:
-                return new Square(properties[0], color);
+                return new Square(getRandomLength(), color);
             case RECTANGLE:
-                return new Rectangle(properties[0], properties[1], color);
+                return new Rectangle(getRandomLength(), getRandomLength(), color);
             case RIGHT_TRIANGLE:
-                return new RightTriangle(properties[0], properties[1], color);
+                return new RightTriangle(getRandomLength(), getRandomLength(), color);
             case CIRCLE:
-                return new Circle(properties[0], color);
+                return new Circle(getRandomLength(), color);
             case ISOSCELES_TRAPEZOID:
-                return new IsoscelesTrapezoid(properties[0], properties[1], properties[2], color);
+                return new IsoscelesTrapezoid(getRandomLength(), getRandomLength(), 
+                        getRandomLength(), color);
             default:
                 return null;
         }
