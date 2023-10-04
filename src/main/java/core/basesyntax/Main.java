949221872273f9198030d3ec6d5bd3c7b1548f4d
nazariykolesnikoww <nazarkolesnikov536@gmail.com>
2023-10-04 11:12:34 +0300

    Fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String [] args) {
         Figure[] figures = new Figure[6];
         FigureSupplier figureSupplier = new FigureSupplier();
+
         int groupFigures = 2;
 
         for (int i = 0; i < figures.length; i++) {
