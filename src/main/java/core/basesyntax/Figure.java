4067f3d9614ea460f0288e9abb3e3549e3139572
nazariykolesnikoww <nazarkolesnikov536@gmail.com>
2023-10-04 11:13:42 +0300

    Fix

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public abstract class Figure implements Drawable, AreaCalculator {
     private String color;
+
     public Figure(String color) {
         this.color = color;
     }
