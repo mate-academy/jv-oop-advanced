f97cda4d146e2b0208da1af192abb320be332d53
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-10-05 18:42:41 +0300

    added random color, random area and so on

--- src/main/java/core/basesyntax/Object.java
+++ src/main/java/core/basesyntax/Object.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
-public abstract class Object implements Figure{
+public abstract class Object implements Figure {
     private String color;
 
-    public Object(String color){
+    public Object(String color) {
         this.color = color;
     }
+
     public String getColor() {
         return color;
     }
