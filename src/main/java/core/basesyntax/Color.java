515a8cd6bff0762fbb60762a7e8b577a68ba1407
Maksym Musiienko <maxymvm@gmail.com>
2023-10-10 09:58:53 +0300

    made Color enum instead of ColorSupplier class

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public enum Color {
     RED, GREEN, BLUE, YELLOW, PURPLE, ORANGE, WHITE;
 
-    // Method to get a random color
     public static Color getRandomColor() {
         Random random = new Random();
         return values()[random.nextInt(values().length)];
