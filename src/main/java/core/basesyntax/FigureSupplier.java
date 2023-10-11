01ea44a3811c6f3c5f5845c9d6f99f53f8852d85
Maksym Musiienko <maxymvm@gmail.com>
2023-10-11 23:31:57 +0300

    extracted random.nextDouble() * 10 + 1 to a private method, optimized main method

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,10 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final Random random = new Random();
     private static final int DEFAULT_RADIUS = 10;
     private static final int FIGURES_COUNT = 5;
+    private static final int SIDE_CONSTANT = 10;
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
+    private double getRandomSide() {
+        return random.nextDouble() * SIDE_CONSTANT + 1;
+    }
 
     public Figure getRandomFigure() {
         int choice = random.nextInt(FIGURES_COUNT);
@@ -14,19 +19,19 @@ public class FigureSupplier {
 
         switch (choice) {
             case 0:
-                return new Square(color, random.nextDouble() * 10 + 1);
+                return new Square(color, getRandomSide());
             case 1:
-                return new Rectangle(color, random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1);
+                return new Rectangle(color, getRandomSide(),
+                        getRandomSide());
             case 2:
-                return new RightTriangle(color, random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1);
+                return new RightTriangle(color, getRandomSide(),
+                        getRandomSide());
             case 3:
-                return new Circle(color, random.nextDouble() * 10 + 1);
+                return new Circle(color, getRandomSide());
             case 4:
-                return new IsoscelesTrapezoid(color, random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1);
+                return new IsoscelesTrapezoid(color, getRandomSide(),
+                        getRandomSide(),
+                        getRandomSide());
             default:
                 return getDefaultFigure();
         }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,21 +1,17 @@
 package core.basesyntax;
 
 public class Main {
+    private final static int FIGURES_COUNT = 6;
+
     public static void main(String[] args) {
-        final int N = 6;
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[N];
+        Figure[] figures = new Figure[FIGURES_COUNT];
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
-        }
-
-        for (int i = 3; i < 6; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
-        }
-
-        for (Figure figure : figures) {
-            figure.draw();
+            figures[FIGURES_COUNT - i - 1] = figureSupplier.getDefaultFigure();
+            figures[i].draw();
+            figures[FIGURES_COUNT - i - 1].draw();
         }
     }
 }
