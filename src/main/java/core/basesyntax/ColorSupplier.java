2c50e600203eada57bfc6e3a224d029bc3841db8
Tony Hut <tonnyhut@outlook.com>
2023-10-11 16:43:43 +0200

    implemented class ColorSupplier

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import core.basesyntax.enums.Color;
+
+import java.util.Random;
+
+public class ColorSupplier {
+    private Random random = new Random();
+
+    public String getRandomColor() {
+        return Color.values()[random.nextInt(Color.values().length)].toString();
+    }
+}
