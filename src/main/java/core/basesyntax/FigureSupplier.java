bf0e20d19bbf1f59f02759284b72d10572d666fa
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-12 15:58:52 +0300

    Uppercase enum variable

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,39 +3,39 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private final int maxNumber = 100;
+    private static final int MAX_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Figure getRandomFigure() {
         FigureType randomFigure = FigureType.values()[random.nextInt(FigureType.values().length)];
         switch (randomFigure) {
-            case Circle:
+            case CIRCLE:
                 return new Circle(colorSupplier.getRandomColor(),
-                        random.nextInt(maxNumber));
-            case Square:
+                        random.nextInt(MAX_NUMBER));
+            case SQUARE:
                 return new Square(colorSupplier.getRandomColor(),
-                        random.nextInt(maxNumber));
-            case Rectangle:
+                        random.nextInt(MAX_NUMBER));
+            case RECTANGLE:
                 return new Rectangle(colorSupplier.getRandomColor(),
-                        random.nextInt(maxNumber),
-                        random.nextInt(maxNumber));
-            case RightTriangle:
+                        random.nextInt(MAX_NUMBER),
+                        random.nextInt(MAX_NUMBER));
+            case RIGHT_RECTANGLE:
                 return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextInt(maxNumber),
-                        random.nextInt(maxNumber));
-            case IsoscelesTrapezoid:
+                        random.nextInt(MAX_NUMBER),
+                        random.nextInt(MAX_NUMBER));
+            case ISOSCELES_TRAPEZOID:
                 return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(maxNumber),
-                        random.nextInt(maxNumber),
-                        random.nextInt(maxNumber));
+                        random.nextInt(MAX_NUMBER),
+                        random.nextInt(MAX_NUMBER),
+                        random.nextInt(MAX_NUMBER));
             default:
                 return getDefaultFigure();
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.white,10);
+        return new Circle(Color.WHITE,10);
     }
 
 }
