f5e32203f0e1b5ff57172ba3be0c7e5fd186e815
DankevichMisha <83833016+dankevichmisha@users.noreply.github.com>
2023-10-13 21:34:20 +0300

    Add files via upload

---
+++ jv-oop-advancedty/src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class ColorSupplier {
+    private final Random random = new Random();
+    private final Color[] colors = Color.values();
+
+    public Color getRandomColor() {
+        return colors[random.nextInt(colors.length)];
+    }
+}
