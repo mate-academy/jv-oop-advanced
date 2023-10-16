4fe9026177dde2e5aa1590f8c7c7ef6b46ad4d7d
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-16 16:37:54 +0300

    generate dynamic array

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,15 +1,18 @@
 package core.basesyntax;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Figure[] figures = new Figure[4];
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of figures: ");
+        int number = scanner.nextInt();
+        Figure[] figures = new Figure[number];
         FigureSupplier figureSupplier = new FigureSupplier();
-        figures[0] = figureSupplier.getRandomFigure();
-        figures[1] = figureSupplier.getRandomFigure();
-        figures[2] = new Circle(Color.WHITE, 10);
-        figures[3] = new Square(Color.BLACK, 5);
-        for (Figure figure : figures) {
-            figure.draw();
+        for (int i = 0;i < number;i++) {
+            figures[i] = (i < number / 2)
+                    ? figureSupplier.getRandomFigure() : figureSupplier.getDefaultFigure();
+            figures[i].draw();
         }
     }
 }
