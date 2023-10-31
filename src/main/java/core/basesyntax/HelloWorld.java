990c7028af0c40762d6f5cbf2e53bd9344bf156b
MaksOsypchuk <Promaks_ua@icloud.com>
2023-10-31 16:58:51 +0200

    Corrected the formulas. I combined 3 loops in 1.

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,8 +4,9 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
-    private final FigureSupplier figureSupplier = new FigureSupplier();
+
     public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[6];
 
         for (int i = 0; i < figures.length; i++) {
