574ca60df7a8f9e51b1faa04539180631032d516
MilianCode <milian.code@gmail.com>
2023-11-09 19:14:46 +0100

    Added solution to jv-oop-advanced

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -1,28 +1,28 @@
 package core.basesyntax;
 
 public class RightTriangle extends Figure {
-    private double length;
-    private double width;
+    private double base;
+    private double height;
 
-    public RightTriangle(String color, double length, double width) {
+    public RightTriangle(String color, double base, double height) {
         this.color = color;
-        this.length = length;
-        this.width = width;
+        this.base = base;
+        this.height = height;
     }
 
     @Override
     public void draw() {
-        System.out.println("Figure: rectangle, area: "
+        System.out.println("Figure: right triangle, area: "
                 + getArea()
-                + " sq.units, length: "
-                + length
-                + " units, width: "
-                + width
+                + " sq.units, base: "
+                + base
+                + " units, height: "
+                + height
                 + " units, color: " + color);
     }
 
     @Override
     public double getArea() {
-        return length * width;
+        return base * height / 2;
     }
 }
