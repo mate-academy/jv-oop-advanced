c75e24617576e827ddbe04ea6a4b3d8e5df50a6f
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2023-11-10 22:14:34 +0200

    deleted redundant blank lines & extracted creating figures to the separate methods

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -14,35 +14,55 @@ public class FigureSupplier {
         int figureType = random.nextInt(NUMBER_OF_FIGURES);
         switch (figureType) {
             case 0:
-                double squareSide = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                String squareColor = colorSupplier.getRandomColor();
-                return new Square(squareColor, squareSide);
+                return createSquare();
             case 1:
-                double rectangleHeight = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                double rectangleLength = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                String rectangleColor = colorSupplier.getRandomColor();
-                return new Rectangle(rectangleColor, rectangleHeight, rectangleLength);
+                return createRectangle();
             case 2:
-                double rightTriangleFirstLeg = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                double rightTriangleSecondLeg = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                String rightTriangleColor = colorSupplier.getRandomColor();
-                return new RightTriangle(rightTriangleColor, rightTriangleFirstLeg,
-                        rightTriangleSecondLeg);
+                return createRightTriangle();
             case 3:
-                double circleRadius = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                String circleColor = colorSupplier.getRandomColor();
-                return new Circle(circleColor, circleRadius);
+                return createCircle();
             default:
-                double isoscelesTrapezoidTopBase = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                double isoscelesTrapezoidBottomBase = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                double isoscelesTrapezoidHeight = random.nextDouble(DEFAULT_RANDOM_BOUND);
-                String isoscelesTrapezoidColor = colorSupplier.getRandomColor();
-                return new IsoscelesTrapezoid(isoscelesTrapezoidColor, isoscelesTrapezoidTopBase,
-                        isoscelesTrapezoidBottomBase, isoscelesTrapezoidHeight);
+                return createIsoscelesTrapezoid();
         }
     }
 
     public Figure getDefaultFigure() {
         return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
+    }
+
+    private Figure createSquare() {
+        double squareSide = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        String squareColor = colorSupplier.getRandomColor();
+        return new Square(squareColor, squareSide);
+    }
+
+    private Figure createRectangle() {
+        double rectangleHeight = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        double rectangleLength = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        String rectangleColor = colorSupplier.getRandomColor();
+        return new Rectangle(rectangleColor, rectangleHeight, rectangleLength);
+    }
+
+    private Figure createRightTriangle() {
+        double rightTriangleFirstLeg = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        double rightTriangleSecondLeg = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        String rightTriangleColor = colorSupplier.getRandomColor();
+        return new RightTriangle(rightTriangleColor, rightTriangleFirstLeg,
+                rightTriangleSecondLeg);
+    }
+
+    private Figure createCircle() {
+        double circleRadius = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        String circleColor = colorSupplier.getRandomColor();
+        return new Circle(circleColor, circleRadius);
+    }
+
+    private Figure createIsoscelesTrapezoid() {
+        double isoscelesTrapezoidTopBase = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        double isoscelesTrapezoidBottomBase = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        double isoscelesTrapezoidHeight = random.nextDouble(DEFAULT_RANDOM_BOUND);
+        String isoscelesTrapezoidColor = colorSupplier.getRandomColor();
+        return new IsoscelesTrapezoid(isoscelesTrapezoidColor, isoscelesTrapezoidTopBase,
+                isoscelesTrapezoidBottomBase, isoscelesTrapezoidHeight);
     }
 }
