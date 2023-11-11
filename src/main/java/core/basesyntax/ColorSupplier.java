08569c4a51ce8ed0bf9f53845b7b0bbf770351ea
mykolatsupryk <mykola.tsupryk1994@gmail.com>
2023-11-11 18:47:57 +0200

    fixed mvn mistakes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static Random random = new Random();
+
     public String getRandomColor() {
         Color[] values = Color.values();
         return values[random.nextInt(values.length)].name();
