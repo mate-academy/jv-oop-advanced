6b7a199cf0128045bf40d8a3064703bcab65fc61
mykolatsupryk <mykola.tsupryk1994@gmail.com>
2023-11-11 17:45:55 +0200

    fixed enum class

--- src/main/java/core/basesyntax/model/enums/Color.java
+++ src/main/java/core/basesyntax/model/enums/Color.java
@@ -1,5 +1,5 @@
 package core.basesyntax.model.enums;
 
 public enum Color {
-    RED, BLUE, GREEN, BLACK, YELLOW, WHITE, PURPLE, ORANGE;
+    RED, BLUE, GREEN, BLACK, YELLOW, WHITE, PURPLE, ORANGE
 }
