4a8ffb73b4115395fbd15feac20cfffab2c15da6
mykolatsupryk <mykola.tsupryk1994@gmail.com>
2023-11-11 18:46:12 +0200

    fixed ColorSuplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,9 @@ import core.basesyntax.model.enums.Color;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static Random random = new Random();
     public String getRandomColor() {
         Color[] values = Color.values();
-        return values[new Random().nextInt(values.length)].name();
+        return values[random.nextInt(values.length)].name();
     }
 }
