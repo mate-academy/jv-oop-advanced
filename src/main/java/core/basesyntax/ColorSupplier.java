58cd526d6c7621e1e54a51f42a019e003ab4a675
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:03:48 +0200

    The program creates figures with random or default parameters, calculates their area, and prints information about each figure using the draw() method.

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class ColorSupplier {
+
+    private static final String[] COLORS = {"red", "blue", "green", "yellow",
+            "orange", "purple", "black"};
+
+    private Random random = new Random();
+
+    public String getRandomColor() {
+
+        return COLORS[random.nextInt(COLORS.length)];
+    }
+}
---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,33 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+
+    private static final double MAX_SIZE = 10;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        String color = colorSupplier.getRandomColor();
+        switch (random.nextInt(5)) {
+            case 0:
+                return new Square(color, random.nextInt((int) MAX_SIZE) + 1);
+            case 1:
+                return new Rectangle(color, random.nextInt((int) MAX_SIZE) + 1);
+            case 2:
+                return new RightTriangle(color, random.nextInt((int) MAX_SIZE) + 1);
+            case 3:
+                return new Circle(color, random.nextInt((int) MAX_SIZE) + 1);
+            case 4:
+                return new IsoscelesTrapezoid(color, random.nextInt((int) MAX_SIZE) + 1,
+                        random.nextInt((int) MAX_SIZE) + 1, random.nextInt((int) MAX_SIZE) + 1);
+            default:
+                return getDefaultFigure();
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle("white", MAX_SIZE);
+    }
+}
