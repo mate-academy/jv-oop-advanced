ddb5ed59f24d81167311e70d14a54aa3faffd1fb
Bohdan <kbs20043131@gmail.com>
2023-11-14 20:37:53 +0200

    delete empty lines and add const

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
 public class Main {
+    public static final int FIGURE_AMOUNT = 6;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] arr = new Figure[6];
+        Figure[] arr = new Figure[FIGURE_AMOUNT];
         for (int i = 0; i < arr.length; i++) {
             if (i < 3) {
                 arr[i] = figureSupplier.getRandomFigure();
