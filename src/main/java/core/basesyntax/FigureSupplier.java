cb788182f82333935cb8f2aa915cf50959b2ee5d
Artem Kushniruk <artem.kushniruck@gmail.com>
2023-12-01 20:02:04 +0200

    if is replaced by switch

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -12,29 +12,30 @@ public class FigureSupplier {
 
     public Figure getRandomFigure() {
         int index = random.nextInt(FIGURES_NUMBER);
-        if (index == 1) {
-            int side = random.nextInt(BOUND) + 1;
-            return new Square(side, colorSupplier.getRandomColor());
+        switch (index) {
+            case 1:
+                int squareSide = random.nextInt(BOUND) + 1;
+                return new Square(squareSide, colorSupplier.getRandomColor());
+            case 2:
+                int rectangleHeight = random.nextInt(BOUND) + 1;
+                int rectangleWidth = random.nextInt(BOUND) + 1;
+                return new Rectangle(rectangleHeight, rectangleWidth,
+                        colorSupplier.getRandomColor());
+            case 3:
+                int rightTriangleSide = random.nextInt(BOUND) + 1;
+                int rightTriangleHeight = random.nextInt(BOUND) + 1;
+                return new RightTriangle(rightTriangleSide, rightTriangleHeight,
+                        colorSupplier.getRandomColor());
+            case 4:
+                int radius = random.nextInt(BOUND) + 1;
+                return new Circle(radius, colorSupplier.getRandomColor());
+            default:
+                int isoHeight = random.nextInt(BOUND) + 1;
+                int firstParallel = random.nextInt(BOUND) + 1;
+                int secondParallel = random.nextInt(BOUND) + 1;
+                return new IsoscelesTrapezoid(isoHeight, firstParallel, secondParallel,
+                    colorSupplier.getRandomColor());
         }
-        if (index == 2) {
-            int height = random.nextInt(BOUND) + 1;
-            int width = random.nextInt(BOUND) + 1;
-            return new Rectangle(height, width, colorSupplier.getRandomColor());
-        }
-        if (index == 3) {
-            int side = random.nextInt(BOUND) + 1;
-            int height = random.nextInt(BOUND) + 1;
-            return new RightTriangle(side, height, colorSupplier.getRandomColor());
-        }
-        if (index == 4) {
-            int radius = random.nextInt(BOUND) + 1;
-            return new Circle(radius, colorSupplier.getRandomColor());
-        }
-        int height = random.nextInt(BOUND) + 1;
-        int firstParallel = random.nextInt(BOUND) + 1;
-        int secondParallel = random.nextInt(BOUND) + 1;
-        return new IsoscelesTrapezoid(height, firstParallel, secondParallel,
-                colorSupplier.getRandomColor());
     }
 
     public Figure getDefaultFigure() {
