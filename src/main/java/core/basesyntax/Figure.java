754051c238e7b1e10aa59f4c918493e40cf3d047
Vlad Bondarev <bondarev.21@icloud.com>
2023-12-03 19:55:07 +0200

    final solution VdBondarev

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public abstract class Figure implements AreaCalculator, Drawable {
     private String color;
 
-    public String getColor() {
-        return color;
-    } // I need this getter to get color in overrided method "draw" in every figure class
-
     public Figure(String color) {
         this.color = color;
     }
+
+    public String getColor() {
+        return color;
+    } // I need this getter to get color in overrided method "draw" in every figure class
 }
