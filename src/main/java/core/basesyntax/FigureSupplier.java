ca960df955aeae01d87d97c83bbb61c7287177a7
skillfuldev <chernonog.evgeniy@gmail.com>
2023-12-04 20:50:21 +0200

    added_second_step_of_hw-jv-oop-advanced-solution

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private Random random = new Random();
+
     public Figure getRandomFigure() {
-        Random random = new Random();
+
         int bound = 100;
         Figure[] array = {new Square(new ColorSupplier().getRandomColor(), random.nextInt(bound)),
                 new RightTriangle(new ColorSupplier().getRandomColor(), random.nextInt(bound),
@@ -13,10 +15,12 @@ public class FigureSupplier {
                         random.nextInt(bound)),
                 new IsoscelesTrapezoid(new ColorSupplier().getRandomColor(), random.nextInt(bound),
                         random.nextInt(bound), random.nextInt(bound)),
-                new Circle(Color.WHITE.toString(), 10)
+                new Circle(new ColorSupplier().getRandomColor(), random.nextInt(bound))
 
         };
         return array[random.nextInt(4)];
-
+    }
+    public Figure getDefaultFigure(){
+        return new Circle(Color.WHITE.toString(), 10);
     }
 }
