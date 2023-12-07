67f2abb9fc4f88cf9ef97df9c1423505ef855f01
Valentyn Sharshon <woosay333@gmail.com>
2023-12-07 22:09:49 +0200

    created Rectangle class, which inherits from Figure abstract class, has width and length fields, implementation of getArea() method and overrides toString() method

---
+++ src/main/java/core/basesyntax/figure/Rectangle.java
@@ -0,0 +1,25 @@
+package core.basesyntax.figure;
+
+public class Rectangle extends Figure {
+
+    private final double width;
+    private final double length;
+
+    public Rectangle(String color, double width, double length) {
+        super(color);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + ", width: " + width
+                + ", length: " + length;
+    }
+}
