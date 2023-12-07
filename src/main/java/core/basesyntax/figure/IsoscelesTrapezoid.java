8df33ec550dec1b0f7b07375dee38fefed19ee3d
Valentyn Sharshon <woosay333@gmail.com>
2023-12-07 22:10:43 +0200

    created IsoscelesTrapezoid class, which inherits from Figure abstract class, has firstSide, secondSide and height fields, implementation of getArea() method and overrides toString() method

---
+++ src/main/java/core/basesyntax/figure/IsoscelesTrapezoid.java
@@ -0,0 +1,28 @@
+package core.basesyntax.figure;
+
+public class IsoscelesTrapezoid extends Figure {
+
+    private final double firstSide;
+    private final double secondSide;
+    private final double height;
+
+    public IsoscelesTrapezoid(String color, double firstSide, double secondSide, double height) {
+        super(color);
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return (firstSide + secondSide) / 2 * height;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + ", first side: " + firstSide
+                + ", second side: " + secondSide
+                + ", height: " + height;
+    }
+}
