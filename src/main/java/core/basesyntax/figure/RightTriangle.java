2ff12a618afc3586538fc015bbd0ef7a7a294a42
Valentyn Sharshon <woosay333@gmail.com>
2023-12-07 22:10:10 +0200

    created RightTriangle class, which inherits from Figure abstract class, has firstLeg and secondLeg fields, implementation of getArea() method and overrides toString() method

---
+++ src/main/java/core/basesyntax/figure/RightTriangle.java
@@ -0,0 +1,25 @@
+package core.basesyntax.figure;
+
+public class RightTriangle extends Figure {
+
+    private final double firstLeg;
+    private final double secondLeg;
+
+    public RightTriangle(String color, double firstLeg, double secondLeg) {
+        super(color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double getArea() {
+        return (firstLeg * secondLeg) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + ", first leg: " + firstLeg
+                + ", second leg: " + secondLeg;
+    }
+}
