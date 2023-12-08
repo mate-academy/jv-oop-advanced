02a5c26d5b539ca5f41b87e9ad6be5787672b67c
Valentyn Sharshon <woosay333@gmail.com>
2023-12-08 10:10:57 +0200

    generated the array of figures and displayed the entire list of objects that we had

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
+import static java.lang.System.out;
+
+import core.basesyntax.figure.Figure;
+import core.basesyntax.supplier.FigureSupplier;
+
 public class HelloWorld {
 
+    private static final int ARRAY_LENGTH = 6;
+
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[6];
+        FigureSupplier figureSupplier = new FigureSupplier();
+
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
+
+        for (Figure figure : figures) {
+            out.println(figure);
+        }
+    }
 }
