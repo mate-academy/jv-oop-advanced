1acf6593f43f1ca7bd8dee71550b8f64c77fe5a3
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-14 09:49:54 +0100

    Class Rectangle implementation

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,36 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private double firstSide;
+    private double secondSide;
+
+    @Override
+    public void calculateArea() {
+        this.setArea(firstSide * secondSide);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + this.getClass()
+                + ", area: " + this.getArea() + " sq. units,"
+                + " first side: " + this.firstSide + " units, "
+                + " second side: " + this.secondSide + " units, "
+                + "color: " + this.getColor());
+    }
+
+    public double getFirstSide() {
+        return firstSide;
+    }
+
+    public double getSecondSide() {
+        return secondSide;
+    }
+
+    public void setFirstSide(double firstSide) {
+        this.firstSide = firstSide;
+    }
+
+    public void setSecondSide(double secondSide) {
+        this.secondSide = secondSide;
+    }
+}
