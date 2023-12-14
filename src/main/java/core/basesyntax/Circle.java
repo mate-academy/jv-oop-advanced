adf91dfac840d1852c59b921a1defe0480347f37
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-14 09:54:52 +0100

    Class Circle implementation

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void calculateArea() {
+        this.setArea(radius * radius * Math.PI);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + this.getClass()
+                + ", area: " + this.getArea() + " sq. units,"
+                + " radius: " + this.radius + " units, "
+                + "color: " + this.getColor());
+    }
+}
