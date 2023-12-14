3b1bc78c80f1b17ade335592f5b288ef1330e254
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-14 09:25:35 +0100

    Abstract Class Figure implementation

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+public abstract class Figure {
+    private double area;
+    private String color;
+
+    public abstract void draw();
+
+    public double getArea() {
+        return area;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract void calculateArea();
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+}
