f20107056692583e0b5d54b7697002791f95b7a2
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-14 10:36:41 +0100

    ENUM Colors implementation

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    ORANGE,
+    BROWN,
+    GREY,
+    BLACK,
+    WHITE,
+    PURPLE,
+    PINK,
+}
