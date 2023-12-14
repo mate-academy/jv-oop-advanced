c2393e486a398773af0356903c79ccfc6b668296
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-14 09:38:38 +0100

    Class Square implementation

---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+public class Square extends Figure {
+    private double side;
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    @Override
+    public void calculateArea() {
+        this.setArea(side * side);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + this.getClass()
+                + ", area: " + this.getArea() + " sq. units,"
+                + " side: " + this.side + " units, "
+                + "color: " + this.getColor());
+    }
+}
