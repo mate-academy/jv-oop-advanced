8be18d52eb5d34890516b04b76c72d6d49513fa7
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-15 12:16:12 +0100

    getRandomColor changed from static to nonstatic added class fields for Color/FigureSupplier

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,22 +5,24 @@ import java.util.Random;
 public class FigureSupplier {
     static final int figuresNumber = 5;
     static final double defaultRadius = 10;
+    static Random random = new Random();
+
+    static ColorSupplier colorSupplier = new ColorSupplier();
 
     public static Figure getDefaultFigure() {
         return new Circle(Color.WHITE.name(), defaultRadius);
     }
 
     public static Figure getRandomFigure() {
-        Random random = new Random();
         int randomFigure = random.nextInt(figuresNumber);
         return switch (randomFigure) {
-            case 0 -> new Square(ColorSupplier.getRandomColor(), random.nextDouble());
-            case 1 -> new Rectangle(ColorSupplier.getRandomColor(), random.nextDouble(),
+            case 0 -> new Square(colorSupplier.getRandomColor(), random.nextDouble());
+            case 1 -> new Rectangle(colorSupplier.getRandomColor(), random.nextDouble(),
                     random.nextDouble());
-            case 2 -> new Circle(ColorSupplier.getRandomColor(), random.nextDouble());
-            case 3 -> new RightTriangle(ColorSupplier.getRandomColor(), random.nextDouble(),
+            case 2 -> new Circle(colorSupplier.getRandomColor(), random.nextDouble());
+            case 3 -> new RightTriangle(colorSupplier.getRandomColor(), random.nextDouble(),
                     random.nextDouble());
-            default -> new IsoscelesTrapezoid(ColorSupplier.getRandomColor(), random.nextDouble(),
+            default -> new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextDouble(),
                     random.nextDouble(), random.nextDouble());
         };
     }
