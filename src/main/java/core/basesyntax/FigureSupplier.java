96dd21f2d2eccf8e00a76243aa00580e999d1e17
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-15 12:18:23 +0100

    Constants Names changed to FIGURES_NUMBER and DEFAULT_RADIUS

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,18 +3,18 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    static final int figuresNumber = 5;
-    static final double defaultRadius = 10;
+    static final int FIGURES_NUMBER = 5;
+    static final double DEFAULT_RADIUS = 10;
     static Random random = new Random();
 
     static ColorSupplier colorSupplier = new ColorSupplier();
 
     public static Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), defaultRadius);
+        return new Circle(Color.WHITE.name(), DEFAULT_RADIUS);
     }
 
     public static Figure getRandomFigure() {
-        int randomFigure = random.nextInt(figuresNumber);
+        int randomFigure = random.nextInt(FIGURES_NUMBER);
         return switch (randomFigure) {
             case 0 -> new Square(colorSupplier.getRandomColor(), random.nextDouble());
             case 1 -> new Rectangle(colorSupplier.getRandomColor(), random.nextDouble(),
