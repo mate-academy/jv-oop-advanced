7de92f6f8e5907d8fd0a8719f31e5354df3211d7
yaroslav olieniev <olieniev.od.ua@gmail.com>
2023-12-19 15:59:19 +0300

    fixed getRandomFigure method

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,20 +5,28 @@ import java.util.Random;
 public class FigureSupplier {
     private static final int DEFAULT_RADIUS = 10;
     private static final int NUMBER_OF_FIGURES = 5;
-    private static ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
+
+    private Double createRandomDouble() {
+        return random.nextDouble();
+    }
+
+    private String createRandomColor() {
+        return colorSupplier.getRandomColor();
+    }
 
     public Figure getRandomFigure() {
         int randomFigureNumber = random.nextInt(NUMBER_OF_FIGURES);
         return switch (randomFigureNumber) {
-            case 0 -> new Square(random.nextDouble(), colorSupplier.getRandomColor());
-            case 1 -> new Rectangle(random.nextDouble(), random.nextDouble(),
-                    colorSupplier.getRandomColor());
-            case 2 -> new RightTriangle(random.nextDouble(), random.nextDouble(),
-                    colorSupplier.getRandomColor());
-            case 3 -> new Circle(random.nextDouble(), colorSupplier.getRandomColor());
-            default -> new IsoscelesTrapezoid(random.nextDouble(),random.nextDouble(),
-                    random.nextDouble(), colorSupplier.getRandomColor());
+            case 0 -> new Square(createRandomDouble(), createRandomColor());
+            case 1 -> new Rectangle(createRandomDouble(), createRandomDouble(),
+                    createRandomColor());
+            case 2 -> new RightTriangle(createRandomDouble(), createRandomDouble(),
+                    createRandomColor());
+            case 3 -> new Circle(createRandomDouble(), createRandomColor());
+            default -> new IsoscelesTrapezoid(createRandomDouble(), createRandomDouble(),
+                    createRandomDouble(), createRandomColor());
         };
     }
 
