5f1b7235eaaefe85d27be6185075119b1d41c9cb
YEvhen Korobka <korobkaevgeny@gmail.com>
2023-12-24 20:50:32 +0200

    random figures second fix code

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,29 +5,23 @@ import java.util.Random;
 public class FigureSupplier {
     public static final int FIGURE_COUNT = 5;
     public static final int MAX_VALUES = 10;
-    public static final String DEFAULT_COLOR = "white";
+    public static final String DEFAULT_COLOR = Color.values()[0].name().toLowerCase();
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
-
-    private int randomValues() {
-        return random.nextInt(MAX_VALUES) + 1;
-    }
 
     public Figure getRandomFigure() {
         int figureNumber = random.nextInt(FIGURE_COUNT) + 1;
         switch (figureNumber) {
             case 1:
-                return new Square(colorSupplier.getRandomColor(), randomValues());
+                return new Square(randomColor(), randomValues());
             case 2:
-                return new Rectangle(colorSupplier.getRandomColor(),
-                        randomValues(), randomValues());
+                return new Rectangle(randomColor(), randomValues(), randomValues());
             case 3:
-                return new RightTriangle(colorSupplier.getRandomColor(),
-                        randomValues(), randomValues());
+                return new RightTriangle(randomColor(), randomValues(), randomValues());
             case 4:
-                return new Circle(colorSupplier.getRandomColor(), randomValues());
+                return new Circle(randomColor(), randomValues());
             case 5:
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                return new IsoscelesTrapezoid(randomColor(),
                         randomValues(), randomValues(), randomValues());
             default:
                 return getDefaultFigure();
@@ -37,4 +31,13 @@ public class FigureSupplier {
     public Figure getDefaultFigure() {
         return new Circle(DEFAULT_COLOR, MAX_VALUES);
     }
+
+    private int randomValues() {
+        return random.nextInt(MAX_VALUES) + 1;
+    }
+
+    private String randomColor() {
+        return colorSupplier.getRandomColor();
+    }
+
 }
