38b1942e3d58fc16d306577d5f30455a62bb42c0
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-01-08 19:28:05 +0200

    reworked loop in main method, created getRandomSide method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Main {
     private static int ARRAY_LENGTH = 11;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[ARRAY_LENGTH];
         for (int i = 0; i < figures.length; i++) {
-            figures[i] = (i <= figures.length/2) ? figureSupplier.getRandomFigure()
+            figures[i] = (i <= figures.length / 2) ? figureSupplier.getRandomFigure()
                     : figureSupplier.getDefaultFigure();
         }
         for (Figure figure : figures) {
