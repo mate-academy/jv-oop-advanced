a929688e9601b03db230ae7768849ed21a718ac9
Vitaliy <vitaliyufc@gmail.com>
2024-01-16 00:41:34 +0300

    Solution

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,31 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure {
+    private int base;
+    private int height;
+
+    public RightTriangle(Color color, int base, int height) {
+        super(color);
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double area() {
+        return (base * height) / 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: square, area: " + area() + "sq. units, side: " + base
+                + "sq. units, height: " + "sq. color: " + getColor());
+    }
+}
---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Square extends Figure {
+    private int side;
+
+    public Square(Color color, int side) {
+        super(color);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    @Override
+    public double area() {
+        return Math.pow(side, 2);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: square, area: " + area()
+                + "sq. units, side: " + side + "sq. units, color: " + getColor());
+    }
+}
