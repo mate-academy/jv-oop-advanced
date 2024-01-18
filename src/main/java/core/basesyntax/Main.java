9e19da00a1a3a04fd047a5193cc5125d729823ec
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2024-01-18 22:04:37 +0200

    added random color random area and so on

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,19 +2,16 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        FigureSupplier figureSupplier = new FigureSupplier(colorSupplier);
+        int arraySize = 6;
+        Figure[] figures = new Figure[arraySize];
 
-        Figure[] figures = new Figure[6];
-
-        for (int i = 0; i < 3; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
+        for (int i = 0; i < arraySize / 2; i++) {
+            figures[i] = FigureSupplier.getRandomFigure();
         }
 
-        for (int i = 3; i < 6; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
+        for (int i = arraySize / 2; i < arraySize; i++) {
+            figures[i] = FigureSupplier.getDefaultFigure();
         }
-
         for (Figure figure : figures) {
             figure.draw();
         }
