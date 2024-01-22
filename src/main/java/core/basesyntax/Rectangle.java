399226c85bc98f5db3480aa796e02f22334191f9
Andriy Beha <apgroup.ua@gmail.com>
2024-01-22 12:39:38 +0100

    Empty lines after class definition fixed

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class Rectangle extends Figure {
-
     private int height;
     private int length;
 
--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class Square extends Figure {
-
     private int side;
 
     public void setSide(int side) {
