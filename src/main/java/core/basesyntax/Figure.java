05635f23f478503bfc5ad57ef4709a5f8167643b
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-01-27 19:47:46 +0200

    Made requested changes: removed comments in the code fixed public void draw() method and renamed interface "Drawable" instead of InfoPrint made private non-static Random in the ColorSupplier class used default method in swith-case loop in the FigureSupplier class and used constants instead of numbers created the specific field for each case in case area in the getRandomFigure method fixed getDefaultFigure() method moved Random to class field in the FigureSupplier class used constants instead of numbers in Main class used Figure instead of InfoPrint created one loop "for" in the Main class created the Enum FigureConstants

--- src/main/java/core/basesyntax/Figure.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public abstract class Figure implements AreaCalculator, InfoPrint {
-    protected String color;
-
-    public Figure(String color) {
-        this.color = color;
-    }
-}
