6c4b88322ea3f692e10660f946ca508733dc28f3
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-02-07 16:38:29 +0200

    solve the task

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private double radius;
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.setColor(color);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: circle, area: " + this.getArea()
+                + " sq. units, radius: " + radius
+                + "units, color: " + this.getColor());
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
