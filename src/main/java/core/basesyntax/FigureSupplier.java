9a608625f8449ad2fba44b851309d10ec715b272
Myroslav Stefanchuk <stefanchykmyroslav@gmail.com>
2024-02-09 19:29:43 +0100

    added fixes

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,21 +3,20 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    public static class Constants {
-        public static final int DEFAULT_RADIUS = 10;
-        public static final String DEFAULT_COLOR = "white";
-    }
-
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
+    private static final int DEFAULT_RADIUS = 10;
+    private static final int FIGURE_COUNT = 5;
+    private static final int MAX_LENGTH = 10;
     private ColorSupplier colorSupplier;
     private Random random;
 
     public FigureSupplier(ColorSupplier colorSupplier) {
         this.colorSupplier = colorSupplier;
-        this.random = new Random();
+        random = new Random();
     }
 
     public AbstractFigure getRandomFigure() {
-        int figureType = random.nextInt(5);
+        int figureType = random.nextInt(FIGURE_COUNT);
 
         switch (figureType) {
             case 0:
@@ -26,24 +25,21 @@ public class FigureSupplier {
                 return new Rectangle(getRandomLength(), getRandomLength(), colorSupplier
                         .getRandomColor());
             case 2:
-                return new RightTriangle(getRandomLength(), getRandomLength(), colorSupplier
-                        .getRandomColor());
+                return new RightTriangle(getRandomLength(), getRandomLength(),
+                        colorSupplier.getRandomColor());
             case 3:
                 return new Circle(getRandomLength(), colorSupplier.getRandomColor());
-            case 4:
+            default:
                 return new IsoscelesTrapezoid(getRandomLength(), getRandomLength(),
                         getRandomLength(), colorSupplier.getRandomColor());
-            default:
-                return getDefaultFigure();
-
         }
     }
 
     private double getRandomLength() {
-        return random.nextDouble() * 10 + 1;
+        return random.nextDouble() * MAX_LENGTH + 1;
     }
 
     public AbstractFigure getDefaultFigure() {
-        return new Circle(Constants.DEFAULT_RADIUS, Color.WHITE);
+        return new Circle(FigureSupplier.DEFAULT_RADIUS, Color.valueOf(DEFAULT_COLOR));
     }
 }
