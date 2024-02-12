89c3dcb42105c601bd750535fef978cd318a1aa4
Oleksandr Farion <sashafarion@gmail.com>
2024-02-12 18:28:00 +0200

    removed area var.

--- src/main/java/core/basesyntax/model/Figure.java
+++ src/main/java/core/basesyntax/model/Figure.java
@@ -2,7 +2,6 @@ package core.basesyntax.model;
 
 public abstract class Figure implements AreaCalculator, FigurePainter {
     private String color;
-    private double area;
 
     public String getColor() {
         return color;
@@ -11,4 +10,5 @@ public abstract class Figure implements AreaCalculator, FigurePainter {
     public void setColor(String color) {
         this.color = color;
     }
+
 }
