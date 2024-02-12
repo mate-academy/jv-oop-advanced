27bf3e00a1f889adff995eda1858cd0d37a4c94c
Valentyn Sharshon <woosay333@gmail.com>
2024-02-12 21:33:11 +0200

    delete unnecessary static import, changed the type of constructor parameter from String to Color

--- src/main/java/core/basesyntax/figure/Square.java
+++ src/main/java/core/basesyntax/figure/Square.java
@@ -1,19 +1,16 @@
 package core.basesyntax.figure;
 
-import static java.lang.Math.pow;
-
 public class Square extends Figure {
-
     private final double side;
 
-    public Square(String color, double side) {
+    public Square(Color color, double side) {
         super(color);
         this.side = side;
     }
 
     @Override
     public double getArea() {
-        return pow(side, 2);
+        return Math.pow(side, 2);
     }
 
     @Override
