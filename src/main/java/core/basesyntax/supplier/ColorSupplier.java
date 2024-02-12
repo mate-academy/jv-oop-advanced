5f0f0f75c1050be6d6c4959726797362c06bd43d
Valentyn Sharshon <woosay333@gmail.com>
2024-02-12 21:36:00 +0200

    changed the return value type of the getRandomColor() method

--- src/main/java/core/basesyntax/supplier/ColorSupplier.java
+++ src/main/java/core/basesyntax/supplier/ColorSupplier.java
@@ -1,14 +1,13 @@
 package core.basesyntax.supplier;
 
-import core.basesyntax.figure.FigureColor;
+import core.basesyntax.figure.Color;
 import java.util.Random;
 
 public class ColorSupplier {
-
     private final Random random = new Random();
 
-    public String getRandomColor() {
-        int randomColorIndex = random.nextInt(FigureColor.values().length);
-        return FigureColor.values()[randomColorIndex].name().toLowerCase();
+    public Color getRandomColor() {
+        int randomColorIndex = random.nextInt(Color.values().length);
+        return Color.values()[randomColorIndex];
     }
 }
