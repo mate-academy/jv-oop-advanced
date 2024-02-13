963dc209dc3f269ed35690d257b663e66ffec0f1
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-02-13 16:14:11 +0200

    Implemented the Rectangle class

--- src/main/java/core/basesyntax/figures/Rectangle.java
+++ src/main/java/core/basesyntax/figures/Rectangle.java
@@ -1,14 +1,36 @@
 package core.basesyntax.figures;
 
+import core.basesyntax.AreaCalculator;
 import core.basesyntax.Drawable;
 
-public class Rectangle extends Figure implements Drawable {
-    public Rectangle(String color) {
+public class Rectangle extends Figure implements Drawable, AreaCalculator {
+    private final double firstSide;
+    private final double secondSide;
+
+    public Rectangle(String color, double firstSide, double secondSide) {
         super(color);
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
     }
 
     @Override
     public void draw(Figure rectangle) {
+        System.out.println("Figure: rectangle, area: " + getArea(rectangle) + " sq. units, "
+                + "first side: " + getFirstSide() + " units, "
+                + "second side: " + getSecondSide() + " units, "
+                + "color: " + rectangle.color);
+    }
 
+    public double getFirstSide() {
+        return firstSide;
+    }
+
+    public double getSecondSide() {
+        return secondSide;
+    }
+
+    @Override
+    public double getArea(Figure figure) {
+        return firstSide * secondSide;
     }
 }
