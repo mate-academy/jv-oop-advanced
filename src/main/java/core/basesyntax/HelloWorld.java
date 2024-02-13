5f1bfccb2a5631037d200762026f0758ae6cb228
Maksym Zahurskyi <zagurskijmaksim3@gmail.com>
2024-02-13 12:27:02 +0200

    added new classes and methods

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,5 +4,23 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    private static final int ARRAY_SIZE = 6;
+
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[ARRAY_SIZE];
+
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
 
 }
---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private final double upperBase;
+    private final double lowerBase;
+    private final double height;
+
+    public IsoscelesTrapezoid(String color, double upperBase, double lowerBase, double height) {
+        super(color);
+        this.upperBase = upperBase;
+        this.lowerBase = lowerBase;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return (upperBase + lowerBase) / 2 * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, area: "
+                + getArea() + " sq. units, height: "
+                + height + " units, firstParallel: "
+                + upperBase + " units, secondParallel: "
+                + lowerBase + " units, color: " + getColor());
+    }
+
+}
