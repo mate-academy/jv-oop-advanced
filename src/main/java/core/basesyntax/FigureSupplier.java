46378432b5c37fe0f664d63e8ae7a547a9ad0fe6
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-02-13 17:01:01 +0200

    redo final variable

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class FigureSupplier {
 
-    private static final int MAX_LENGTH_SIDE = 10;
+    private static final int MAX_LENGTH_SIDE = 11;
     private static final String COLOR = "WHITE";
     private static final short AMOUNT_OF_SHAPES = 5;
 
@@ -17,27 +17,27 @@ public class FigureSupplier {
 
         switch (rand) {
             case 0:
-                figure = new Circle(random.nextInt(MAX_LENGTH_SIDE) + 1,
+                figure = new Circle(random.nextInt(MAX_LENGTH_SIDE),
                         colorSupplier.getRandomColor());
                 break;
             case 1:
-                int base = random.nextInt(MAX_LENGTH_SIDE) + 1;
-                int top = random.nextInt(base) + 1;
-                int height = random.nextInt(MAX_LENGTH_SIDE) + 1;
+                int base = random.nextInt(MAX_LENGTH_SIDE);
+                int top = random.nextInt(base);
+                int height = random.nextInt(MAX_LENGTH_SIDE);
                 figure = new IsoscelesTrapezoid(base, top, height, colorSupplier.getRandomColor());
                 break;
             case 2:
-                int length = random.nextInt(MAX_LENGTH_SIDE) + 1;
-                int width = random.nextInt(MAX_LENGTH_SIDE) + 1;
+                int length = random.nextInt(MAX_LENGTH_SIDE);
+                int width = random.nextInt(MAX_LENGTH_SIDE);
                 figure = new Rectangle(length, width, colorSupplier.getRandomColor());
                 break;
             case 3:
-                int base2 = random.nextInt(MAX_LENGTH_SIDE) + 1;
-                int height2 = random.nextInt(MAX_LENGTH_SIDE) + 1;
+                int base2 = random.nextInt(MAX_LENGTH_SIDE);
+                int height2 = random.nextInt(MAX_LENGTH_SIDE);
                 figure = new RightTriangle(base2, height2, colorSupplier.getRandomColor());
                 break;
             case 4:
-                int side = random.nextInt(MAX_LENGTH_SIDE) + 1;
+                int side = random.nextInt(MAX_LENGTH_SIDE);
                 figure = new Square(side, colorSupplier.getRandomColor());
                 break;
             default:
