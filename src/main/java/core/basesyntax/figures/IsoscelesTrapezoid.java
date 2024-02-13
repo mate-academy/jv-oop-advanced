131915f8edee381d93cbec7bc4fa672302a10eb0
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-02-13 16:28:44 +0200

    implemented th IsoscelesTrapezoid class

--- src/main/java/core/basesyntax/figures/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/figures/IsoscelesTrapezoid.java
@@ -1,11 +1,30 @@
 package core.basesyntax.figures;
 
+import core.basesyntax.AreaCalculator;
 import core.basesyntax.Drawable;
 
-public class IsoscelesTrapezoid implements Drawable {
+public class IsoscelesTrapezoid extends Figure implements Drawable, AreaCalculator {
+    private final double upperSide;
+    private final double lowerSide;
+    private final double height;
+
+    public IsoscelesTrapezoid(String color, double upperSide, double lowerSide, double height) {
+        super(color);
+        this.upperSide = upperSide;
+        this.lowerSide = lowerSide;
+        this.height = height;
+    }
 
     @Override
-    public void draw(Figure figure) {
+    public void draw(Figure trapezoid) {
+        System.out.println("Figure: rectangle, area: " + getArea(trapezoid) + " sq. units, "
+                + "first side: " + upperSide + " units, "
+                + "second side: " + lowerSide + " units, "
+                + "color: " + trapezoid.color);
+    }
 
+    @Override
+    public double getArea(Figure figure) {
+        return (upperSide + lowerSide) / 2 * height;
     }
 }
