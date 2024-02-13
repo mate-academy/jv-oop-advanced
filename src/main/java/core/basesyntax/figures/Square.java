0edd6910b62b804d42feea686d2407801c144083
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-02-13 16:09:28 +0200

    implemented the Square class

--- src/main/java/core/basesyntax/figures/Square.java
+++ src/main/java/core/basesyntax/figures/Square.java
@@ -1,11 +1,29 @@
 package core.basesyntax.figures;
 
+import core.basesyntax.AreaCalculator;
 import core.basesyntax.Drawable;
 
-public class Square implements Drawable {
+public class Square extends Figure implements Drawable, AreaCalculator {
+    private final double side;
+
+    public Square(String color, double side) {
+        super(color);
+        this.side = side;
+    }
 
     @Override
-    public void draw(Figure figure) {
+    public void draw(Figure square) {
+        System.out.println("Figure: square, area: " + getArea(square) + " sq. units, "
+                + "side: " + getSide() + " units, "
+                + "color: " + square.color);
+    }
 
+    @Override
+    public double getArea(Figure figure) {
+        return Math.pow(this.getSide(), 2);
+    }
+
+    public double getSide() {
+        return side;
     }
 }
