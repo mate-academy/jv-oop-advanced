a2e6cf6f1290cd99211f86c3f920b8c61d91fc2d
makaroshyna <katya23225@gmail.com>
2024-02-14 22:01:21 +0200

    Create class Circle.

---
+++ src/main/java/core/basesyntax/figures/Circle.java
@@ -0,0 +1,21 @@
+package core.basesyntax.figures;
+
+public class Circle extends Figure {
+    private final int radius;
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: %s\nArea: %.2f\nRadius: %d\nColor: %s\n",
+                getClass().getSimpleName(), getArea(), radius, getColor());
+    }
+}
