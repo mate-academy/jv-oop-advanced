823d812cc7c958e99b52a174d7ce100733cb2dce
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-02-14 16:42:32 +0200

    fexed common mistakes

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawable,AreaCalculable {
+public abstract class Figure implements Drawable, AreaCalculator {
     protected String color;
 
     public Figure(String color) {
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,26 +3,25 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int NUMBERS_OF_FIGURES = 5;
+    private static final int NUMBERS_OF_FIGURES = 4;
     private static final int DEFAULT_RADIUS_FOR_CIRCLE = 10;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Figure getRandomFigure() {
         int type = random.nextInt(NUMBERS_OF_FIGURES);
+        double areaValue = random.nextDouble();
         switch (type) {
             case 0 :
-                return new Rectangle(colorSupplier.getRandomColor(), random.nextDouble(), random.nextDouble());
+                return new Rectangle(colorSupplier.getRandomColor(), areaValue, areaValue);
             case 1 :
-                return new Square(colorSupplier.getRandomColor(),random.nextDouble());
+                return new Square(colorSupplier.getRandomColor(), areaValue);
             case 2 :
-                return new RightTriangle(colorSupplier.getRandomColor(),random.nextDouble(), random.nextDouble());
+                return new RightTriangle(colorSupplier.getRandomColor(), areaValue, areaValue);
             case 3 :
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextDouble(), random.nextDouble());
-            case 4 :
-                return new Circle(colorSupplier.getRandomColor(), random.nextDouble());
+                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), areaValue, areaValue);
             default:
-                return null;
+                return new Circle(colorSupplier.getRandomColor(), areaValue);
         }
     }
 
