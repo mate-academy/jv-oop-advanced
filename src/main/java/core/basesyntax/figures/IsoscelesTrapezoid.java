4cd65b2353891a7676d51017d65da70ff5ec7ffa
makaroshyna <katya23225@gmail.com>
2024-02-14 22:01:37 +0200

    Create class IsoscelesTrapezoid.

---
+++ src/main/java/core/basesyntax/figures/IsoscelesTrapezoid.java
@@ -0,0 +1,25 @@
+package core.basesyntax.figures;
+
+public class IsoscelesTrapezoid extends Figure {
+    private final int height;
+    private final int firstSide;
+    private final int secondSide;
+
+    public IsoscelesTrapezoid(String color, int firstSide, int secondSide, int height) {
+        super(color);
+        this.height = height;
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+
+    @Override
+    public double getArea() {
+        return (firstSide + secondSide) * 0.5 * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: %s\nArea: %.2f\nFirst side: %d\nSecond side: %d\nColor: %s\n",
+                getClass().getSimpleName(), getArea(), firstSide, secondSide, getColor());
+    }
+}
