29abf765ffd08623f294649b3291f035b94d0e60
makaroshyna <katya23225@gmail.com>
2024-02-14 22:02:16 +0200

    Create class RightTriangle.

---
+++ src/main/java/core/basesyntax/figures/RightTriangle.java
@@ -0,0 +1,23 @@
+package core.basesyntax.figures;
+
+public class RightTriangle extends Figure {
+    private final int firstLeg;
+    private final int secondLeg;
+
+    public RightTriangle(String color, int firstLeg, int secondLeg) {
+        super(color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * firstLeg * secondLeg;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: %s\nArea: %.2f\nFirst leg: %d\nSecond leg: %d\nColor: %s\n",
+                getClass().getSimpleName(), getArea(), firstLeg, secondLeg, getColor());
+    }
+}
