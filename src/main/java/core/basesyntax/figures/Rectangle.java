91be8b8f4ea46f3e13b2e43051225f0f16a7526d
makaroshyna <katya23225@gmail.com>
2024-02-14 22:01:48 +0200

    Create class Rectangle.

---
+++ src/main/java/core/basesyntax/figures/Rectangle.java
@@ -0,0 +1,23 @@
+package core.basesyntax.figures;
+
+public class Rectangle extends Figure {
+    private final int firstSide;
+    private final int secondSide;
+
+    public Rectangle(String color, int firstSide, int secondSide) {
+        super(color);
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+
+    @Override
+    public double getArea() {
+        return firstSide * secondSide;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("Figure: %s\nArea: %.2f\nFirst side: %d\nSecond side: %d\nColor: %s\n",
+                getClass().getSimpleName(), getArea(), firstSide, secondSide, getColor());
+    }
+}
