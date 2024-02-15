69ffb73856e606fe5d1a5f5bf5371cad985ef8ff
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-02-15 17:55:10 +0200

    added variable random to class, made default color as constant

--- src/main/java/core/basesyntax/supplier/ColorSupplier.java
+++ src/main/java/core/basesyntax/supplier/ColorSupplier.java
@@ -1,14 +1,14 @@
 package core.basesyntax.supplier;
 
 import core.basesyntax.enums.Color;
-
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
 
     public String getRandomColor() {
         Color[] colors = Color.values();
-        int index = new Random().nextInt(colors.length);
+        int index = random.nextInt(colors.length);
         Color color = colors[index];
         return color.name();
     }
--- src/main/java/core/basesyntax/supplier/FigureSupplier.java
+++ src/main/java/core/basesyntax/supplier/FigureSupplier.java
@@ -1,14 +1,20 @@
 package core.basesyntax.supplier;
 
 import core.basesyntax.enums.Color;
-import core.basesyntax.figure.*;
-
+import core.basesyntax.figure.Circle;
+import core.basesyntax.figure.Figure;
+import core.basesyntax.figure.IsoscelesTrapezoid;
+import core.basesyntax.figure.Rectangle;
+import core.basesyntax.figure.RightTriangle;
+import core.basesyntax.figure.Square;
 import java.util.Random;
 
 public class FigureSupplier {
     private static final int NUMBERS_OF_FIGURES = 4;
     private static final int DEFAULT_RADIUS_FOR_CIRCLE = 10;
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+
     private final Random random = new Random();
 
     public Figure getRandomFigure() {
@@ -29,6 +35,6 @@ public class FigureSupplier {
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), DEFAULT_RADIUS_FOR_CIRCLE);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS_FOR_CIRCLE);
     }
 }
