007274dbd909db8ee6c70f1122a809f82f36d4ad
Taras Fedak <tarasfedak@me.com>
2024-02-16 15:43:52 +0200

    Refactored according to mentors suggestions.
    Several minor fixes....

--- src/main/java/core/basesyntax/Geometry.java
+++ src/main/java/core/basesyntax/Geometry.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class Geometry {
 
     private static final int MAX_NUMBER_OF_FIGURES_TO_GENERATE = 20;
+
     public static void main(String[] args) {
         Random random = new Random();
         int numberOfFigures = random.nextInt(MAX_NUMBER_OF_FIGURES_TO_GENERATE) + 1;
--- src/main/java/core/basesyntax/supplier/FigureSupplier.java
+++ src/main/java/core/basesyntax/supplier/FigureSupplier.java
@@ -28,23 +28,28 @@ public class FigureSupplier {
         switch (index) {
             case 0:
                 double radius = getRandomValue();
-                return new Circle(randomColor, radius);
+                randomFigure = new Circle(randomColor, radius);
+                break;
             case 1:
                 double side = getRandomValue();
-                randomFigure =  new Square(randomColor, side);
+                randomFigure = new Square(randomColor, side);
+                break;
             case 2:
                 double firstBase = getRandomValue();
                 double secondBase = getRandomValue();
                 double height = getRandomValue();
                 randomFigure = new IsoscelesTrapezoid(randomColor, firstBase, secondBase, height);
+                break;
             case 3:
                 double side1 = getRandomValue();
                 double side2 = getRandomValue();
                 randomFigure = new Rectangle(randomColor, side1, side2);
+                break;
             case 4:
                 double firstLeg = getRandomValue();
                 double secondLeg = getRandomValue();
                 randomFigure = new RightTriangle(randomColor, firstLeg, secondLeg);
+                break;
             default:
                 randomFigure = getDefaultFigure();
         }
