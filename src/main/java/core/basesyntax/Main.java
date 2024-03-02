179e41c6959573e2f09214bb8f0b95a38b19dcbd
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-03-02 14:38:18 +0200

    Update Main.java
    use constant field

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        final int FIGURE_ARRAY_SIZE = 6;
-        Figure[] figures = new Figure[FIGURE_ARRAY_SIZE];
+        final int figureArraySize = 6;
+        Figure[] figures = new Figure[figureArraySize];
 
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
