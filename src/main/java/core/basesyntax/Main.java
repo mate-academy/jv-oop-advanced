db8137dec1ded4b43f9ad6fc5070ff4719cf7e12
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-03-02 19:01:54 +0200

    Update Main.java
    made FIGURE_ARRAY_SIZE value as a private static final field, instead of the figureArraySize constant
    use figures[i].draw() outside if/else block

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,19 +1,19 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int FIGURE_ARRAY_SIZE = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        final int figureArraySize = 6;
-        Figure[] figures = new Figure[figureArraySize];
+        Figure[] figures = new Figure[FIGURE_ARRAY_SIZE];
 
-        for (int i = 0; i < figures.length; i++) {
-            if (i < figures.length / 2) {
+        for (int i = 0; i < FIGURE_ARRAY_SIZE; i++) {
+            if (i < FIGURE_ARRAY_SIZE / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
-                figures[i].draw();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
-                figures[i].draw();
             }
+            figures[i].draw();
         }
     }
 }
