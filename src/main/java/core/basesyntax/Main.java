ae28eaaa950cddaea872dc22b176c9bd077b9809
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-03-02 14:05:54 +0200

    Update Main.java
    use constant field

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,18 +2,18 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        RandomFigure randomFigure = new RandomFigure();
-
-        Figure[] figures = new Figure[6];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        final int FIGURE_ARRAY_SIZE = 6;
+        Figure[] figures = new Figure[FIGURE_ARRAY_SIZE];
 
         for (int i = 0; i < figures.length; i++) {
-            figures[i] = randomFigure.getRandomFigures();
             if (i < figures.length / 2) {
-                figures[i].drawFigure();
+                figures[i] = figureSupplier.getRandomFigure();
+                figures[i].draw();
             } else {
-                figures[i].getDefaultFigure().drawFigure();
+                figures[i] = figureSupplier.getDefaultFigure();
+                figures[i].draw();
             }
         }
-
     }
 }
