f05b956eee4370b17fb7feee740d0ebabc35e820
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-04 14:00:36 +0200

    Rectangle.java

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -17,7 +17,6 @@ public class Rectangle extends Shape {
 
     @Override
     public void draw() {
-        System.out.println("Figure: rectangle, area: " + getArea() + " sq.units," 
-                + " length: " + length + ", width: " + width + ", color: " + color);
+        System.out.println("Figure: rectangle, area: " + getArea() + " sq.units, length: " + length + ", width: " + width + ", color: " + color);
     }
 }
