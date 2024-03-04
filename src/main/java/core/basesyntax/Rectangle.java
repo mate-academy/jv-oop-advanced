c442f339fa44d7ac0f86d1c8a495fbecb37242d7
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-04 07:51:47 +0200

    Rectangle.java

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: " + getArea() + " sq.units, length: " + length + ", width: " + width + ", color: " + color);
+    }
+}
