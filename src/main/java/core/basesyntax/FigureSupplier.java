0d83e0dde8cc9c199b7d875556336b33ad0a4bbb
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-04 14:01:53 +0200

    FigureSupplier.java

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,7 +6,7 @@ public class FigureSupplier {
     private static final int MAX_PARAMETER = 10;
     private Random random = new Random();
 
-    public Figure getRandomFigure() {
+    public Shape getRandomFigure() {
         int figureType = random.nextInt(5);
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
@@ -16,30 +16,15 @@ public class FigureSupplier {
             case 1:
                 return new Square(color, random.nextInt(MAX_PARAMETER) + 1);
             case 2:
-                return new Rectangle(
-                    color, 
-                    random.nextInt(MAX_PARAMETER) + 1, 
-                    random.nextInt(MAX_PARAMETER) + 1
-                );
+                return new Rectangle(color, random.nextInt(MAX_PARAMETER) + 1, random.nextInt(MAX_PARAMETER) + 1);
             case 3:
-                return new RightTriangle(
-                    color, 
-                    random.nextInt(MAX_PARAMETER) + 1, 
-                    random.nextInt(MAX_PARAMETER) + 1
-                );
-            case 4:
-                return new IsoscelesTrapezoid(
-                    color, 
-                    random.nextInt(MAX_PARAMETER) + 1, 
-                    random.nextInt(MAX_PARAMETER) + 1, 
-                    random.nextInt(MAX_PARAMETER) + 1
-                );
+                return new IsoscelesTrapezoid(color, random.nextInt(MAX_PARAMETER) + 1, random.nextInt(MAX_PARAMETER) + 1, random.nextInt(MAX_PARAMETER) + 1);
             default:
                 return getDefaultFigure();
         }
     }
 
-    public Figure getDefaultFigure() {
-        return new Circle("white", 10);
-    }
+        public Shape getDefaultFigure() {
+            return new Circle("white", 10);
+        }
 }
