4bc18765d5efda85a94b76fea5f791b175f91a78
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-04 18:02:13 +0200

    FigureSupplier.java

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,33 +4,38 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAX_PARAMETER = 10;
+    private static final int FIGURE_TYPES = 5;
     private Random random = new Random();
 
     public Shape getRandomFigure() {
-        int figureType = random.nextInt(5);
+        int figureType = random.nextInt(FIGURE_TYPES);
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
         switch (figureType) {
             case 0:
-                return new Circle(color, random.nextInt(MAX_PARAMETER) + 1);
+                return new Circle(color, getRandomParameter());
             case 1:
-                return new Square(color, random.nextInt(MAX_PARAMETER) + 1);
+                return new Square(color, getRandomParameter());
             case 2:
                 return new Rectangle(
                     color, 
-                    random.nextInt(MAX_PARAMETER) + 1, 
-                    random.nextInt(MAX_PARAMETER) + 1
+                    getRandomParameter(), 
+                    getRandomParameter()
                 );
             case 3:
                 return new IsoscelesTrapezoid(
                     color, 
-                    random.nextInt(MAX_PARAMETER) + 1, 
-                    random.nextInt(MAX_PARAMETER) + 1, 
-                    random.nextInt(MAX_PARAMETER) + 1
+                    getRandomParameter(), 
+                    getRandomParameter(), 
+                    getRandomParameter()
                 );
             default:
                 return getDefaultFigure();
         }
+    }
+
+    private int getRandomParameter() {
+        return random.nextInt(MAX_PARAMETER) + 1;
     }
 
     public Shape getDefaultFigure() {
