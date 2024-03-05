305a558300d739ba655c724a2139691e33f4e5cf
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-05 10:27:01 +0200

    FigureSupplier.java

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -16,11 +16,24 @@ public class FigureSupplier {
             case 1:
                 return new Square(color, random.nextInt(MAX_PARAMETER) + 1);
             case 2:
-                return new Rectangle(color, random.nextInt(MAX_PARAMETER) + 1, random.nextInt(MAX_PARAMETER) + 1);
+                return new Rectangle(
+                    color, 
+                    random.nextInt(MAX_PARAMETER) + 1, 
+                    random.nextInt(MAX_PARAMETER) + 1
+                );
             case 3:
-                return new IsoscelesTrapezoid(color, random.nextInt(MAX_PARAMETER) + 1, random.nextInt(MAX_PARAMETER) + 1, random.nextInt(MAX_PARAMETER) + 1);
+                return new IsoscelesTrapezoid(
+                    color, 
+                    random.nextInt(MAX_PARAMETER) + 1, 
+                    random.nextInt(MAX_PARAMETER) + 1, 
+                    random.nextInt(MAX_PARAMETER) + 1
+                );
             case 4:
-                return new RightTriangle(color, random.nextInt(MAX_PARAMETER) + 1, random.nextInt(MAX_PARAMETER) + 1);
+                return new RightTriangle(
+                    color, 
+                    random.nextInt(MAX_PARAMETER) + 1, 
+                    random.nextInt(MAX_PARAMETER) + 1
+                );
             default:
                 return getDefaultFigure();
         }
