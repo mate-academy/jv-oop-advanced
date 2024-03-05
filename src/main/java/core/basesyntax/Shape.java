1fbe882a8c34529f19ebd8768761fde955f3c3dd
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-05 10:33:16 +0200

    Shape.java

--- src/main/java/core/basesyntax/Shape.java
+++ src/main/java/core/basesyntax/Shape.java
@@ -1,13 +1,15 @@
 package core.basesyntax;
 
-public abstract class Shape {
+public abstract class Shape implements FigureBehavior {
     protected Color color;
 
     public Shape(Color color) {
         this.color = color;
     }
 
-    public abstract void draw();
-
+    @Override
     public abstract double calculateArea();
+
+    @Override
+    public abstract void draw();
 }
