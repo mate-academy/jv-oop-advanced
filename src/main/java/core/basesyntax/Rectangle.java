b23250298ed7518c9105c3c00d4f6e3387105638
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-05 10:36:08 +0200

    Rectangle.java

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,23 +1,26 @@
 package core.basesyntax;
 
-public class Rectangle extends Shape {
-    private double length;
-    private double width;
+public class IsoscelesTrapezoid extends Shape {
+    private double topBase;
+    private double bottomBase;
+    private double height;
 
-    public Rectangle(Color color, double length, double width) {
+    public IsoscelesTrapezoid(Color color, double topBase, double bottomBase, double height) {
         super(color);
-        this.length = length;
-        this.width = width;
+        this.topBase = topBase;
+        this.bottomBase = bottomBase;
+        this.height = height;
     }
 
     @Override
     public double calculateArea() {
-        return length * width;
+        return ((topBase + bottomBase) / 2) * height;
     }
 
     @Override
     public void draw() {
-        System.out.println("Figure: rectangle, area: " + calculateArea() + " sq.units, length: " 
-                + length + ", width: " + width + ", color: " + color.name().toLowerCase());
+        System.out.println("Figure: isosceles trapezoid, area: " + calculateArea() + " sq.units,");
+        System.out.println("topBase: " + topBase + ", bottomBase: " + bottomBase + 
+                           ", height: " + height + ", color: " + color.name().toLowerCase());
     }
 }
