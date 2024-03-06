338930de50bddbf768466fb206cee9c5544ff62c
Banderan <bartoszwojcik2004@gmail.com>
2024-03-06 08:40:44 +0100

    Adding comments

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,32 +5,32 @@ import java.util.Random;
 public class FigureSupplier {
 
     public Figure getRandomFigure() {
-        Random random = new Random();
+        Random random = new Random();//adding bound: 1000 to avoid wrong area and to leave clarity code
         ColorSupplier colorSupplier = new ColorSupplier();
         Figure[] figures = {
                 new IsoscelesTrapezoid(
-                        colorSupplier.getRandomColor(),
-                        random.nextInt(1000),
-                        random.nextInt(1000),
-                        random.nextInt(1000)
+                        colorSupplier.getRandomColor(),//Color
+                        random.nextInt(1000),//upperSide
+                        random.nextInt(1000),//downSide
+                        random.nextInt(1000)//height
                 ),
                 new Circle(
-                        colorSupplier.getRandomColor(),
-                        random.nextInt(1000)
+                        colorSupplier.getRandomColor(),//color
+                        random.nextInt(1000)//radius
                 ),
                 new RightTriangle(
-                        colorSupplier.getRandomColor(),
-                        random.nextInt(1000),
-                        random.nextInt(1000)
+                        colorSupplier.getRandomColor(),//color
+                        random.nextInt(1000),//firstLeg
+                        random.nextInt(1000)//secondLeg
                 ),
                 new Rectangle(
-                        colorSupplier.getRandomColor(),
-                        random.nextInt(1000),
-                        random.nextInt(1000)
+                        colorSupplier.getRandomColor(),//color
+                        random.nextInt(1000),//sideA
+                        random.nextInt(1000)//sideB
                 ),
                 new Square(
-                        colorSupplier.getRandomColor(),
-                        random.nextInt(1000)
+                        colorSupplier.getRandomColor(),//color
+                        random.nextInt(1000)//side
                 )};
         return figures[random.nextInt(figures.length)];
     }
