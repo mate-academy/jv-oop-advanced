240bfb613636aa5b8792b2c3330fbc36e1cd0f7c
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-13 13:39:30 +0200

    Generating list of Figures, and drawing them

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,5 +2,20 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[6];
+
+        for (int i = 0; i < figures.length; i++) {
+            if (i >= figures.length / 2) {
+                figures[i] = figureSupplier.getDefaultFigure();
+                continue;
+            }
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (Figure fig : figures) {
+            fig.draw();
+        }
+
     }
 }
