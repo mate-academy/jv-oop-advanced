2be7f63b6574ba8d9c1bd07e469d7a84ad15a83e
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-13 15:02:19 +0200

    added public mod

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-abstract class Figure implements Figurable {
+public abstract class Figure implements Figurable {
     private final String color;
 
     public Figure(String color) {
