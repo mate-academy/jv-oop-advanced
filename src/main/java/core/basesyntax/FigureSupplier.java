054cec3db20a0894d7833fda8191c523a602051e
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-13 13:38:56 +0200

    FigureSupplier with getRandomFigure and getDefaultFigure methods

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,40 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        int numberOfFigure = random.nextInt(5);
+        String randomColor = colorSupplier.getRandomColor();
+
+        switch (numberOfFigure) {
+            case 0: {
+                return new Circle(randomColor, getRandomValueForFigureParameter());
+            }
+            case 1: {
+                return new IsoscelesTrapezoid(randomColor, getRandomValueForFigureParameter(), getRandomValueForFigureParameter(), getRandomValueForFigureParameter());
+            }
+            case 2: {
+                return new Rectangle(randomColor, getRandomValueForFigureParameter(), getRandomValueForFigureParameter());
+            }
+            case 3: {
+                return new RightTriangle(randomColor, getRandomValueForFigureParameter(), getRandomValueForFigureParameter());
+            }
+            default: {
+                return new Square(randomColor, getRandomValueForFigureParameter());
+            }
+        }
+    }
+
+    private Double getRandomValueForFigureParameter() {
+        int randomMultiplier = 100;
+        return random.nextDouble(randomMultiplier);
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle(colorSupplier.getRandomColor(), 10);
+    }
+}
