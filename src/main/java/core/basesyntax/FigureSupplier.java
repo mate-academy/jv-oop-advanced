dea4f2f5436d77f5be56111d7bb18252c09030b4
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-14 16:33:55 +0200

    Constants in uppercase, added ColorSupplier field as dependence of FigureSupplier

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,14 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int figuresCount = 5;
-    private static final int randomFigureRangeParameter = 100;
-    private static final int initialCircleRadius = 10;
+    private static final int FIGURES_COUNT = 5;
+    private static final int RANDOM_FIGURE_RANGE_PARAMETER = 100;
+    private static final int INITIAL_CIRCLE_RADIUS = 10;
     private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public AbstractFigure getRandomFigure() {
-        int numberOfFigure = random.nextInt(figuresCount);
-        String randomColor = ColorSupplier.getRandomColor();
+        int numberOfFigure = random.nextInt(FIGURES_COUNT);
+        String randomColor = colorSupplier.getRandomColor();
 
         switch (numberOfFigure) {
             case 0: {
@@ -40,10 +41,10 @@ public class FigureSupplier {
     }
 
     private double getRandomValueForFigureParameter() {
-        return random.nextDouble(randomFigureRangeParameter);
+        return random.nextDouble(RANDOM_FIGURE_RANGE_PARAMETER);
     }
 
     public AbstractFigure getDefaultFigure() {
-        return new CircleFigure(ColorSupplier.getRandomColor(), initialCircleRadius);
+        return new CircleFigure(colorSupplier.getRandomColor(), INITIAL_CIRCLE_RADIUS);
     }
 }
