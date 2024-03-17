2a073a54c05a976d39b48db9332ce1863e0eeaaa
Bartlomiej Smreczak <b.smreczak@gmail.com>
2024-03-17 21:33:40 +0100

    Fixes after code review, made colorSupplier variable as constant.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -7,25 +7,25 @@ public class FigureSupplier {
     private static final int DEFAULT_RADIUS = 10;
     private static final int BOUND_MAX = 10000;
     private static final int FIGURES_QUANTITY = 5;
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
 
     private final Random random = new Random();
 
     public Figure getRandomFigure() {
         int randomIndex = random.nextInt(FIGURES_QUANTITY);
-        ColorSupplier colorSupplier = new ColorSupplier();
 
         return switch (randomIndex) {
-            case 0 -> new Circle(colorSupplier.getRandomColor(),
+            case 0 -> new Circle(COLOR_SUPPLIER.getRandomColor(),
                     random.nextInt(BOUND_MAX));
-            case 1 -> new Square(colorSupplier.getRandomColor(),
+            case 1 -> new Square(COLOR_SUPPLIER.getRandomColor(),
                     random.nextInt(BOUND_MAX));
-            case 2 -> new Rectangle(colorSupplier.getRandomColor(),
+            case 2 -> new Rectangle(COLOR_SUPPLIER.getRandomColor(),
                     random.nextInt(BOUND_MAX),
                     random.nextInt(BOUND_MAX));
-            case 3 -> new RightTriangle(colorSupplier.getRandomColor(),
+            case 3 -> new RightTriangle(COLOR_SUPPLIER.getRandomColor(),
                     random.nextInt(BOUND_MAX),
                     random.nextInt(BOUND_MAX));
-            case 4 -> new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+            case 4 -> new IsoscelesTrapezoid(COLOR_SUPPLIER.getRandomColor(),
                     random.nextInt(BOUND_MAX),
                     random.nextInt(BOUND_MAX),
                     random.nextInt(BOUND_MAX));
