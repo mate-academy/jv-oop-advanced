ce0fafcd2692587a6f49afaa3d4c81481e47d5b0
Artem Kravets <kravetsartyom@gmail.com>
2024-03-21 19:39:09 +0200

    implement figures

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Rectangle extends Square {
+    private static final double DEFAULT_DECIMAL_RANK = 10.0;
+    private int secondSide;
+
+    public Rectangle(String color, int side, int secondSide) {
+        super(color, side);
+        this.secondSide = secondSide;
+    }
+
+    public double getArea() {
+        return Math.round(getSide() * secondSide * DEFAULT_DECIMAL_RANK) / DEFAULT_DECIMAL_RANK;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: " + this.getClass().getSimpleName().toLowerCase()
+                + ", area: " + getArea() + " sq. units,"
+                + " side: " + getSide() + " units,"
+                + " secondSide: " + secondSide + " units,"
+                + " color: " + getColor()
+        );
+    }
+}
