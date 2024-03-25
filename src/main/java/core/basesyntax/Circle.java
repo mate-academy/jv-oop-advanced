84133108c61ec1aeca41d5768834e07596d6ece0
Artem Kravets <kravetsartyom@gmail.com>
2024-03-25 20:11:47 +0200

    figures classes

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private int radius;
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: circle, area: " + Math.PI * radius * radius + " sq. units,"
+                + " radius: " + radius + " units, color: " + this.getColor());
+    }
+}
---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,47 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private int firstBase;
+    private int secondBase;
+    private int height;
+
+    public IsoscelesTrapezoid(String color, int firstBase, int secondBase, int height) {
+        super(color);
+        this.firstBase = firstBase;
+        this.secondBase = secondBase;
+        this.height = height;
+    }
+
+    public int getFirstBase() {
+        return firstBase;
+    }
+
+    public void setFirstBase(int firstBase) {
+        this.firstBase = firstBase;
+    }
+
+    public int getSecondBase() {
+        return secondBase;
+    }
+
+    public void setSecondBase(int secondBase) {
+        this.secondBase = secondBase;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, "
+                + "area: " + (firstBase + secondBase) / 2 * height + " sq. units, "
+                + "firstBase: " + firstBase + " units, secondBase: " + secondBase
+                + " units, height: " + height + " units,"
+                + " color: " + this.getColor());
+    }
+}
