74b071b0abea525d9a782a49919feb82d5c09225
Vadym <Servicepost.v@gmail.com>
2024-03-28 16:47:09 +0100

    fixed syntax mistakes. Changed getRandomFigure method. Added new methods in FigureSupplier class.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,53 +4,107 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final Color DEFAULT_COLOR = Color.WHITE;
+    private static final int FIGURES_COUNT = 5;
     private static final double DEFAULT_RADIUS = 10.0;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        Figure[] randomFigures = new Figure[] {
-                new Circle(),
-                new Square(),
-                new IsoscelesTrapezoid(),
-                new RightTriangle(),
-                new Rectangle()
+        final int randomIndex = random.nextInt(FIGURES_COUNT);
+        return switch (randomIndex) {
+            case 0 -> getRandomCircle();
+            case 1 -> getRandomSquare();
+            case 2 -> getRandomRectangle();
+            case 3 -> getRandomIsoscelesTrapezoid();
+            default -> getRandomRightTriangle();
         };
 
-        final int randomIndex = random.nextInt(randomFigures.length);
-        Figure figures = randomFigures[randomIndex];
 
-        if (figures instanceof Circle circle) {
-            circle.setRadius(random.nextDouble());
-            circle.setColor(colorSupplier.getRandomColor());
-        }
-        if (figures instanceof Square square) {
-            square.setSide(random.nextDouble());
-            square.setColor(colorSupplier.getRandomColor());
-        }
-        if (figures instanceof IsoscelesTrapezoid isoscelesTrapezoid) {
-            isoscelesTrapezoid.setHeight(random.nextDouble());
-            isoscelesTrapezoid.setSideA(random.nextDouble());
-            isoscelesTrapezoid.setSideB(random.nextDouble());
-            isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
-        }
-        if (figures instanceof RightTriangle rightTriangle) {
-            rightTriangle.setHeight(random.nextDouble());
-            rightTriangle.setBase(random.nextDouble());
-            rightTriangle.setColor(colorSupplier.getRandomColor());
-        }
-        if (figures instanceof Rectangle rectangle) {
-            rectangle.setLength(random.nextDouble());
-            rectangle.setWidth(random.nextDouble());
-            rectangle.setColor(colorSupplier.getRandomColor());
-        }
-        return figures;
     }
 
-    public Figure getDefaultFigure() {
+    private Figure getRandomCircle() {
         Circle circle = new Circle();
-        circle.setColor(DEFAULT_COLOR.name());
-        circle.setRadius(DEFAULT_RADIUS);
+        circle.setRadius(random.nextDouble());
+        circle.setColor(colorSupplier.getRandomColor());
         return circle;
     }
+
+    private Figure getRandomRightTriangle() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(random.nextDouble());
+        rectangle.setWidth(random.nextDouble());
+        rectangle.setColor(colorSupplier.getRandomColor());
+        return rectangle;
+    }
+
+    private Figure getRandomIsoscelesTrapezoid() {
+        IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid();
+        isoscelesTrapezoid.setHeight(random.nextDouble());
+        isoscelesTrapezoid.setSideA(random.nextDouble());
+        isoscelesTrapezoid.setSideB(random.nextDouble());
+        isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
+        return isoscelesTrapezoid;
+    }
+
+    private Figure getRandomRectangle() {
+        RightTriangle rightTriangle = new RightTriangle();
+        rightTriangle.setHeight(random.nextDouble());
+        rightTriangle.setBase(random.nextDouble());
+        rightTriangle.setColor(colorSupplier.getRandomColor());
+        return rightTriangle;
+    }
+
+    private Figure getRandomSquare() {
+        Square square = new Square();
+        square.setSide(random.nextDouble());
+        square.setColor(colorSupplier.getRandomColor());
+        return square;
+    }
+
+    public Figure getDefaultFigure () {
+            Circle circle = new Circle();
+            circle.setColor(DEFAULT_COLOR.name());
+            circle.setRadius(DEFAULT_RADIUS);
+            return circle;
+        }
+
+
 }
+
+
+//        Figure[] randomFigures = new Figure[] {
+//                new Circle(),
+//                new Square(),
+//                new IsoscelesTrapezoid(),
+//                new RightTriangle(),
+//                new Rectangle()
+//        };
+//
+//        final int randomIndex = random.nextInt(randomFigures.length);
+//        Figure figures = randomFigures[randomIndex];
+//
+//        if (figures instanceof Circle circle) {
+//            circle.setRadius(random.nextDouble());
+//            circle.setColor(colorSupplier.getRandomColor());
+//        }
+//        if (figures instanceof Square square) {
+//            square.setSide(random.nextDouble());
+//            square.setColor(colorSupplier.getRandomColor());
+//        }
+//        if (figures instanceof IsoscelesTrapezoid isoscelesTrapezoid) {
+//            isoscelesTrapezoid.setHeight(random.nextDouble());
+//            isoscelesTrapezoid.setSideA(random.nextDouble());
+//            isoscelesTrapezoid.setSideB(random.nextDouble());
+//            isoscelesTrapezoid.setColor(colorSupplier.getRandomColor());
+//        }
+//        if (figures instanceof RightTriangle rightTriangle) {
+//            rightTriangle.setHeight(random.nextDouble());
+//            rightTriangle.setBase(random.nextDouble());
+//            rightTriangle.setColor(colorSupplier.getRandomColor());
+//        }
+//        if (figures instanceof Rectangle rectangle) {
+//            rectangle.setLength(random.nextDouble());
+//            rectangle.setWidth(random.nextDouble());
+//            rectangle.setColor(colorSupplier.getRandomColor());
+//        }
+//        return figures;
