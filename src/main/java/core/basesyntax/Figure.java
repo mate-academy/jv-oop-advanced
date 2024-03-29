4a8b283072e653104efe0941eb35ed110558e206
Igor Glebov <igor.glebov0312@gmail.com>
2024-03-29 12:43:18 +0200

    Fixed principe of creation figure

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public abstract class Figure implements Drawable, AreaCalculate {
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private String color =  colorSupplier.getRandomColor();
+    private String color = colorSupplier.getRandomColor();
 
     public void setColor(String color) {
         this.color = color;
