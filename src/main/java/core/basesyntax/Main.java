e86d250eb8db0de0305a406699abe02c254f51eb
Xoteki <xotekijke@gmail.com>
2024-04-15 17:41:44 +0300

    fixed an issue in main method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import core.basesyntax.model.Figure;
 
 public class Main {
-    private static final int amountOfCreatedFigures = 5;
+    private static final int amountOfCreatedFigures = 4;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[amountOfCreatedFigures];
         for (int i = 0; i < amountOfCreatedFigures; i++) {
-            if (i == 1) {
+            if (i < amountOfCreatedFigures / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
