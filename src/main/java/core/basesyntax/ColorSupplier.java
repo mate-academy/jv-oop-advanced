ef8fdf1158bc981ed7e6c70d315a3c4937f07f0a
Xoteki <xotekijke@gmail.com>
2024-04-16 21:49:00 +0300

    recommended fixes were made

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,6 +7,7 @@ public class ColorSupplier {
     private final Random random = new Random();
 
     public Color getRandomColor() {
-        return Color.values()[random.nextInt(Color.values().length)];
+        int randomIndex = random.nextInt(Color.values().length);
+        return Color.values()[randomIndex];
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import core.basesyntax.model.Figure;
 
 public class Main {
-    private static final int amountOfCreatedFigures = 4;
+    private static final int FIGURES_COUNT = 4;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[amountOfCreatedFigures];
-        for (int i = 0; i < amountOfCreatedFigures; i++) {
-            if (i < amountOfCreatedFigures / 2) {
+        Figure[] figures = new Figure[FIGURES_COUNT];
+        for (int i = 0; i < FIGURES_COUNT; i++) {
+            if (i < FIGURES_COUNT / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
