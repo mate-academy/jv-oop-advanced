2b93f296ecec51bd1242c2f8574671c93935122a
flamehost <flamehost@syneforge.com>
2024-04-17 21:35:13 +0300

    display edits

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -18,7 +18,7 @@ public class Rectangle extends Figure {
     @Override
     public void draw() {
         System.out.println("Figure: rectangle, "
-                + ", area: " + areaCalculation() + " sq. units"
+                + "area: " + areaCalculation() + " sq. units"
                 + ", firstSide: " + firstSide + " units"
                 + ", secondSide: " + secondSide + " units"
                 + ", color: " + getColor());
