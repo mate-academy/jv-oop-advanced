cff96208bb4b43bdfedc4e80f10249aa651b34a2
Alisa Hoys <hoys@protonmail.com>
2024-04-19 18:25:31 +0300

    Prevented returning null in FigureSupplier. Extracted to constants: DEFAULT_RADIUS, DEFAULT_COLOR, MAX_SIZE in FigureSupplier.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,9 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private final int maxSize = 10;
+    private static final Color DEFAULT_COLOR = Color.WHITE;
+    private static final int DEFAULT_RADIUS = 10;
+    private static final int MAX_SIZE = 10;
+    private final ColorSupplier colorSupplier;
+
+    {
+        colorSupplier = new ColorSupplier();
+    }
+
     private final Random random = new Random();
-    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         Color color = colorSupplier.getRandomColor();
@@ -14,24 +21,24 @@ public class FigureSupplier {
 
         switch (figureType) {
             case 0:
-                return new Square(random.nextDouble() * maxSize,color);
+                return new Square(random.nextDouble() * MAX_SIZE,color);
             case 1:
-                return new Rectangle(random.nextDouble() * maxSize,
-                        random.nextDouble() * maxSize, color);
+                return new Rectangle(random.nextDouble() * MAX_SIZE,
+                        random.nextDouble() * MAX_SIZE, color);
             case 2:
-                return new RightTriangle(random.nextDouble() * maxSize,
-                        random.nextDouble() * maxSize, color);
+                return new RightTriangle(random.nextDouble() * MAX_SIZE,
+                        random.nextDouble() * MAX_SIZE, color);
             case 3:
-                return new Circle(random.nextDouble() * maxSize, color);
+                return new Circle(random.nextDouble() * MAX_SIZE, color);
             case 4:
-                return new IsoscelesTrapezoid(random.nextDouble() * maxSize,
-                        random.nextDouble() * maxSize, random.nextDouble() * maxSize, color);
             default:
-                return null;
+                return new IsoscelesTrapezoid(random.nextDouble() * MAX_SIZE,
+                        random.nextDouble() * MAX_SIZE, random.nextDouble() * MAX_SIZE, color);
+
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(maxSize, Color.WHITE);
+        return new Circle(DEFAULT_RADIUS, DEFAULT_COLOR);
     }
 }
