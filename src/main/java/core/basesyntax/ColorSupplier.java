752941a018f8a84ea425cc03fe525590cc22bf4e
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-04-20 11:37:42 +0300

    add random to ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class ColorSupplier {
-    private final Random random = new Random();
-    public String getRandomColor() {
+public interface ColorSupplier {
+    Random random = new Random();
+    static String getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
