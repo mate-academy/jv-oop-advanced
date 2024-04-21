6bb79b5f9b31a9e4471a28e6793dd2805936edde
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-04-21 18:00:31 +0300

    add Figure and FigureSupplier classes

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
-public interface Figure {
-    public double getArea();
-    void draw();
+public abstract class Figure implements Drawable {
+    String color;
+    abstract int getArea();
+
 }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,35 +4,37 @@ import java.util.Random;
 
 public class FigureSupplier implements ColorSupplier {
     private static final int FIGURE_COUNT = 5;
-    private static final int FIGURE_PARAMETER = 20;
+    private static final int FIGURE_PARAMETER = 10;
     private static final int DEFAULT_RADIUS = 10;
     private static final String DEFAULT_COLOR = "WHITE";
-    private static final String color = ColorSupplier.getRandomColor();
     private final Random random = new Random();
 
-  public Figure getRandomFigure() {
-    int index = random.nextInt(FIGURE_COUNT);
-    switch (index) {
-        case 0:
-            return new Circle(random.nextInt(FIGURE_PARAMETER),color);
+    public Figure getRandomFigure() {
+        final String color = ColorSupplier.getRandomColor();
+        int index = random.nextInt(FIGURE_COUNT);
+        switch (index) {
+            case 0:
+                return new Circle(random.nextInt(FIGURE_PARAMETER),color);
 
-        case 1:
-            return new IsoscelesTrapezoid(random.nextInt(FIGURE_PARAMETER),random.nextInt(FIGURE_PARAMETER),random.nextInt(FIGURE_PARAMETER),color);
+            case 1:
+                return new IsoscelesTrapezoid(random.nextInt(FIGURE_PARAMETER),
+                        random.nextInt(FIGURE_PARAMETER),random.nextInt(FIGURE_PARAMETER),color);
 
-        case 2:
-            return new Rectangle(random.nextInt(FIGURE_PARAMETER), random.nextInt(FIGURE_PARAMETER),color);
+            case 2:
+                return new Rectangle(random.nextInt(FIGURE_PARAMETER),
+                         random.nextInt(FIGURE_PARAMETER),color);
 
-        case 3:
-            return new Square(random.nextInt(FIGURE_PARAMETER),color);
+            case 3:
+                return new Square(random.nextInt(FIGURE_PARAMETER),color);
 
-        case 4:
-            default: return new RightTriangle(random.nextInt(FIGURE_PARAMETER), random.nextInt(FIGURE_PARAMETER),color);
+            case 4:
+            default:
+                return new RightTriangle(random.nextInt(FIGURE_PARAMETER),
+                         random.nextInt(FIGURE_PARAMETER),color);
+        }
     }
 
-  }
     public Figure getDefaultFigure() {
         return new Circle(DEFAULT_RADIUS,DEFAULT_COLOR);
-
-
     }
 }
