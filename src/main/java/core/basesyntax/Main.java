75460dc6528f234d9ce26a7c08db3352b5249c00
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-04-24 17:23:45 +0200

    a generator of random figures with random parameters has been created

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
+        final int Size_Array = 6;
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[Size_Array];
 
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
@@ -12,7 +13,6 @@ public class Main {
                 figures[i] = figureSupplier.getDefaultFigure();
             }
             figures[i].draw();
-
         }
     }
 }
