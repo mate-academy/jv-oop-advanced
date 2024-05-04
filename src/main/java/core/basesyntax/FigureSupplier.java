709f3d7b7ee942cc8bd3c6cd8c0e1d263583b147
Ppllupold <kotnazar2005@gmail.com>
2024-05-04 17:48:36 +0300

    jv-oop advanced avoided dups.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,26 +4,26 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int SIDE_SIZE_LIMIT = 100;
-    private static final int NUMBER_OF_FIGURE_TYPES = 4;
+    private static final int NUMBER_OF_FIGURE_TYPES = 5;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final int paramsRandomizer1 = random.nextInt(SIDE_SIZE_LIMIT);
+    private final int paramsRandomizer2 = random.nextInt(SIDE_SIZE_LIMIT);
+    private final String randomColor = colorSupplier.getRandomColor();
 
     public Figure getRandomFigure() {
-        int paramsRandomizer1 = random.nextInt(SIDE_SIZE_LIMIT);
-        int paramsRandomizer2 = random.nextInt(SIDE_SIZE_LIMIT);
-        String randomColor = colorSupplier.getRandomColor();
+        FigureSupplier figureSupplier = new FigureSupplier();
         switch (random.nextInt(NUMBER_OF_FIGURE_TYPES)) {
             case 0:
-                return new Circle(randomColor, paramsRandomizer1);
+                return figureSupplier.getRandomCircle();
             case 1:
-                return new Rectangle(randomColor, paramsRandomizer1, paramsRandomizer2);
+                return figureSupplier.getRandomRectangle();
             case 2:
-                return new IsoscelesTrapezoid(randomColor, paramsRandomizer1,
-                        paramsRandomizer2, random.nextInt(SIDE_SIZE_LIMIT));
+                return figureSupplier.getRandomIscTrap();
             case 3:
-                return new Square(randomColor, paramsRandomizer1);
+                return figureSupplier.getRandomSquare();
             case 4:
-                return new RightTriangle(randomColor, paramsRandomizer1);
+                return figureSupplier.getRandomRightTriangle();
             default:
                 return getDefaultFigure();
         }
@@ -32,4 +32,26 @@ public class FigureSupplier {
     public Circle getDefaultFigure() {
         return new Circle("white", 10);
     }
+
+    public Circle getRandomCircle() {
+        return new Circle(randomColor, paramsRandomizer1);
+    }
+
+    public Rectangle getRandomRectangle() {
+        return new Rectangle(randomColor, paramsRandomizer1, paramsRandomizer2);
+    }
+
+    public Square getRandomSquare() {
+        return new Square(randomColor, paramsRandomizer1);
+    }
+
+    public IsoscelesTrapezoid getRandomIscTrap() {
+        return new IsoscelesTrapezoid(randomColor, paramsRandomizer1,
+                paramsRandomizer2, random.nextInt(SIDE_SIZE_LIMIT));
+    }
+
+    public RightTriangle getRandomRightTriangle() {
+        return new RightTriangle(randomColor, paramsRandomizer1);
+    }
+
 }
