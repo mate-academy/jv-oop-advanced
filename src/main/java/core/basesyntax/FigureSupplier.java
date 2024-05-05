e8736b3e2e9a805ac20d0af1c1c11c2eaa37313c
Ppllupold <kotnazar2005@gmail.com>
2024-05-05 21:48:16 +0300

    jv-oop advanced fixed hardcode

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,27 +5,32 @@ import java.util.Random;
 public class FigureSupplier {
     private static final String DEFAULT_FIGURE_COLOR = Color.WHITE.name();
     private static final int DEFAULT_CIRCLE_RADUIS = 10;
-    private static final int SIDE_SIZE_LIMIT = 100;
     private static final int NUMBER_OF_FIGURE_TYPES = 5;
+    private static final int SIDE_SIZE_LIMIT = 101;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int paramsRandomizer1 = random.nextInt(SIDE_SIZE_LIMIT);
-    private final int paramsRandomizer2 = random.nextInt(SIDE_SIZE_LIMIT);
-    private final String randomColor = colorSupplier.getRandomColor();
+    private final int paramsRandomizer1;
+    private final int paramsRandomizer2;
+    private final String randomColor;
+
+    public FigureSupplier() {
+        paramsRandomizer1 = random.nextInt(SIDE_SIZE_LIMIT);
+        paramsRandomizer2 = random.nextInt(SIDE_SIZE_LIMIT);
+        randomColor = colorSupplier.getRandomColor();
+    }
 
     public Figure getRandomFigure() {
-        FigureSupplier figureSupplier = new FigureSupplier();
         switch (random.nextInt(NUMBER_OF_FIGURE_TYPES)) {
             case 0:
-                return figureSupplier.getRandomCircle();
+                return getRandomCircle();
             case 1:
-                return figureSupplier.getRandomRectangle();
+                return getRandomRectangle();
             case 2:
-                return figureSupplier.getRandomIscTrap();
+                return getRandomIscTrap();
             case 3:
-                return figureSupplier.getRandomSquare();
+                return getRandomSquare();
             case 4:
-                return figureSupplier.getRandomRightTriangle();
+                return getRandomRightTriangle();
             default:
                 return getDefaultFigure();
         }
