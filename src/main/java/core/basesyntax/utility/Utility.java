9479121c2e87b4983df4381db613a1e2056e69db
Greshch <greshchook@gmail.com>
2024-05-11 15:49:29 +0300

    coefficient

--- src/main/java/core/basesyntax/utility/Utility.java
+++ src/main/java/core/basesyntax/utility/Utility.java
@@ -2,7 +2,7 @@ package core.basesyntax.utility;
 
 public class Utility {
     public static double roundAfterPoint(double number, int position) {
-        int koeficient = (int) Math.pow(10, position);
-        return (double) ((long) (number * koeficient)) / koeficient;
+        int coefficient = (int) Math.pow(10, position);
+        return (double) ((long) (number * coefficient)) / coefficient;
     }
 }
