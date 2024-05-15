28d711cd5a94e9f361f25640f6eeee5626ca2ced
Anton <panyutin15@gmail.com>
2024-05-15 15:18:35 +0300

    fixed mistakes according pull request from kerrrusha

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,25 +1,17 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class Main {
     public static void main(String[] args) {
         Figure[] figures = new Figure[6];
         FigureSupplier figureSupplier = new FigureSupplier();
 
-        for (int i = 0; i < figures.length / 2; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-
-        for (int i = figures.length / 2; i < figures.length; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
-        }
-
         for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
             figures[i].draw();
         }
-
     }
-
 }
