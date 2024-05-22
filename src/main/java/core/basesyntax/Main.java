2655684dbb5071e0440477da6acbef477cc093da
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-05-22 18:58:14 +0200

    remove all and change name of class and add loop for chooses figures

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+
+public class Main {
+    private static final int NUMBER_OF_FIGURES = 6;
+
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[NUMBER_OF_FIGURES];
+
+        for (int i = 0; i < figures.length / 2; i++) {
+            figures[i] = (Figure) figureSupplier.getRandomFigure();
+        }
+        for (int i = figures.length / 2; i < figures.length; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
+}
