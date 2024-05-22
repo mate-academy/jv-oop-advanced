2720d5dd2edc1604688981ea11846faee93f8fb1
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-05-22 18:57:31 +0200

    create a  IsoscelesTrapezoid and add math command

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private final double base1;
+    private final double base2;
+    private final double height;
+
+    public IsoscelesTrapezoid(String color, double base1, double base2, double height) {
+        super(color);
+        this.base1 = base1;
+        this.base2 = base2;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return ((base1 + base2) / 2) * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, area: " + calculateArea() + " sq. units, base1: " + base1 + " units, base2: " + base2 + " units, height: " + height + " units, color: " + getColor());
+    }
+}
