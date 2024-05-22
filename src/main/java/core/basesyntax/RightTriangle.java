64b0f11eb7475b981aa1645575bdb28d97a5cb52
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-05-22 18:59:30 +0200

    create a righttriangle and add a math command

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class RightTriangle extends Figure {
+    private final double firstLeg;
+    private final double secondLeg;
+
+    public RightTriangle(String color, double firstLeg, double secondLeg) {
+        super(color);
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double calculateArea() {
+        return (firstLeg * secondLeg) / 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: right triangle, area: " + calculateArea()
+                + " sq. units, firstLeg: "
+                + firstLeg
+                + " units, secondLeg: "
+                + secondLeg
+                + " units, color: " + getColor());
+    }
+}
