16b3c06f35cf7c7ec1b711129bafc44a4faf6ccb
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-05-22 19:09:52 +0200

    pull

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -15,7 +15,8 @@ public class Square extends Figure {
 
     @Override
     public void draw() {
-        System.out.println("Figure: square, area: " + calculateArea() +
-                " sq. units, side: " + side + " units, color: " + getColor());
+        System.out.println("Figure: square, area: "
+                + calculateArea()
+                + " sq. units, side: " + side + " units, color: " + getColor());
     }
 }
