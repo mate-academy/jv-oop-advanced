333afaa3721409687b0fdd83ee54cb3a59d16514
Oleksii <obabych1@stu.vistula.edu.pl>
2024-06-01 19:04:43 +0200

    fixed mistake 'Figures parent class Object should implement interfaces that describe common behavior for all classes of figures'

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure {
+public abstract class Figure implements Drawable, Measurable {
 
     private final String color;
 
@@ -12,7 +12,9 @@ public abstract class Figure {
         return color;
     }
 
+    @Override
     public abstract double getArea();
 
+    @Override
     public abstract void draw();
 }
