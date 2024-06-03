fb4fff29ed1c4994ec57b5289a45eb6a7240f399
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-03 08:08:47 +0300

    Circle.java

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -15,6 +15,7 @@ public class Circle extends Figure {
 
     @Override
     public void draw() {
-        System.out.println("Figure: circle, area: " + getArea() + " sq. units, radius: " + radius + " units, color: " + getColor());
+        System.out.println("Figure: circle, area: " + getArea() + " sq. units, radius: " 
+                + radius + " units, color: " + getColor());
     }
 }
