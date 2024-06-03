a7e2f8e72141834139e2ad31096dc88123f0dae5
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-03 08:10:33 +0300

    RightTriangle.java

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -17,6 +17,7 @@ public class RightTriangle extends Figure {
 
     @Override
     public void draw() {
-        System.out.println("Figure: right triangle, area: " + getArea() + " sq. units, firstLeg: " + firstLeg + " units, secondLeg: " + secondLeg + " units, color: " + getColor());
+        System.out.println("Figure: right triangle, area: " + getArea() + " sq. units, firstLeg: " 
+                + firstLeg + " units, secondLeg: " + secondLeg + " units, color: " + getColor());
     }
 }
