7712ef9af9f18b181a964df224b135a2d358c8ec
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-03 08:09:04 +0300

    Square.java

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -15,6 +15,7 @@ public class Square extends Figure {
 
     @Override
     public void draw() {
-        System.out.println("Figure: square, area: " + getArea() + " sq. units, side: " + side + " units, color: " + getColor());
+        System.out.println("Figure: square, area: " + getArea() + " sq. units, side: " 
+                + side + " units, color: " + getColor());
     }
 }
