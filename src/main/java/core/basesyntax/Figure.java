62314d5f20929f877b25a5bc9ef8149f2c87e0ac
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-03 09:45:06 +0300

    Figure.java

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public abstract class Figure implements Drawable, AreaCalculator {
-    private String color;
+    private Color color;
 
-    public Figure(String color) {
+    public Figure(Color color) {
         this.color = color;
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 }
