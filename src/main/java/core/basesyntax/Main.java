b2d9ec4cbf2f46960ebac4fddf9d1cde29ba5d0f
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-03 09:42:36 +0300

    Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,16 +1,14 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int FIGURE_ARRAY_SIZE = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[FIGURE_ARRAY_SIZE];
 
-        for (int i = 0; i < figures.length / 2; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-
-        for (int i = figures.length / 2; i < figures.length; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
+        for (int i = 0; i < figures.length; i++) {
+            figures[i] = (i < figures.length / 2) ? figureSupplier.getRandomFigure() : figureSupplier.getDefaultFigure();
         }
 
         for (Figure figure : figures) {
