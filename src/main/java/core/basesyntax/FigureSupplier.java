aacfa6b572eb992c54fbd0021b6e46ca933d8d07
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-03 08:00:57 +0300

    FigureSupplier.java

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class FigureSupplier {
+    private static final int FIGURE_COUNT = 5;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        int figureType = random.nextInt(FIGURE_COUNT);
+        String color = colorSupplier.getRandomColor();
+        switch (figureType) {
+            case 0:
+                return new Circle(color, random.nextDouble() * 10);
+            case 1:
+                return new Square(color, random.nextDouble() * 10);
+            case 2:
+                return new Rectangle(color, random.nextDouble() * 10, random.nextDouble() * 10);
+            case 3:
+                return new RightTriangle(color, random.nextDouble() * 10, random.nextDouble() * 10);
+            case 4:
+                return new IsoscelesTrapezoid(color, random.nextDouble() * 10, random.nextDouble() * 10, random.nextDouble() * 10);
+            default:
+                return getDefaultFigure();
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle("white", 10);
+    }
+}
