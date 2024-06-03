22dd8bd1cbe6db19a21e591df06699f0617e1bbf
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-03 09:49:37 +0300

    IsoscelesTrapezoid.java

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -20,7 +20,7 @@ public class IsoscelesTrapezoid extends Figure {
     @Override
     public void draw() {
         System.out.println("Figure: isosceles trapezoid, area: " + getArea() + " sq. units, base1: "
-                + base1 + " units, base2: " + base2 + " units, height: " 
-                           + height + " units, color: "+ getColor().name().toLowerCase());
+                + base1 + " units, base2: " + base2 + " units, height: " + height + " units, color: "
+                + getColor().name().toLowerCase());
     }
 }
