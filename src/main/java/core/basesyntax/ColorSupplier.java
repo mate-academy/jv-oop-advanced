55d51bfc500ce285a61b12b096bfdc2376084abd
Fedir Lozovyi <sumkin112@gmail.com>
2024-06-05 13:50:58 +0200

    add enum Colors create constants fixed things

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 class ColorSupplier {
-    private static final String[] COLORS = {"red", "green", "blue",
-            "yellow", "purple", "black", "white"};
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        Random random = new Random();
-        return COLORS[random.nextInt(COLORS.length)];
+        int index = random.nextInt(Colors.values().length);
+        return Colors.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,35 +3,30 @@ package core.basesyntax;
 import java.util.Random;
 
 class FigureSupplier {
+    private static final int BOUND = 10;
+    private static final int RADIUS = 10;  //Default Circle radius
+    private static final String COLOR = "WHITE"; //Default Circle color
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int figureType = random.nextInt(5);
 
-        switch (figureType) {
-            case 0:
-                return new Square(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
-            case 1:
-                return new Rectangle(colorSupplier.getRandomColor(), random.nextInt(10) + 1,
-                        random.nextInt(10) + 1);
-            case 2:
-                return new RightTriangle(colorSupplier.getRandomColor(), random.nextInt(10) + 1,
-                        random.nextInt(10) + 1);
-            case 3:
-                return new Circle(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
-            case 4:
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(10) + 1,
-                        random.nextInt(10) + 1, random.nextInt(10) + 1);
-            default:
-                return null;
-        }
+        return switch (figureType) {
+            case 0 -> new Square(colorSupplier.getRandomColor(), random.nextInt(BOUND) + 1);
+            case 1 -> new Rectangle(colorSupplier.getRandomColor(), random.nextInt(BOUND) + 1,
+                    random.nextInt(BOUND) + 1);
+            case 2 -> new RightTriangle(colorSupplier.getRandomColor(), random.nextInt(BOUND) + 1,
+                    random.nextInt(BOUND) + 1);
+            case 3 -> new Circle(colorSupplier.getRandomColor(), random.nextInt(BOUND) + 1);
+            case 4 -> new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                    random.nextInt(BOUND) + 1,
+                    random.nextInt(BOUND) + 1, random.nextInt(BOUND) + 1);
+            default -> null;
+        };
     }
 
     public Figure getDefaultFigure() {
-        return new Circle("white", 10);
+        return new Circle(COLOR, RADIUS);
     }
 }
-
-
