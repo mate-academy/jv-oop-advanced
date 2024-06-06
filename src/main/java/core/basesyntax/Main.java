8a3b6558ef311a5efdfb81ee2e88f974d8e4810f
Fedir Lozovyi <sumkin112@gmail.com>
2024-06-06 13:30:10 +0200

    add class SideSupplier removed 3 loops create constants fixed things

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        final int SIZE = 6;
+        final int arraySize = 6;
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[SIZE];
+        Figure[] figures = new Figure[arraySize];
 
-        for (int i = 0; i < SIZE; i++) {
-            if (i < SIZE / 2) {
+        for (int i = 0; i < arraySize; i++) {
+            if (i < arraySize / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
