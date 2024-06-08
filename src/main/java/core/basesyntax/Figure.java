709337541e5f760e3e9645cce7909fe87130076d
Anna Stepanenko <spashiba.anna@gmail.com>
2024-06-08 18:50:05 +0300

    Completed all tasks in the assignment

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,13 +1,15 @@
 package core.basesyntax;
 
-public abstract class Figure {
+public abstract class Figure implements Drawable, AreaCalculable {
     protected String color;
 
     public Figure(String color) {
         this.color = color;
     }
 
+    @Override
     public abstract double getArea();
 
+    @Override
     public abstract void draw();
 }
