08f99d3a84e5fa610a2244aca050733fa89e43bd
Ivan <happytroya2001@gmail.com>
2024-06-07 23:03:48 -0700

    changed magic number 3 to figures.length / 2

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Figure[] figures = new Figure[6];
 
         for (int i = 0; i < figures.length; i++) {
-            if (i < 3) {
+            if (i < figures.length / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
                 figures[i].draw();
             } else {
