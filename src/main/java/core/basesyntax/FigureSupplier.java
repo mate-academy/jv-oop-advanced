fd02d58aa4cfc379d9c2c584ce0a2eff08980571
Panchenko Dmytro <panchenko0795@gmail.com>
2024-06-15 00:25:34 +0300

    Introduced DEFAULT_COLOR, DEFAULT_RADIUS constants in FigureSupplier class; switch on enum directly in FigureSupplier class; removed static for dependency injections in Main class

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,33 +4,34 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAXIMUM_VALUE = 50;
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
+    private static final double DEFAULT_RADIUS = 10;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
         int index = random.nextInt(FigureType.values().length);
-        String figureName = FigureType.values()[index].name();
+        FigureType figureType = FigureType.values()[index];
 
         String randomColor = colorSupplier.getRandomColor();
-        switch (figureName) {
-            case "CIRCLE":
+        switch (figureType) {
+            case CIRCLE:
                 return new Circle(randomColor, randomSize());
-            case "ISOSCELES_TRAPEZOID":
+            case ISOSCELES_TRAPEZOID:
                 return new IsoscelesTrapezoid(randomColor,
                         randomSize(), randomSize(), randomSize());
-            case "RECTANGLE":
+            case RECTANGLE:
                 return new Rectangle(randomColor, randomSize(), randomSize());
-            case "RIGHT_TRIANGLE":
+            case RIGHT_TRIANGLE:
                 return new RightTriangle(randomColor, randomSize());
-            case "SQUARE":
-                return new Square(randomColor, randomSize());
+            case SQUARE:
             default:
                 return new Square(randomColor, randomSize());
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), 10);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 
     private double randomSize() {
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Main {
     private static final int MAX_ARRAY_SIZE = 10;
-    private static final FigureSupplier figureSupplier = new FigureSupplier();
-    private static final Random random = new Random();
 
     public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Random random = new Random();
         int arraySize = random.nextInt(1, MAX_ARRAY_SIZE);
         Figure[] figures = new Figure[arraySize];
         for (int i = 0; i < figures.length; i++) {
