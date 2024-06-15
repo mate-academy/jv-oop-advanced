94d517981b614ba3d37f1a0b602625d5b6cab52d
vdsfww <loppprt@gmail.com>
2024-06-15 14:35:08 +0300

    hw fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int FIGURE_COUNT = 6;
+
     public static void main(String[] args) {
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[FIGURE_COUNT];
         FigureSupplier figureSupplier = new FigureSupplier();
 
         for (int i = 0; i < figures.length; i++) {
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -1,27 +1,41 @@
 package core.basesyntax;
 
-public class FigureSupplier {
+import java.util.Random;
 
-    private static final double RANDOM_BOUND = 100.00;
+public class FigureSupplier {
+    private static final int FIGURE_COUNT = 5;
+    private static final double RANDOM_BOUND = 100.0;
+    private static final Color DEFAULT_COLOR = Color.WHITE;
+    private static final double DEFAULT_RADIUS = 10.0;
+
+    private Random random = new Random();
 
     public Figure getRandomFigure() {
-
         ColorSupplier colorSupplier = new ColorSupplier();
         Color randomColor = colorSupplier.getRandomColor();
-        double randomNumber = ColorSupplier.random.nextDouble(RANDOM_BOUND);
+        double randomNumber = random.nextDouble() * RANDOM_BOUND;
 
-        Figure[] figures = new Figure[]{new Circle(randomColor, randomNumber),
-                new Rectangle(randomColor, randomNumber, randomNumber),
-                new Square(randomColor, randomNumber),
-                new RightTriangle(randomColor, randomNumber, randomNumber),
-                new IsoscelesTrapezoid(randomColor, randomNumber, randomNumber, randomNumber)
-        };
-
-        Figure randomFigure = figures[(int) (Math.random() * figures.length)];
-        return randomFigure;
+        switch (random.nextInt(FIGURE_COUNT)) {
+            case 0:
+                return new Circle(randomColor, randomNumber);
+            case 1:
+                return new Rectangle(randomColor, randomNumber, random.nextDouble() * RANDOM_BOUND);
+            case 2:
+                return new Square(randomColor, randomNumber);
+            case 3:
+                return new RightTriangle(randomColor, randomNumber,
+                        random.nextDouble() * RANDOM_BOUND);
+            case 4:
+                return new IsoscelesTrapezoid(randomColor, randomNumber,
+                        random.nextDouble() * RANDOM_BOUND,
+                        random.nextDouble() * RANDOM_BOUND);
+            default:
+                throw new IllegalStateException("Unexpected value: "
+                        + random.nextInt(FIGURE_COUNT));
+        }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE, 10.0);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
