385217ec996b80121adda629d46f558c3fde92cc
ClubJam <165167185+ClubJamm@users.noreply.github.com>
2024-06-19 17:19:10 +0300

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,2 +1,9 @@
-package core.basesyntax;public class ColorSupplier {
+package core.basesyntax;
+public class ColorSupplier {
+  private String[] colors = {"red", "blue", "green", "yellow", "pink", "white", "black"};
+
+    public String getRandomColor() {
+        Random random = new Random();
+        return colors[random.nextInt(colors.length)];
+    }
 }
