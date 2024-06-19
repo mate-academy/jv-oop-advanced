5046de339a2731644f7d2d1388450d710db97578
ClubJam <165167185+ClubJamm@users.noreply.github.com>
2024-06-19 17:28:34 +0300

    Update FIgureSupplier.java

--- src/main/java/core/basesyntax/FIgureSupplier.java
+++ src/main/java/core/basesyntax/FIgureSupplier.java
@@ -2,38 +2,38 @@ package core.basesyntax;
 
 public class FIgureSupplier {
   private ColorSupplier colorSupplier = new ColorSupplier();
-    private Random random = new Random();
+  private Random random = new Random();
 
-    public Figure getRandomFigure() {
-        int figureType = random.nextInt(5);
-        String color = colorSupplier.getRandomColor();
+  public Figure getRandomFigure() {
+      int figureType = random.nextInt(5);
+      String color = colorSupplier.getRandomColor();
 
-        switch (figureType) {
-            case 0:
-                return new Square(color,
-                        random.nextDouble() * 10 + 1);
-            case 1:
-                return new Rectangle(color,
-                        random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1);
-            case 2:
-                return new Circle(color,
-                        random.nextDouble() * 10 + 1);
-            case 3:
-                return new RightTriangle(color,
-                        random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1);
-            case 4:
-                return new IsoscelesTrapezoid(color,
-                        random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1,
-                        random.nextDouble() * 10 + 1);
-            default:
-                return getRandomFigure();
+      switch (figureType) {
+          case 0:
+              return new Square(color,
+                      random.nextDouble() * 10 + 1);
+          case 1:
+              return new Rectangle(color,
+                      random.nextDouble() * 10 + 1,
+                      random.nextDouble() * 10 + 1);
+          case 2:
+              return new Circle(color,
+                      random.nextDouble() * 10 + 1);
+          case 3:
+              return new RightTriangle(color,
+                      random.nextDouble() * 10 + 1,
+                      random.nextDouble() * 10 + 1);
+          case 4:
+              return new IsoscelesTrapezoid(color,
+                      random.nextDouble() * 10 + 1,
+                      random.nextDouble() * 10 + 1,
+                      random.nextDouble() * 10 + 1);
+          default:
+              return getRandomFigure();
         }
     }
 
-    public Figure getDefaultFigure() {
-        return new Circle("white", 10);
-    }
+  public Figure getDefaultFigure() {
+      return new Circle("white", 10);
+   }
 }
