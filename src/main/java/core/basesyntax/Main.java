169571165363269f366376ee66759ad5e9cf5f10
ClubJam <165167185+ClubJamm@users.noreply.github.com>
2024-06-19 17:24:43 +0300

    Update Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,4 +1,20 @@
 package core.basesyntax;
 
 public class Main {
+    public static void main(String[] args) {
+        FIgureSupplier figureSupplier = new FIgureSupplier();
+        Figure[] figures = new Figure[6];
+
+        for (int i = 0; i < figures.length / 2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (int i = figures.length / 2; i < figures.length; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
 }
