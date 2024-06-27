aaeeb8e9b8bb34e084820762da4a09a8ced3f57c
skrypkasv <skrypkasv@gmail.com>
2024-06-27 19:06:07 +0300

    add abstract Figure

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,4 +1,9 @@
 package core.basesyntax;
 
-public class Figure {
+import core.basesyntax.interfaces.AreaCalculator;
+import core.basesyntax.interfaces.Describer;
+
+public abstract class Figure implements Describer, AreaCalculator {
+    public String type = "circle";
+    public String color = "WHITE";
 }
