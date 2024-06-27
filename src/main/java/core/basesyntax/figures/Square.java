cca407b5b79a73c099a2ea5e0204f0f894909fd2
skrypkasv <skrypkasv@gmail.com>
2024-06-27 18:57:56 +0300

    create Square

--- src/main/java/core/basesyntax/figures/Square.java
+++ src/main/java/core/basesyntax/figures/Square.java
@@ -1,6 +1,34 @@
 package core.basesyntax.figures;
 
 import core.basesyntax.Figure;
+import core.basesyntax.interfaces.AreaCalculator;
+import core.basesyntax.interfaces.Describer;
 
 public class Square extends Figure {
+    private static final String type = "square";
+    private final int side;
+
+    public Square(int side, String color) {
+        this.side = side;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return (double) side * side;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Figure: "
+                + type + ", "
+                + "area: "
+                + this.getArea()
+                + " sq. units, "
+                + "side: "
+                + side
+                + " units, "
+                + "color: "
+                + color;
+    }
 }
