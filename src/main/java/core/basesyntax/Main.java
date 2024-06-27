035ed163eec4c8b1b772ecc7190e2db6e213216d
skrypkasv <skrypkasv@gmail.com>
2024-06-27 19:09:08 +0300

    create Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,18 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] arrayOfFigures = new Figure[6];
+        for (int i = 0; i < arrayOfFigures.length; i++) {
+            if (i < arrayOfFigures.length / 2) {
+                arrayOfFigures[i] = figureSupplier.getRandomFigure();
+            } else {
+                arrayOfFigures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
 
+        for (Figure figure : arrayOfFigures) {
+            System.out.println(figure.getInfo());
+        }
     }
 }
