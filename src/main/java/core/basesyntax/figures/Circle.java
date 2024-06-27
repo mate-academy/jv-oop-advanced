bca7051087ac5a8c9bb3c3b4cc9ff555a2d0c6cc
skrypkasv <skrypkasv@gmail.com>
2024-06-27 18:58:49 +0300

    create Circle

--- src/main/java/core/basesyntax/figures/Circle.java
+++ src/main/java/core/basesyntax/figures/Circle.java
@@ -1,6 +1,37 @@
 package core.basesyntax.figures;
 
 import core.basesyntax.Figure;
+import core.basesyntax.interfaces.AreaCalculator;
+import core.basesyntax.interfaces.Describer;
 
 public class Circle extends Figure {
+    private final int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public Circle(int radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Figure: "
+                + type + ", "
+                + "area: "
+                + this.getArea()
+                + " sq. units, "
+                + "radius: "
+                + radius
+                + " units, "
+                + "color: "
+                + color;
+    }
 }
