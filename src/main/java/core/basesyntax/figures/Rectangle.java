ec58262b27ec80809b65a3364d26bba282a595bd
skrypkasv <skrypkasv@gmail.com>
2024-06-27 18:58:09 +0300

    create Rectangle

--- src/main/java/core/basesyntax/figures/Rectangle.java
+++ src/main/java/core/basesyntax/figures/Rectangle.java
@@ -1,6 +1,40 @@
 package core.basesyntax.figures;
 
 import core.basesyntax.Figure;
+import core.basesyntax.interfaces.AreaCalculator;
+import core.basesyntax.interfaces.Describer;
 
 public class Rectangle extends Figure {
+    private static final String type = "rectangle";
+    private final int width;
+    private final int height;
+
+
+    public Rectangle(int width, int height, String color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return (double) width * height;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Figure: "
+                + type + ", "
+                + "area: "
+                + this.getArea()
+                + " sq. units, "
+                + "width: "
+                + width
+                + " units, "
+                + "height: "
+                + height
+                + " units, "
+                + "color: "
+                + color;
+    }
 }
