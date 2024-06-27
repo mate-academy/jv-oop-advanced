49e3ffdf81ab10d33c2fc4f17a330374eec35b80
skrypkasv <skrypkasv@gmail.com>
2024-06-27 18:58:34 +0300

    create RightTriangle

--- src/main/java/core/basesyntax/figures/RightTriangle.java
+++ src/main/java/core/basesyntax/figures/RightTriangle.java
@@ -1,6 +1,40 @@
 package core.basesyntax.figures;
 
 import core.basesyntax.Figure;
+import core.basesyntax.interfaces.AreaCalculator;
+import core.basesyntax.interfaces.Describer;
 
 public class RightTriangle extends Figure {
+    private static final String type = "triangle";
+    private final int firstLeg;
+    private final int secondLeg;
+
+    public RightTriangle(int firstLeg, int secondLeg, String color) {
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return (double) firstLeg * secondLeg * 0.5;
+    }
+
+    @Override
+    public String getInfo() {
+        String name = "triangle";
+        return "Figure: "
+                + type + ", "
+                + "area: "
+                + this.getArea()
+                + " sq. units, "
+                + "firstLeg: "
+                + firstLeg
+                + " units, "
+                + "secondLeg: "
+                + secondLeg
+                + " units, "
+                + "color: "
+                + color;
+    }
 }
