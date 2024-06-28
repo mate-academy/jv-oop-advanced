beefdbf1b85aac0d8271a31841e24da462334713
Rafal <pyszkowski.r@gmail.com>
2024-06-28 19:41:52 +0200

    work in progres

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Main {
+
+    public static void main(String[] Args) {
+        final int ARRAY_LENGHT = 6;
+        Figure figures[] = new Figure[6];
+
+        for (int i = 0; i < ARRAY_LENGHT; i++) {
+            if(i < ARRAY_LENGHT / 2) {
+                figures[i] = new FigureSupplier().getRandomFigure();
+                figures[i].draw();
+                }
+            else {
+                figures[i] = new FigureSupplier().getDefaultFigure();
+                figures[i].draw();
+            }
+            }
+
+        }
+    }
+
