8c62d0ca27834d1c440315deb41ecaf54fc0592b
skrypkasv <skrypkasv@gmail.com>
2024-06-28 13:54:09 +0300

    add ARRAY_LENGTH constant in Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int ARRAY_LENGTH = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] arrayOfFigures = new Figure[6];
+        Figure[] arrayOfFigures = new Figure[ARRAY_LENGTH];
         for (int i = 0; i < arrayOfFigures.length; i++) {
             if (i < arrayOfFigures.length / 2) {
                 arrayOfFigures[i] = figureSupplier.getRandomFigure();
