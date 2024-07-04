b438a3dc907780770dda73c2b4efecbba2c98bd4
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-07-04 16:55:53 +0300

    rename AreaCalculator and avoided 3 for loops in main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,22 +3,16 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        int arraySize = 6; // Можна змінити на інший розмір
+        final int arraySize = 6; // Можна змінити на інший розмір
         Figure[] figures = new Figure[arraySize];
 
-        // Заповнення першої половини масиву випадковими фігурами
-        for (int i = 0; i < arraySize / 2; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-
-        // Заповнення другої половини масиву стандартними фігурами
-        for (int i = arraySize / 2; i < arraySize; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
-        }
-
-        // Виведення всіх фігур
-        for (Figure figure : figures) {
-            figure.draw();
+        for (int i = 0; i < arraySize; i++) {
+            if (i < arraySize / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+            figures[i].draw();
         }
     }
 }
