f852960688610441786d9fbf8694b3331c4e470c
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-07-04 17:26:01 +0300

    add constant

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int arraySize = 6;
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        final int arraySize = 6; // Можна змінити на інший розмір
         Figure[] figures = new Figure[arraySize];
 
         for (int i = 0; i < arraySize; i++) {
