13c5d64358c12c7749e39e90cb2992cc5d813e61
KLICKICE <ledmaskiight@gmail.com>
2024-07-08 13:15:06 +0300

    @KLICKICE I didn't get it, why i have to replace in ColorSupplier my method public Color getRandomColor() to public String getRandomColor()?

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int MAX_COUNT = 5;
+    private static final double DEFAULT_RADIUS = 10;
+    private static final Color DEFAULT_COLOR = Color.WHITE;
     public Figure getDefaultFigure() {
-        final int DEFOUT_RADIUS = 10;
-        final Color DEFOULT_COLOR = Color.WHITE;
-
-        return new Circle(DEFOUT_RADIUS, DEFOULT_COLOR);
+        return new Circle(DEFAULT_RADIUS, DEFAULT_COLOR);
     }
 
     public Figure getRandomFigure() {
-        final int MAX_COUNT = 5;
         int number = new Random().nextInt(MAX_COUNT);
         ColorSupplier colorSupplier = new ColorSupplier();
         int index = new Random().nextInt(MAX_COUNT);
@@ -22,7 +21,8 @@ public class FigureSupplier {
             case 1:
                 return new Rectangle(number, number, colorSupplier.getRandomColor());
             case 2:
-                return new IsoscelesTrapezoid(number, number, number, colorSupplier.getRandomColor());
+                return new IsoscelesTrapezoid(number, number, number,
+                        colorSupplier.getRandomColor());
             case 3:
                 return new RightTriangle(number, number, colorSupplier.getRandomColor());
             case 4:
