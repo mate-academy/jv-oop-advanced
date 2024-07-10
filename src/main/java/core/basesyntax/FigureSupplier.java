9c6139b1cf6d7afb3b50af41d52ca1d28ce34805
Nastia Pyshak <nastapisak618@gmail.com>
2024-07-10 22:19:41 +0200

    Improved after mentor's review

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,40 +3,45 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private final int figureCount = 5;
-    private final int randomFigureParameter = 100;
+    private static final int FIGURE_COUNT = 5;
+    private static final int RANDOM_FIGURE_PARAMETER = 100;
+    private static final int DEFAULT_FIGURE_RADIUS = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Figure getRandomFigure() {
-        int randomFigureNum = random.nextInt(figureCount);
+        int randomFigureNum = random.nextInt(FIGURE_COUNT);
+        String color = colorSupplier.getRandomColor();
+        int firstRandomParameter = random.nextInt(RANDOM_FIGURE_PARAMETER);
+        int secondRandomParameter = random.nextInt(RANDOM_FIGURE_PARAMETER);
+        int thirdRandomParameter = random.nextInt(RANDOM_FIGURE_PARAMETER);
         switch (randomFigureNum) {
             case 0:
-                return new Square(colorSupplier.getRandomColor(),
-                        random.nextInt(randomFigureParameter));
+                return new Square(color,
+                        firstRandomParameter);
             case 1:
-                return new Rectangle(colorSupplier.getRandomColor(),
-                        random.nextInt(randomFigureParameter),
-                        random.nextInt(randomFigureParameter));
+                return new Rectangle(color,
+                        firstRandomParameter,
+                        secondRandomParameter);
             case 2:
-                return new RightTriangle(colorSupplier.getRandomColor(),
-                        random.nextInt(randomFigureParameter),
-                        random.nextInt(randomFigureParameter));
+                return new RightTriangle(color,
+                        firstRandomParameter,
+                        secondRandomParameter);
             case 3:
-                return new Circle(random.nextInt(randomFigureParameter),
-                        colorSupplier.getRandomColor());
+                return new Circle(firstRandomParameter,
+                        color);
             case 4:
-                return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
-                        random.nextInt(randomFigureParameter),
-                        random.nextInt(randomFigureParameter),
-                        random.nextInt(randomFigureParameter));
+                return new IsoscelesTrapezoid(color,
+                        firstRandomParameter,
+                        secondRandomParameter,
+                        thirdRandomParameter);
             default:
                 throw new IllegalStateException("Unexpected value: " + randomFigureNum);
         }
     }
 
     public Figure getDefaultFigure() {
-        Figure figure = new Circle(10,"WHITE");
+        Figure figure = new Circle(DEFAULT_FIGURE_RADIUS,"WHITE");
         return figure;
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,18 +1,18 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int SIZEOFARRAY = 6;
+
     public static void main(String[] args) {
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[SIZEOFARRAY];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length; i++) {
-            if (i < 3) {
+            if (i < SIZEOFARRAY / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
             }
-        }
-        for (Figure figure: figures) {
-            figure.draw();
+            figures[i].draw();
         }
     }
 }
