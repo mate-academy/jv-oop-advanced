122c93123e8541ff51c5834dcce4ba97f574b341
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-07-17 22:32:44 +0300

    create figure Circle override methods getArea and draw.

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,30 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private double radius;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String draw() {
+        return "Figure: circle, area: "
+                + getArea() + " sq. units, radius: "
+                + getRadius() + " units, color: " + getColor();
+    }
+}
