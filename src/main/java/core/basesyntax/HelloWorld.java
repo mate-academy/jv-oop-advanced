e601c35d54f22293889004ba02762b0844736bd2
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-07-18 11:19:27 +0300

    created psvm method added 6 figures to array and draw it.

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,21 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorld {
+import java.util.Arrays;
 
+public class HelloWorld {
+    private static final int NUMBER_OF_FIGURES = 6;
+
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[NUMBER_OF_FIGURES];
+        for (int i = 0; i < figures.length; i++) {
+            if(i >= figures.length / 2){
+                figures[i] = figureSupplier.getDefaultFigure();
+            } else {
+                figures[i] = figureSupplier.getRandomFigure();
+            }
+            System.out.println(figures[i].draw());
+        }
+
+    }
 }
