d39eb68038fd51fe056cbdc1abb99b1b40fc3104
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-07-18 18:27:45 +0300

    Moved all magic numbers as constants. Implemented methods for creating each figure.

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,22 +4,42 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int BOUND = 100;
+    private static final int DEFAULT_RADIUS = 100;
+    private static final String DEFAULT_COLOR = Colors.WHITE.name();
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
+    private final Figure[] figures = new Figure[5];
+
+    private Figure getCircle() {
+        return new Circle(colorSupplier.getRandomColor(), random.nextDouble(BOUND));
+    }
+
+    private Figure getSquare() {
+        return new Square(colorSupplier.getRandomColor(), random.nextDouble(BOUND));
+    }
+
+    private Figure getRectangle() {
+        return new Rectangle(colorSupplier.getRandomColor(), random.nextDouble(BOUND),
+                random.nextDouble(BOUND));
+    }
+
+    private Figure getIsoscelesTrapezoid() {
+        return new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextDouble(BOUND),
+                random.nextDouble(BOUND), random.nextDouble(BOUND));
+    }
+
+    private Figure getRightTriangle() {
+        return new RightTriangle(colorSupplier.getRandomColor(), random.nextDouble(BOUND),
+                random.nextDouble(BOUND));
+    }
 
     public Figure getRandomFigure() {
-        Figure[] figures = {new Circle(colorSupplier.getRandomColor(),random.nextDouble(BOUND)),
-            new Square(colorSupplier.getRandomColor(), random.nextDouble(BOUND)),
-            new Rectangle(colorSupplier.getRandomColor(), random.nextDouble(BOUND),
-                    random.nextDouble(BOUND)),
-            new IsoscelesTrapezoid(colorSupplier.getRandomColor(), random.nextDouble(BOUND),
-                    random.nextDouble(BOUND), random.nextDouble(BOUND)),
-            new RightTriangle(colorSupplier.getRandomColor(), random.nextDouble(BOUND),
-                    random.nextDouble(BOUND))};
+        Figure[] figures = {getCircle(), getRectangle(), getSquare(),
+                getIsoscelesTrapezoid(), getRightTriangle()};
         return figures[random.nextInt(figures.length)];
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Colors.WHITE.name(), 10);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
