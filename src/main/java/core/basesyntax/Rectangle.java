7b6b3c5e478255fb6bf1ed68eaffaaa28170c121
ShevchuckM <maxim.shevchu34@gmail.com>
2024-07-22 14:23:51 +0300

    added methods and classes

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class Rectangle implements Figure {
+    private String color;
+    private double width;
+    private double height;
+
+    public Rectangle(String color, double height, double width) {
+        this.color = color;
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return height * width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: "
+                + getArea()
+                + " sq. units, width: "
+                + width
+                + " units, height: "
+                + height + " units, color: "
+                + color);
+    }
+}
