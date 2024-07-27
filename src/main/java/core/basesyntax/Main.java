ca2e328ec85fb12865efe515bc19c9940f962ab3
Nikolai <colya.reznick@gmail.com>
2024-07-27 10:30:58 +0300

    Added style fixes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,7 +8,7 @@ public class Main {
         FigureSupplier figureSupplier = new FigureSupplier();
 
         for (int i = 0; i < figureArray.length; i++) {
-            if (i < figureArray.length/2) {
+            if (i < figureArray.length / 2) {
                 figureArray[i] = figureSupplier.getRandomFigure();
             } else {
                 figureArray[i] = figureSupplier.getDefaultFigure();
