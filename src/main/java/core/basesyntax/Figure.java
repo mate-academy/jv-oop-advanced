9f38369215042924b975eea518056a938608ff75
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-08-05 19:40:45 +0300

    worked on constants

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public abstract class Figure implements AreaCalculator, Drawable {
-    public static final int REQUESTED_LENGTH = 6;
     private String color;
     private String name;
 
