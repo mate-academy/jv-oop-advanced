3c4461edc5ec73242b3fb26b23a3b51aa1056435
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-08-06 16:15:33 +0300

    no more magic nambers

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,12 +2,14 @@ package core.basesyntax;
 
 public class Main {
     public static final int REQUESTED_LENGTH = 6;
+    public static final int REQUESTED_PART = 2;
 
     public static void main(String[] args) {
-        Figure[] figuresArray = new Figure [REQUESTED_LENGTH];
+        Figure[] figuresArray = new Figure[REQUESTED_LENGTH];
+
         FigureSupplier figureSupplier = new FigureSupplier();
-        for (int i = 0; i <= 5; i++) {
-            if (i <= 2) {
+        for (int i = 0; i < REQUESTED_LENGTH; i++) {
+            if (i <= REQUESTED_PART) {
                 figuresArray[i] = figureSupplier.getRandomFigure();
             } else {
                 figuresArray[i] = figureSupplier.getDefaultFigure();
