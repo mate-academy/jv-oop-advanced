acf8d9944444ad9927d6761968e0eaa8eb177d6d
Artem Lavrivskyi <lavrivskyi@gmail.com>
2024-08-11 23:01:02 +0300

    main method implemented

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[6];
+        for (int i = 0; i < figures.length; i++) {
+            if (i < 3) {
+                figures[i] = new FigureSupplier().getRandomFigure();
+            } else {
+                figures[i] = new FigureSupplier().getDefaultFigure();
+            }
+        }
+
+        for (Figure figure: figures) {
+            figure.draw();
+        }
+    }
+}
