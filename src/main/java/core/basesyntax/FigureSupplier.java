18f8ff420987bd2afbc5c1104faef55dc9fb4885
StoneBlood <brkalix@gmail.com>
2024-08-23 15:29:14 +0300

    Created an array with figurs in the Main class and returned getInfo of each figure

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -12,19 +12,22 @@ public class FigureSupplier {
 
         switch (value) {
             case 0: {
-                return new IsoscelesTrapezoid(color, random.nextInt(), random.nextInt(), random.nextInt());
+                return new IsoscelesTrapezoid(color, random.nextInt(10) + 1,
+                        random.nextInt(10) + 1, random.nextInt(10) + 1);
             }
             case 1: {
-                return new Circle(color, random.nextInt());
+                return new Circle(color, random.nextInt(10) + 1);
             }
             case 2: {
-                return  new RightTriangle(color, random.nextInt(), random.nextInt());
+                return  new RightTriangle(color, random.nextInt(10) + 1,
+                        random.nextInt(10) + 1);
             }
             case 3: {
-                return new Rectangle(color, random.nextInt(), random.nextInt());
+                return new Rectangle(color, random.nextInt(10) + 1,
+                        random.nextInt(10) + 1);
             }
             case 4: {
-                return new Square(color, random.nextInt());
+                return new Square(color, random.nextInt(10) + 1);
             }
             default: {
                 return getDefaultFigure();
