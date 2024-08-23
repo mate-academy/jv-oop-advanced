790b1c8a7d8929dc13f8a7af719516f19b5ac7ed
StoneBlood <brkalix@gmail.com>
2024-08-23 16:41:43 +0300

    Moved the `Random` object to the class level.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
     private static final String[] COLORS
             = {"red", "green", "blue", "yellow", "pink", "black", "white"};
 
     public String getRandomColor() {
-        Random random = new Random();
         return COLORS[random.nextInt(COLORS.length)];
     }
 }
