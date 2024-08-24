cc7ec7fcb62db35b161605d534193774722755bb
Alinka Lyn <alinka.lyn@ukr.net>
2024-08-24 15:06:01 +0300

    packages changed

--- src/main/java/core/basesyntax/supplier/FigureSupplier.java
+++ src/main/java/core/basesyntax/supplier/FigureSupplier.java
@@ -1,6 +1,11 @@
 package core.basesyntax.supplier;
 
-import core.basesyntax.figure.*;
+import core.basesyntax.figure.Circle;
+import core.basesyntax.figure.Figure;
+import core.basesyntax.figure.IsoscelesTrapezoid;
+import core.basesyntax.figure.Rectangle;
+import core.basesyntax.figure.RightTriangle;
+import core.basesyntax.figure.Square;
 
 import java.util.Random;
 
