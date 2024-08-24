67a7f792f4335cdc529f21bc9e4e2ae2e640e15e
StoneBlood <brkalix@gmail.com>
2024-08-24 22:49:03 +0300

    created constants NUMBER_OF_FIGURES and MAX_RANDOM_VALUE

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,31 +3,34 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int NUMBER_OF_FIGURES = 4;
+    private static final int MAX_RANDOM_VALUE = 10;
     private Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Drawable getRandomFigure() {
-        int value = random.nextInt(4);
+        int value = random.nextInt(NUMBER_OF_FIGURES);
         String color = colorSupplier.getRandomColor().toString();
 
         switch (value) {
             case 0: {
-                return new IsoscelesTrapezoid(color, random.nextInt(10) + 1,
-                        random.nextInt(10) + 1, random.nextInt(10) + 1);
+                return new IsoscelesTrapezoid(color, random.nextInt(MAX_RANDOM_VALUE) + 1,
+                        random.nextInt(MAX_RANDOM_VALUE) + 1,
+                        random.nextInt(MAX_RANDOM_VALUE) + 1);
             }
             case 1: {
-                return new Circle(color, random.nextInt(10) + 1);
+                return new Circle(color, random.nextInt(MAX_RANDOM_VALUE) + 1);
             }
             case 2: {
-                return new RightTriangle(color, random.nextInt(10) + 1,
-                        random.nextInt(10) + 1);
+                return new RightTriangle(color, random.nextInt(MAX_RANDOM_VALUE) + 1,
+                        random.nextInt(MAX_RANDOM_VALUE) + 1);
             }
             case 3: {
-                return new Rectangle(color, random.nextInt(10) + 1,
-                        random.nextInt(10) + 1);
+                return new Rectangle(color, random.nextInt(MAX_RANDOM_VALUE) + 1,
+                        random.nextInt(MAX_RANDOM_VALUE) + 1);
             }
             case 4: {
-                return new Square(color, random.nextInt(10) + 1);
+                return new Square(color, random.nextInt(MAX_RANDOM_VALUE) + 1);
             }
             default: {
                 return getDefaultFigure();
