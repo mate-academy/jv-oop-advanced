9d91566054337ec2bd552018121a14a766d4b2ff
StoneBlood <brkalix@gmail.com>
2024-08-28 10:48:19 +0300

    Use a single loop to initialize figures array

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,19 +2,20 @@ package core.basesyntax;
 
 public class Main {
     private static final int ARRAY_SIZE = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Drawable[] figures = new Drawable[ARRAY_SIZE];
+        Figure[] figures = new Figure[ARRAY_SIZE];
 
-        for (int i = 0; i < ARRAY_SIZE / 2; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            if (i < ARRAY_SIZE / 2) {
+                figures[i] = (Figure) figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = (Figure) figureSupplier.getDefaultFigure();
+            }
         }
 
-        for (int i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
-        }
-
-        for (Drawable figure: figures) {
+        for (Figure figure : figures) {
             figure.draw();
         }
     }
