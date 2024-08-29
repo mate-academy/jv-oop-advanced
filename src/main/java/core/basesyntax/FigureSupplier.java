670bd067446bdc5a72705ca36f4f4bb92635beb3
StoneBlood <brkalix@gmail.com>
2024-08-29 18:31:17 +0300

    made the variable random final

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,8 +6,8 @@ public class FigureSupplier {
     private static final int NUMBER_OF_FIGURES = 5;
     private static final int MAX_RANDOM_VALUE = 10;
     private static final int DEFAULT_RADIUS = 10;
-    private static final String DEFAULT_COLOR = "white";
-    private Random random = new Random();
+    private static final String DEFAULT_COLOR = Color.WHITE.name().toLowerCase();
+    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Drawable getRandomFigure() {
