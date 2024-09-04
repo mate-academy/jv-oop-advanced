3d3b491c24e6687a3e89f06ff58b1f3cb9c601bc
bilsinas <Nevnerzhitskaiadaryna@gmail.com>
2024-09-04 23:53:25 +0300

    ff

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -5,7 +5,7 @@ import java.util.Random;
 abstract class Figure implements AreaCalculator, Drawable {
     private static final double MAX_SIZE = 10.0;
     private static final String DEFAULT_COLOR = Color.WHITE.name();
-    private static String color;
+    private String color;
     private final Random random = new Random();
 
     public Figure(String color) {
