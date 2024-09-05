bc2611beb28a1bf8959d3b9233658dcb1df074e5
Nikita <91829339+aunkerrr@users.noreply.github.com>
2024-09-05 17:53:30 +0200

    Update src/main/java/core/basesyntax/FigureSupplier.java
    Co-authored-by: Olena Bruiako <bruyako.elena@gmail.com>

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,13 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int MAX_SIDE_LENGTH = 100;
-    private static final int MAX_RADIUS = 50;
-    private static final int MAX_TRIANGLE_LEG = 100;
-    private static final int MAX_TRAPEZOID_BASE = 100;
-    private static final int MAX_TRAPEZOID_HEIGHT = 50;
-    private static final Random RANDOM = new Random();
-
+    private static final int MAX_SIDE = 100;
+    private final Random RANDOM = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getDefaultFigure() {
