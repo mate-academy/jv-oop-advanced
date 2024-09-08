64dd7dae4fb5eb0f8a3bc70bcdfe1db9d561b04b
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2024-09-08 16:07:54 +0200

    hw-10-oop-advanced

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,24 +2,19 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        int arrraysize = 3;
+        int arrraysize = 6;
         FigureSupplier figureSupplier = new FigureSupplier();
 
         Figure[] figures = new Figure[arrraysize];
         figures[0] = figureSupplier.getRectangle();
         figures[1] = figureSupplier.getCircle();
         figures[2] = figureSupplier.getRightTriangle();
+        figures[3] = new Square(5.0, Color.RED);
+        figures[4] = new IsoscelesTrapezoid(10, 10, 20, Color.GREEN);
+        figures[5] = new Circle(30, Color.BLUE);
 
         for (Figure figure : figures) {
             figure.drawable();
         }
-
-        Square squareFirst = new Square(5.0, Color.RED);
-        squareFirst.drawable();
-        IsoscelesTrapezoid isoscelesTrapezoidFirst
-                = new IsoscelesTrapezoid(10, 10, 20, Color.GREEN);
-        isoscelesTrapezoidFirst.drawable();
-        Circle circleFirst = new Circle(30, Color.BLUE);
-        circleFirst.drawable();
     }
 }
