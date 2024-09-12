d143c5077e582ca2c65ec877ae827b45cd55fe6e
Maks <petliukofficial@gmail.com>
2024-09-12 11:14:53 +0300

    fixed code according to okuzan requirements

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final double DEFAULT_RADIUS = 10.0;
+    private static final double MAX_RANDOM_VALUE = 10.0;
     private static final int NUMBER_OF_FIGURES = 5;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
@@ -44,6 +45,6 @@ public class FigureSupplier {
     }
 
     private double getRandomValue() {
-        return random.nextDouble() * 10;
+        return random.nextDouble() * MAX_RANDOM_VALUE;
     }
 }
