2b058ae1e77a2cd46694d7b90369b072c8043d12
Oleh Bihun <o.bihun@ukr.net>
2024-09-13 08:59:38 +0300

    removed getMaxNumber method, created getRandomSide method, added Array_Size constant in Main

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -6,7 +6,6 @@ public class FigureSupplier {
     private static final String DEFAULT_COLOR = Color.WHITE.name();
     private static final int DEFAULT_RADIUS = 10;
     private static final int MIN_NUMBER = 1;
-    private static final int MAX_FIGURES = 6;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
@@ -14,26 +13,23 @@ public class FigureSupplier {
         return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 
-    public int getMaxNumber() {
-        return MAX_FIGURES;
+    private int getRandomSide() {
+        return random.nextInt(MIN_NUMBER, DEFAULT_RADIUS);
     }
 
     public Figure getRandomFigure() {
-        int randomNumberFirst = random.nextInt(MIN_NUMBER, DEFAULT_RADIUS);
-        int randomNumberSecond = random.nextInt(MIN_NUMBER, DEFAULT_RADIUS);
-        int randomNumberThird = random.nextInt(MIN_NUMBER, DEFAULT_RADIUS);
         int index = random.nextInt(RandomFigure.values().length);
         RandomFigure randomFigure = RandomFigure.values()[index];
         String randomColor = colorSupplier.getRandomColor();
 
         return switch (randomFigure) {
-            case CIRCLE -> new Circle(randomColor, randomNumberFirst);
-            case SQUARE -> new Square(randomColor, randomNumberFirst);
-            case RECTANGLE -> new Rectangle(randomColor, randomNumberFirst, randomNumberSecond);
-            case RIGHT_TRIANGLE -> new RightTriangle(randomColor, randomNumberFirst,
-                    randomNumberSecond);
-            case ISOSCELES_TRAPEZOID -> new IsoscelesTrapezoid(randomColor, randomNumberFirst,
-                    randomNumberSecond, randomNumberThird);
+            case CIRCLE -> new Circle(randomColor, getRandomSide());
+            case SQUARE -> new Square(randomColor, getRandomSide());
+            case RECTANGLE -> new Rectangle(randomColor, getRandomSide(), getRandomSide());
+            case RIGHT_TRIANGLE -> new RightTriangle(randomColor, getRandomSide(),
+                    getRandomSide());
+            case ISOSCELES_TRAPEZOID -> new IsoscelesTrapezoid(randomColor, getRandomSide(),
+                    getRandomSide(), getRandomSide());
         };
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int ARRAY_SIZE = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[figureSupplier.getMaxNumber()];
+        Figure[] figures = new Figure[ARRAY_SIZE];
 
         for (int i = 0; i < figures.length; i++) {
             if (i < (figures.length / 2)) {
