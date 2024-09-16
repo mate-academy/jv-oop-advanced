70a2afca86cb94a2ad4f5b67ca37decad47ef0ef
Volodymyr Pryt <prytvova94@gmail.com>
2024-09-16 16:46:56 +0300

    work with edits

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private static final int CIRCLE_RADIUS = 10;
+    private static final String CIRCLE_COLOR = "white";
     private static final int NUMBER_OF_FIGURES = 5;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
@@ -12,22 +14,26 @@ public class FigureSupplier {
         int figureType = random.nextInt(NUMBER_OF_FIGURES);
         switch (figureType) {
             case 0:
-                return new Circle(color, random.nextDouble() * 10);
+                return new Circle(color, getRandomValue());
             case 1:
-                return new Square(color, random.nextDouble() * 10);
+                return new Square(color, getRandomValue());
             case 2:
-                return new Rectangle(color, random.nextDouble() * 10, random.nextDouble() * 10);
+                return new Rectangle(color, getRandomValue(), getRandomValue());
             case 3:
-                return new RightTriangle(color, random.nextDouble() * 10, random.nextDouble() * 10);
+                return new RightTriangle(color, getRandomValue(), getRandomValue());
             case 4:
-                return new IsoscelesTrapezoid(color, random.nextDouble() * 10,
-                        random.nextDouble() * 10, random.nextDouble() * 10);
+                return new IsoscelesTrapezoid(color, getRandomValue(),
+                        getRandomValue(), getRandomValue());
             default:
                 return getDefaultFigure();
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle("white", 10);
+        return new Circle(CIRCLE_COLOR, CIRCLE_RADIUS);
+    }
+
+    private double getRandomValue() {
+        return random.nextDouble() * 10;
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,14 +1,17 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int RANDOM_COUNT = 6;
+
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure[] figures = new Figure[6];
-        for (int i = 0; i < figures.length / 2; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-        for (int i = figures.length / 2; i < figures.length; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
+        Figure[] figures = new Figure[RANDOM_COUNT];
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
         }
         for (Figure figure : figures) {
             figure.draw();
