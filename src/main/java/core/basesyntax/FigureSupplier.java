c981dad8bb6b7d46351ef9d62d41b6832b3ccc58
Volodymyr Pryt <prytvova94@gmail.com>
2024-09-16 17:09:34 +0300

    new work with edits

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private static final int CIRCLE_RADIUS = 10;
-    private static final String CIRCLE_COLOR = "white";
+    private static final int DEFAULT_RADIUS = 10;
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
     private static final int NUMBER_OF_FIGURES = 5;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
@@ -30,7 +30,7 @@ public class FigureSupplier {
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(CIRCLE_COLOR, CIRCLE_RADIUS);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 
     private double getRandomValue() {
