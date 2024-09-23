d41ba5858cb5baf8f059285e173fc24b98b24720
Kate Kraska <kate2002.kk42@gmail.com>
2024-09-23 20:50:05 +0300

    Initial commit for hw-solution

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Circle extends Figure {
+    private double radius;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Circle(String color) {
+        super(color);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Фігура: коло, площа: " + getArea()
+               + " кв. одиниць, радіус: " + radius + " одиниць, колір: " + getColor());
+    }
+}
