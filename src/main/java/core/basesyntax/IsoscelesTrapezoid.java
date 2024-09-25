7dcce85b7d8aaa0ae1334609d1710c945d6ba7b9
Mykhailo Chernykh <270775mik@gmail.com>
2024-09-25 15:25:44 +0200

    rename var

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -1,25 +1,25 @@
 package core.basesyntax;
 
 public class IsoscelesTrapezoid extends Figure {
-    private double base1;
-    private double base2;
+    private double firstBase;
+    private double secondBase;
     private double height;
 
-    public IsoscelesTrapezoid(String color, double base1, double base2, double height) {
+    public IsoscelesTrapezoid(String color, double firstBase, double secondBase, double height) {
         super(color);
-        this.base1 = base1;
-        this.base2 = base2;
+        this.firstBase = firstBase;
+        this.secondBase = secondBase;
         this.height = height;
     }
 
     @Override
     public double getArea() {
-        return ((base1 + base2) / 2) * height;
+        return ((firstBase + secondBase) / 2) * height;
     }
 
     @Override
     protected String getProperties() {
-        return "base1: " + base1 + " units, base2: " + base2
+        return "base1: " + firstBase + " units, base2: " + secondBase
                 + " units, height: " + height + " units";
     }
 }
