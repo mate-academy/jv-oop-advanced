8d0990115bf377c951a2a77d1ac7a5c9d818a396
jekaG <jekagrusha@gmail.com>
2024-09-29 20:39:36 +0300

    fixed after review

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Square extends Figure {
     private final int side;
+
     public Square(String color, int side) {
         super(color);
         this.side = side;
