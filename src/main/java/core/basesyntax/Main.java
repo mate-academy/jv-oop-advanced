ce215ee2028d277e2673fb0c4d69c0316ae5cb91
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-10-06 22:48:37 +0200

    Update and rename HelloWorld.java to Main.java

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[6];
+
+        // First half - random figures
+        for (int i = 0; i < figures.length / 2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        // Second half - default figures
+        for (int i = figures.length / 2; i < figures.length; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
+
+        // Draw all figures
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
+}
