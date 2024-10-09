5e78d8bd863daa8c14c3bd20bb378c5ad92cbea2
Alexander Tyschenko <qwitelite@gmail.com>
2024-10-09 20:45:29 +0300

    created classes and interfaces for figures

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,30 @@
+package core.basesyntax;
+
+public class Circle extends Figure implements Area, Draw {
+    private int radius;
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void toDraw() {
+        System.out.println("The Figure: Circle; radius = " + radius
+                +  "/n" + " Area: " + getArea()
+                + "/n" + "; Color: " + getColor());
+    }
+}
---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,52 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure implements Area, Draw {
+    private int firstBase;
+    private int secondBase;
+    private int height;
+
+    public IsoscelesTrapezoid(String color, int firstBase, int secondBase, int height) {
+        super(color);
+        this.firstBase = firstBase;
+        this.secondBase = secondBase;
+        this.height = height;
+    }
+
+    public int getFirstBase() {
+        return firstBase;
+    }
+
+    public void setFirstBase(int firstBase) {
+        this.firstBase = firstBase;
+    }
+
+    public int getSecondBase() {
+        return secondBase;
+    }
+
+    public void setSecondBase(int secondBase) {
+        this.secondBase = secondBase;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * (firstBase + secondBase) * height;
+    }
+
+    @Override
+    public void toDraw() {
+        System.out.println("The Figure: Trapezoid; First base = " + firstBase
+                + "   Second base = " + secondBase + " Height = " + height
+                + "/n" + " Area: " + getArea()
+                + "/n" + "; Color: " + getColor());
+
+    }
+}
