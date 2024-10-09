a9bfeae88117010322bf29c035d79cc57e342752
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-10-09 11:21:18 +0200

    feat: Add figures classes (Circle, IsoscelesTrapezoid, Rectangle, RightTriangle, Square) with area calculation and drawing functionality

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends BaseEntity {
+    private double firstParallelSide;
+    private double secondParallelSide;
+    private double height;
+
+    public IsoscelesTrapezoid(double firstParallelSide, double secondParallelSide, double height,
+                              String color) {
+        super(color);
+        this.firstParallelSide = firstParallelSide;
+        this.secondParallelSide = secondParallelSide;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return (firstParallelSide + secondParallelSide) * height / 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, area: " + getArea()
+                + " sq. units, firstParallelSide: " + firstParallelSide
+                + ", secondParallelSide: " + secondParallelSide
+                + ", height: " + height
+                + " units, color: " + getColor());
+    }
+}
