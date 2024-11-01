b8f62a04a80d76144e4614a72122bb14aaed71ab
yevhen <zheckakoroluk@gmail.com>
2024-11-01 12:25:04 +0200

    hw-5

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -10,7 +10,7 @@ public class Circle extends FigureAbstract implements Figure {
 
     @Override
     public void draw() {
-        System.out.println("Figure: circle, area: " + getArea() + ", radius: " + radius + ", color: " + getColor());
+        System.out.println("Figure: circle, are: " + getArea() + ", radius: " + radius + ", color: " + getColor());
     }
 
     @Override
