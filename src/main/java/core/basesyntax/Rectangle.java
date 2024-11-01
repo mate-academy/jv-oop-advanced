1c2f5265b96f686e242c78ecbe10a7d25bb3ffa8
YevhenSrdk <eugeneserdyuk23@gmail.com>
2024-11-01 17:34:37 +0200

    Developed random figure functionality

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure implements Drawable {
+    private double width;
+    private double height;
+    private double diagonal;
+
+    public Rectangle(String color, double width,
+                     double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+        this.diagonal = Math.sqrt(width * width + height * height);;
+
+    }
+
+    @Override
+    public double getArea() {
+        return height * width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: "
+                + getArea() + "sq. units, width: " + width
+                + " units, height: " + height
+                + " units, diagonal: " + diagonal + "units, color: " + getColor());
+    }
+}
