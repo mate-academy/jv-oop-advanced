880f82bd352243dc4538eb5006440cac1dc20e4d
OleksandrDarchyk <oledar01@easv365.dk>
2024-11-11 22:41:43 +0100

    Оновив Rectangle, RightTriangle та Square

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -4,10 +4,10 @@ public class Rectangle extends Figure {
     private double width;
     private double height;
 
-    public Rectangle(String color ,double width,double height) {
+    public Rectangle(String color, double width, double height) {
         super(color);
-        this.height= height;
         this.width = width;
+        this.height = height;
     }
 
     @Override
