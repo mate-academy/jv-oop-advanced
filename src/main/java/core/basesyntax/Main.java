f5680467d2a0c0f066779153ce9f17c2ae675d13
Roman Leshchyshyn <romchuk1969@gmail.com>
2024-11-12 08:00:31 +0300

    sfvxjf vcvsvvd  cccd

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,17 +5,14 @@ public class Main {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[6];
 
-        // Generate random figures for the first half of the array
         for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = figureSupplier.getRandomFigure();
         }
 
-        // Use default figure for the second half of the array
         for (int i = figures.length / 2; i < figures.length; i++) {
             figures[i] = figureSupplier.getDefaultFigure();
         }
 
-        // Draw all figures
         for (Figure figure : figures) {
             figure.draw();
         }
