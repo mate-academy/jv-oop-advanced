b39304fa0b92a45d3ee1e5c5511fc150da655fb4
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-18 19:05:45 +0000

    232983

--- src/main/java/core/basesyntax/AbstractColor.java
+++ src/main/java/core/basesyntax/AbstractColor.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public abstract class AbstractColor {
     private String color;
 
-public AbstractColor(String color) {
-    this.color = color;
-}
+    public AbstractColor(String color) {
+        this.color = color;
+    }
 
-public String getColor() {
-    return color;
-}
+    public String getColor() {
+        return color;
+    }
 }
