1579066cd56066483e51ffb4b62931fca461d89e
eulu <anegamega@gmail.com>
2024-12-11 17:13:07 +0200

    Refactor magic figures arr length into constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int NUMBER_OF_FIGURES = 6;
+
     public static void main(String[] args) {
         ColorSupplier colorSupplier = new ColorSupplier();
         FigureSupplier figureSupplier = new FigureSupplier(colorSupplier);
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[NUMBER_OF_FIGURES];
         int halfSize = figures.length / 2;
         for (int i = 0; i < halfSize; i++) {
             figures[i] = figureSupplier.getRandomFigure();
