639839b2fb972e3ee9477506a8346a09a2cf854a
Igor <sokol.igor29@gmail.com>
2024-12-16 17:28:12 +0200

    made solution of the task

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,8 +4,6 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+
 }
 
