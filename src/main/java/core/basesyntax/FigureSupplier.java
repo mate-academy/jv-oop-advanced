b72ffb0d04f0ccfea63b24fcb3bb00e3eecd944b
Mizuo <mizuo.personal@gmail.com>
2025-01-02 19:32:53 +0100

    checksyntax x4

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -2,23 +2,23 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class FigureSupplier  {
-    private final Random PICKER = new Random();
-    private final int RANDOM_NUMBER = 4;
+public class FigureSupplier {
+    private final Random picker = new Random();
+    private final int randomNum = 4;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        return switch (PICKER.nextInt(RANDOM_NUMBER)) {
+        return switch (picker.nextInt(randomNum)) {
             case 0 -> new Circle(colorSupplier.getRandomColor(),
-                    PICKER.nextInt(1, 50));
+                    picker.nextInt(1, 50));
             case 1 -> new Rectangle(colorSupplier.getRandomColor(),
-                    PICKER.nextInt(1, 50),
-                    PICKER.nextInt(1, 50));
+                    picker.nextInt(1, 50),
+                    picker.nextInt(1, 50));
             case 2 -> new RightTriangle(colorSupplier.getRandomColor(),
-                    PICKER.nextInt(1, 50),
-                    PICKER.nextInt(1, 50));
+                    picker.nextInt(1, 50),
+                    picker.nextInt(1, 50));
             case 3 -> new Square(colorSupplier.getRandomColor(),
-                    PICKER.nextInt(1, 50));
+                    picker.nextInt(1, 50));
             default -> new Circle(Colors.WHITE, 10);
         };
     }
