5828a06dc1fd46a318604a63652742f268f63e0d
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2025-01-07 08:56:48 -0500

    build is fixed AND CHANGED LOOP

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,17 +2,18 @@ package core.basesyntax;
 
 public class Main {
     private static final int FIGURE_COUNT = 5;
+    private static final int FIRST_HALF = 3;
 
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
         Figure[] figures = new Figure[FIGURE_COUNT];
 
-        for (int i = 0; i < 3; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-
-        for (int i = 3; i < FIGURE_COUNT; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
+        for (int i = 0; i < FIGURE_COUNT; i++) {
+            if (i < FIRST_HALF) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
         }
 
         for (Figure figure : figures) {
