98e07b4d338fa49518f3b10aed63cedd3df69083
Oleksandr Beztsennyi <alexbez152@gmail.com>
2025-01-22 19:14:25 +0200

    the project, its classes and methods were created

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        AbstractFigure[] figures = new AbstractFigure[6];
+
+        for (int i = 0; i < figures.length / 2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (int i = figures.length / 2; i < figures.length; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
+
+        for (AbstractFigure figure : figures) {
+            figure.draw();
+        }
+    }
+}
