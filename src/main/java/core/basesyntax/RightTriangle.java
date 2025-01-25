21aa552524eb65776344036eefe5de2590f3f764
Daniil Sitailo <serzsito2709@gmail.com>
2025-01-25 22:02:37 +0200

    Done task requirements

---
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+class RightTriangle implements Figure {
+    private final String color;
+    private final double firstLeg;
+    private final double secondLeg;
+
+    public RightTriangle(String color, double firstLeg, double secondLeg) {
+        this.color = color;
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * firstLeg * secondLeg;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: right triangle, area: " + getArea()
+                + " sq. units, firstLeg: " + firstLeg + " units, secondLeg: "
+                + secondLeg + " units, color: " + color);
+    }
+}
