bdf48137547889ceed6f37344c54ad46af042a30
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-10 11:30:56 +0100

    advanced

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -10,23 +10,18 @@ public class FigureSupplier {
         String color = colorSupplier.getRandomColor();
         int figureType = random.nextInt(5);
 
-        switch (figureType) {
-            case 0:
-                return new Square(color, random.nextInt(10) + 1);
-            case 1:
-                return new Rectangle(color, random.nextInt(10) + 1, random.nextInt(10) + 1);
-            case 2:
-                return new RightTriangle(color, random.nextInt(10) + 1, random.nextInt(10) + 1);
-            case 3:
-                return new Circle(color, random.nextInt(10) + 1);
-            case 4:
-                return new IsoscelesTrapezoid(color, random.nextInt(10) + 1, random.nextInt(10) + 1, random.nextInt(10) + 1);
-            default:
-                return getDefaultFigure();
-        }
+        return switch (figureType) {
+            case 0 -> new Square(color, random.nextInt(10) + 1);
+            case 1 -> new Rectangle(color, random.nextInt(10) + 1, random.nextInt(10) + 1);
+            case 2 -> new RightTriangle(color, random.nextInt(10) + 1, random.nextInt(10) + 1);
+            case 3 -> new Circle(color, random.nextInt(10) + 1);
+            case 4 -> new IsoscelesTrapezoid(color, random.nextInt(10) + 1, random.nextInt(10) + 1, random.nextInt(10) + 1);
+            default -> getDefaultFigure();
+        };
     }
 
     public Figure getDefaultFigure() {
         return new Circle("white", 10);
     }
 }
+
