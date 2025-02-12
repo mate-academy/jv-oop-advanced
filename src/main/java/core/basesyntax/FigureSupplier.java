e925e3a83a485f56ef193edd6c1cf66cf72d61f7
Mateusz <mateusz.kiryk@o2.pl>
2025-02-12 22:30:33 +0100

    First solution

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+import java.util.Random;
+
+
+public class FigureSupplier {
+    private static final Random RANDOM = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        String color = colorSupplier.getRandomColor();
+        switch (RANDOM.nextInt(5)) {
+            case 0: return new Circle(color, RANDOM.nextInt(10) + 1);
+            case 1: return new Square(color, RANDOM.nextInt(10) + 1);
+            case 2: return new Rectangle(color, RANDOM.nextInt(10) + 1, RANDOM.nextInt(10) + 1);
+            case 3: return new RightTriangle(color, RANDOM.nextInt(10) + 1, RANDOM.nextInt(10) + 1);
+            default: return new IsoscelesTrapezoid(color, RANDOM.nextInt(10) + 1, RANDOM.nextInt(10) + 1, RANDOM.nextInt(10) + 1);
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle("white", 10);
+    }
+}
