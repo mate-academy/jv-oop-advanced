273fee7b9d20281498b1669117ba394e7b30782a
Roman Zaharii <romazagariy2003@gmail.com>
2025-02-25 08:08:28 +0200

    Refactored FigureSupplier.java and Main.java according to review

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,21 +3,23 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    Random random = new Random();
+    public static final int MAX_SIZE = 100;
     public Figure getRandomFigure() {
-        Figure figure = getDefaultFigure();
+        Figure figure;
         ColorSupplier colorSupplier = new ColorSupplier();
         Color randomColor = colorSupplier.getRandomColor();
-        double size1 = new Random().nextDouble(100);
-        double size2 = new Random().nextDouble(100);
-        double size3 = new Random().nextDouble(100);
-        int index = new Random().nextInt(1,6);
+        double size1 = random.nextDouble(MAX_SIZE);
+        double size2 = random.nextDouble(MAX_SIZE);
+        double size3 = random.nextDouble(MAX_SIZE);
+        int index = random.nextInt(1,6);
         switch (index) {
             case 1 -> figure = new Square(randomColor, size1);
             case 2 -> figure = new Rectangle(randomColor, size1, size2);
             case 3 -> figure = new RightTriangle(randomColor, size1, size2);
             case 4 -> figure = new Circle(randomColor, size1);
             case 5 -> figure = new IsoscelesTrapezoid(randomColor, size1, size2, size3);
-            default -> System.out.println("ERROR!");
+            default -> figure = getDefaultFigure();
         }
         return figure;
     }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,9 +3,14 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         Figure[] figures = new Figure[10];
-        for (int i = 0; i < 10; i++) {
-            FigureSupplier figureSupplier = new FigureSupplier();
-            figures[i] = figureSupplier.getRandomFigure();
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            }
+            else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
         }
         for (Figure figure : figures) {
             figure.draw();
