5ac176e67c0736c2da12418043faa6d11ecaa159
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-02-26 21:37:04 +0100

    solution

---
+++ src/main/java/core/basesyntax/Figure.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public abstract class Figure implements Shape, Drawable {
+    private String color;
+
+    public Figure(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract double getArea();
+
+    public abstract void draw();
+
+
+}
