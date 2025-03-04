62c2577ef2df92a5188474adbf82366f1005be38
DZotsenko <diana.zotsenko@gmail.com>
2025-03-04 21:18:12 +0200

    added HALF_OF_ARRAY constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static final int ARRAY_LENGTH = 7;
+    public static final int HALF_OF_ARRAY = ARRAY_LENGTH / 2;
 
     public static void main(String[] args) {
         Figure[] figures = new Figure[ARRAY_LENGTH];
         FigureSupplier figureSupplier = new FigureSupplier();
         for (int i = 0; i < figures.length; i++) {
-            figures[i] = (i < ARRAY_LENGTH / 2)
+            figures[i] = (i < HALF_OF_ARRAY)
                     ? figureSupplier.getRandomFigure()
                     : figureSupplier.getDefaultFigure();
             figures[i].draw();
