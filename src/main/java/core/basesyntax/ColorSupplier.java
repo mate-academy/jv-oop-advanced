244048d5799743b7c3d81d8912018100fa7892c3
Rostyslav Olekha <ros123ole@gmail.com>
2025-03-06 23:42:15 +0200

    Fixed the code

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
-        Color color = Color.values()[index];
-        return color.toString();
+    public String getRandomColor() {
+        String[] color = {"White", "Red", "Yellow", "Blue", "Black"};
+        int index = new Random().nextInt(color.length);
+        String randomColor = color[index];
+        return randomColor;
     }
 }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,33 +3,24 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
-    public static Figure getRandomFigure() {
+    public Figure getRandomFigure() {
         Random random = new Random();
-        int shapeType = random.nextInt(5);
-
-        switch (shapeType) {
-            case 0:
-                return new Square(ColorSupplier.getRandomColor(),
-                        random.nextDouble());
-            case 1:
-                return new Rectangle(ColorSupplier.getRandomColor(),
-                        random.nextDouble(), random.nextDouble());
-            case 2:
-                return new RightTriangle(random.nextDouble(),
-                        random.nextDouble(), ColorSupplier.getRandomColor());
-            case 3:
-                return new Circle(random.nextDouble(),
-                        ColorSupplier.getRandomColor());
-            case 4:
-                return new IsoscelesTrapezoid(ColorSupplier.getRandomColor(),
-                        random.nextDouble(), random.nextDouble(), random.nextDouble());
-            default:
-                return null;
-        }
+        Figure[] figures = {new Square(colorSupplier.getRandomColor(),
+                random.nextDouble()), new Rectangle(colorSupplier.getRandomColor(),
+                random.nextDouble(), random.nextDouble()),
+                new RightTriangle(random.nextDouble(),
+                        random.nextDouble(), colorSupplier.getRandomColor()),
+                new Circle(random.nextDouble(),
+                        colorSupplier.getRandomColor()),
+                new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                        random.nextDouble(), random.nextDouble(), random.nextDouble())};
+        int index = random.nextInt(figures.length);
+        return figures[index];
     }
 
-    public static Figure getDefaultFigure() {
+    public Figure getDefaultFigure() {
         return new Circle(10, "White");
 
     }
