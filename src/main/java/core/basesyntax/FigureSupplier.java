044716284c17776a3d3a259a9f93677e81d64563
Nazarii Vypor <vypor25102006@gmail.com>
2025-03-06 19:28:59 +0200

    Створив класи для (Square, Rectangle, RightTriangle, Circle, IsoscelesTrapezoid). Усі фігури мають колір. Кожна фігура має унікальні властивості. Можна отримати площу будь-якої фігури. Можна намалювати фігуру. Перша половина масиву має випадкові параметри. Друга половина  має стандартні параметри. ColorSupplier з методом public String getRandomColor(), генерує випадковий колір. FigureSupplier з методом public Figure getRandomFigure(), генерує випадкову фігуру. У FigureSupplier  метод public Figure getDefaultFigure(), який завжди повертає біле коло з радіусом 10.

---
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class FigureSupplier {
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Figure getRandomFigure() {
+        switch (random.nextInt(5)) {
+            case 0: return new Square(colorSupplier.getRandomColor(),
+                    random.nextInt(10) + 1);
+            case 1: return new Rectangle(colorSupplier.getRandomColor(),
+                    random.nextInt(10) + 1,
+                    random.nextInt(10) + 1);
+            case 2: return new RightTriangle(colorSupplier.getRandomColor(),
+                    random.nextInt(10) + 1,
+                    random.nextInt(10) + 1);
+            case 3: return new Circle(colorSupplier.getRandomColor(), random.nextInt(10) + 1);
+            default: return new IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                    random.nextInt(10) + 1,
+                    random.nextInt(10) + 1, random.nextInt(10) + 1);
+        }
+    }
+
+    public Figure getDefaultFigure() {
+        return new Circle("white", 10);
+    }
+}
