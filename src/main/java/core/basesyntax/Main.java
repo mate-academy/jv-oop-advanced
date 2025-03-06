67dbf450824f66e203f1d4a6645d9c69ba704cba
Ks228337 <kiskemova11@gmail.com>
2025-03-06 19:56:51 +0200

    OOP principles task commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import core.basesyntax.FigureSupplier;
-
 import java.util.Random;
 
 public class Main {
@@ -30,20 +28,17 @@ public class Main {
             int figureType = random.nextInt(5);
             String color = colorSupplier.getRandomColor();
 
-            switch (figureType) {
-                case 0:
-                    return new Square(color, random.nextDouble() * 10);
-                case 1:
-                    return new Rectangle(color, random.nextDouble() * 10, random.nextDouble() * 10);
-                case 2:
-                    return new RightTriangle(color, random.nextDouble() * 10, random.nextDouble() * 10);
-                case 3:
-                    return new Circle(color, random.nextDouble() * 10);
-                case 4:
-                    return new IsoscelesTrapezoid(color, random.nextDouble() * 10, random.nextDouble() * 10, random.nextDouble() * 10);
-                default:
-                    return getDefaultFigure();
-            }
+            return switch (figureType) {
+                case 0 -> new Square(color, random.nextDouble() * 10);
+                case 1 -> new Rectangle(color, random.nextDouble() * 10, random.nextDouble() * 10);
+                case 2 -> new RightTriangle(color, random.nextDouble() * 10,
+                        random.nextDouble() * 10);
+                case 3 -> new Circle(color, random.nextDouble() * 10);
+                case 4 ->
+                        new IsoscelesTrapezoid(color, random.nextDouble() * 10, random.nextDouble()
+                                * 10, random.nextDouble() * 10);
+                default -> getDefaultFigure();
+            };
         }
 
         public Figure getDefaultFigure() {
