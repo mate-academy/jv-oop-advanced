ae7d82a0cf62da27433b434ac2c24b927c9e7952
Slawomir <slawomir_pacek@op.pl>
2025-03-07 11:53:48 +0100

    do sprawdzenia8

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -56,7 +56,7 @@ public class IsoscelesTrapezoid extends Figure implements Area, Draw {
     @Override
     public void toDraw() {
         System.out.println("IsoscelesTrapezoid color: " + getColor() + " The top length is: "
-                + getTop() + " The bottom length is: " + getBottom() +
-                " The height of the trapezoid is: " + getHight());
+                + getTop() + " The bottom length is: " + getBottom()
+                + " The height of the trapezoid is: " + getHight());
     }
 }
