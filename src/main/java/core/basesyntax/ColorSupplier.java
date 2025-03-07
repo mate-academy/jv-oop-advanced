616c67e2b7b2636e6efbd822f8bf44531f1965f9
Slawomir <slawomir_pacek@op.pl>
2025-03-07 11:38:18 +0100

    do sprawdzenia6

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class ColorSupplier {
     public static Color getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
-        Color color = Color.values()[index]; //Color.values()[index] pobiera stałą enum o wygenerowanym losowo indeksie
+        //Color.values()[index] pobiera stałą enum o wygenerowanym losowo indeksie
+        Color color = Color.values()[index];
         return color;
     }
 }
