6b3db89163d966260c067b3d41e557453c15c74c
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-04-29 19:42:48 +0300

    Update ci.yml

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class ColorSupplier {
+    private final Random random = new Random();
+
+    public Color getRandomColor() {
+        return Color.values()[random.nextInt(Color.values().length)];
+    }
+}
---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,34 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure {
+    private double width;
+    private double height;
+
+    public Rectangle(Color color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height
+                + 10;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: " + getArea()
+                + " sq. units, width: " + width + " units, height: " + height
+                + " units, color: " + getColor());
+
+    }
+}
