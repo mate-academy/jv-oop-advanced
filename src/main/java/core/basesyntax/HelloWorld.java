9010eedfb439d079f65fbe57ddf683c25609f277
Serhiy <sergiy.kusiv@gmail.com>
2025-06-01 19:36:19 +0300

    Was correcting maven a little and solving the problem with java version(project structure and project settings)

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,16 +1,20 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class HelloWorld {
     public static void main(String[] args) {
-        Figure[] listOfFigures = {new Circle("white",10),new Rectangle("red",5,5),
-                                  new IsoscelesTrapezoid("red",3,3,3), new Square("black",2),
-                                  new RightTriangle("yellow",4,4)};
 
-        for (Figure el : listOfFigures) {
-            el.draw();
+        Figure[] listOfArray = new Figure[6];
+        FigureSupplier figure = new FigureSupplier();
+        for (int i = 0; i < listOfArray.length; i++) {
+            if (i < listOfArray.length / 2) {
+                listOfArray[i] = figure.getRandomFigure();
+            } else {
+                listOfArray[i] = figure.getDefaultFigure();
+            }
+        }
+
+        for (int i = 0; i < listOfArray.length; i++) {
+            listOfArray[i].draw();
         }
     }
 
