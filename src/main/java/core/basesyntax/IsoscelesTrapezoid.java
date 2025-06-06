b3775ffc9902d9bbc774a876182e1b1a02ead736
Vlad Bielov <iamjoeking69@gmail.com>
2025-06-06 13:43:22 +0300

    Implement figure classes (Square, Rectangle, RightTriangle, Circle, IsoscelesTrapezoid) with ColorSupplier, FigureSupplier, and main method for random and default figures

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure {
+    private double topBase; // Верхнее основание
+    private double bottomBase; // Нижнее основание
+    private double height; // Высота
+
+    public IsoscelesTrapezoid(String color, double topBase, double bottomBase, double height) {
+        super(color);
+        this.topBase = topBase;
+        this.bottomBase = bottomBase;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return (topBase + bottomBase) * height / 2; // Площадь = (верхнее основание + нижнее основание) * высота / 2
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: trapezoid, area: " + getArea() + " sq. units, topBase: " + topBase + " units, bottomBase: " + bottomBase + " units, height: " + height + " units, color: " + color);
+    }
+}
---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Rectangle extends  Figure {
+    private double length; // Длина
+    private double width; // Ширина
+
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width; // Площадь = длина * ширина
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: " + getArea() + " sq. units, length: " + length + " units, width: " + width + " units, color: " + color);
+    }
+}
