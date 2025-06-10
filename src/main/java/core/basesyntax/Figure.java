424ba3429e64dd626b894e54d975cc497f335892
Vlad Bielov <iamjoeking69@gmail.com>
2025-06-10 16:32:08 +0300

    Add Shape interface and make Figure implement it to fix StructureTest failure; fix remaining Checkstyle errors

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,20 +1,10 @@
 package core.basesyntax;
 
-public abstract class Figure {
+public abstract class Figure implements Shape {
     protected String color; // Цвет фигуры. protected - виден этому классу и его наследникам
 
     // Конструктор, который будет вызываться из конструкторов дочерних классов
     public Figure(String color) {
         this.color = color;
     }
-
-    // Абстрактный метод для получения площади.
-    // Каждая конкретная фигура должна будет реализовать этот метод.
-    public abstract double getArea();
-
-    // Абстрактный метод для "рисования" фигуры.
-    // Каждая конкретная фигура должна будет реализовать этот метод.
-    // Он не должен переопределять toString()!
-    public abstract void draw();
-
 }
---
+++ src/main/java/core/basesyntax/Shape.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public interface Shape {
+
+    double getArea();
+
+    void draw();
+}
