da3fc0ea36c91dd3842b062da24613828f17bf13
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-18 14:22:21 +0300

    Create Main.java

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,26 @@
+package core.basesyntax
+
+public class Main {
+    public static void main(String[] args) {
+        FigureSupplier supplier = new FigureSupplier();
+        int arraySize = 6;
+
+        Figure[] figures = new Figure[arraySize];
+
+        // First half - random figures
+        for (int i = 0; i < arraySize / 2; i++) {
+            figures[i] = supplier.getRandomFigure();
+        }
+
+        // Second half - default figures
+        for (int i = arraySize / 2; i < arraySize; i++) {
+            figures[i] = supplier.getDefaultFigure();
+        }
+
+        // Print all figures
+        for (Figure figure : figures) {
+            figure.todraw();
+            System.out.println("-------------------------");
+        }
+    }
+}
