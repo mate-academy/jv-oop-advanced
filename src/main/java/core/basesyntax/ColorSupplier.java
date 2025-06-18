34dd273c71c75c2d14058f4c27da44257a7a1bc9
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-18 14:09:45 +0300

    Create ColorSupplier.java

---
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class ColorSupplier {
+  public String getRandomColor() {
+    Random random = new Random();
+    return Color.values()[random.nextInt(Color.values().length)].toString();
+  }
+}
