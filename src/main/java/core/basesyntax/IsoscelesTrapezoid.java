4456906e8244480c4f1cb9bf3c41e4d1dae2bbbc
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-18 13:57:39 +0300

    Create IsoscelesTrapezoid.java

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,24 @@
+package core.basesyntax
+
+public class IsoscelesTrapezoid extends Figure {
+  private double base1, base2, height;
+  public Circle(String color, double base1, double base2, double height) {
+    super(color);
+    this.base1 = base1;
+    this.base2 = base2;
+    this.height = height;
+  }
+
+  @Override
+  public double getArea() {
+    return height * ((base1 + base2) / 2);
+  }
+
+  @Override
+  public void todraw() {
+    super.todraw();
+    System.out.println("Base 1 : " + base1);
+    System.out.println("Base 2 : " + base2);
+    System.out.println("Height : " + height);
+  }
+}
