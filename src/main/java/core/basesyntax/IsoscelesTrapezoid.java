d175f141dac5e1b1ee22a40e4da30253b1721a9f
Maksym Maksymenko <Maksimfonmm2@gmail.com>
2025-06-24 12:08:37 +0300

    Fix Figure class: implement Drawable and AreaCalculable interfaces, fix figure classes

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -14,16 +14,14 @@ public class IsoscelesTrapezoid extends Figure {
 
     @Override
     public double getArea() {
-        double height = Math.sqrt(leg * leg
-                - Math.pow((base2 - base1) / 2, 2));
+        double height = Math.sqrt(leg * leg - Math.pow((base2 - base1) / 2, 2));
         return ((base1 + base2) / 2) * height;
     }
 
     @Override
     public void draw() {
         System.out.printf(
-                "Figure: isosceles trapezoid, area: %.2f sq. units, base1: %.2f units, "
-                        + "base2: %.2f units, leg: %.2f units, color: %s%n",
+                "Figure: isosceles trapezoid, area: %.2f sq. units, base1: %.2f units, base2: %.2f units, leg: %.2f units, color: %s%n",
                 getArea(), base1, base2, leg, color);
     }
 }
--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -18,8 +18,7 @@ public class RightTriangle extends Figure {
     @Override
     public void draw() {
         System.out.printf(
-                "Figure: triangle, area: %.2f sq. units, firstLeg: %.2f units, "
-                        + "secondLeg: %.2f units, color: %s%n",
+                "Figure: triangle, area: %.2f sq. units, firstLeg: %.2f units, secondLeg: %.2f units, color: %s%n",
                 getArea(), firstLeg, secondLeg, color);
     }
 }
